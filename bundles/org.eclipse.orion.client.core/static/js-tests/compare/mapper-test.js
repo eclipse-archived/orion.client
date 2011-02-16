6f17f76b947b682b7fd7a5057f3ef80f98964497
skaegi <simon_kaegi@ca.ibm.com>
2011-02-16 17:03:28 -0500

    adding compare tests -- work in progress still -- missed a file last time

--- bundles/org.eclipse.orion.client.core/static/js-tests/compare/mapper-test.js
+++ bundles/org.eclipse.orion.client.core/static/js-tests/compare/mapper-test.js
@@ -7,36 +7,36 @@
  * Contributors: IBM Corporation - initial API and implementation
  ******************************************************************************/
 
-function compareResult(result , expectedOutput , expectedMapper){
+function compareResult(result, expectedOutput, expectedMapper) {
 	var output = result[0];
 	var mapping = result[1];
-	for(var i = 0 ; i < mapping.length ; i++){
-		for (var j = 0; j < 3;j++){
-			if(expectedMapper[i][j] !== mapping[i][j])
-				return "mapper failed at   " +i+":"+j;
+	for ( var i = 0; i < mapping.length; i++) {
+		for ( var j = 0; j < 3; j++) {
+			if (expectedMapper[i][j] !== mapping[i][j])
+				return "mapper failed at   " + i + ":" + j;
 		}
-	} 
-	if(expectedOutput !== output )
+	}
+	if (expectedOutput !== output)
 		return "out put file failed\n" + output;
 	return "success !!!!!!!!!!!!!!!!!!!";
 }
 
 function runTests() {
-    var diffParser = new eclipse.DiffParser();
-    var testFrom = 1;
-    var testTo =  mapperTestCases.length;
-    
-	for(var i=(testFrom-1); i<testTo ; i++) {
+	var diffParser = new eclipse.DiffParser();
+	var testFrom = 1;
+	var testTo = mapperTestCases.length;
+
+	for ( var i = (testFrom - 1); i < testTo; i++) {
 		var testCase = mapperTestCases[i];
 		var input = testCase[0];
 		var diff = testCase[1];
 		var expectedOutput = testCase[2];
 		var expectedMapper = testCase[3];
 		var description = testCase[4];
-		var j = i+1;
-		console.log("*************************  test case " + j +":  " + description +"*********************");
-		var result = diffParser.parse(input ,diff);
-		var message = compareResult(result , expectedOutput , expectedMapper);
+		var j = i + 1;
+		console.log("*************************  test case " + j + ":  " + description + "*********************");
+		var result = diffParser.parse(input, diff);
+		var message = compareResult(result, expectedOutput, expectedMapper);
 		console.log(message);
 	}
 }
