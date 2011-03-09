59c64ab38433d96551863387e5c166b20ae6c81c
skaegi <simon_kaegi@ca.ibm.com>
2011-03-09 09:25:27 -0500

    adding default preferences support

--- bundles/org.eclipse.orion.client.core/static/js/preferences.js
+++ bundles/org.eclipse.orion.client.core/static/js/preferences.js
@@ -14,13 +14,14 @@
  */
 var eclipse = eclipse || {};
 
-eclipse.Preferences = function(_name, _provider) {
+eclipse.Preferences = function(_name, _userProvider, _defaultsProvider) {
 	
 	var _flushPending = false;
 	var _store;
+	var _defaults;
 	
 	function _flush() {
-		return _provider.put(_name, _store);
+		return _userProvider.put(_name, _store);
 	}
 	
 	function _scheduleFlush() {
@@ -36,7 +37,6 @@ eclipse.Preferences = function(_name, _provider) {
 		},0);
 	}
 	
-	
 	this.keys = function() {
 		var i, 
 			result = [];
@@ -45,15 +45,19 @@ eclipse.Preferences = function(_name, _provider) {
 				result.push(i);
 			}
 		}
+		for (i in _defaults) {
+			if (_defaults.hasOwnProperty(i) && !_store.hasOwnProperty(i) && i.charAt(0) !== '/') {
+				result.push(i);
+			}
+		}
 		return result;
 	};
-
 	
 	this.get = function(key) {
 		if (key.charAt(0) === '/') {
 			throw new Error("Bad character in key name: " + key);
 		}
-		return _store[key];
+		return _store.hasOwnProperty(key) ? _store[key] : _defaults[key];
 	};
 	
 	this.put = function(key, value) {
@@ -95,21 +99,25 @@ eclipse.Preferences = function(_name, _provider) {
 			_flushPending = false;
 			return _flush();
 		}
-		return _provider.get(_name).then(function(store) {
-			_store = store;
+		
+		return _defaultsProvider.get(_name).then(function(defaults) {
+			_defaults = defaults;
+			return _userProvider.get(_name).then(function(store) {
+				_store = store;
+			});		
 		});
 	};
 	
 	this.flush = _flush;
 };
 
-eclipse.UserPreferenceProvider = function(location) {
+eclipse.UserPreferencesProvider = function(location) {
 	
-	var currentPromise;
+	var _currentPromise;
 	
 	this.get = function(name) {
-		if (currentPromise) {
-			return promise;
+		if (_currentPromise) {
+			return _currentPromise;
 		}
 		var d = new dojo.Deferred();
 		var key = "/orion/preferences/user" + name;
@@ -119,7 +127,7 @@ eclipse.UserPreferenceProvider = function(location) {
 				d.resolve(JSON.parse(data));
 			},0);
 		} else {
-			currentPromise = d;
+			_currentPromise = d;
 			dojo.xhrGet({
 				url: location + name,
 				headers: {
@@ -129,14 +137,14 @@ eclipse.UserPreferenceProvider = function(location) {
 				timeout: 15000,
 				load: function(jsonData, ioArgs) {
 					localStorage.setItem(key, JSON.stringify(jsonData));
-					currentPromise = null;
+					_currentPromise = null;
 					d.resolve(jsonData);
 				},
 				error: function(response, ioArgs) {
 					if (ioArgs.xhr.status === 401) {
 						handleGetAuthenticationError(ioArgs.xhr, ioArgs);
 					} else {
-						currentPromise = null;
+						_currentPromise = null;
 						d.resolve({});
 					}
 				}
@@ -174,16 +182,62 @@ eclipse.UserPreferenceProvider = function(location) {
 	};
 };
 
-eclipse.PreferencesService = function(serviceRegistry, location) {
+eclipse.DefaultPreferencesProvider = function(location) {
 	
-	var _userProvider = new eclipse.UserPreferenceProvider(location);
+	var _currentPromise;
+	
+	this.get = function(name) {
+		if (_currentPromise) {
+			return _currentPromise;
+		}
+		var d = new dojo.Deferred();
+		var data = localStorage.getItem("/orion/preferences/default");
+		if (data !== null) {
+			setTimeout(function() {
+				d.resolve(JSON.parse(data)[name] || {});
+			},0);
+		} else {
+			_currentPromise = d;
+			dojo.xhrGet({
+				url: location,
+				headers: {
+					"Orion-Version": "1"
+				},
+				handleAs: "json",
+				timeout: 15000,
+				load: function(jsonData, ioArgs) {
+					localStorage.setItem("/orion/preferences/default", JSON.stringify(jsonData));
+					_currentPromise = null;
+					d.resolve(jsonData[name]|| {});
+				},
+				error: function(response, ioArgs) {
+					if (ioArgs.xhr.status === 401) {
+						handleGetAuthenticationError(ioArgs.xhr, ioArgs);
+					} else {
+						_currentPromise = null;
+						d.resolve({});
+					}
+				}
+			});
+		}
+		return d;
+	};
+};
+
+eclipse.PreferencesService = function(serviceRegistry, userPreferencesLocation, defaultPreferencesLocation) {
+	
+	userPreferencesLocation = userPreferencesLocation || "/prefs/user";
+	defaultPreferencesLocation = defaultPreferencesLocation || "/defaults.pref";
+	
+	var _userProvider = new eclipse.UserPreferencesProvider(userPreferencesLocation);
+	var _defaultsProvider = new eclipse.DefaultPreferencesProvider(defaultPreferencesLocation);
 	
 	/**
 	 * Retrieves the preferences of the given node name.
 	 * @param {String} node Path to a preference node
 	 */
 	this.getPreferences = function(name) {
-		var preferences = new eclipse.Preferences(name, _userProvider);
+		var preferences = new eclipse.Preferences(name, _userProvider, _defaultsProvider);
 		var promise = preferences.sync().then(function() {
 			return preferences;
 		});
