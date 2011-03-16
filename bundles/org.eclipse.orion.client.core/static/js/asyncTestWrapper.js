a1b1f671a85b0c2ce9d2ffa8c68de7eb11046bc6
Andrew Niefer <aniefer@ca.ibm.com>
2011-03-16 16:38:10 -0400

    fix async tests and enable remaining test plugins

--- bundles/org.eclipse.orion.client.core/static/js/asyncTestWrapper.js
+++ bundles/org.eclipse.orion.client.core/static/js/asyncTestWrapper.js
@@ -9,8 +9,8 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
  
-/*global AsyncTestCase */
-/*global console */
+/*global jstestdriver AsyncTestCase fail*/
+/*global eclipse console dojo*/
 
 var orion = orion || {};
 
@@ -25,52 +25,73 @@ orion.JSTestAdapter = (function() {
 		var noop, errback;
 		var deferred = new dojo.Deferred();
 		var exports = {};
-		var suiteRunning = false;
+		var testSuite = {};
 		
 		/* This is the TestCase which will hold all the tests for the given test plugin */
 		exports.testCase = function() {}; //AsyncTestCase(test);	
-
-		function setup(testSuite) {
-			return function() {
-				if (!suiteRunning) {
-					/* js test driver has called the first test of this test suite, install
-					 * the test plugin and start running tests.  As each test finishes, we mark the
-					 * corresponding js test driver async test as finished.
-					 */
-					var loaderServiceRegistry = new eclipse.ServiceRegistry();
-					var loaderPluginRegistry = new eclipse.PluginRegistry(loaderServiceRegistry, {});
-					loaderPluginRegistry.installPlugin(test).then(
-						function() {
-							return loaderServiceRegistry.getService("testRunner");
-						}
-					).then(
-						function(service) {
-							service.addEventListener("testDone", function(testName, testResult) {
-								if (typeof testSuite[testName] !== "undefined") {
-									//remove each test from the suite as they finish
-									var finishedTest = testSuite[testName];
-									delete testSuite[testName];
-									if (testSuite.remainingTests() === 0) {
-										//when we finish the last test, shutdown the plugin registry before telling
-										//jstestdriver that we are done.
-										console.log("Shutting down registry for " + test);
-										loaderPluginRegistry.shutdown();
-									}
-
-									finishedTest.testDone(testResult);
-								}
-							});
-						
-							console.log("Launching test suite: " + test);
-							service.run();
-						}
-					);
-					
-					suiteRunning = true;
+		
+		function _launch(queue) {
+			/* The entire test service is run in the queue for the first jstestdriver test
+			   This is because in between jstestdriver tests, the html body is cleared and we lose the 
+			   service registry.
+			 */
+			for (var property in testSuite) {
+				if (property.match(/^test.+/)) {
+					testSuite[property].testMethod(queue);
 				}
-			};
+			}
+			
+			/* After the tests, the registry must be cleaned up */
+			var shutdown = new dojo.Deferred();
+			queue.call("Shutdown " + test , function(callbacks) {
+				shutdown.resolve(callbacks.noop());
+			});
+			
+			var loaderServiceRegistry = new eclipse.ServiceRegistry();
+			var loaderPluginRegistry = new eclipse.PluginRegistry(loaderServiceRegistry, {});
+			loaderPluginRegistry.installPlugin(test).then(
+				function() {
+					return loaderServiceRegistry.getService("testRunner");
+				}
+			).then(
+				function(service) {
+					service.addEventListener("testDone", function(testName, testResult) {
+						if (typeof testSuite[testName] !== "undefined") {
+							testSuite[testName].testDone(testResult);
+						}
+					});
+					service.addEventListener("runDone", function(runName, obj) {
+						shutdown.then(function(noop) {
+							loaderPluginRegistry.shutdown();
+							noop();
+						});
+					});
+						
+					console.log("Launching test suite: " + test);
+					service.run();
+				}
+			);
 		}
 		
+		var first = true;
+		function _test(testName) {
+			return function(queue) {
+				if (first) {
+					_launch(queue);
+					first = false;
+				}
+	
+				queue.call("Results: " + testName, function(callbacks) {
+					/* At this point, all tests are finished, we are just reporting results */
+					if (testSuite[testName].testResult.result !== true) {
+						fail(testSuite[testName].testResult.message);
+					}
+				});
+			};
+		}
+			
+		/* Loading the test methods, #ready is called once we have the set of tests to run 
+		   #error is called if there is a problem loading the test plugin */
 		exports.loadMethod = function(queue) {
 			queue.call("Load test " + test, function(callbacks) {
 				noop = callbacks.noop();
@@ -79,9 +100,16 @@ orion.JSTestAdapter = (function() {
 			});
 		};
 		
-		exports.ready = function(testSuite) {
+		exports.ready = function(suite) {
 			deferred.then(function() { 
-				exports.testCase.prototype.setUp = setup(testSuite);
+				testSuite = suite;
+							
+				for(var testName in suite) {
+					if(suite.hasOwnProperty(testName)) {
+						exports.testCase.prototype[testName] = _test(testName);
+					}
+				}
+				
 				var testCaseInfo = new jstestdriver.TestCaseInfo(test, exports.testCase, jstestdriver.TestCaseInfo.ASYNC_TYPE);		
 				jstestdriver.testRunner.testRunsConfiguration_.push(testCaseInfo.getDefaultTestRunConfiguration());
 				noop(); 
@@ -113,6 +141,7 @@ orion.JSTestAdapter = (function() {
 		};
 		
 		exports.testDone = function(testResult) {
+			this.testResult = testResult;
 			/* the real test has completed, mark the jstestdriver test as complete */
 			deferred.then( function(callbacks) {
 				if (testResult.result) {
@@ -145,23 +174,12 @@ orion.JSTestAdapter = (function() {
 			function(service) {
 				service.list().then( function(testNames) {
 					var testName;
-					var testSuite = { 
-						remainingTests: function() {
-							var count = 0;
-							for (property in this) {
-								if (property.match(/^test.+/)) {
-									count++;
-								}
-							}
-							return count;
-						}
-					};
+					var testSuite = {};
 					
 					/* for each test in the suite, hook up an jstestdriver async test */
 					for (var i = 0; i < testNames.length; i++) {
 						testName = testNames[i];
 						testSuite[testName] = asyncTest(testName);
-						loader.testCase.prototype[testName] = testSuite[testName].testMethod;
 					} 
 					console.log(fileURI + " : registered " + testNames.length + " tests" );
 					
