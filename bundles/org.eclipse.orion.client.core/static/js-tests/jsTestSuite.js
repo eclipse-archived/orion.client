e0e741e45e234a02d3ee256e225027b9d727251a
Andrew Niefer <aniefer@ca.ibm.com>
2011-05-27 15:26:51 -0400

    Bug 345483 - move to requirejs - async jstestdriver tests

--- bundles/org.eclipse.orion.client.core/static/js-tests/jsTestSuite.js
+++ bundles/org.eclipse.orion.client.core/static/js-tests/jsTestSuite.js
@@ -9,14 +9,47 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 
-/*global orion */
+/*global orion require*/
 
-orion.JSTestAdapter.runTests("All Tests", [
-	"http://localhost:8080/js-tests/commonjs-unittesting/test.html",
-	"http://localhost:8080/js-tests/compare/test.html",
-	"http://localhost:8080/js-tests/serviceRegistry/test.html",
-	"http://localhost:8080/js-tests/preferences/test.html",
-	"http://localhost:8080/js-tests/pluginRegistry/test.html",
-	"http://localhost:8080/js-tests/testRunAsynch/test.html",
-	"http://localhost:8080/js-tests/textMateStyler/test.html"
-]);
+require({
+	baseUrl: '',
+	packages: [
+		{
+	      name: 'dojo',
+	      location: '/org.dojotoolkit/dojo',
+	      main: 'lib/main-browser',
+	      lib: '.'
+	    }
+	],
+	paths: {
+		orion: '/js',
+		text: '/requirejs/text',
+		i18n: '/requirejs/i18n'
+	}
+});
+
+/* Have one jstestdriver test that can wait for requirejs */
+var BootstrapTest = AsyncTestCase("bootstrap");
+BootstrapTest.prototype.testBootstrap = function(queue) {
+	queue.call("Bootstrapping asyncTestWrapper ", function(callbacks) {
+		var noop = callbacks.noop();
+	
+		require(['orion/asyncTestWrapper'], function(orion) {
+			var loaderSuite = orion.JSTestAdapter.runTests("All Tests", [
+				"http://localhost:8080/js-tests/commonjs-unittesting/test.html",
+				"http://localhost:8080/js-tests/compare/test.html",
+				"http://localhost:8080/js-tests/serviceRegistry/test.html",
+				"http://localhost:8080/js-tests/preferences/test.html",
+				"http://localhost:8080/js-tests/pluginRegistry/test.html",
+				"http://localhost:8080/js-tests/testRunAsynch/test.html",
+				"http://localhost:8080/js-tests/textMateStyler/test.html"
+			]);
+		
+			var testCaseInfo = new jstestdriver.TestCaseInfo("Bootstrap", loaderSuite, jstestdriver.TestCaseInfo.ASYNC_TYPE);		
+			jstestdriver.testRunner.testRunsConfiguration_.push(testCaseInfo.getDefaultTestRunConfiguration());
+			
+			noop();
+		});
+	});
+};
+
