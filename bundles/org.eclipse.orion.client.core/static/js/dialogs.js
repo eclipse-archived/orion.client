d3f6f9c1811b1f36f8a467f935156c39110c14a1
John Arthorne <John_Arthorne@ca.ibm.com>
2011-06-02 10:54:15 -0400

    jsdoc added

--- bundles/org.eclipse.orion.client.core/static/js/dialogs.js
+++ bundles/org.eclipse.orion.client.core/static/js/dialogs.js
@@ -7,34 +7,45 @@
  *
  * Contributors: IBM Corporation - initial API and implementation
  *******************************************************************************/
- 
-/**
- * @namespace The global container for eclipse APIs.
- */ 
 
+/*global define window */
+ 
 define([], function(){
 
-
-var eclipse = eclipse || {};
-
-/**
- * Dialog Service
- * @class Common dialog services
- */
-eclipse.DialogService = function(serviceRegistry) {
-	this._serviceRegistry = serviceRegistry;
-	this._serviceRegistration = serviceRegistry.registerService("orion.page.dialog", this);
-};
- 
-eclipse.DialogService.prototype = {
-	confirm : function(msg, onDone) {
-		onDone(window.confirm(msg));;
-	},
-	openFiles : function(onDone) {
-		// TODO
+	/**
+	 * Constructs a new dialog service. Clients should obtain a dialog service by
+	 * requesting the service <tt>orion.page.dialog</tt> from the service registry. This 
+	 * constructor should only be used by configuration code that is initializing
+	 * a service registry
+	 * @class The dialog service provides common helper dialogs such as confirmation and
+	 * information dialogs.
+	 * @name orion.dialogs.DialogService
+	 */
+	function DialogService(serviceRegistry) {
+		this._serviceRegistry = serviceRegistry;
+		this._serviceRegistration = serviceRegistry.registerService("orion.page.dialog", this);
 	}
-};
-return eclipse;
+	 
+	DialogService.prototype = /** @lends orion.dialogs.DialogService.prototype */ {
+		/**
+		 * Prompts the user for configuration, and executes the provided function when done.
+		 * @param {String} message The confirmation message
+		 * @param {Function} onDone The function to invoke upon confirmation.
+		 */
+		confirm : function(msg, onDone) {
+			onDone(window.confirm(msg));
+		},
+		/**
+		 * Prompts the user to select one or more files
+		 * @param {Function} onDone The function to invoke upon confirmation.
+		 */
+		openFiles : function(onDone) {
+			// TODO
+		}
+	};
+	DialogService.prototype.constructor = DialogService;
+	//return module exports
+	return {DialogService: DialogService};
 });
 	
 		
