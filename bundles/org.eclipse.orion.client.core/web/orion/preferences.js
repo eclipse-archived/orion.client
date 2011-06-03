cff30c921520f8dad0a6b95ce240f3b874dc9d4f
John Arthorne <John_Arthorne@ca.ibm.com>
2011-06-03 17:35:27 -0400

    jsdocify

--- bundles/org.eclipse.orion.client.core/web/orion/preferences.js
+++ bundles/org.eclipse.orion.client.core/web/orion/preferences.js
@@ -8,250 +8,302 @@
  * Contributors: IBM Corporation - initial API and implementation
  ******************************************************************************/
 
-/*global dojo window handleGetAuthenticationError handlePutAuthenticationError console setTimeout localStorage*/
-
-/**
- * @namespace The global container for eclipse APIs.
- */
+/*global define window localStorage */
 
 define(['dojo', 'orion/auth'], function(dojo, mAuth){
 
-var eclipse = eclipse || {};
-
-eclipse.Preferences = function(_name, _userProvider, _defaultsProvider) {
-	
-	var _flushPending = false;
-	var _store;
-	var _defaults;
-	
-	function _flush() {
-		return _userProvider.put(_name, _store);
+	/**
+	 * Constructs a new preferences instance. This constructor is not
+	 * intended to be used by clients. Preferences should instead be
+	 * obtained from a preference service
+	 * @class A preferences object returned by the preferences service
+	 * @name orion.preferences.Preferences
+	 * @see orion.preferences.PreferencesService
+	 */
+	function Preferences(_name, _userProvider, _defaultsProvider) {
+		this._name = _name;
+		this._userProvider = _userProvider;
+		this._defaultsProvider = _defaultsProvider;
+		this._flushPending = false;
+		this._store = null;
+		this._defaults = null;
 	}
-	
-	function _scheduleFlush() {
-		if (_flushPending) {
-			return;
-		}
-		_flushPending = true;
-		window.setTimeout( function() {
-			if (_flushPending) {
-				_flushPending = false;
-				_flush();
-			}
-		},0);
-	}
-	
-	this.keys = function() {
-		var i, 
-			result = [];
-		for (i in _store) {
-			if (_store.hasOwnProperty(i) && i.charAt(0) !== '/') {
-				result.push(i);
-			}
-		}
-		for (i in _defaults) {
-			if (_defaults.hasOwnProperty(i) && !_store.hasOwnProperty(i) && i.charAt(0) !== '/') {
-				result.push(i);
-			}
-		}
-		return result;
-	};
-	
-	this.get = function(key) {
-		if (key.charAt(0) === '/') {
-			throw new Error("Bad character in key name: " + key);
-		}
-		return _store.hasOwnProperty(key) ? _store[key] : _defaults[key];
-	};
-	
-	this.put = function(key, value) {
-		if (key.charAt(0) === '/') {
-			throw new Error("Bad character in key name: " + key);
-		}
+	Preferences.prototype = /** @lends orion.preferences.Preferences.prototype */ {
 		
-		if (_store[key] !== value) {
-			_store[key] = value;
-			_scheduleFlush();
-		}
-	};
-	
-	this.remove = function(key) {
-		if (key.charAt(0) === '/') {
-			throw new Error("Bad character in key name: " + key);
-		}
+		_flush: function() {
+			return this._userProvider.put(this._name, this._store);
+		},
 		
-		if (_store[key]) {
-			delete _store[key];
-			_scheduleFlush();
-			return true;			
-		}
-		return false;
-	};
-		
-	this.clear = function() {
-		var i;
-		for (i in _store) {
-			if (_store.hasOwnProperty(i) && i.charAt(0) !== '/') {
-				delete _store[i];
+		_scheduleFlush: function() {
+			if (this._flushPending) {
+				return;
 			}
-		}
-		_scheduleFlush();
-	};
-	
-	this.sync = function() {
-		if(_flushPending) {
-			_flushPending = false;
-			return _flush();
-		}
-		
-		return _defaultsProvider.get(_name).then(function(defaults) {
-			_defaults = defaults;
-			return _userProvider.get(_name).then(function(store) {
-				_store = store;
-			});		
-		});
-	};
-	
-	this.flush = _flush;
-};
-
-eclipse.UserPreferencesProvider = function(location) {
-	
-	var _currentPromise;
-	
-	this.get = function(name) {
-		if (_currentPromise) {
-			return _currentPromise;
-		}
-		var d = new dojo.Deferred();
-		var key = "/orion/preferences/user" + name;
-		var data = localStorage.getItem(key);
-		if (data !== null) {
-			setTimeout(function() {
-				d.resolve(JSON.parse(data));
+			this._flushPending = true;
+			window.setTimeout( function() {
+				if (this._flushPending) {
+					this._flushPending = false;
+					this._flush();
+				}
 			},0);
-		} else {
-			_currentPromise = d;
-			dojo.xhrGet({
-				url: location + name,
+		},
+
+		/**
+		 * Returns an array of String preference keys available in this node.
+		 */
+		keys: function() {
+			var i, 
+				result = [];
+			for (i in this._store) {
+				if (this._store.hasOwnProperty(i) && i.charAt(0) !== '/') {
+					result.push(i);
+				}
+			}
+			for (i in this._defaults) {
+				if (this._defaults.hasOwnProperty(i) && !this._store.hasOwnProperty(i) && i.charAt(0) !== '/') {
+					result.push(i);
+				}
+			}
+			return result;
+		},
+		
+		/**
+		 * Returns the value of the preference with the given key
+		 * @param {String} key The preference key to return
+		 */
+		get: function(key) {
+			if (key.charAt(0) === '/') {
+				throw new Error("Bad character in key name: " + key);
+			}
+			return this._store.hasOwnProperty(key) ? this._store[key] : this._defaults[key];
+		},
+		
+		/**
+		 * Associates a new preference value with the given key,
+		 * replacing any existing value.
+		 * @param {String} key The preference key
+		 * @param {String} value The preference value
+		 */
+		put: function(key, value) {
+			if (key.charAt(0) === '/') {
+				throw new Error("Bad character in key name: " + key);
+			}
+			
+			if (this._store[key] !== value) {
+				this._store[key] = value;
+				this._scheduleFlush();
+			}
+		},
+		
+		/**
+		 * Removes the preference with the given key. Has no
+		 * effect if no such key is defined.
+		 * @param {String} key The preference key to remove
+		 */
+		remove: function(key) {
+			if (key.charAt(0) === '/') {
+				throw new Error("Bad character in key name: " + key);
+			}
+			
+			if (this._store[key]) {
+				delete this._store[key];
+				this._scheduleFlush();
+				return true;			
+			}
+			return false;
+		},
+		
+		/**
+		 * Removes all preferences from this preference node.
+		 */
+		clear: function() {
+			var i;
+			for (i in this._store) {
+				if (this._store.hasOwnProperty(i) && i.charAt(0) !== '/') {
+					delete this._store[i];
+				}
+			}
+			this._scheduleFlush();
+		},
+		
+		/**
+		 * Synchronizes this preference node with its storage. Any new values
+		 * in the storage area will become available to this preference object.
+		 */
+		sync:  function() {
+			if(this._flushPending) {
+				this._flushPending = false;
+				return this._flush();
+			}
+			
+			return this._defaultsProvider.get(this._name).then(function(defaults) {
+				this._defaults = defaults;
+				return this._userProvider.get(this._name).then(function(store) {
+					this._store = store;
+				});		
+			});
+		},
+		/**
+		 * Flushes all preference changes in this node to its backing storage.
+		 * @function
+		 */
+		flush: this._flush
+	};
+	
+	function UserPreferencesProvider(location) {
+		this.location = location;
+		this._currentPromise = null;
+	}
+	UserPreferencesProvider.prototype = {
+		
+		get: function(name) {
+			if (this._currentPromise) {
+				return this._currentPromise;
+			}
+			var d = new dojo.Deferred();
+			var key = "/orion/preferences/user" + name;
+			var data = localStorage.getItem(key);
+			if (data !== null) {
+				window.setTimeout(function() {
+					d.resolve(JSON.parse(data));
+				},0);
+			} else {
+				this._currentPromise = d;
+				dojo.xhrGet({
+					url: this.location + name,
+					headers: {
+						"Orion-Version": "1"
+					},
+					handleAs: "json",
+					timeout: 15000,
+					load: function(jsonData, ioArgs) {
+						localStorage.setItem(key, JSON.stringify(jsonData));
+						this._currentPromise = null;
+						d.resolve(jsonData);
+					},
+					error: function(response, ioArgs) {
+						response.log=false;
+						if (ioArgs.xhr.status === 401) {
+							mAuth.handleGetAuthenticationError(this, ioArgs);
+						} else {
+							this._currentPromise = null;
+							d.resolve({});
+						}
+					},
+					failOk: true
+				});
+			}
+			return d;
+		},
+		
+		put: function(name, data) {
+			var d = new dojo.Deferred();
+			var key = "/orion/preferences/user" + name;
+			var jsonData = JSON.stringify(data);
+			localStorage.setItem(key, jsonData);
+			dojo.xhrPut({
+				url: this.location + name,
+				putData: jsonData,
 				headers: {
 					"Orion-Version": "1"
 				},
 				handleAs: "json",
+				contentType: "application/json",
 				timeout: 15000,
 				load: function(jsonData, ioArgs) {
-					localStorage.setItem(key, JSON.stringify(jsonData));
-					_currentPromise = null;
-					d.resolve(jsonData);
+					d.resolve();
 				},
 				error: function(response, ioArgs) {
-					response.log=false;
 					if (ioArgs.xhr.status === 401) {
-						mAuth.handleGetAuthenticationError(this, ioArgs);
+						mAuth.handlePutAuthenticationError(this, ioArgs);
 					} else {
-						_currentPromise = null;
-						d.resolve({});
+						d.resolve(); // consider throwing here
 					}
-				},
-				failOk: true
+				}
 			});
+			return d;
 		}
-		return d;
 	};
 	
-	this.put = function(name, data) {
-		var d = new dojo.Deferred();
-		var key = "/orion/preferences/user" + name;
-		var jsonData = JSON.stringify(data);
-		localStorage.setItem(key, jsonData);
-		dojo.xhrPut({
-			url: location + name,
-			putData: jsonData,
-			headers: {
-				"Orion-Version": "1"
-			},
-			handleAs: "json",
-			contentType: "application/json",
-			timeout: 15000,
-			load: function(jsonData, ioArgs) {
-				d.resolve();
-			},
-			error: function(response, ioArgs) {
-				if (ioArgs.xhr.status === 401) {
-					handlePutAuthenticationError(this, ioArgs);
-				} else {
-					d.resolve(); // consider throwing here
-				}
+	function DefaultPreferencesProvider(location) {
+		this.location = location;
+		this._currentPromise = null;
+	}
+	DefaultPreferencesProvider.prototype = {
+		
+		get: function(name) {
+			if (this._currentPromise) {
+				return this._currentPromise;
 			}
-		});
-		return d;
-	};
-};
-
-eclipse.DefaultPreferencesProvider = function(location) {
-	
-	var _currentPromise;
-	
-	this.get = function(name) {
-		if (_currentPromise) {
-			return _currentPromise;
-		}
-		var d = new dojo.Deferred();
-		var data = localStorage.getItem("/orion/preferences/default");
-		if (data !== null) {
-			setTimeout(function() {
-				d.resolve(JSON.parse(data)[name] || {});
-			},0);
-		} else {
-			_currentPromise = d;
-			dojo.xhrGet({
-				url: location,
-				headers: {
-					"Orion-Version": "1"
-				},
-				handleAs: "json",
-				timeout: 15000,
-				load: function(jsonData, ioArgs) {
-					localStorage.setItem("/orion/preferences/default", JSON.stringify(jsonData));
-					_currentPromise = null;
-					d.resolve(jsonData[name]|| {});
-				},
-				error: function(response, ioArgs) {
-					if (ioArgs.xhr.status === 401) {
-						mAuth.handleGetAuthenticationError(ioArgs.xhr, ioArgs);
-					} else {
-						_currentPromise = null;
-						d.resolve({});
+			var d = new dojo.Deferred();
+			var data = localStorage.getItem("/orion/preferences/default");
+			if (data !== null) {
+				window.setTimeout(function() {
+					d.resolve(JSON.parse(data)[name] || {});
+				},0);
+			} else {
+				this._currentPromise = d;
+				dojo.xhrGet({
+					url: this.location,
+					headers: {
+						"Orion-Version": "1"
+					},
+					handleAs: "json",
+					timeout: 15000,
+					load: function(jsonData, ioArgs) {
+						localStorage.setItem("/orion/preferences/default", JSON.stringify(jsonData));
+						this._currentPromise = null;
+						d.resolve(jsonData[name]|| {});
+					},
+					error: function(response, ioArgs) {
+						if (ioArgs.xhr.status === 401) {
+							mAuth.handleGetAuthenticationError(ioArgs.xhr, ioArgs);
+						} else {
+							this._currentPromise = null;
+							d.resolve({});
+						}
 					}
-				}
-			});
+				});
+			}
+			return d;
 		}
-		return d;
 	};
-};
-
-eclipse.PreferencesService = function(serviceRegistry, userPreferencesLocation, defaultPreferencesLocation) {
-	
-	userPreferencesLocation = userPreferencesLocation || "/prefs/user";
-	defaultPreferencesLocation = defaultPreferencesLocation || "/defaults.pref";
-	
-	var _userProvider = new eclipse.UserPreferencesProvider(userPreferencesLocation);
-	var _defaultsProvider = new eclipse.DefaultPreferencesProvider(defaultPreferencesLocation);
 	
 	/**
-	 * Retrieves the preferences of the given node name.
-	 * @param {String} node Path to a preference node
+	 * Constructs a new preference service. Clients should obtain a preference service
+	 * by requesting the service <tt>orion.core.preference</tt> from the service registry.
+	 * This service constructor is only intended to be used by page service registry
+	 * initialization code.
+	 * @class The preferences service manages a hierarchical set of preference
+	 * nodes. Each node consistents of preference key/value pairs. 
+	 * @name orion.preferences.PreferencesService
+	 * @see orion.preferences.Preferences
 	 */
-	this.getPreferences = function(name) {
-		var preferences = new eclipse.Preferences(name, _userProvider, _defaultsProvider);
-		var promise = preferences.sync().then(function() {
-			return preferences;
-		});
-		return promise;
+	function PreferencesService(serviceRegistry, userPreferencesLocation, defaultPreferencesLocation) {
+		
+		userPreferencesLocation = userPreferencesLocation || "/prefs/user";
+		defaultPreferencesLocation = defaultPreferencesLocation || "/defaults.pref";
+		this._userProvider = new UserPreferencesProvider(userPreferencesLocation);
+		this._defaultsProvider = new DefaultPreferencesProvider(defaultPreferencesLocation);
+		this._serviceRegistration = serviceRegistry.registerService("orion.core.preference", this);
+	}
+	PreferencesService.prototype = /** @lends orion.preferences.PreferencesService.prototype */ {
+		
+		/**
+		 * Retrieves the preferences of the given node name.
+		 * @param {String} name A slash-delimited path to the preference node to return
+		 */
+		getPreferences: function(name) {
+			var preferences = new Preferences(name, this._userProvider, this._defaultsProvider);
+			var promise = preferences.sync().then(function() {
+				return preferences;
+			});
+			return promise;
+		}
+		
 	};
-	
-	var _serviceRegistration = serviceRegistry.registerService("orion.core.preference", this);
-};
-
-return eclipse;
+	//return module exports
+	return {
+		Preferences: Preferences,
+		PreferencesService: PreferencesService
+	};
 });
--- bundles/org.eclipse.orion.client.core/web/orion/selection.js
+++ bundles/org.eclipse.orion.client.core/web/orion/selection.js
@@ -7,57 +7,68 @@
  *
  * Contributors: IBM Corporation - initial API and implementation
  *******************************************************************************/
- /*global dojo */
+ /*global define */
  
-/**
- * @namespace The global container for orion APIs.
- */ 
-
 define(["dojo"], function(dojo){
 
-var orion = orion || {};
-
-/**
- * Selection 
- * @class Can provide one or more selections describing objects of interest.  Used to
- *		establish input and output relationships between components.  For example, the selection
- *		in one component can serve as the input of another component.
- */
-orion.Selection = function(serviceRegistry) {
-	this._serviceRegistry = serviceRegistry;
-	this._serviceRegistration = serviceRegistry.registerService("orion.page.selection", this);
-	this._selections = [];
-};
- 
-orion.Selection.prototype = {
-	// consumer
-	getSelection : function(onDone) {
-		onDone(this._getSingleSelection());
-	},
-	
-	getSelections: function(onDone) {
-		onDone(this._selections);
-	},
-	
-	_getSingleSelection: function() {
-		if (this._selections && this._selections.length > 0) {
-			return this._selections[0];
-		} 
-		return null;
-	},
-	
-	// provider
-	setSelections: function(itemOrArray) {
-		if (dojo.isArray(itemOrArray)) {	
-			this._selections = itemOrArray;
-		} else if (itemOrArray) {
-			this._selections = [itemOrArray];
-		} else {
-			this._selections = null;
-		}
-		this._serviceRegistration.dispatchEvent("selectionChanged", this._getSingleSelection(), this._selections);
+	/**
+	 * Constructs a new selection service. Clients should obtain a selection service
+	 * by requesting the service <tt>orion.page.selection</tt> from the service registry.
+	 * This service constructor is only intended to be used by page service registry
+	 * initialization code.
+	 * @name orion.selection.Selection
+	 * @class Can provide one or more selections describing objects of interest.  Used to
+	 * establish input and output relationships between components.  For example, the selection
+	 * in one component can serve as the input of another component.
+	 */
+	function Selection(serviceRegistry) {
+		this._serviceRegistry = serviceRegistry;
+		this._serviceRegistration = serviceRegistry.registerService("orion.page.selection", this);
+		this._selections = [];
 	}
-};
+	 
+	Selection.prototype = /** @lends orion.selection.Selection.prototype */ {
+		/**
+		 * Obtains the current single selection and passes it to the provided function.
+		 * @param onDone The function to invoke with the selection
+		 */
+		getSelection : function(onDone) {
+			//TODO this should return a promise rather than having an onDone parameter
+			onDone(this._getSingleSelection());
+		},
+		
+		/**
+		 * Obtains all current selections and passes them to the provided function.
+		 * @param onDone The function to invoke with the selections
+		 */
+		getSelections: function(onDone) {
+			onDone(this._selections);
+		},
+		
+		_getSingleSelection: function() {
+			if (this._selections && this._selections.length > 0) {
+				return this._selections[0];
+			} 
+			return null;
+		},
+		
+		/**
+		 * Sets the current selection
+		 * @param itemOrArray A single selected item or an array of selected items
+		 */
+		setSelections: function(itemOrArray) {
+			if (dojo.isArray(itemOrArray)) {	
+				this._selections = itemOrArray;
+			} else if (itemOrArray) {
+				this._selections = [itemOrArray];
+			} else {
+				this._selections = null;
+			}
+			this._serviceRegistration.dispatchEvent("selectionChanged", this._getSingleSelection(), this._selections);
+		}
+	};
+	Selection.prototype.constructor = Selection;
 
-return orion;
+	//return module exports
+	return {Selection: Selection};
 });
