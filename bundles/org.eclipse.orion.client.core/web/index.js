d1fdee605a9e58a753c1a608b971d95d4c8ae8bb
John Arthorne <John_Arthorne@ca.ibm.com>
2011-06-06 14:42:14 -0400

    fix jslint global list

--- bundles/org.eclipse.orion.client.core/web/index.js
+++ bundles/org.eclipse.orion.client.core/web/index.js
@@ -9,8 +9,7 @@
  ******************************************************************************/
 
 /*jslint browser:true devel:true*/
-/*global dijit dojo eclipse widgets serviceRegistry:true*/
-
+/*global define */
 
 define(['dojo', 'orion/serviceregistry', 'orion/preferences', 'orion/pluginregistry', 'orion/commands', 'orion/searchClient', 'orion/status', 'orion/globalCommands',
         'dojo/parser', 'dijit/layout/BorderContainer', 'dijit/layout/ContentPane'/*, 'dojox/widget/Portlet', 'dojox/widget/FeedPortlet'*/], 
