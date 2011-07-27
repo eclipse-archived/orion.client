cd80752f6a06eebb28404ac67e288bab131163af
Piotr Janik <janikpiotrek@gmail.com>
2011-07-27 12:28:10 +0200

    Bug 353191 - [client] Git Log should show branches for commits
    Change-Id: I21149112935d2e4fb81272c90fb6a6ba4cbf7252

--- bundles/org.eclipse.orion.client.git/web/orion/git/gitCommands.js
+++ bundles/org.eclipse.orion.client.git/web/orion/git/gitCommands.js
@@ -963,7 +963,7 @@ var exports = {};
 									function(jsonData, secondArg) {
 										var trId = jsonData.Location.replace(/[^\.\:\-\_0-9A-Za-z]/g, "");
 										var tr = dojo.byId(trId);
-										dojo.place(document.createTextNode(tagName), dojo.create("p", {style: "margin: 5px"}, tr.children[5] /* tags column */, "last"), "only");
+										dojo.place(document.createTextNode(tagName), dojo.create("p", {style: "margin: 5px"}, tr.children[6] /* tags column */, "last"), "only");
 									});
 							});
 					return clientDeferred;
