70771a304fceacdf421cda9d918fa574eb856293
skaegi <simon_kaegi@ca.ibm.com>
2011-09-20 11:10:21 -0400

    Refactoring diff provider slightly

--- bundles/org.eclipse.orion.client.core/web/compare/compare.js
+++ bundles/org.eclipse.orion.client.core/web/compare/compare.js
@@ -1,92 +1,81 @@
 /*******************************************************************************
- * Copyright (c) 2011 IBM Corporation and others. 
- * All rights reserved. This program and the accompanying materials are made 
- * available under the terms of the Eclipse Public License v1.0 
- * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
- * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ * Copyright (c) 2011 IBM Corporation and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 (http://www.eclipse.org/legal/epl-v10.html),
+ * and the Eclipse Distribution License v1.0
+ * (http://www.eclipse.org/org/documents/edl-v10.html).
  * 
  * Contributors: IBM Corporation - initial API and implementation
  ******************************************************************************/
-define(['dojo', 'orion/serviceregistry', 'orion/preferences', 'orion/pluginregistry', 'orion/status', 'orion/commands', 
-	        'orion/fileClient', 'orion/searchClient', 'orion/globalCommands', 'orion/compare/compare-features', 'orion/compare/diff-provider', 'orion/compare/compare-container',
-	        'dojo/parser', 'dojo/hash', 'dijit/layout/BorderContainer', 'dijit/layout/ContentPane'], 
-			function(dojo, mServiceregistry, mPreferences, mPluginRegistry, mStatus, mCommands, mFileClient, mSearchClient, mGlobalCommands, mCompareFeatures, mDiffProvider, mCompareContainer) {
+define(['dojo', 'orion/serviceregistry', 'orion/preferences', 'orion/pluginregistry', 'orion/status', 'orion/commands', 'orion/fileClient', 'orion/searchClient', 'orion/globalCommands',
+		'orion/compare/compare-features', 'orion/compare/diff-provider', 'orion/compare/compare-container', 'dojo/parser', 'dojo/hash', 'dijit/layout/BorderContainer', 'dijit/layout/ContentPane'],
+		function(dojo, mServiceregistry, mPreferences, mPluginRegistry, mStatus, mCommands, mFileClient, mSearchClient, mGlobalCommands, mCompareFeatures, mDiffProvider, mCompareContainer) {
 
-dojo.addOnLoad(function() {
-	document.body.style.visibility = "visible";
-	dojo.parser.parse();
-	// initialize service registry and EAS services
-	var serviceRegistry = new mServiceregistry.ServiceRegistry();
-	var pluginRegistry = new mPluginRegistry.PluginRegistry(serviceRegistry);
-	var commandService = new mCommands.CommandService({serviceRegistry: serviceRegistry});
-	var preferenceService = new mPreferences.PreferencesService(serviceRegistry, "/prefs/user");
-	var searcher = new mSearchClient.Searcher({serviceRegistry: serviceRegistry});
-	// File operations
-	var fileClient = new mFileClient.FileClient(serviceRegistry);
-	
-	mGlobalCommands.generateBanner("toolbar", serviceRegistry, commandService, preferenceService, searcher);
-	/* experiment for multiple compare containers
-	var fileClient1 = new eclipse.FileClient(fileService);
-	var uiFactory1 = new orion.CompareMergeUIFactory({parentDivID : "compareContainer1" , showTitle : false , showLineStatus : false});
-	uiFactory1.buildUI();
-	var compareMergeContainer1 = new orion.CompareMergeContainer(readOnly ,new orion.DiffProvider(serviceRegistry),serviceRegistry , commandService, fileClient1,uiFactory1);
-	compareMergeContainer1.resolveDiff(dojo.hash(), 
-			  function(newFile , oldFile){
-			     handleTile(newFile , oldFile , uiFactory);
-	  		  },
-			  function(errorResponse , ioArgs){
-	  			 handleErrorTile(errorResponse , ioArgs , uiFactory);
-			  }
-	);
-	*/
-	var uiFactory = new mCompareFeatures.CompareMergeUIFactory({parentDivID : "compareContainer" , showTitle : true , showLineStatus : true});
-	uiFactory.buildUI();
-	
-	// Diff operations
-	var readOnly = isReadOnly();
-	compareMergeContainer = new mCompareContainer.CompareMergeContainer(readOnly, new mDiffProvider.DiffProvider(serviceRegistry),serviceRegistry , commandService, fileClient,uiFactory);
-	compareMergeContainer.resolveDiff(dojo.hash(), 
-			  function(newFile , oldFile){
-			     handleTile(newFile , oldFile , uiFactory);
-	  		  },
-			  function(errorResponse , ioArgs){
-	  			 handleErrorTile(errorResponse , ioArgs , uiFactory);
-			  }
-	);
-	
-	//every time the user manually changes the hash, we need to load the diff
-	dojo.subscribe("/dojo/hashchange", compareMergeContainer, function() {
-		compareMergeContainer = new mCompareContainer.CompareMergeContainer(readOnly, new mDiffProvider.DiffProvider(serviceRegistry),serviceRegistry , commandService , fileClient,uiFactory);
-		compareMergeContainer.resolveDiff(dojo.hash(), 
-				  function(newFile , oldFile){
-					 handleTile(newFile , oldFile , uiFactory);
-				  },
-				  function(errorResponse , ioArgs){
-					  handleErrorTile(errorResponse , ioArgs , uiFactory);
-				  });
-	});
-});
+			dojo.addOnLoad(function() {
+				document.body.style.visibility = "visible";
+				dojo.parser.parse();
+				// initialize service registry and EAS services
+				var serviceRegistry = new mServiceregistry.ServiceRegistry();
+				var pluginRegistry = new mPluginRegistry.PluginRegistry(serviceRegistry);
+				var commandService = new mCommands.CommandService({
+					serviceRegistry: serviceRegistry
+				});
+				var preferenceService = new mPreferences.PreferencesService(serviceRegistry, "/prefs/user");
+				var searcher = new mSearchClient.Searcher({
+					serviceRegistry: serviceRegistry
+				});
+				// File operations
+				var fileClient = new mFileClient.FileClient(serviceRegistry);
+				var diffProvider = new mDiffProvider.DiffProvider(serviceRegistry);
 
-function isReadOnly(){
-	var queryParams = dojo.queryToObject(window.location.search.slice(1));
-	return queryParams["readonly"] != null;
-};
+				mGlobalCommands.generateBanner("toolbar", serviceRegistry, commandService, preferenceService, searcher);
+				var uiFactory = new mCompareFeatures.CompareMergeUIFactory({
+					parentDivID: "compareContainer",
+					showTitle: true,
+					showLineStatus: true
+				});
+				uiFactory.buildUI();
 
-function handleTile(newFile , oldFile , uiFactory){
-	if(uiFactory.getTitleDivId(true) && uiFactory.getTitleDivId(false)){
-		dojo.place(document.createTextNode(newFile), uiFactory.getTitleDivId(true), "only");				  
-		dojo.place(document.createTextNode(oldFile), uiFactory.getTitleDivId(false), "only");	
-	}
-};
+				// Diff operations
+				var readOnly = isReadOnly();
+				compareMergeContainer = new mCompareContainer.CompareMergeContainer(readOnly, diffProvider, serviceRegistry, commandService, fileClient, uiFactory);
+				compareMergeContainer.resolveDiff(dojo.hash(), function(newFile, oldFile) {
+					handleTile(newFile, oldFile, uiFactory);
+				}, function(errorResponse, ioArgs) {
+					handleErrorTile(errorResponse, ioArgs, uiFactory);
+				});
 
-function handleErrorTile(errorResponse , ioArgs , uiFactory){
-	if(uiFactory.getTitleDivId(true) && uiFactory.getTitleDivId(false)){
-		  var message = typeof(errorResponse.message) === "string" ? errorResponse.message : ioArgs.xhr.statusText; 
-		  dojo.place(document.createTextNode(message), uiFactory.getTitleDivId(true), "only");				  
-		  dojo.place(document.createTextNode(message), uiFactory.getTitleDivId(false), "only");				  
-		  dojo.style(uiFactory.getTitleDivId(true), "color", "red");
-		  dojo.style(uiFactory.getTitleDivId(false), "color", "red");
-	}
-};
+				// every time the user manually changes the hash, we need to
+				// load the diff
+				dojo.subscribe("/dojo/hashchange", compareMergeContainer, function() {
+					compareMergeContainer = new mCompareContainer.CompareMergeContainer(readOnly, diffProvider, serviceRegistry, commandService, fileClient, uiFactory);
+					compareMergeContainer.resolveDiff(dojo.hash(), function(newFile, oldFile) {
+						handleTile(newFile, oldFile, uiFactory);
+					}, function(errorResponse, ioArgs) {
+						handleErrorTile(errorResponse, ioArgs, uiFactory);
+					});
+				});
+			});
 
-});
+			function isReadOnly() {
+				var queryParams = dojo.queryToObject(window.location.search.slice(1));
+				return queryParams["readonly"] != null;
+			}
+
+			function handleTile(newFile, oldFile, uiFactory) {
+				if (uiFactory.getTitleDivId(true) && uiFactory.getTitleDivId(false)) {
+					dojo.place(document.createTextNode(newFile), uiFactory.getTitleDivId(true), "only");
+					dojo.place(document.createTextNode(oldFile), uiFactory.getTitleDivId(false), "only");
+				}
+			}
+
+			function handleErrorTile(errorResponse, ioArgs, uiFactory) {
+				if (uiFactory.getTitleDivId(true) && uiFactory.getTitleDivId(false)) {
+					var message = typeof (errorResponse.message) === "string" ? errorResponse.message : ioArgs.xhr.statusText;
+					dojo.place(document.createTextNode(message), uiFactory.getTitleDivId(true), "only");
+					dojo.place(document.createTextNode(message), uiFactory.getTitleDivId(false), "only");
+					dojo.style(uiFactory.getTitleDivId(true), "color", "red");
+					dojo.style(uiFactory.getTitleDivId(false), "color", "red");
+				}
+			}
+		});
