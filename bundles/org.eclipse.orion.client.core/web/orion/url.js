5d12f85b9f09a902cddc8263cea5dc1de3d0f796
Boris Bokowski <bokowski@gmail.com>
2011-10-28 16:07:19 +0200

    Bug 362313 - Add JSDoc @license tags to copyright headers

--- bundles/org.eclipse.orion.client.core/web/orion/url.js
+++ bundles/org.eclipse.orion.client.core/web/orion/url.js
@@ -1,4 +1,5 @@
 /*******************************************************************************
+ * @license
  * Copyright (c) 2011 IBM Corporation and others. All rights reserved. This
  * program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 (http://www.eclipse.org/legal/epl-v10.html),
