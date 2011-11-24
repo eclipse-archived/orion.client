4a3d432e12edd767a4db98eb72007a748c4d1207
Mark Macdonald <mamacdon@ca.ibm.com>
2011-11-24 16:56:01 -0500

    Allow asyncStyler to contribute highlight problems as annotations

--- bundles/org.eclipse.orion.client.editor/web/orion/editor/asyncStyler.js
+++ bundles/org.eclipse.orion.client.editor/web/orion/editor/asyncStyler.js
@@ -11,17 +11,20 @@
 /*jslint browser:true regexp:true*/
 /*global define console*/
 define([], function() {
+	var HIGHLIGHT_ERROR_ANNOTATION = "orion.annotation.highlightError";
+	
 	/**
 	 * Pushes styles provided by a service into the textView.
 	 */
-	function AsyncStyler(textView, service) {
-		this.initialize(textView, service);
+	function AsyncStyler(textView, service, annotationModel) {
+		this.initialize(textView, service, annotationModel);
 		this.lineStyles = [];
 	}
 	AsyncStyler.prototype = {
-		initialize: function(textView, service) {
+		initialize: function(textView, service, annotationModel) {
 			this.textView = textView;
 			this.service = service;
+			this.annotationModel = annotationModel; 
 			var self = this;
 			this._listener = {
 				onModelChanging: function(e) {
@@ -79,6 +82,7 @@ define([], function() {
 		onStyleReady: function(e) {
 			var style = e.style;
 			var min = Number.MAX_VALUE, max = -1;
+			var model = this.textView.getModel();
 			for (var lineIndex in style) {
 				if (style.hasOwnProperty(lineIndex)) {
 //					console.debug("Got style for line# " + lineIndex);
@@ -88,7 +92,39 @@ define([], function() {
 				}
 			}
 			min = Math.max(min, 0);
-			max = Math.min(max, this.textView.getModel().getLineCount());
+			max = Math.min(max, model.getLineCount());
+			
+			var annotationModel = this.annotationModel;
+			if (annotationModel) {
+				var annos = annotationModel.getAnnotations(model.getLineStart(min), model.getLineEnd(max));
+				var toRemove = [];
+				while (annos.hasNext()) {
+					var anno = annos.next();
+					if (anno.type === HIGHLIGHT_ERROR_ANNOTATION) {
+						toRemove.push(anno);
+					}
+				}
+				var toAdd = [];
+				for (var i = min; i <= max; i++) {
+					lineIndex = i;
+					var lineStyle = this.lineStyles[lineIndex], errors = lineStyle && lineStyle.errors;
+					var lineStart = model.getLineStart(lineIndex);
+					if (errors) {
+						for (var j=0; j < errors.length; j++) {
+							var err = errors[j];
+							toAdd.push({
+								start: lineStart + err.start,
+								end: lineStart + err.end,
+								type: HIGHLIGHT_ERROR_ANNOTATION,
+								title: "Syntax error.",
+								html: "<div class='annotationHTML error'></div>",
+								rangeStyle: {styleClass: "annotationRange error"}
+								});
+						}
+					}
+				}
+				annotationModel.replaceAnnotations(toRemove, toAdd);
+			}
 			this.textView.redrawLines(min, max + 1);
 		},
 		onLineStyle: function(e) {
