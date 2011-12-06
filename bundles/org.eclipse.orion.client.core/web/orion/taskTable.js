c69d0e157d68ac5db19336c1c841894057b77f92
Malgorzata Janczarska <malgorzata.tomczyk@pl.ibm.com>
2011-12-06 18:33:31 +0100

    Bug 364987 -[client][progress service] Tasks service for new API and tasks navigator

--- bundles/org.eclipse.orion.client.core/web/orion/taskTable.js
+++ bundles/org.eclipse.orion.client.core/web/orion/taskTable.js
@@ -8,8 +8,8 @@
  * 
  * Contributors: IBM Corporation - initial API and implementation
  ******************************************************************************/
-define([ 'require', 'dojo', 'orion/explorer' ], function(require, dojo,
-		mExplorer) {
+define([ 'require', 'dojo', 'orion/explorer', 'orion/taskCommands' ], function(require, dojo,
+		mExplorer, mTasksCommands) {
 	var exports = {};
 
 	exports.TasksExplorer = (function() {
@@ -26,8 +26,38 @@ define([ 'require', 'dojo', 'orion/explorer' ], function(require, dojo,
 		TasksExplorer.prototype = new mExplorer.Explorer();
 		
 		TasksExplorer.prototype.loadTasks = function(taskList){
+			taskList.Children.sort(function(task1, task2){return parseInt(task2.Modified) - parseInt(task1.Modified);});
 			this.tasks = taskList;
+			mTasksCommands.updateNavTools(this.registry, this, this.toolbarId, this.selectionToolsId, this.tasks);
 			this.createTree(this.parentId, new mExplorer.ExplorerFlatModel(null, null, taskList.Children));
+		};
+		
+		TasksExplorer.prototype.removeCompletedTasks = function(){
+			var newTasks = [];
+			for(var taskId in this.tasks.Children){
+				if(this.tasks.Children[taskId].Running){
+					newTasks.push(this.tasks.Children[taskId]);
+				}
+			}
+			this.tasks.Children = newTasks;
+			this.loadTasks(this.tasks);		
+		};
+		
+		TasksExplorer.prototype.removeTasks = function(tasksToRemove){
+			var newTasks = [];
+			for(var taskId in this.tasks.Children){
+				var foundTask = false;
+				for(var i in tasksToRemove){
+					if(tasksToRemove[i].Location === this.tasks.Children[taskId].Location){
+						foundTask = true;
+						break;
+					}
+				}
+				if(!foundTask)
+					newTasks.push(this.tasks.Children[taskId]);
+			}
+			this.tasks.Children = newTasks;
+			this.loadTasks(this.tasks);
 		};
 		
 		TasksExplorer.prototype.mergeTasks = function(tasksToMerge){
@@ -82,10 +112,10 @@ define([ 'require', 'dojo', 'orion/explorer' ], function(require, dojo,
 					return dojo.create("th", {style: "padding-left: 5px; padding-right: 5px", innerHTML: "<h2>Name</h2>"});
 					break;
 				case 1: 
-					return dojo.create("th", {style: "padding-left: 5px; padding-right: 5px", innerHTML: "<h2>Message</h2>"});
+					return dojo.create("th", {style: "padding-left: 5px; padding-right: 5px", innerHTML: "<h2>Status</h2>"});
 					break;
 				case 2:
-					return dojo.create("th", {style: "padding-left: 5px; padding-right: 5px", innerHTML: "<h2>Status</h2>"});
+					return dojo.create("th", {style: "padding-left: 5px; padding-right: 5px", innerHTML: "<h2>Actions</h2>"});
 					break;
 			}
 			
@@ -97,14 +127,27 @@ define([ 'require', 'dojo', 'orion/explorer' ], function(require, dojo,
 				var col = dojo.create("td", {style: "padding-left: 5px; padding-right: 5px", innerHTML: item.Name});
 				var div = dojo.create("div", null, col, "only");
 				link = dojo.create("a", {innerHTML: item.Name, className: "navlinkonpage"}, div, "last");
+
 				dojo.place(document.createTextNode(item.Name), link, "only");
+				
+				var taskIcon = dojo.create("span", null, div, "first");
+				dojo.addClass(taskIcon, "imageSprite");
+				if(item.Running===true)
+					dojo.addClass(taskIcon, "core-sprite-start");
+				else if(item.Canceled===true)
+					dojo.addClass(taskIcon, "core-sprite-stop");
+				else if(item.Failed===true)
+					dojo.addClass(taskIcon, "core-sprite-error");
+				else
+					dojo.addClass(taskIcon, "core-sprite-ok");
+				
 				return col;
 				break;
 			case 1:
 				return dojo.create("td", {style: "padding-left: 5px; padding-right: 5px", innerHTML: item.Message});
 				break;
 			case 2:
-				return dojo.create("td", {style: "padding-left: 5px; padding-right: 5px", innerHTML: item.Running ? "running" : "completed"});
+				return this.getActionsColumn(item, tableRow);
 				break;
 			}
 		};
