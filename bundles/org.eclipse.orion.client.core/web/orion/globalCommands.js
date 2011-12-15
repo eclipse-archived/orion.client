206d9993a294738f9e0cf27c762c1eef54726da2
sfranklin <susan_franklin@us.ibm.com>
2011-12-15 00:21:22 -0500

    Bug 366756 - Update Report A Bug link to version 0.4

--- bundles/org.eclipse.orion.client.core/web/orion/globalCommands.js
+++ bundles/org.eclipse.orion.client.core/web/orion/globalCommands.js
@@ -313,7 +313,7 @@ define(['require', 'dojo', 'dijit', 'orion/commands', 'orion/util', 'orion/textv
 		'This is a Beta build of Orion.  You can use it, play with it and explore the ' +
 		'capabilities but BEWARE your data may be lost. &nbsp;| '+ 
 		'<a href="http://wiki.eclipse.org/Orion/FAQ">FAQ</a> | ' + 
-		'<a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=Orion&version=0.3">Report a Bug</a> | ' +
+		'<a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=Orion&version=0.4">Report a Bug</a> | ' +
 		'<a href="http://www.eclipse.org/legal/privacy.php">Privacy Policy</a> | ' + 
 		'<a href="http://www.eclipse.org/legal/termsofuse.php">Terms of Use</a> | '+ 
 		'<a href="http://www.eclipse.org/legal/copyright.php">Copyright Agent</a>'; 
