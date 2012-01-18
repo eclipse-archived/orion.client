a65908763f671a3cf18f082859ee63becf228367
skaegi <simon_kaegi@ca.ibm.com>
2012-01-18 10:12:25 -0500

    Adding PageUtil and support for matching URITemplates of the form http://someserver/a#{resource, params*}

--- bundles/org.eclipse.orion.client.core/web/orion/url.js
+++ bundles/org.eclipse.orion.client.core/web/orion/url.js
@@ -9,7 +9,7 @@
  * Contributors: IBM Corporation - initial API and implementation
  ******************************************************************************/
 
-/* global define */
+/*global define */
 define(function() {
 	var buildMap = {};
 	function jsEscape(text) {
