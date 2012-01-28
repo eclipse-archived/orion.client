aec61ee106eb6deff3aba0fa800554f8280d53b5
libing wang <libingw@ca.ibm.com>
2012-01-28 11:39:44 -0500

    Bug 369739 - [Search]Improve file/match iteration in result page.--Disable arrow key scroll in result page. Listen to key event only in result div.

--- bundles/org.eclipse.orion.client.core/web/orion/globalCommands.js
+++ bundles/org.eclipse.orion.client.core/web/orion/globalCommands.js
@@ -657,8 +657,8 @@ define(['require', 'dojo', 'dijit', 'orion/commands', 'orion/extensionCommands',
 		if (!searchField) {
 			throw "failed to generate HTML for banner";
 		}
-		dojo.connect(searchField, "onkeydown", function(e){
-			if (e.keyCode === dojo.keys.ENTER) {
+		dojo.connect(searchField, "onkeypress", function(e){
+			if (e.charOrCode === dojo.keys.ENTER) {
 				if (searcher) {
 					if (searchField.value.length > 0) {
 						var query = searcher.createSearchQuery(searchField.value);
@@ -668,7 +668,6 @@ define(['require', 'dojo', 'dijit', 'orion/commands', 'orion/extensionCommands',
 					window.alert("Can't search: no search service is available");
 				}
 			}
-			e.stopPropagation();
 		});
 		
 		// Put page title in title area.  
