88506144c233479f79fbb4fa36fd0666a8b3ddca
Malgorzata Janczarska <malgorzata.tomczyk@pl.ibm.com>
2012-02-01 13:52:35 +0100

    Bug 370148 -Commit entries on git-log page are not links

--- bundles/org.eclipse.orion.client.git/web/git/git-log.js
+++ bundles/org.eclipse.orion.client.git/web/git/git-log.js
@@ -274,12 +274,6 @@ function initTitleBar(fileClient, navigator, item, searcher, commandService){
 					navigator.isDirectory = metadata.Directory;
 					mGitCommands.updateNavTools(serviceRegistry, navigator, "pageActions", "selectionTools", navigator._lastTreeRoot);
 					navigator.updateCommands();
-					if(metadata.Directory){
-						//remove links to commit
-						dojo.query(".navlinkonpage").forEach(function(node, i) {
-							node.removeAttribute("href");
-						});
-					}
 				}),
 				dojo.hitch(this, function(error) {
 					console.error("Error loading file metadata: " + error.message);
