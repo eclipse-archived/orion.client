3ac868200052ac403db94687c820cb339e2e0d0e
skaegi <simon_kaegi@ca.ibm.com>
2012-02-06 09:38:37 -0500

    Updating asyncTestWrapper to handle deferred Test results correctly

--- bundles/org.eclipse.orion.client.core/web/orion/asyncTestWrapper.js
+++ bundles/org.eclipse.orion.client.core/web/orion/asyncTestWrapper.js
@@ -13,14 +13,9 @@
 /*global jstestdriver AsyncTestCase fail*/
 /*global eclipse console dojo define*/
 
-
-
 define( ['dojo', 'orion/serviceregistry', 'orion/pluginregistry', 'dojo/DeferredList' ], function(dojo, mServiceregistry, mPluginregistry){
-var orion = orion || {};
-
-orion.JSTestAdapter = (function() {
 	
-	var testLoader = function(test) {
+	function TestLoader(test) {
 		/* This loader is a single test on the main suite, the loader test
 		 * is finished once we have loaded the test plugin and retrieved the list of tests it contains.
 		 * Once the all the suite's load tests are done, then the real tests are ready to go.
@@ -28,11 +23,13 @@ orion.JSTestAdapter = (function() {
 
 		var noop, errback;
 		var deferred = new dojo.Deferred();
-		var exports = {};
 		var testSuite = {};
 		
+				
+		this.testName = test;
+		
 		/* This is the TestCase which will hold all the tests for the given test plugin */
-		exports.testCase = function() {}; //AsyncTestCase(test);	
+		this.testCase = function() {}; //AsyncTestCase(test);	
 		
 		function _launch(queue) {
 			/* The entire test service is run in the queue for the first jstestdriver test
@@ -45,14 +42,9 @@ orion.JSTestAdapter = (function() {
 				}
 			}
 			
-			/* After the tests, the registry must be cleaned up */
-			var shutdown = new dojo.Deferred();
-			queue.call("Shutdown " + test, function(callbacks) {
-				shutdown.resolve(callbacks.noop());
-			});
-			
 			var loaderServiceRegistry = new mServiceregistry.ServiceRegistry();
 			var loaderPluginRegistry = new mPluginregistry.PluginRegistry(loaderServiceRegistry, {});
+			var testRunnerDeferred = new dojo.Deferred();
 			loaderPluginRegistry.installPlugin(test).then(function() {
 				var service = loaderServiceRegistry.getService("orion.test.runner");
 				service.addEventListener("testDone", function(testName, testResult) {
@@ -60,20 +52,26 @@ orion.JSTestAdapter = (function() {
 						testSuite[testName].testDone(testResult);
 					}
 				});
-				service.addEventListener("runDone", function(runName, obj) {
-					shutdown.then(function(noop) {
+				var runCount = 0;
+				service.addEventListener("runStart", function() {
+					runCount++;
+				});
+				service.addEventListener("runDone", function() {
+					runCount--;
+					if (runCount === 0) {
 						loaderPluginRegistry.shutdown();
-						noop();
-					});
+						testRunnerDeferred.resolve();
+					}
 				});
 					
 				console.log("Launching test suite: " + test);
 				service.run();
 			});
+			return testRunnerDeferred;
 		}
 		
 		var first = true;
-		function _test(testName) {
+		function createTest(testName) {
 			return function(queue) {
 				if (first) {
 					_launch(queue);
@@ -91,7 +89,7 @@ orion.JSTestAdapter = (function() {
 			
 		/* Loading the test methods, #ready is called once we have the set of tests to run 
 		   #error is called if there is a problem loading the test plugin */
-		exports.loadMethod = function(queue) {
+		this.loadMethod = function(queue) {
 			queue.call("Load test " + test, function(callbacks) {
 				noop = callbacks.noop();
 				errback = callbacks.addErrback('Error');
@@ -99,138 +97,122 @@ orion.JSTestAdapter = (function() {
 			});
 		};
 		
-		exports.ready = function(suite) {
+		this.ready = function(suite) {
+			var that = this;
 			deferred.then(function() { 
 				testSuite = suite;
 							
 				for(var testName in suite) {
 					if(suite.hasOwnProperty(testName)) {
-						exports.testCase.prototype[testName] = _test(testName);
+						that.testCase.prototype[testName] = createTest(testName);
 					}
 				}
 				
-				var testCaseInfo = new jstestdriver.TestCaseInfo(test, exports.testCase, jstestdriver.TestCaseInfo.ASYNC_TYPE);		
+				var testCaseInfo = new jstestdriver.TestCaseInfo(test, that.testCase, jstestdriver.TestCaseInfo.ASYNC_TYPE);		
 				jstestdriver.testRunner.testRunsConfiguration_.push(testCaseInfo.getDefaultTestRunConfiguration());
 				noop(); 
 			});
 		};
-		exports.error = function(e) {
+		
+		this.error = function(e) {
 			deferred.then(function(msg) {
 				errback(msg);
 			}(e));
 		};
-		
-		exports.testName = test;
-		
-		return exports;
-	};
+	}
 	
-	var asyncTest = function(testName, test) {
+	function createAsyncTest(testName, test) {
 		/* This is a single jstestdriver async test case, it is just a placeholder and 
 		 * testDone() will be called when the real test is finished
 		 */
-		var exports = {};
-		var noop, errback;
-		var deferred = new dojo.Deferred();
+		var noop, errback, deferred = new dojo.Deferred();
 		
-		exports.testMethod = function (queue) {
+		function testMethod(queue) {
 			/* jstestdriver calls this method to run the test */
 			queue.call("Run test " + testName, function(callbacks) {
 				noop = callbacks.noop();
-				errback = callbacks.addErrback('Error');
-				deferred.resolve( { "noop" : noop , "errback" : errback } );
+				errback = callbacks.addErrback("Error");
+				deferred.resolve();
 			});
-		};
+		}
 		
-		exports.testDone = function(testResult) {
+		function testDone(testResult) {
 			this.testResult = testResult;
 			/* the real test has completed, mark the jstestdriver test as complete */
-			deferred.then( function(callbacks) {
+			
+			deferred.then(function() {
 				if (testResult.result) {
-					callbacks.noop();
+					noop();
 				} else {
-					callbacks.errback(testResult.message);
+					errback(testResult.message);
 				}
 			});
-		};
+		}
 		
-		return exports;
-	};
+		return {testMethod: testMethod, testDone: testDone};
+	}
 	
 	
 	function _loadTests(fileURI) {	 
-		var loader = testLoader(fileURI);
+		var loader = new TestLoader(fileURI);
 		var testServiceRegistry = new mServiceregistry.ServiceRegistry();
 		var testPluginRegistry = new mPluginregistry.PluginRegistry(testServiceRegistry, {});
-		var deferred = new dojo.Deferred();
 		
 		/* Install the test plugin and get the list of tests it contains */
-		testPluginRegistry.installPlugin(fileURI).then(
-			function() {
-				var service =  testServiceRegistry.getService("orion.test.runner");
-				service.list().then( function(testNames) {
-					var testName;
-					var testSuite = {};
-					
-					/* for each test in the suite, hook up an jstestdriver async test */
-					for (var i = 0; i < testNames.length; i++) {
-						testName = testNames[i];
-						testSuite[testName] = asyncTest(testName);
-					} 
-					console.log(fileURI + " : registered " + testNames.length + " tests" );
-					
-					/* jstestdriver will clear out the html body when the loader test is finished,
-					 * we must shut down the plugin registry nicely, and then reload the test plugin later */
-					testPluginRegistry.shutdown();
-					
-					loader.ready(testSuite);
-					deferred.resolve(loader);
-				});
-			}, 
-			function(e) {
-				console.log(fileURI + " : error getting orion.test.runner service " + e);
-				loader.error(e);
-				deferred.resolve( { 
-					testName: fileURI, 
-					loadMethod: function() { 
-						throw e; 
-					} 
-				});
-				return e;
-			}
-		);
-		
-		return deferred.promise;
+		return testPluginRegistry.installPlugin(fileURI).then(function() {
+			var service = testServiceRegistry.getService("orion.test.runner");
+			return service.list().then( function(testNames) {
+				var testName;
+				var testSuite = {};
+				
+				/* for each test in the suite, hook up an jstestdriver async test */
+				for (var i = 0; i < testNames.length; i++) {
+					testName = testNames[i];
+					testSuite[testName] = createAsyncTest(testName);
+				} 
+				console.log(fileURI + " : registered " + testNames.length + " tests" );
+				
+				/* jstestdriver will clear out the html body when the loader test is finished,
+				 * we must shut down the plugin registry nicely, and then reload the test plugin later */
+				testPluginRegistry.shutdown();
+				
+				loader.ready(testSuite);
+				return loader;
+			});
+		}, function(e) {
+			console.log(fileURI + " : error getting orion.test.runner service " + e);
+			loader.error(e);
+			return { 
+				testName: fileURI, 
+				loadMethod: function() { 
+					throw e; 
+				} 
+			};
+		});
 	}
 
+	function JSTestAdapter() {
+	}
 	
-	function runTests(suiteName, testURIs) {
-		var ready = new dojo.Deferred();
-
+	JSTestAdapter.runTests = function(suiteName, testURIs) {
 		var loaders = [];
 		for (var i = 0; i < testURIs.length; i++) {
 			loaders.push(_loadTests(testURIs[i]));
 		}
-		
-		new dojo.DeferredList(loaders, false, false, true).then(function(results) {
-			var suite = new AsyncTestCase(suiteName);
+		var dl = new dojo.DeferredList(loaders, false, false, true);
+		return dl.then(function(results) {
+			var OrionTestLoader = new AsyncTestCase(suiteName);
 			for(var i = 0; i < results.length; i++) {
 				var test = results[i][1];		
-				if (typeof test.testName !== "undefined") {
-					suite.prototype["testLoader " + test.testName] = test.loadMethod;
+				if (test.testName) {
+					OrionTestLoader.prototype["testLoader " + test.testName] = test.loadMethod;
 				}
 			}
-
-			var testCaseInfo = new jstestdriver.TestCaseInfo(suiteName, suite, jstestdriver.TestCaseInfo.ASYNC_TYPE);		
+			
+			var testCaseInfo = new jstestdriver.TestCaseInfo(suiteName, OrionTestLoader, jstestdriver.TestCaseInfo.ASYNC_TYPE);		
 			jstestdriver.testRunner.testRunsConfiguration_.push(testCaseInfo.getDefaultTestRunConfiguration());
-
-			ready.resolve();
 		});
-		
-		return ready.promise;
-	}
-	return { runTests: runTests };
-}());
+	};
+	return {JSTestAdapter: JSTestAdapter};
 
-return orion;
 });
