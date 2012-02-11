45de54ea779db576e7669d5db33f7f7ef8032f79
libing wang <libingw@ca.ibm.com>
2012-02-11 13:51:52 -0500

    Bug 371311 - [Search]De-couple in-file-search and replace APIs from searchResultModel.

--- bundles/org.eclipse.orion.client.core/web/orion/searchUtils.js
+++ bundles/org.eclipse.orion.client.core/web/orion/searchUtils.js
@@ -171,6 +171,209 @@ orion.searchUtils.replaceStringLiteral = function(text, keyword, replacingStr){
 	return orion.searchUtils.replaceRegEx(text,regexp, replacingStr);
 };
 
+orion.searchUtils.searchOnelineLiteral =  function(inFileQuery, lineString, onlyOnce){
+	var i,startIndex = 0;
+	var found = false;
+	var result = [];
+	while(true){
+		i = lineString.indexOf(inFileQuery.searchStr, startIndex);
+		if (i < 0) {
+			break;
+		} else {
+			result.push({startIndex: i});
+			found = true;
+			if(onlyOnce){
+				break;
+			}
+			startIndex = i + inFileQuery.searchStrLength;
+		}
+	}
+	if(found) {
+		return result;
+	}
+	return null;
+	
+};
+
+/**
+ * Helper for finding regex matches in text contents.
+ * 
+ * @param {String}
+ *            pattern A valid regexp pattern.
+ * @param {String}
+ *            flags Valid regexp flags: [is]
+ * @param {Number}
+ *            [startIndex] Default is false.
+ * @return {Object} An object giving the match details, or
+ *         <code>null</code> if no match found. The
+ *         returned object will have the properties:<br />
+ *         {Number} index<br />
+ *         {Number} length
+ */
+orion.searchUtils.findRegExp =  function(text, pattern, flags, startIndex) {
+	if (!pattern) {
+		return null;
+	}
+	flags = flags || "";
+	// 'g' makes exec() iterate all matches, 'm' makes ^$
+	// work linewise
+	flags += (flags.indexOf("g") === -1 ? "g" : "")
+			+ (flags.indexOf("m") === -1 ? "m" : "");
+	var regexp = new RegExp(pattern, flags);
+	var result = null, match = null;
+	result = regexp.exec(text.substring(startIndex));
+	return result && {
+		startIndex : result.index + startIndex,
+		length : result[0].length
+	};
+};
+
+orion.searchUtils.searchOnelineRegEx =  function(inFileQuery, lineString, onlyOnce){
+	var i,startIndex = 0;
+	var found = false;
+	var result = [];
+	while(true){
+		var regExResult = orion.searchUtils.findRegExp(lineString, inFileQuery.regExp.pattern, inFileQuery.regExp.flags, startIndex);
+		if(regExResult){
+			result.push(regExResult);
+			found = true;
+			if(onlyOnce){
+				break;
+			}
+			startIndex = regExResult.startIndex + regExResult.length;
+		} else {
+			break;
+		}
+	}
+	if(found) {
+		return result;
+	}
+	return null;
+};
+
+orion.searchUtils.generateNewContents = function( oldContents, newContents, fileModelNode, replaceStr, searchStrLength){
+	if(fileModelNode && oldContents){
+		var updating;
+		if(newContents.length > 0){
+			updating = true;
+		} else {
+			updating = false;
+		}
+		for(var i = 0; i < oldContents.length ; i++){
+			var lineStringOrigin = oldContents[i];
+			var changingLine = false;
+			var checked = false;
+			var fullChecked = false;
+			var checkedMatches = [];
+			var originalMatches;
+			var startNumber = 0;
+			for(var j = 0; j < fileModelNode.children.length; j++){
+				var lnumber = fileModelNode.children[j].lineNumber - 1;
+				if(lnumber === i){
+					startNumber = j;
+					for(var k = 0; k < fileModelNode.children[j].matches.length; k++ ){
+						if(fileModelNode.children[j+k].checked !== false){
+							checkedMatches.push(k);
+						}
+					}
+					checked = (checkedMatches.length > 0);
+					fullChecked = (checkedMatches.length === fileModelNode.children[j].matches.length);
+					originalMatches = fileModelNode.children[j].matches; 
+					changingLine = true;
+					break;
+				}
+			}
+			if(changingLine){
+				var newStr;
+				if(!checked){
+					newStr = lineStringOrigin;
+					for(var k = 0; k < fileModelNode.children[startNumber].matches.length; k++ ){
+						fileModelNode.children[startNumber+k].newMatches = fileModelNode.children[startNumber+k].matches;
+					}
+				} else{
+					var result =  orion.searchUtils.replaceCheckedMatches(lineStringOrigin, replaceStr, originalMatches, checkedMatches, searchStrLength);
+					newStr = result.replacedStr;
+					for(var k = 0; k < fileModelNode.children[startNumber].matches.length; k++ ){
+						fileModelNode.children[startNumber+k].newMatches = result.newMatches;
+					}
+				}
+				if(updating){
+					newContents[i] = newStr;
+				} else {
+					newContents.push(newStr);
+				}
+			} else if(!updating){
+				newContents.push(lineStringOrigin);
+			}
+		}
+	}
+};
+
+orion.searchUtils.generateMatchContext = function(contextAroundLength, fileContents, lineNumber/*zero based*/){
+	var context = [];
+	var totalContextLength = contextAroundLength*2 + 1;
+	var startFrom, endTo;
+	if(fileContents.length <= totalContextLength){
+		startFrom = 0;
+		endTo = fileContents.length -1;
+	} else {
+		startFrom = lineNumber - contextAroundLength;
+		if(startFrom < 0){
+			startFrom = 0;
+			endTo = startFrom + totalContextLength - 1;
+		} else {
+			endTo = lineNumber + contextAroundLength;
+			if(endTo > (fileContents.length -1)){
+				endTo = fileContents.length -1;
+				startFrom = endTo - totalContextLength + 1;
+			}
+			
+		}
+	}
+	for(var i = startFrom; i <= endTo; i++){
+		context.push({context: fileContents[i], current: (i === lineNumber)});
+	}
+	return context;
+};
+
+orion.searchUtils.searchWithinFile = function( inFileQuery, fileModelNode, fileContentText, lineDelim, replacing){
+	var fileContents = fileContentText.split(lineDelim);
+	if(replacing){
+		fileModelNode.contents = fileContents;
+	}
+	if(fileModelNode){
+		fileModelNode.children = [];
+		var totalMatches = 0;
+		for(var i = 0; i < fileContents.length ; i++){
+			var lineStringOrigin = fileContents[i];
+			if(lineStringOrigin && lineStringOrigin.length > 0){
+				var lineString = lineStringOrigin.toLowerCase();
+				var result;
+				if(inFileQuery.wildCard){
+					result = orion.searchUtils.searchOnelineRegEx(inFileQuery, lineString);
+				} else {
+					result = orion.searchUtils.searchOnelineLiteral(inFileQuery, lineString);
+				}
+				if(result){
+					var lineNumber = i+1;
+					if(!replacing){
+						var detailNode = {parent: fileModelNode, context: orion.searchUtils.generateMatchContext(2, fileContents, i), checked: fileModelNode.checked, type: "detail", matches: result, lineNumber: lineNumber, name: lineStringOrigin, linkLocation: fileModelNode.linkLocation + ",line=" + lineNumber, location: fileModelNode.location + "-" + lineNumber};
+						fileModelNode.children.push(detailNode);
+					} else {
+						for(var j = 0; j < result.length; j++){
+							var matchNumber = j+1;
+							var detailNode = {parent: fileModelNode, checked: fileModelNode.checked, type: "detail", matches: result, lineNumber: lineNumber, matchNumber: matchNumber, name: lineStringOrigin, location: fileModelNode.location + "-" + lineNumber + "-" + matchNumber};
+							fileModelNode.children.push(detailNode);
+						}
+					}
+					totalMatches += result.length;
+				}
+			}
+		}
+		fileModelNode.totalMatches = totalMatches;
+	}
+};
+
 orion.searchUtils.replaceCheckedMatches = function(text, replacingStr, originalMatches, checkedMatches, defaultMatchLength){
 	var gap = defaultMatchLength;
 	var startIndex = 0;
