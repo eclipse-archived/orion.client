6479d4a616ff49d6a781f41bbfe1c1aeafd40a0a
sfranklin <susan_franklin@us.ibm.com>
2012-03-06 19:45:06 -0500

    Bug 372914 - implement an "orion.page.content" service extension

---
+++ bundles/org.eclipse.orion.client.core/web/content/saveHook.js
@@ -0,0 +1,97 @@
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2012 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*global define dojo dijit orion window widgets*/
+/*jslint browser:true*/
+
+/*
+ * Glue code for content.html
+ */
+
+define(['require', 'dojo', 'orion/bootstrap', 'orion/status', 'orion/progress', 'orion/commands', 'orion/fileClient', 'orion/operationsClient',
+	        'orion/searchClient', 'orion/dialogs', 'orion/globalCommands', 'orion/breadcrumbs', 'orion/URITemplate', 'orion/PageUtil', 
+	        'dojo/parser', 'dojo/hash', 'dojo/date/locale', 'dijit/layout/BorderContainer', 'dijit/layout/ContentPane', 'orion/widgets/NewSiteDialog'], 
+			function(require, dojo, mBootstrap, mStatus, mProgress, mCommands, mFileClient, mOperationsClient, mSearchClient, mDialogs, 
+			mGlobalCommands, mBreadcrumbs, URITemplate, PageUtil) {
+
+	dojo.addOnLoad(function() {
+		mBootstrap.startup().then(function(core) {
+			var serviceRegistry = core.serviceRegistry;
+			var preferences = core.preferences;
+			// Register services
+			var dialogService = new mDialogs.DialogService(serviceRegistry);
+			var operationsClient = new mOperationsClient.OperationsClient(serviceRegistry);
+			var statusService = new mStatus.StatusReportingService(serviceRegistry, operationsClient, "statusPane", "notifications", "notificationArea");
+			var progressService = new mProgress.ProgressService(serviceRegistry, operationsClient);
+			var commandService = new mCommands.CommandService({serviceRegistry: serviceRegistry});
+			var fileClient = new mFileClient.FileClient(serviceRegistry);
+			var searcher = new mSearchClient.Searcher({serviceRegistry: serviceRegistry, commandService: commandService, fileService: fileClient});
+			
+			// parse the URL to determine what should be saved.
+			var params = PageUtil.matchResourceParameters(window.location.href);
+			if (params.contentProvider) {
+				// Note that the shape of the "orion.page.content" extension is not in any shape or form that could be considered final.
+				// We've included it to enable experimentation. Please provide feedback on IRC or bugzilla.
+				var contentProviders = serviceRegistry.getServiceReferences("orion.page.content");
+				for (var i=0; i<contentProviders.length; i++) {
+					// Exclude any navigation commands themselves, since we are the navigator.
+					var id = contentProviders[i].getProperty("id");
+					if (id === params.contentProvider) {
+						var impl = serviceRegistry.getService(contentProviders[i]);
+						var info = {};
+						var propertyNames = contentProviders[i].getPropertyNames();
+						for (var j = 0; j < propertyNames.length; j++) {
+							info[propertyNames[j]] = contentProviders[i].getProperty(propertyNames[j]);
+						}
+						if (info.saveToken) {
+							// save tokens would typically have special characters such as '?' or '&' in them so we can't use
+							// the URI template to parse them.  Not sure how we could best express this.  For now we have the plugin
+							// specify a token that signifies the start of the URl and possible terminators
+							var tokens = dojo.isArray(info.saveToken) ? info.saveToken : [info.saveToken];
+							var parameterStart = dojo.hash().indexOf(",");
+							if (parameterStart >= 0) {
+								var parameterString = dojo.hash().substring(parameterStart);
+								for (var i=0; i<tokens.length; i++) {
+									var index = parameterString.indexOf(info.saveToken[i]);
+									if (index >= 0) {
+										var contentURL = parameterString.substring(index+info.saveToken[i].length);
+										if (info.saveTokenTerminator) {
+											var terminators = dojo.isArray(info.saveTokenTerminator) ? info.saveTokenTerminator : [info.saveTokenTerminator];
+											for (var j=0; j<terminators.length; j++) {
+												var ending = contentURL.indexOf(terminators[j]);
+												if (ending >= 0) {
+													contentURL = contentURL.substring(0, ending);
+													break;
+												}
+											}
+										}
+										if (contentURL && contentURL.length > 0) {
+											dojo.place("<p>Content plugin <b>" + info.name + "</b> has saved data at <a href='" + contentURL + "'>" + contentURL + "</a>." +
+											"<p>It is cool that we know this, but now we need a way to read the blob at this URL and save back to fileClient.  See <a href='https://bugs.eclipse.org/bugs/show_bug.cgi?id=373443'>Bug 373443</a></p>" +
+											"<p>Once we are able to save the data, not clear where you would go from this page.  You could use the breadcrumb, main nav links, related pages, browser history," +
+											" etc., to get back to your next task.</p>" +
+											"<p>This area probably just tells you that the save was successful and then lets you click a link to keep editing?..ie..reload the page to return to the original editor.</p>", 
+											"orion.saveRequest" ,"only");
+										}
+										break;
+									}
+								}
+							}
+						}
+						break;
+					}
+				}
+			}
+			document.body.style.visibility = "visible";
+			dojo.parser.parse();
+		});
+	});
+});
