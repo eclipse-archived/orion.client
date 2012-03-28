deceac9c0b51e6bd870a93868f9d59550d198c12
Max Li <maxli@ca.ibm.com>
2012-03-27 20:46:39 -0400

    Bug 370969 - Using the Browse for file button on a dialog with the keyboard submits the dialog

--- bundles/org.eclipse.orion.client.core/web/orion/widgets/ImportDialog.js
+++ bundles/org.eclipse.orion.client.core/web/orion/widgets/ImportDialog.js
@@ -150,6 +150,15 @@ dojo.declare("orion.widgets.ImportDialog", [ dijit.Dialog, orion.widgets._OrionD
 				this.options.func();
 			}), 2000);		
 		}));
+		
+		// Stop the dialog from submitting if trying to activate the upload button with a keyboard.
+		// See https://bugs.eclipse.org/bugs/show_bug.cgi?id=370969
+		
+		dojo.connect(this.importloader, "onKeyPress", function(evt) {
+			if(evt.keyCode === dojo.keys.ENTER) {
+				evt.stopPropagation();
+			}
+		});
 	}
 });
 });
