0984dbbd5b602844c0a6178c14fe3bec94124db3
skaegi <simon_kaegi@ca.ibm.com>
2012-04-03 17:02:08 -0400

    Bug 376014 - Remove dojo dependency on core components -- adding a simple deferred implementation

---
+++ bundles/org.eclipse.orion.client.core/web/orion/Deferred.js
@@ -0,0 +1,141 @@
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2012 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ * 
+ * Contributors: IBM Corporation - initial API and implementation
+ ******************************************************************************/
+/*global define*/
+define(function() {
+	var head, tail, remainingHead, remainingTail, running = false;
+
+	function queueNotification(notify) {
+		if (running) {
+			if (!head) {
+				head = {
+					notify: notify,
+					next: null
+				};
+				tail = head;
+			} else {
+				tail.next = {
+					notify: notify,
+					next: null
+				};
+				tail = tail.next;
+			}
+			return;
+		}
+		running = true;
+		do {
+			while (notify) {
+				notify();
+				if (remainingHead) {
+					if (!head) {
+						head = remainingHead;
+					} else {
+						tail.next = remainingHead;
+					}
+					tail = remainingTail;
+				}
+				if (head) {
+					remainingHead = head.next;
+					remainingTail = tail;
+					notify = head.notify;
+					head = tail = null;
+				} else {
+					notify = null;
+				}
+			}
+			running = false;
+		} while (running);
+	}
+
+	function Deferred() {
+		var result, state, head, tail;
+
+		function notify() {
+			while (head) {
+				var listener = head;
+				head = head.next;
+				var deferred = listener.deferred;
+				if (listener[state]) {
+					try {
+						var listenerResult = listener[state](result);
+						if (listenerResult && typeof listenerResult.then === "function") {
+							listenerResult.then(deferred.resolve, deferred.reject, deferred.update);
+							continue;
+						}
+						deferred.resolve(listenerResult);
+					} catch (e) {
+						deferred.reject(e);
+					}
+				} else {
+					deferred[state](result);
+				}
+			}
+			head = tail = null;
+		}
+
+		this.resolve = function(value) {
+			if (state) {
+				throw new Error("'" + state + "' already called");
+			}
+			state = "resolve";
+			result = value;
+			queueNotification(notify);
+		};
+
+		this.reject = function(error) {
+			if (state) {
+				throw new Error("'" + state + "' already called");
+			}
+			state = "reject";
+			result = error;
+			queueNotification(notify);
+		};
+
+		this.update = function(progress) {
+			var listener = head;
+			while (listener) {
+				if (listener.update) {
+					try {
+						listener.update(progress);
+					} catch (e) {}
+				}
+				listener = listener.next;
+			}
+		};
+
+		this.then = function(onResolve, onReject, onUpdate) {
+			var listener = {
+				resolve: onResolve,
+				reject: onReject,
+				update: onUpdate,
+				deferred: new Deferred()
+			};
+			if (head) {
+				tail.next = listener;
+			} else {
+				head = listener;
+			}
+			tail = listener;
+			if (state) {
+				queueNotification(notify);
+			}
+			return listener.deferred.promise;
+		};
+
+		this.promise = {
+			then: this.then
+		};
+
+		this.state = function() {
+			return state || "wait";
+		};
+	}
+	return Deferred;
+});
