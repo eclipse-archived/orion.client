01b9c596b67bff076bcb838634753b6cfeb15930
Silenio Quarti <silenio_quarti@ca.ibm.com>
2012-04-05 10:37:48 -0400

    adjusting edtior tooltips sizing (clamp to page) and location.

--- bundles/org.eclipse.orion.client.editor/web/orion/textview/tooltip.js
+++ bundles/org.eclipse.orion.client.editor/web/orion/textview/tooltip.js
@@ -127,20 +127,23 @@ define("orion/textview/tooltip", ['i18n!orion/textview/nls/messages', 'orion/tex
 			} else {
 				return;
 			}
-			var left = parseInt(this._getNodeStyle(tooltipDiv, "padding-left", "0"), 10);
-			left += parseInt(this._getNodeStyle(tooltipDiv, "border-left-width", "0"), 10);
+			var documentElement = tooltipDiv.ownerDocument.documentElement;
 			if (info.anchor === "right") {
-				var right = parseInt(this._getNodeStyle(tooltipDiv, "padding-right", "0"), 10);
-				right += parseInt(this._getNodeStyle(tooltipDiv, "border-right-width", "0"), 10);
-				tooltipDiv.style.right = (tooltipDiv.ownerDocument.body.getBoundingClientRect().right - info.x + left + right) + "px";
+				var right = documentElement.clientWidth - info.x;
+				tooltipDiv.style.right = right + "px";
+				tooltipDiv.style.maxWidth = (documentElement.clientWidth - right - 10) + "px";
 			} else {
-				tooltipDiv.style.left = (info.x - left) + "px";
+				var left = parseInt(this._getNodeStyle(tooltipDiv, "padding-left", "0"), 10);
+				left += parseInt(this._getNodeStyle(tooltipDiv, "border-left-width", "0"), 10);
+				left = info.x - left;
+				tooltipDiv.style.left = left + "px";
+				tooltipDiv.style.maxWidth = (documentElement.clientWidth - left - 10) + "px";
 			}
 			var top = parseInt(this._getNodeStyle(tooltipDiv, "padding-top", "0"), 10);
 			top += parseInt(this._getNodeStyle(tooltipDiv, "border-top-width", "0"), 10);
-			tooltipDiv.style.top = (info.y - top) + "px";
-			tooltipDiv.style.maxWidth = info.maxWidth + "px";
-			tooltipDiv.style.maxHeight = info.maxHeight + "px";
+			top = info.y - top;
+			tooltipDiv.style.top = top + "px";
+			tooltipDiv.style.maxHeight = (documentElement.clientHeight - top - 10) + "px";
 			tooltipDiv.style.opacity = "1";
 			tooltipDiv.style.visibility = "visible";
 			if (autoHide) {
