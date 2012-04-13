45ba755c6b389d1f3b9d441fe3cfba6b7ee50721
Malgorzata Janczarska <malgorzata.tomczyk@pl.ibm.com>
2012-04-13 15:59:56 +0200

    Bug 376148 - Globalization of plugins
    Conflicts:
    	bundles/org.eclipse.orion.client.core/web/plugins/pageLinksPlugin.html

---
+++ bundles/org.eclipse.orion.client.core/web/plugins/nls/pl/messages.js
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2012 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ * 
+ ******************************************************************************/
+
+/*global define*/
+
+define( {
+		"Navigator": "Nawigator",
+		"Sites": "Strony",
+		"Repositories": "Repozytoria",
+		"Get Plugins": "Pobierz Pluginy",
+		"Navigate": "Nawiguj",
+		"Go to the navigator": "Przejd� do nawigatora",
+		"Clone a github repository": "Klonuj repozytorium z Githuba",
+		"Clone a repository from github": "Klonuj repozytorium z Githuba",
+		"Upload some code!": "Prześlij kod!",
+		"Upload a zip file from your computer.": "Prześlij plik zip z twojego komputera.",
+		"github": "github",
+		"github description": "Idź do Githuba i skopjuj URL repozytorium, które chcesz sklonować.",
+		"Orion Repositories": "Repozytoria Oriona",
+		"Orion Repositories description": "Idź do repozytoriów Oriona i wybierz \"Klonuj\" w prawym górnym roku strony",
+		"Orion/GitHub How-To": "Pomoc Orion/GitHub",
+		"Orion/GitHub How-To description": "Pomoc opisuje jak skonfigurować uwierzytelnianie.",
+		"Navigator description": "Idź do nawigatora Oriona. Stwórz folder i wybierz importowanie z menu \"Więcej\".",
+		"Generate a web site": "Wygeneruj stronę",
+		"Generate a web site description":"Wygeneruj stronę w HTML 5 za pomocą initializr.com",
+		"Initializr": "Initializr",
+		"Initializr description": "Idź do strony initializr, wybierz opcje, które cię interesują i ściągnij przygotowany wzór na swój komputer.",
+		"Navigator description2": "Idź do nawigatora Oriona. Stworz folder i wybierz importowanie z menu \"Więcej\". Wybierz plik zip który przed chwilą wygenerowałeś."
+});
