c85bc133e367032d5bd22e717181e674dcf6813a
skaegi <simon_kaegi@ca.ibm.com>
2012-05-14 12:17:13 -0400

    Bug 378716 - Localization 404s -- adding tests and refactoring

--- bundles/org.eclipse.orion.client.core/web/orion/nlsPlugin.js
+++ bundles/org.eclipse.orion.client.core/web/orion/nlsPlugin.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * @license
- * Copyright (c) 2010, 2011 IBM Corporation and others.
+ * Copyright (c) 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials are made 
  * available under the terms of the Eclipse Public License v1.0 
  * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
@@ -13,42 +13,78 @@
 define(function() {
 	return {
 		load: function(name, parentRequire, onLoad, config) {
+
+			// as per requirejs i18n definition ignoring irrelevant matching groups
+			// [0] is complete match
+			// [1] is the message bundle prefix
+			// [2] is the locale or suffix for the master bundle
+			// [3] is the message file suffix or empty string for the master bundle
+			var NLS_REG_EXP = /(^.*(?:^|\/)nls(?:\/|$))([^\/]*)\/?([^\/]*)/;
+			var match = NLS_REG_EXP.exec(name);
+			if (!match) {
+				onLoad(null);
+				return;
+			}
+
 			if (parentRequire.defined(name)) {
 				onLoad(parentRequire(name));
+				return;
 			}
+
+			var prefix = match[1],
+				locale = match[3] ? match[2] : "",
+				suffix = match[3] || match[2];
 			parentRequire(['orion/bootstrap'], function(bootstrap) {
 				bootstrap.startup().then(function(core) {
 					var serviceRegistry = core.serviceRegistry;
-					var nlsReferences = serviceRegistry.getServiceReferences("orion.18n.bundle");
-					var bang = name.indexOf("!");
-					if (bang === -1) {
-						// create language entries				
-						var locales = {};
+					var nlsReferences = serviceRegistry.getServiceReferences("orion.i18n.message");
+
+					if (!locale) {
+						// create master language entries				
+						var master = {};
+						var masterReference;
 						nlsReferences.forEach(function(reference) {
-							if (name === reference.getProperty("name")) {
-								var locale = reference.getProperty("locale");
+							var name = reference.getProperty("name");
+							if ((match = NLS_REG_EXP.exec(name)) && prefix === match[1] && suffix === (match[3] || match[2])) {
+								locale = match[3] ? match[2] : "";
 								if (locale) {
-									locales[locale] = true;
-									var bundleName = name.replace("nls", "nls" + "/" + locale);
-									if (!parentRequire.specified(bundleName)) {
-										define(bundleName, ['orion/nlsPlugin!' + locale + '!' + name], function(bundle) {
+									master[locale] = true;
+									if (!parentRequire.specified(name)) {
+										define(name, ['orion/nlsPlugin!' + name], function(bundle) {
 											return bundle;
 										});
 									}
+								} else {
+									masterReference = reference;
 								}
 							}
 						});
 						if (!parentRequire.specified(name)) {
-							define(name, [], locales);
+							if (masterReference) {
+								serviceRegistry.getService(masterReference).getMessageBundle().then(function(bundle) {
+									Object.keys(master).forEach(function(key) {
+										if (typeof bundle[key] === 'undefined') {
+											bundle[key] = master[key];
+										}
+									});
+									define(name, [], bundle);
+									onLoad(bundle);
+								}, function() {
+									define(name, [], master);
+									onLoad(master);
+								});
+							} else {
+								define(name, [], master);
+								onLoad(master);
+							}
+						} else {
+							onLoad(master);
 						}
-						onLoad(locales);
 					} else {
-						var locale = name.substring(0, bang);
-						name = name.substring(bang + 1);
 						var found = nlsReferences.some(function(reference) {
-							if (name === reference.getProperty("name") && locale === reference.getProperty("locale")) {
+							if (name === reference.getProperty("name")) {
 								serviceRegistry.getService(reference).getMessageBundle().then(function(bundle) {
-									onLoad(bundle || {});
+									onLoad(bundle);
 								}, function() {
 									onLoad({});
 								});
@@ -62,7 +98,6 @@ define(function() {
 					}
 				});
 			});
-
 		}
 	};
 });
