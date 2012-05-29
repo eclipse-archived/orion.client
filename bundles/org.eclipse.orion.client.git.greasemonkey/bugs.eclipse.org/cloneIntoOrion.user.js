660ec5ca577a5fef498f124a9bca6eb4c1173f08
Edyta Przymus <edyta.przymus@gmail.com>
2012-05-29 09:36:58 -0400

    Bug 377912 - Orion Greasemonkey scripts should allow to work with any Orion instance not just OrionHub

--- bundles/org.eclipse.orion.client.git.greasemonkey/bugs.eclipse.org/cloneIntoOrion.user.js
+++ bundles/org.eclipse.orion.client.git.greasemonkey/bugs.eclipse.org/cloneIntoOrion.user.js
@@ -51,6 +51,7 @@
  *   ...
  */
 (function () {
+
 	String.prototype.trim = function() {
 		return this.replace(/^\s+|\s+$/g, "");
 	};
@@ -90,21 +91,130 @@
 
 	var gitRepoUrl = map[product + "_" + component];
 	if (gitRepoUrl) {
-		var a = document.createElement("a");
-		a.href = "http://orionhub.org/git/git-repository.html#,cloneGitRepository=" + gitRepoUrl;
-		a.target = "_blank";
-		a.appendChild(document.createTextNode("Clone into Orion"));
-		var span = document.createElement("span");
-		if (!guest) {
-			span.style["padding"] = "10px";
-			span.appendChild(a);
-			var saveBtn = document.getElementById("commit_top");
-			document.getElementsByClassName("knob-buttons")[0].insertBefore(span, saveBtn);
-		} else {
-			span.style["float"] = "right";
-			span.appendChild(a);
-			var desc = document.getElementsByClassName("bz_alias_short_desc_container edit_form")[0];
-			desc.appendChild(span);
+		
+		var url = document.createElement("a");
+		url.id = "anchor";
+		url.innerHTML = gitRepoUrl;
+		url.href = "http://orionhub.org/git/git-repository.html#,cloneGitRepository=" + gitRepoUrl;
+		url.target = "_blank";
+		
+		var radio1 = document.createElement("input");
+		var orionhub = true;
+		radio1.name = "select";
+		radio1.id = "radio1";
+		radio1.style.fontWeight = "normal";
+		radio1.value = orionhub;
+		radio1.type = "radio";
+		radio1.checked = "true";
+		radio1.onclick = function(){
+			    if (document.getElementById("radio1").checked){
+			    document.getElementById("anchor").href = "http:/orionhub.org/git/git-repository.html#,cloneGitRepository=" + gitRepoUrl;
+		    }
 		}
+		
+		var radio2 = document.createElement("input");
+		var oeo = false;
+		radio2.name = "select";
+		radio2.style.fontWeight = "normal";
+		radio2.id = "radio2";
+		radio2.value = oeo;
+		radio2.type = "radio";
+		radio2.onclick = function(){
+			    if (document.getElementById("radio2").checked){
+			    document.getElementById("anchor").href = "http:/orion.eclipse.org.org/git/git-repository.html#,cloneGitRepository=" + gitRepoUrl;
+		    }
+		}
+		
+		var radio3 = document.createElement("input");
+		var other = false;
+		radio3.name = "select";
+		radio3.style.fontWeight = "normal";
+		radio3.value = other;
+		radio3.type = "radio";
+		radio3.id = "radio3";
+		radio3.onclick = function(){
+			    if (document.getElementById("radio3").checked){
+			    document.getElementById("anchor").href = "http:/" + document.getElementById('hostid').value + "/git/git-repository.html#,cloneGitRepository=" + gitRepoUrl;
+		    }
+		}
+		
+		var host = document.createElement("input");
+		host.className = "txt";
+		host.size = "20";
+		host.placeholder = "address";
+		host.id = "hostid";
+		host.onblur=function(){
+		    if(document.getElementById("radio3").checked){
+		    document.getElementById("anchor").href = "http:/" + document.getElementById('hostid').value + "/git/git-repository.html#,cloneGitRepository=" + gitRepoUrl;
+		    }
+		}
+		
+		host.onchange=function(){
+		    if(document.getElementById("radio3").checked){
+		    document.getElementById("anchor").href = "http:/" + document.getElementById('hostid').value + "/git/git-repository.html#,cloneGitRepository=" + gitRepoUrl;
+		    }
+		}
+		
+		host.onkeydown=function(){
+		    if(document.getElementById("radio3").checked){
+		    document.getElementById("anchor").href = "http:/" + document.getElementById('hostid').value + "/git/git-repository.html#,cloneGitRepository=" + gitRepoUrl;
+		    }
+		}
+		host.onkeyup=function(){
+		    if(document.getElementById("radio3").checked){
+		    document.getElementById("anchor").href = "http:/" + document.getElementById('hostid').value + "/git/git-repository.html#,cloneGitRepository=" + gitRepoUrl;
+		    }
+		}
+		
+		var div = document.createElement("div");
+		var hint = document.createElement("div");
+		hint.innerHTML = "Select the Orion instance: ";
+		hint.style.fontStyle = "normal";
+		hint.style.fontWeight = "lighter";
+		hint.style.marginTop = "10px";
+		var hr = document.createElement("hr");;
+		var table = document.getElementById("bz_big_form_parts");
+		table.width = "100%";
+		var row = table.insertRow(0);
+		var cloneTh = row.insertCell(0);
+		cloneTh.innerHTML = "Work with the bug in Orion";
+		cloneTh.style.fontStyle = "bold";
+		cloneTh.style.fontWeight = "bold";
+		cloneTh.style.marginTop = "10px";
+		cloneTh.style.marginBottom = "10px";
+		cloneTh.innerHTML.color = "black";
+		cloneTh.appendChild(hint);
+		cloneTh.appendChild(div);
+		div.appendChild(radio1);
+		var node1 = document.createElement("span");
+		node1.innerHTML = "orionhub.org       ";
+		node1.style.fontWeight = "normal";
+		div.appendChild(node1);
+		div.appendChild(radio2);
+		var node2 = document.createElement("span");
+		node2.innerHTML = "orion.eclipse.org      ";
+		node2.style.fontWeight = "normal";
+		div.appendChild(node2);
+		div.appendChild(radio3);
+		var node3 = document.createElement("span");
+		node3.innerHTML = "other Orion at ";
+		node3.style.fontWeight = "normal";
+		div.appendChild(node3);
+		div.appendChild(host);
+		var node4 = document.createElement("span");
+		node4.innerHTML = "and click the link below to clone ";
+		node4.style.fontWeight = "normal";
+		cloneTh.appendChild(node4);
+
+		var row2 = table.insertRow(1);
+		var cloneTh2 = row2.insertCell(0);
+		cloneTh2.appendChild(url);
+		var cloneTh3 = row2.insertCell(1);
+		hr.width = "100%";
+		url.style.paddingTop = "20px";
+		var row3 = table.insertRow(2);
+		row3.width = "100%";
+		row3.appendChild(hr);
+		
 	}
 })();
