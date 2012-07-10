d154d5fc73ec890f1cec01187919bc99a8913913
Maciej Bendkowski <maciej.bendkowski@pl.ibm.com>
2012-07-10 13:13:08 +0200

    Bug 381759 - Unexternalized strings for SFTP Transfer dialog

--- bundles/org.eclipse.orion.client.core/web/orion/widgets/nls/root/messages.js
+++ bundles/org.eclipse.orion.client.core/web/orion/widgets/nls/root/messages.js
@@ -63,5 +63,11 @@ define({
 	"Profiles": "Profiles",
 	"Information Needed": "Information Needed",
 	"Cancel": "Cancel",
-	"Sorry, the Console is not supported on your browser.": "Sorry, the Console is not supported on your browser."
+	"Sorry, the Console is not supported on your browser.": "Sorry, the Console is not supported on your browser.",
+	"If the same file exists in both the source and destination:" : "If the same file exists in both the source and destination:",
+	"Cancel the transfer" : "Cancel the transfer",
+	"Always overwrite destination" : "Always overwrite destination",
+	"Overwrite if source is newer" : "Overwrite if source is newer",
+	"New" : "New",
+	"Add" : "Add"
 });
