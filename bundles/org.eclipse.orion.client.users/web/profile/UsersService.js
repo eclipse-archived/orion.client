a0d6e5fd2409166119f4a100e3e2afbbeee805f5
Mark Macdonald <mamacdon@ca.ibm.com>
2012-08-14 12:41:57 -0400

    Bug 386767 - remove dojo from userservicePlugin

--- bundles/org.eclipse.orion.client.users/web/profile/UsersService.js
+++ bundles/org.eclipse.orion.client.users/web/profile/UsersService.js
@@ -8,8 +8,32 @@
  * 
  * Contributors: IBM Corporation - initial API and implementation
  ******************************************************************************/
-/*global define handleAuthenticationError*/
-define(["orion/Deferred", "dojo"], function(Deferred, dojo) {
+/*global define escape */
+define(["orion/Deferred", "orion/xhr"], function(Deferred, xhr) {
+	function formEncode(value) {
+		return encodeURIComponent(value).replace(/[!'()*]/g, escape).replace('%20', '+'); //$NON-NLS-0$ //$NON-NLS-1$
+	}
+
+	/**
+	 * @returns The map as application/x-www-form-urlencoded data.
+	 */
+	function formData(map) {
+		var keys = Object.keys(map);
+		var buf = [];
+		for (var i=0; i < keys.length; i++) {
+			var key = keys[i], value = map[key];
+			buf.push(formEncode(key) + "=" + formEncode(value)); //$NON-NLS-0$
+		}
+		return buf.join("&"); //$NON-NLS-0$
+	}
+
+	function getJSON(data) {
+		return data === "" ? null : JSON.parse(data);
+	}
+
+	function getError(xhrResult) {
+		return new Error("Error loading " + xhrResult.args.url + " status: " + xhrResult.status);
+	}
 
 	/**
 	 * @class Provides operations on users and users groups.
@@ -27,111 +51,108 @@ define(["orion/Deferred", "dojo"], function(Deferred, dojo) {
 	{
 		getUsersList : function(onLoad) {
 			var service = this;
-			return dojo.xhrGet({
-				url : "../users", //$NON-NLS-0$
+			return xhr("GET", "../users", { //$NON-NLS-1$ //$NON-NLS-0$
 				headers : {
 					"Orion-Version" : "1" //$NON-NLS-1$ //$NON-NLS-0$
 				},
-				handleAs : "json", //$NON-NLS-0$
-				timeout : 15000,
-				load : function(jsonData, secondArg) {
-					if (onLoad){
-						if(typeof onLoad === "function") //$NON-NLS-0$
-							onLoad(jsonData, secondArg);
-						else
-							service.dispatchEvent(onLoad, jsonData);
-					}
-					return jsonData.users;
-				},
-				error : function(error, ioArgs) {
-					var xhrCall = this;
-					if(!service.info) handleAuthenticationError(error, function(){
-						dojo.xhrGet(xhrCall); // retry GET
-					});
-					return error;
+				timeout: 15000
+			}).then(function(result) {
+				var jsonData = getJSON(result.response);
+				if (onLoad){
+					if(typeof onLoad === "function") //$NON-NLS-0$
+						onLoad(jsonData);
+					else
+						service.dispatchEvent(onLoad, jsonData);
 				}
+				return jsonData.users;
+			}, function(result) {
+				var error = getError(result);
+				if(!service.info) {
+					handleAuthenticationError(error, function(){
+						service.getUsersList(onLoad); // retry GET
+					});
+				}
+				return error;
 			});
 		},
 		deleteUser : function(userURI, onLoad) {
 			var service = this;
-				return dojo.xhrDelete({
-					url : userURI,
-					headers : {
-						"Orion-Version" : "1" //$NON-NLS-1$ //$NON-NLS-0$
-					},
-					handleAs : "json", //$NON-NLS-0$
-					timeout : 15000,
-					load : function(jsonData, secondArg) {
-						if (onLoad){
-							if(typeof onLoad === "function") //$NON-NLS-0$
-								onLoad(jsonData, secondArg);
-							else
-								service.dispatchEvent(onLoad, jsonData);
-						}
-					},
-					error : function(error, ioArgs) {
-						var xhrCall = this;
-						if(!service.info) handleAuthenticationError(error, function(){
-							dojo.xhrDelete(xhrCall); // retry DELETE
-						});
-						return error;
-					}
-				});
-
-		},
-		createUser : function(userName, password, onLoad, onError) {
-			var service = this;
-			return dojo.xhrPost({
-				url : "../users", //$NON-NLS-0$
+			return xhr("DELETE", userURI, { //$NON-NLS-0$
 				headers : {
 					"Orion-Version" : "1" //$NON-NLS-1$ //$NON-NLS-0$
 				},
-				content : {
+				timeout: 15000
+			}).then(function(result) {
+				var jsonData = getJSON(result.response);
+				if (onLoad){
+					if(typeof onLoad === "function") //$NON-NLS-0$
+						onLoad(jsonData);
+					else
+						service.dispatchEvent(onLoad, jsonData);
+				}
+			}, function(result) {
+				var error = getError(result);
+				if(!service.info) {
+					handleAuthenticationError(error, function(){
+						service.deleteUser(userURI, onLoad); // retry DELETE
+					});
+				}
+				return error;
+			});
+		},
+		createUser : function(userName, password, onLoad, onError) {
+			var service = this;
+			return xhr("POST", "../users", { //$NON-NLS-1$ //$NON-NLS-0$
+				headers : {
+					"Content-Type": "application/x-www-form-urlencoded", //$NON-NLS-1$ //$NON-NLS-0$
+					"Orion-Version" : "1" //$NON-NLS-1$ //$NON-NLS-0$
+				},
+				timeout: 15000,
+				data: formData({
 					login : userName,
 					password : password
-				},
-				handleAs : "text", //$NON-NLS-0$
-				timeout : 15000,
-				load : function(jsonData, secondArg) {
-					if (onLoad){
-						if(typeof onLoad === "function") //$NON-NLS-0$
-							onLoad(jsonData, secondArg);
-						else
-							service.dispatchEvent(onLoad, jsonData);
-					}
-					return jsonData;
-				},
-				error : function(error, secondArg) {
-					if (onError)
-						onError(error, secondArg);
-					return error;
+				})
+			}).then(function(result) {
+				var jsonData = getJSON(result.response);
+				if (onLoad){
+					if(typeof onLoad === "function") //$NON-NLS-0$
+						onLoad(jsonData);
+					else
+						service.dispatchEvent(onLoad, jsonData);
 				}
+				return jsonData;
+			}, function(result) {
+				var error = getError(result);
+				if (onError) {
+					onError(error);
+				}
+				return error;
 			});
 		},
 		getUserInfo: function(userURI, onLoad){
 			var service = this;
-			return dojo.xhrGet({
-				url : userURI,
+			return xhr("GET", userURI, { //$NON-NLS-0$
 				headers : {
 					"Orion-Version" : "1" //$NON-NLS-1$ //$NON-NLS-0$
 				},
-				handleAs : "json", //$NON-NLS-0$
-				timeout : 15000,
-				load : function(jsonData, secondArg) {
-					if (onLoad){
-						if(typeof onLoad === "function") //$NON-NLS-0$
-							onLoad(jsonData, secondArg);
-						else
-							service.dispatchEvent(onLoad, jsonData);
-					}
-				},
-				error : function(error, ioArgs) {
-					var xhrCall = this;
-					if(!service.info) handleAuthenticationError(error, function(){
-						dojo.xhrGet(xhrCall); // retry GET
-					});
-					return error;
+				timeout: 15000
+			}).then(function(result) {
+				var jsonData = getJSON(result.response);
+				if (onLoad){
+					if(typeof onLoad === "function") //$NON-NLS-0$
+						onLoad(jsonData);
+					else
+						service.dispatchEvent(onLoad, jsonData);
 				}
+				return jsonData;
+			}, function(result) {
+				var error = getError(result);
+				if(!service.info) {
+					handleAuthenticationError(error, function(){
+						service.getUserInfo(userURI, onLoad); // retry GET
+					});
+				}
+				return error;
 			});
 		},
 		updateUserInfo: function(userUri, data, onLoad){
@@ -145,66 +166,60 @@ define(["orion/Deferred", "dojo"], function(Deferred, dojo) {
 				return ret;
 			}
 
-
-			return dojo.xhrPut({
-				url : uri,
+			return xhr("PUT", uri, { //$NON-NLS-0$
 				headers : {
+					"Content-Type": "application/json", //$NON-NLS-1$ //$NON-NLS-0$
 					"Orion-Version" : "1" //$NON-NLS-1$ //$NON-NLS-0$
 				},
-				handleAs : "json", //$NON-NLS-0$
-				putData: dojo.toJson(data),
 				timeout : 15000,
-				load : function(jsonData, secondArg) {
-					if (onLoad){
-						if(typeof onLoad === "function") //$NON-NLS-0$
-							return onLoad(jsonData, secondArg);
-						else{
-							service.dispatchEvent(onLoad, jsonData);
-							return jsonData;
-						}
-					} else {
+				data: JSON.stringify(data)
+			}).then(function(result) {
+				var jsonData = getJSON(result.response);
+				if (onLoad){
+					if(typeof onLoad === "function") //$NON-NLS-0$
+						return onLoad(jsonData);
+					else{
+						service.dispatchEvent(onLoad, jsonData);
 						return jsonData;
 					}
-				},
-				error : function(error, ioArgs) {
-					var xhrCall = this;
-					if(!service.info) handleAuthenticationError(error, function(){
-						dojo.xhrPut(xhrCall); // retry GET
-					});
-					return error;
+				} else {
+					return jsonData;
 				}
+			}, function(result) {
+				var error = getError(result);
+				if(!service.info) {
+					handleAuthenticationError(error, function(){
+						service.updateUserInfo(userUri, data, onLoad); // retry GET
+					});
+				}
+				return error;
 			});
 		},
 		resetUserPassword: function(login, password, onLoad){
-			
-			return dojo.xhrPost({
-				url : "../users", //$NON-NLS-0$
+			var service = this;
+			return xhr("POST", "../users", { //$NON-NLS-1$ //$NON-NLS-0$
 				headers : {
+					"Content-Type": "application/x-www-form-urlencoded", //$NON-NLS-1$ //$NON-NLS-0$
 					"Orion-Version" : "1" //$NON-NLS-1$ //$NON-NLS-0$
 				},
-				content : {
+				timeout : 15000,
+				data : formData({
 					reset: true,
 					login : login,
 					password : password
-				},
-				handleAs : "json", //$NON-NLS-0$
-				timeout : 15000,
-				load : function(jsonData, secondArg) {
-					if (onLoad){
-						if(typeof onLoad === "function") //$NON-NLS-0$
-							onLoad(jsonData, secondArg);
-						else
-							service.dispatchEvent(onLoad, jsonData);
-					}
-					
-					return jsonData;
-				},
-				error : function(error, secondArg) {
-					return error;
+				})
+			}).then(function(result) {
+				var jsonData = getJSON(result.response);
+				if (onLoad){
+					if(typeof onLoad === "function") //$NON-NLS-0$
+						onLoad(jsonData);
+					else
+						service.dispatchEvent(onLoad, jsonData);
 				}
+				return jsonData;
+			}, function(result) {
+				return result.response;
 			});
-			
-			
 		}
 	};
 	return UsersService;
