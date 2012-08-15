f7149f9ba229e7521fd8710af72fc01286bad1ba
sfranklin <susan_franklin@us.ibm.com>
2012-08-15 16:50:42 -0400

    Bug 360118 - reorganize core CSS files

--- bundles/org.eclipse.orion.client.core/web/orion/globalCommands.js
+++ bundles/org.eclipse.orion.client.core/web/orion/globalCommands.js
@@ -652,6 +652,7 @@ define(['i18n!orion/nls/messages', 'require', 'dojo', 'dijit', 'orion/commonHTML
 		
 		var toolbar = dojo.byId("pageToolbar"); //$NON-NLS-0$
 		if (toolbar) {
+			dojo.addClass(toolbar, "toolbarLayout"); //$NON-NLS-0$
 			dojo.place(commonHTML.toolbarHTMLFragment, toolbar, "only"); //$NON-NLS-0$
 		}
 		
