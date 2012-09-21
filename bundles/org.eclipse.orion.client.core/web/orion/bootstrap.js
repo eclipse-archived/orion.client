bdf7ab85c07e98d20722dfb10f26f26a47a8c7fa
skaegi <simon_kaegi@ca.ibm.com>
2012-09-21 10:45:04 -0400

    Bug 385657 - Align Plugin Headers and API with OSGi Bundle where sensible

--- bundles/org.eclipse.orion.client.core/web/orion/bootstrap.js
+++ bundles/org.eclipse.orion.client.core/web/orion/bootstrap.js
@@ -23,7 +23,7 @@ define(['require', 'orion/Deferred', 'orion/serviceregistry', 'orion/preferences
 		once = new Deferred();
 	
 		// initialize service registry and EAS services
-		var serviceRegistry = new mServiceregistry.ServiceRegistry();
+		var serviceRegistry = window.serviceRegistry = new mServiceregistry.ServiceRegistry();
 	
 		// This is code to ensure the first visit to orion works
 		// we read settings and wait for the plugin registry to fully startup before continuing
@@ -34,7 +34,7 @@ define(['require', 'orion/Deferred', 'orion/serviceregistry', 'orion/preferences
 				var url = require.toUrl(key);
 				configuration.plugins[url] = pluginsPreference[key];
 			});
-			var pluginRegistry = new mPluginRegistry.PluginRegistry(serviceRegistry, configuration);	
+			var pluginRegistry = window.pluginRegistry = new mPluginRegistry.PluginRegistry(serviceRegistry, configuration);	
 			return pluginRegistry.start().then(function() {
 				if (serviceRegistry.getServiceReferences("orion.core.preference.provider").length > 0) { //$NON-NLS-0$
 					return preferences.getPreferences("/plugins", preferences.USER_SCOPE).then(function(pluginsPreference) { //$NON-NLS-0$
@@ -42,8 +42,10 @@ define(['require', 'orion/Deferred', 'orion/serviceregistry', 'orion/preferences
 						pluginsPreference.keys().forEach(function(key) {
 							var url = require.toUrl(key);
 							if (!pluginRegistry.getPlugin(url)) {
-								installs.push(pluginRegistry.installPlugin(url).then(function(plugin) {
-									plugin.start({lazy:true});
+								installs.push(pluginRegistry.installPlugin(url,{autostart: "lazy"}).then(function(plugin) {
+									return plugin.update().then(function() {
+										return plugin.start({lazy:true});
+									});
 								}));
 							}
 						});	
