20dda5450f951d36ba0cfd09538f81f561a752de
sfranklin <susan_franklin@us.ibm.com>
2012-09-25 14:13:42 -0400

    Bug 390257 - banner accessibility issues

--- bundles/org.eclipse.orion.client.core/web/orion/nls/root/messages.js
+++ bundles/org.eclipse.orion.client.core/web/orion/nls/root/messages.js
@@ -79,5 +79,6 @@ define({
 	"Saved searches" : "Saved searches",
 	"Recent searches" : "Recent searches",
 	"Open in new tab" : "Open in new tab", 
-	"Regular expression" : "Regular expression" 
+	"Regular expression" : "Regular expression",
+	"Search options" : "Search options"
 });
