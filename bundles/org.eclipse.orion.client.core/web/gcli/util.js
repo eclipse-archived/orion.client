26436516bc2ffc0ada9f9d7e21f0522a3e4cc755
Grant Gayed <grant_gayed@ca.ibm.com>
2012-10-04 14:23:23 -0400

    GCLI changes to get minification working again

--- bundles/org.eclipse.orion.client.core/web/gcli/util.js
+++ bundles/org.eclipse.orion.client.core/web/gcli/util.js
@@ -332,8 +332,8 @@ function hash(str) {
     return hash;
   }
   for (var i = 0; i < str.length; i++) {
-    var char = str.charCodeAt(i);
-    hash = ((hash << 5) - hash) + char;
+    var chr = str.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
     hash = hash & hash; // Convert to 32bit integer
   }
   return hash;
