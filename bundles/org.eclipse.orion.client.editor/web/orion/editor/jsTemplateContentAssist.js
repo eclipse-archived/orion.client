f929262011680d483972698d6043990380778f26
John Arthorne <John_Arthorne@ca.ibm.com>
2012-10-17 14:09:03 -0400

    Bug 392008 - Copyright updates

--- bundles/org.eclipse.orion.client.editor/web/orion/editor/jsTemplateContentAssist.js
+++ bundles/org.eclipse.orion.client.editor/web/orion/editor/jsTemplateContentAssist.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * @license
- * Copyright (c) 2011 IBM Corporation and others.
+ * Copyright (c) 2011, 2012 IBM Corporation and others.
  * Copyright (c) 2012 VMware, Inc.
  * All rights reserved. This program and the accompanying materials are made 
  * available under the terms of the Eclipse Public License v1.0 
