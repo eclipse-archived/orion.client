59a2d1962dfdcb0e9a2f62d2f8be9e73a2146607
libing wang <libingw@ca.ibm.com>
2012-11-09 16:28:46 -0500

    Fixed typos in advanced search popup.

--- bundles/org.eclipse.orion.client.core/web/orion/globalSearch/nls/root/messages.js
+++ bundles/org.eclipse.orion.client.core/web/orion/globalSearch/nls/root/messages.js
@@ -14,6 +14,6 @@ define({
 	"Files that contain:": "Files that contain:",
 	"Search": "Search",
 	"All types": "All types",
-	"On file type:": "On file type:",
-	"Regular expresion:" : "Regular expresion:"
+	"File type:": "File type:",
+	"Regular expression:" : "Regular expression:"
 });
