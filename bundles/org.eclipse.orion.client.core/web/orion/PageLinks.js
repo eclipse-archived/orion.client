24f5ae54a6ca353296bc94477c1bf7a93480545e
Mark Macdonald <Mark_MacDonald@ca.ibm.com>
2012-11-30 12:14:52 -0500

    Add service 'orion.page.link.user' for contributing to the UserMenu

--- bundles/org.eclipse.orion.client.core/web/orion/PageLinks.js
+++ bundles/org.eclipse.orion.client.core/web/orion/PageLinks.js
@@ -10,26 +10,18 @@
 /*global define document window*/
 define(["require", "orion/Deferred", "orion/PageUtil", "orion/URITemplate"], function(require, Deferred, PageUtil, URITemplate) {
 
-	function createLink(href, target, textContent) {
-		var a = document.createElement("a");
-		a.href = href;
-		a.target = target;
-		a.className = "targetSelector";
-		a.textContent = textContent;
-		return a;
-	}
-
 	/**
-	 * Build links from <code>orion.page.*</code> service extensions.
+	 * Read info from an <code>orion.page.*</code> service extension.
+	 * @methodOf orion.PageLinks
 	 * @param {orion.ServiceRegistry} serviceRegistry The service registry.
 	 * @param {String} serviceName Service name to read extensions from.
-	 * @return {Deferred} A promise that resolves to an Array of link elements.
+	 * @return {Deferred} A promise that resolves to an Array of info objects.
 	 */
-	function createPageLinks(serviceRegistry, serviceName) {
+	function getPageLinksInfo(serviceRegistry, serviceName) {
 		serviceName = serviceName || "orion.page.link"; //$NON-NLS-0$
 		// Note that the shape of the "orion.page.link" extension is not in any shape or form that could be considered final.
 		// We've included it to enable experimentation. Please provide feedback on IRC or bugzilla.
-		
+
 		// The shape of a contributed navigation link is (for now):
 		// info - information about the navigation link (object).
 		//     required attribute: name - the name of the navigation link
@@ -41,8 +33,7 @@ define(["require", "orion/Deferred", "orion/PageUtil", "orion/URITemplate"], fun
 		// TODO: should not be necessary, see bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=373450
 		var hostName = window.location.protocol + "//" + window.location.host; //$NON-NLS-0$
 		var locationObject = {OrionHome: hostName, Location: params.resource};
-		var target = "_self"; //$NON-NLS-0$
-		var links = [];
+		var infos = [];
 		for (var i=0; i<navLinks.length; i++) {
 			var info = {};
 			var propertyNames = navLinks[i].getPropertyKeys();
@@ -55,20 +46,56 @@ define(["require", "orion/Deferred", "orion/PageUtil", "orion/URITemplate"], fun
 					var uriTemplate = new URITemplate(info.uriTemplate);
 					var expandedHref = window.decodeURIComponent(uriTemplate.expand(locationObject));
 					expandedHref = PageUtil.validateURLScheme(expandedHref);
-					d.resolve(createLink(expandedHref, target, (info.nameKey? commandMessages[info.nameKey]: info.name)));
+
+					info.href = expandedHref;
+					info.textContent = (info.nameKey? commandMessages[info.nameKey]: info.name);
+					d.resolve(info);
 				});
-				links.push(d);
+				infos.push(d);
 			} else if (info.uriTemplate && info.name) {
 				var uriTemplate = new URITemplate(info.uriTemplate);
 				var expandedHref = window.decodeURIComponent(uriTemplate.expand(locationObject));
 				expandedHref = PageUtil.validateURLScheme(expandedHref);
-				links.push(new Deferred.resolve(createLink(expandedHref, target, info.name)));
+
+				info.href = expandedHref;
+				info.textContent = info.name;
+				infos.push(new Deferred().resolve(info));
 			}
 		}
-		return Deferred.all(links);
+		return Deferred.all(infos);
 	}
 
+	function createLink(href, target, textContent) {
+		var a = document.createElement("a");
+		a.href = href;
+		a.target = target;
+		a.classList.add("targetSelector");
+		a.textContent = textContent;
+		return a;
+	}
+
+	/**
+	 * Build links from an <code>orion.page.link</code> service extension.
+	 * @methodOf orion.PageLinks
+	 * @param {orion.ServiceRegistry} serviceRegistry The service registry.
+	 * @param {String} serviceName Service name to read extensions from.
+	 * @return {Deferred} A promise that resolves to an Array of DOM elements which are the links.
+	 */
+	function createPageLinks(serviceRegistry, serviceName) {
+		return getPageLinksInfo(serviceRegistry, serviceName).then(function(links) {
+			return links.map(function(info) {
+				return createLink(info.href, "_self", info.textContent); //$NON-NLS-0$
+			});
+		});
+	}
+
+	/**
+	 * @name orion.PageLinks
+	 * @class Utilities for reading <code>orion.page.links</code> services.
+	 * @description Utilities for reading <code>orion.page.links</code> services.
+	 */
 	return {
-		createPageLinks: createPageLinks
+		createPageLinks: createPageLinks,
+		getPageLinksInfo: getPageLinksInfo
 	};
 });
