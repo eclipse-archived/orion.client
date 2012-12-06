e5d57338ac2104c3d92d5f7da31e8ae512b42815
sfranklin <susan_franklin@us.ibm.com>
2012-12-06 11:06:21 -0500

    Bug 395759 - Stray bull roaming in outline menu

--- bundles/org.eclipse.orion.client.core/web/orion/outliner.js
+++ bundles/org.eclipse.orion.client.core/web/orion/outliner.js
@@ -228,7 +228,7 @@ define(['i18n!orion/nls/messages', 'dojo', 'orion/uiUtils', 'orion/section', 'or
 			for (var i=0; i < this.outlineProviders.length; i++) {
 				var provider = this.outlineProviders[i],
 				    name = provider.displayName || provider.getProperty("name") || (provider.name + provider.serviceId) || "undefined", //$NON-NLS-1$ //$NON-NLS-0$
-				    prefix = (provider.getProperty("id") === this.providerId) ? "&bull; " : ""; //$NON-NLS-1$ //$NON-NLS-0$
+				    prefix = (provider.getProperty("id") === this.providerId) ? "* " : ""; //$NON-NLS-1$ //$NON-NLS-0$
 				choices.push({
 					name: prefix + name,
 					callback: dojo.hitch(this, this.setSelectedProvider, provider)});
