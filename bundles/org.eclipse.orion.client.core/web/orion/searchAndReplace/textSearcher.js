181552303bb6f174b2d5dfb1b1bc917b828ca38e
John Arthorne <john_arthorne@ca.ibm.com>
2012-12-14 10:42:45 -0500

    Updated copyright to reflect contributed fix.

--- bundles/org.eclipse.orion.client.core/web/orion/searchAndReplace/textSearcher.js
+++ bundles/org.eclipse.orion.client.core/web/orion/searchAndReplace/textSearcher.js
@@ -6,7 +6,9 @@
  * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution
  * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html).
  * 
- * Contributors: IBM Corporation - initial API and implementation
+ * Contributors: 
+ *	IBM Corporation - initial API and implementation
+ *	Adrian Aichner - regular expression capture group support in replace
  ******************************************************************************/
 /*global define window document navigator*/
 
