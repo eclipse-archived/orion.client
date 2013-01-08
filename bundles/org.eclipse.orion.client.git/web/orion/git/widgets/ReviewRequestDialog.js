c99b73cbbb11e1f23b903b7408107a6ca63ed0e5
Szymon Brandys <Szymon.Brandys@pl.ibm.com>
2013-01-08 14:40:37 +0100

    Removing dojo/dijit from ReviewRequestDialog.js

--- bundles/org.eclipse.orion.client.git/web/orion/git/widgets/ReviewRequestDialog.js
+++ bundles/org.eclipse.orion.client.git/web/orion/git/widgets/ReviewRequestDialog.js
@@ -1,81 +1,62 @@
 /*******************************************************************************
- * @license
- * Copyright (c) 2012 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials are made 
- * available under the terms of the Eclipse Public License v1.0 
- * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
- * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
-/*jslint browser:true*/
-/*global define orion window dojo dijit*/
-
-define(['i18n!git/nls/gitmessages', 'require', 'dojo', 'dijit', 'dijit/Dialog', 'dijit/form/TextBox', 
-		'orion/widgets/_OrionDialogMixin', 'text!orion/git/widgets/templates/ReviewRequestDialog.html'], function(messages, require, dojo, dijit) {
-
-/**
- * Usage: <code>new orion.git.widgets.ReviewRequestDialog(options).show();</code>
+ * @license Copyright (c) 2012 IBM Corporation and others. All rights reserved.
+ *          This program and the accompanying materials are made available under
+ *          the terms of the Eclipse Public License v1.0
+ *          (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse
+ *          Distribution License v1.0
+ *          (http://www.eclipse.org/org/documents/edl-v10.html).
  * 
- * @name orion.git.widgets.ReviewRequestDialog
- */
-var ReviewRequestDialog = dojo.declare("orion.git.widgets.ReviewRequestDialog", [dijit.Dialog, orion.widgets._OrionDialogMixin], //$NON-NLS-0$
-		/** @lends orion.git.widgets.ReviewRequestDialog.prototype */ {
-	widgetsInTemplate : true,
-	templateString : dojo.cache('orion', 'git/widgets/templates/ReviewRequestDialog.html'), //$NON-NLS-1$ //$NON-NLS-0$
-	
-	SEARCH_DELAY: 500,
-	timeoutId: null,
-	time: null,
-	options: null,
-	
-	/** @private */
-	constructor : function() {
-		this.inherited(arguments);
-		this.timeoutId = null;
-		this.time = 0;
-		this.options = arguments[0];
-		
-		this.url = this.options.url;
-	},
-	
-	/** @private */
-	postMixInProperties : function() {
-		this.options.title = this.options.title || messages["Contribution Review Request"];
-		this.selectFile = messages["Paste link in email or IM"];
-		this.reviewerName = messages["Send the link to the reviewer"];
+ * Contributors: IBM Corporation - initial API and implementation
+ ******************************************************************************/
 
-		this.inherited(arguments);
-	},
-	
-	/** @private */
-	postCreate: function() {
-		this.inherited(arguments);
-		this.ReviewReqUrl.set("value", this.options.url);
-	},
-	
-	
-	/**
-	 * Displays the dialog.
-	 */
-	show: function() {
-		this.inherited(arguments);
-		this.ReviewReqUrl.focus();
-	},
-	
-	/** @private */
-	onHide: function() {
-		clearTimeout(this.timeoutId);
-		this.inherited(arguments);
-	},
-	
-	execute: function() {
-		if(this.options.func)
-			this.options.func(this.ReviewerName.value);
-		delete this.options.func; //prevent performing this action twice (IE)
+define([ 'i18n!git/nls/gitmessages', 'orion/webui/dialog' ], function(messages, dialog) {
+
+	function ReviewRequestDialog(options) {
+		this._init(options);
 	}
-	
-});
-return ReviewRequestDialog;
+
+	ReviewRequestDialog.prototype = new dialog.Dialog();
+
+	ReviewRequestDialog.prototype.TEMPLATE = '<div>'
+		+ '<div id="treeContentPane" style="padding: 8px">'
+		+ '<label for="reviewReqUrl">${Paste link in email or IM}</label><br>'
+		+ '<input id="reviewReqUrl" style="width: 50em" value=""/>' 
+		+ '</div>'
+		+ '<div id="newBranchPane" style="padding: 8px">' 
+		+ '<label for="reviewerName">${Send the link to the reviewer}</label><br>'
+		+ '<input id="reviewerName" style="width: 30em"  value=""/>' 
+		+ '</div>' 
+		+ '</div>';
+
+	ReviewRequestDialog.prototype._init = function(options) {
+		var that = this;
+
+		this.title = options.title || messages["Contribution Review Request"];
+		this.modal = true;
+		this.messages = messages;
+		this.url = options.url;
+		this.func = options.func;
+
+		this.buttons = [];
+
+		this.buttons.push({ callback : function() {
+			that.destroy();
+			that.func(that.$reviewerName.value);
+		},
+		text : 'Submit'
+		});
+
+		// Start the dialog initialization.
+		this._initialize();
+	};
+
+	ReviewRequestDialog.prototype._bindToDom = function(parent) {
+		this.$reviewReqUrl.value = this.url;
+	};
+
+	ReviewRequestDialog.prototype.constructor = ReviewRequestDialog;
+
+	// return the module exports
+	return { ReviewRequestDialog : ReviewRequestDialog
+	};
 });
