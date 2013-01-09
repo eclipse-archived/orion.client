9661d935e12049ca9709042f22f7a3b897283ccb
Libing Wang <libingw@ca.ibm.com>
2013-01-09 15:56:09 -0500

    Bug 397633 - Add NPM support on shell page.-- fixed cwd issues when starting server with -w .

--- bundles/org.eclipse.orion.client.core/web/shell/shellPage.js
+++ bundles/org.eclipse.orion.client.core/web/shell/shellPage.js
@@ -548,8 +548,7 @@ define(["i18n!orion/shell/nls/messages", "require", "dojo", "orion/bootstrap", "
 				/* Use orion/Deferred since it supports progress, gcli/promise does not */
 				//var promise = context.createPromise();
 				var promise = new Deferred();
-				var node = shellPageFileService.getCurrentDirectory();
-				var location = node ? node.Location : (getCWD() || ROOT_ORIONCONTENT);
+				var location = getCWD();
 				service.callback(args, {cwd:location}).then(
 					function(result) {
 						promise.resolve(result);
