447a5c458ba786a15406e325789f812a234a0c22
sfranklin <susan_franklin@us.ibm.com>
2013-01-08 23:19:03 -0500

    Bug 395393 - Remove Dojo from primary pages.  operations list.

--- bundles/org.eclipse.orion.client.core/web/operations/list.js
+++ bundles/org.eclipse.orion.client.core/web/operations/list.js
@@ -12,41 +12,38 @@
 /*jslint browser:true devel:true*/
 /*global define*/
 
-define(['i18n!orion/operations/nls/messages', 'require', 'dojo', 'orion/bootstrap', 'orion/commands', 'orion/selection', 'orion/fileClient', 'orion/searchClient', 'orion/operationsClient', 'orion/status', 'orion/progress', 'orion/globalCommands',
+define(['i18n!orion/operations/nls/messages', 'require', 'orion/bootstrap', 'orion/commands', 'orion/selection', 'orion/fileClient', 'orion/searchClient', 'orion/operationsClient', 'orion/status', 'orion/progress', 'orion/globalCommands',
         'orion/operationsTable', 'orion/operationsCommands'], 
-		function(messages, require, dojo, mBootstrap, mCommands, mSelection, mFileClient, mSearchClient, mOperationsClient, mStatus, mProgress, mGlobalCommands, mOperationsTable, mOperationsCommands) {
+		function(messages, require, mBootstrap, mCommands, mSelection, mFileClient, mSearchClient, mOperationsClient, mStatus, mProgress, mGlobalCommands, mOperationsTable, mOperationsCommands) {
 
-	dojo.addOnLoad(function() {
-		mBootstrap.startup().then(function(core) {
-			var serviceRegistry = core.serviceRegistry;
-			var preferences = core.preferences;
-			var selection = new mSelection.Selection(serviceRegistry);	
-			var commandService = new mCommands.CommandService({serviceRegistry: serviceRegistry, selection: selection});
-			var fileClient = new mFileClient.FileClient(serviceRegistry);
-			var searcher = new mSearchClient.Searcher({serviceRegistry: serviceRegistry, commandService: commandService, fileService: fileClient});
-			var operationsClient = new mOperationsClient.OperationsClient(serviceRegistry);
-			var statusService = new mStatus.StatusReportingService(serviceRegistry, operationsClient, "statusPane", "notifications", "notificationArea"); //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
-			new mProgress.ProgressService(serviceRegistry, operationsClient);
-				
-			var operationsTable = new mOperationsTable.OperationsExplorer(serviceRegistry, selection, operationsClient, "tasks-list", "pageActions", "selectionTools", "operationItems"); //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+	mBootstrap.startup().then(function(core) {
+		var serviceRegistry = core.serviceRegistry;
+		var preferences = core.preferences;
+		var selection = new mSelection.Selection(serviceRegistry);	
+		var commandService = new mCommands.CommandService({serviceRegistry: serviceRegistry, selection: selection});
+		var fileClient = new mFileClient.FileClient(serviceRegistry);
+		var searcher = new mSearchClient.Searcher({serviceRegistry: serviceRegistry, commandService: commandService, fileService: fileClient});
+		var operationsClient = new mOperationsClient.OperationsClient(serviceRegistry);
+		var statusService = new mStatus.StatusReportingService(serviceRegistry, operationsClient, "statusPane", "notifications", "notificationArea"); //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		new mProgress.ProgressService(serviceRegistry, operationsClient);
 			
-			// global commands
-			mGlobalCommands.generateBanner("orion-operationList", serviceRegistry, commandService, preferences, searcher); //$NON-NLS-0$
-			mGlobalCommands.setPageTarget({task: "Operations"});
-			mOperationsCommands.createOperationsCommands(serviceRegistry, commandService, operationsTable, operationsClient);
-			
-			commandService.addCommandGroup("pageActions", "eclipse.taskGroup.unlabeled", 100); //$NON-NLS-1$ //$NON-NLS-0$
-			commandService.addCommandGroup("selectionTools", "eclipse.selectionGroup", 500, messages["More"]); //$NON-NLS-1$ //$NON-NLS-0$
-			
-			commandService.registerCommandContribution("pageActions", "eclipse.removeCompletedOperations", 1, "eclipse.taskGroup.unlabeled"); //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
-			commandService.registerCommandContribution("operationItems", "eclipse.removeOperation", 1); //$NON-NLS-1$ //$NON-NLS-0$
-			commandService.registerCommandContribution("selectionTools", "eclipse.removeOperation", 1, "eclipse.selectionGroup"); //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
-			commandService.registerCommandContribution("operationItems", "eclipse.cancelOperation", 2); //$NON-NLS-1$ //$NON-NLS-0$
-			commandService.registerCommandContribution("selectionTools", "eclipse.cancelOperation", 2, "eclipse.selectionGroup"); //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
-			
-			dojo.hitch(operationsTable, operationsTable.loadOperations)();
-			
-		});
+		var operationsTable = new mOperationsTable.OperationsExplorer(serviceRegistry, selection, operationsClient, "tasks-list", "pageActions", "selectionTools", "operationItems"); //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		
+		// global commands
+		mGlobalCommands.generateBanner("orion-operationList", serviceRegistry, commandService, preferences, searcher); //$NON-NLS-0$
+		mGlobalCommands.setPageTarget({task: "Operations"});
+		mOperationsCommands.createOperationsCommands(serviceRegistry, commandService, operationsTable, operationsClient);
+		
+		commandService.addCommandGroup("pageActions", "eclipse.taskGroup.unlabeled", 100); //$NON-NLS-1$ //$NON-NLS-0$
+		commandService.addCommandGroup("selectionTools", "eclipse.selectionGroup", 500, messages["More"]); //$NON-NLS-1$ //$NON-NLS-0$
+		
+		commandService.registerCommandContribution("pageActions", "eclipse.removeCompletedOperations", 1, "eclipse.taskGroup.unlabeled"); //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		commandService.registerCommandContribution("operationItems", "eclipse.removeOperation", 1); //$NON-NLS-1$ //$NON-NLS-0$
+		commandService.registerCommandContribution("selectionTools", "eclipse.removeOperation", 1, "eclipse.selectionGroup"); //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		commandService.registerCommandContribution("operationItems", "eclipse.cancelOperation", 2); //$NON-NLS-1$ //$NON-NLS-0$
+		commandService.registerCommandContribution("selectionTools", "eclipse.cancelOperation", 2, "eclipse.selectionGroup"); //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		
+		operationsTable.loadOperations.bind(operationsTable)();
+		
 	});
-	
 });
