a6b1a79c00cc6c02f34eae83c8b5394c5501471a
Szymon Brandys <Szymon.Brandys@pl.ibm.com>
2013-01-16 16:50:01 +0100

    Removing dojo/dijit from ApplyPatchDialog.js

--- bundles/org.eclipse.orion.client.git/web/orion/git/widgets/ApplyPatchDialog.js
+++ bundles/org.eclipse.orion.client.git/web/orion/git/widgets/ApplyPatchDialog.js
@@ -1,76 +1,75 @@
 /*******************************************************************************
- * @license
- * Copyright (c) 2011, 2012 IBM Corporation and others. 
- * All rights reserved. This program and the accompanying materials are made 
- * available under the terms of the Eclipse Public License v1.0 
- * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
- * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ * @license Copyright (c) 2011, 2013 IBM Corporation and others. All rights
+ *          reserved. This program and the accompanying materials are made
+ *          available under the terms of the Eclipse Public License v1.0
+ *          (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse
+ *          Distribution License v1.0
+ *          (http://www.eclipse.org/org/documents/edl-v10.html).
  * 
  * Contributors: IBM Corporation - initial API and implementation
  ******************************************************************************/
-/*global dojo dijit dojox widgets*/
-/*jslint browser:true */
 
-define(['i18n!git/nls/gitmessages', 'dojo', 'dijit', 'dojox', 'dijit/Dialog', 'dojo/data/ItemFileReadStore', 'dojox/form/Uploader', 'dojox/form/uploader/FileList', 'dojox/form/uploader/plugins/IFrame', 'dijit/form/Button', 'dijit/ProgressBar', 'orion/widgets/_OrionDialogMixin', 'text!orion/git/widgets/templates/ApplyPatchDialog.html'], function(messages, dojo, dijit, dojox) {
+define([ 'i18n!git/nls/gitmessages', 'orion/webui/dialog' ], function(messages, dialog) {
 
-/**
- */
-dojo.declare("orion.git.widgets.ApplyPatchDialog", [ dijit.Dialog, orion.widgets._OrionDialogMixin ], { //$NON-NLS-0$
-	widgetsInTemplate : true,
-	templateString : dojo.cache('orion', 'git/widgets/templates/ApplyPatchDialog.html'), //$NON-NLS-1$ //$NON-NLS-0$
-
-	constructor : function() {
-		this.inherited(arguments);
-		this.options = arguments[0] || {};
-	},
-	postMixInProperties : function() {
-		this.inherited(arguments);
-		this.title = messages["Apply Patch"];
-	},
-	postCreate : function() {
-		this.inherited(arguments);		
-		dojo.byId("applyPatchDialog.myForm").action = this.options.diffLocation; //$NON-NLS-0$
-		dojo.byId("applyPatchDialog.uploader").url = this.options.diffLocation; //$NON-NLS-0$
-
-		var self = this;
-
-		function toogle(disableFile) {
-			dijit.byId("files").set("disabled", disableFile); //$NON-NLS-1$ //$NON-NLS-0$
-			dijit.byId("applyPatchDialog.uploader").set("disabled", disableFile); //$NON-NLS-1$ //$NON-NLS-0$
-			dijit.byId("applyPatchDialog.url").set("disabled", !disableFile); //$NON-NLS-1$ //$NON-NLS-0$
-		}
-		toogle(true);
-
-		dojo.connect(dijit.byId("applyPatchDialog.urlRadio"), "onclick", dojo.hitch(this, function(dataArray) { //$NON-NLS-1$ //$NON-NLS-0$
-			if (dataArray.target.id === "applyPatchDialog.urlRadio") //$NON-NLS-0$
-				toogle(true);
-		}));
-
-		dojo.connect(dijit.byId("applyPatchDialog.fileRadio"), "onclick", dojo.hitch(this, function(dataArray) { //$NON-NLS-1$ //$NON-NLS-0$
-			if (dataArray.target.id === "applyPatchDialog.fileRadio") //$NON-NLS-0$
-				toogle(false);
-		}));
-
-		dojo.connect(dijit.byId("applyPatchDialog.uploader"), "onError", dojo.hitch(this, function(dataArray) { //$NON-NLS-1$ //$NON-NLS-0$
-			setTimeout(dojo.hitch(this, function(){
-				this.hide();
-			}), 2000);		
-		}));
-	
-		dojo.connect(dijit.byId("applyPatchDialog.uploader"), "onComplete", dojo.hitch(this, function(dataArray) { //$NON-NLS-1$ //$NON-NLS-0$
-			setTimeout(dojo.hitch(this, function(){
-				this.hide();
-			}), 2000);		
-		}));
-		
-		// Stop the dialog from submitting if trying to activate the upload button with a keyboard.
-		// See https://bugs.eclipse.org/bugs/show_bug.cgi?id=370969
-		
-		dojo.connect(dijit.byId("applyPatchDialog.uploader"), "onKeyPress", function(evt) { //$NON-NLS-1$ //$NON-NLS-0$
-			if(evt.keyCode === dojo.keys.ENTER) {
-				evt.stopPropagation();
-			}
-		});
+	function ApplyPatchDialog(options) {
+		this._init(options);
 	}
-});
+
+	ApplyPatchDialog.prototype = new dialog.Dialog();
+
+	ApplyPatchDialog.prototype.TEMPLATE =
+
+	'<div style="padding:4px"><input type="radio" name="radio" value="urlRadio" id="urlRadio" checked/>URL:' 
+		+ '<input type="text" name="url" id="url"/></div>'
+			+'<div style="padding:4px"><input type="radio" name="radio" value="fileRadio" id="fileRadio"/>File:'
+			+ '<input type="file" name="selectedFile" id="selectedFile" class="uploadChooser" /></div>';
+
+	ApplyPatchDialog.prototype._init = function(options) {
+		var that = this;
+
+		this.title = messages["Apply Patch"];
+		this.modal = true;
+		this.messages = messages;
+		this.options = options;
+
+		this.buttons = [];
+
+		this.buttons.push({ callback : function() {
+			that._applyPatch();
+		},
+		text : 'OK'
+		});
+
+		// Start the dialog initialization.
+		this._initialize();
+	};
+
+	ApplyPatchDialog.prototype._bindToDom = function(parent) {
+		// nothing to do
+	};
+
+	ApplyPatchDialog.prototype._applyPatch = function(parent) {
+		var formData = new FormData();
+		formData.append("uploadedfile", this.$selectedFile.files[0]);
+		formData.append("url", this.$url.value);
+		formData.append("radio", this.$fileRadio.checked ? "fileRadio" : "urlRadio");
+
+		this.req = new XMLHttpRequest();
+		this.req.open('post', this.options.diffLocation);
+		this.req.onreadystatechange = this.handleReadyState.bind(this);
+		this.req.send(formData);
+	};
+
+	ApplyPatchDialog.prototype.handleReadyState = function(state) {
+		if (this.req.readyState === 4) {
+			this.hide();
+		}
+	};
+
+	ApplyPatchDialog.prototype.constructor = ApplyPatchDialog;
+
+	// return the module exports
+	return { ApplyPatchDialog : ApplyPatchDialog
+	};
+
 });
