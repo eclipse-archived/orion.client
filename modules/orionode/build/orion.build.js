16f504e77f221e0575c4f76590b83d6fe7b5c61b
Mark Macdonald <Mark_MacDonald@ca.ibm.com>
2013-01-18 14:16:39 -0500

    Node: fix build script - Fix dojo/dijit/dojox module paths - Update customTargets.xml after client refactor (Bug 397034) - Remove Dojo CSS step from build - Prevent xcopy prompting for input and hanging the script

--- modules/orionode/build/orion.build.js
+++ modules/orionode/build/orion.build.js
@@ -20,17 +20,17 @@
     // mamacdon: I hacked the dojo path to resolve --  dojo is just too gigantic to copy into a staging dir
     packages: [{
         name: 'dojo',
-        location: '../../lib/dojo/org.dojotoolkit/dojo',
+        location: '../../node_modules/dojo',
         main: 'lib/main-browser',
         lib: '.'
     }, {
         name: 'dijit',
-        location: '../../lib/dojo/org.dojotoolkit/dijit',
+        location: '../../node_modules/dijit',
         main: 'lib/main',
         lib: '.'
     }, {
         name: 'dojox',
-        location: '../../lib/dojo/org.dojotoolkit/dojox',
+        location: '../../node_modules/dojox',
         main: 'lib/main',
         lib: '.'
     }],
