b1f3be9128cbf606a886512cacc7b313194bd971
Grant Gayed <grant_gayed@ca.ibm.com>
2013-01-19 00:21:00 -0500

    Bug 398559 - [Shell] expose file objects to plugins safely, enable return values with additional types

--- bundles/org.eclipse.orion.client.ui/web/shell/paramType-file.js
+++ bundles/org.eclipse.orion.client.ui/web/shell/paramType-file.js
@@ -11,12 +11,127 @@
  *******************************************************************************/
 
 /*global define*/
+/*jslint browser:true*/
 
-define(["i18n!orion/shell/nls/messages", "orion/widgets/Shell", "orion/i18nUtil"],
-	function(messages, mShell, i18nUtil) {
+define(["i18n!orion/shell/nls/messages", "orion/widgets/Shell", "orion/i18nUtil", "orion/Deferred", "shell/resultWriters"],
+	function(messages, mShell, i18nUtil, Deferred, mResultWriters) {
 
 	var orion = {};
 	orion.shellPage = {};
+
+	function FileResult(value, typeSpec, typedPath, shellPageFileService) {
+		this.value = value;
+		this.typeSpec = typeSpec;
+		this.typedPath = typedPath;
+		this.shellPageFileService = shellPageFileService;
+	}
+
+	FileResult.prototype = {
+		getPluginRepresentation: function() {
+			var promise = new Deferred();
+			if (typeof(this.value) === "string") { //$NON-NLS-0$
+				promise.resolve({file: {path: this.value}});
+			} else {
+				/* value is an array of nodes */
+				var index = this.typedPath.lastIndexOf(this.shellPageFileService.SEPARATOR, this.typedPath.length - 2);
+				var path = index !== -1 ? this.typedPath.substring(0, index) : "."; //$NON-NLS-0$
+
+				var self = this;
+				var waitCount = 0;
+				var nodesQueue = this.value.slice(0);
+				var result = [];
+				var resultFn = function(file) {
+					if (file) {
+						result.push(file);
+					}
+					if (waitCount === 0 && nodesQueue.length === 0) {
+						if (self.typeSpec.multiple) {
+							promise.resolve(result);
+						} else {
+							promise.resolve(result[0]);
+						}
+					}
+				};
+
+				var processNodesQueue = function() {
+					while (nodesQueue.length > 0) {
+						var node = nodesQueue[0];
+						var file = {
+							isDirectory: node.Directory,
+							path: path
+						};
+						if (node.recursePath) {
+							node.recursePath.forEach(function(current) {
+								file.path += self.shellPageFileService.SEPARATOR + current;
+							});
+						}
+						file.path += self.shellPageFileService.SEPARATOR + node.Name;
+						if (node.Directory && self.typeSpec.multiple && self.typeSpec.recurse) {
+							(function(node) {
+								waitCount++;
+								self.shellPageFileService.withChildren(
+									node,
+									function(children) {
+										waitCount--;
+										var recursePath = node.recursePath ? node.recursePath.slice(0) : [];
+										recursePath.push(node.Name);
+										children.forEach(function(current) {
+											current.recursePath = recursePath;
+										});
+										nodesQueue.push.apply(nodesQueue, children);
+										if (nodesQueue.length !== children.length) {
+											/* queue was already being processed, so just let it continue */
+											return;
+										}
+										if (children.length === 0) {
+											resultFn();	/* waitCount may now be 0 */
+										} else {
+											processNodesQueue();
+										}
+									},
+									function(error) {
+										waitCount--;
+										resultFn();	/* waitCount may now be 0 */
+									}
+								);
+							})(node);
+						}
+
+						nodesQueue.splice(0,1);
+						if (self.typeSpec.content && !file.isDirectory) {
+							(function(file) {
+								waitCount++;
+								self.shellPageFileService.readBlob(node).then(
+									function(content) {
+										waitCount--;
+										file.blob = content;
+										resultFn(file);
+									},
+									function(error) {
+										waitCount--;
+										resultFn(file);
+									}
+								);
+							}(file));
+						} else {
+							resultFn(file);
+						}
+					}
+				};
+				processNodesQueue();
+			}
+			return promise;
+		},
+		getValue: function() {
+			return this.value;
+		},
+		resourceExists: function() {
+			return typeof(this.value) !== "string"; //$NON-NLS-0$
+		},
+		stringify: function() {
+			return this.typedPath;
+		}
+	};
 
 	orion.shellPage.ParamTypeFile = (function() {
 		function ParamTypeFile(name, shellPageFileService) {
@@ -38,7 +153,128 @@ define(["i18n!orion/shell/nls/messages", "orion/widgets/Shell", "orion/i18nUtil"
 					string = string.substring(0, string.length - 1);
 				}
 				var predictions = this._getPredictions(string);
-				return this._createCompletion(string, predictions, typeSpec.directory, typeSpec.file, typeSpec.exist);
+				return this._createCompletion(string, predictions, typeSpec);
+			},
+
+			/**
+			 * This function handles results of this custom type returned from commands.
+			 */
+			processResult: function(promise, result, output) {
+				var element = document.createElement("div"); //$NON-NLS-0$
+
+				var self = this;
+				var waitCount = 0;
+				var successFn = function(file) {
+					this.callback = function() {
+						var string = i18nUtil.formatMessage(
+							messages["Wrote ${0}"],
+							typeof(file) === "string" ? file : self.shellPageFileService.computePathString(file)); //$NON-NLS-0$
+						var writer = new mResultWriters.ShellStringWriter(element);
+						writer.appendText(string);
+						writer.appendNewline();
+						writer.write();
+						if (--waitCount === 0) {
+							promise.resolve(element);
+						} else {
+							promise.progress(element);
+						}
+					};
+					return this;
+				};
+				var errorFn = function(file) {
+					this.callback = function(error) {
+						var string = i18nUtil.formatMessage(
+							messages["Failed to write ${0}"],
+							typeof(file) === "string" ? file : self.shellPageFileService.computePathString(file)); //$NON-NLS-0$
+						string += " [" + error + "]"; //$NON-NLS-1$ //$NON-NLS-0$
+						var writer = new mResultWriters.ShellStringWriter(element);
+						writer.appendText(string);
+						writer.appendNewline();
+						writer.write();
+						if (--waitCount === 0) {
+							promise.resolve(element);
+						} else {
+							promise.progress(element);
+						}
+					};
+					return this;
+				};
+
+				var destination;
+				if (!output) {
+					destination = this.shellPageFileService.getCurrentDirectory();
+				} else if (output.resourceExists()) {
+					/* value is an array of nodes, in this context will always have a size of 1 */
+					destination = output.getValue()[0];
+				} else {
+					/* value is a string representing a non-existent resource */
+					destination = output.getValue();
+				}
+
+				waitCount++;
+				this.shellPageFileService.ensureDirectory(null, destination).then(
+					function(directory) {
+						waitCount--;
+
+						var files = result.getValue();
+						if (!result.isArray()) {
+							files = [files];
+						}
+						files.forEach(function(file) {
+							waitCount++;
+							var pathSegments = file.path.split(self.shellPageFileService.SEPARATOR);
+
+							/* normalize instances of '.' and '..' in the path */
+							var index = 0;
+							while (index < pathSegments.length) {
+								var segment = pathSegments[index];
+								if (segment === ".") { //$NON-NLS-0$
+									pathSegments.splice(index, 1);
+								} else if (segment === "..") { //$NON-NLS-0$
+									if (index === 0) {
+										/* invalid, destination must be a descendent of the cwd */
+										errorFn(i18nUtil.formatMessage(messages["Cannot write ${0}, it is not a descendent of the output directory"], file.path));
+										return;
+									}
+									pathSegments.splice(index-- - 1, 2);
+								} else {
+									index++;
+								}
+							}
+
+							var writeFile = function(parentNode, fileToWrite, pathSegments) {
+								var segment = pathSegments[0];
+								pathSegments.splice(0,1);
+								var nodeString = self.shellPageFileService.computePathString(parentNode) + self.shellPageFileService.SEPARATOR + segment;
+								if (pathSegments.length === 0) {
+									/* attempt to write the new resource */
+									if (fileToWrite.isDirectory) {
+										self.shellPageFileService.ensureDirectory(parentNode, segment).then(successFn(nodeString).callback, errorFn(nodeString).callback);
+									} else {
+										self.shellPageFileService.ensureFile(parentNode, segment).then(
+											function(file) {
+												var writer = new mResultWriters.FileBlobWriter(file, self.shellPageFileService);
+												writer.addBlob(fileToWrite.blob);
+												writer.write().then(successFn(file).callback, errorFn(file).callback);
+											},
+											errorFn(nodeString).callback
+										);
+									}
+									return;
+								}
+								/* more lead-up path segments to go */
+								self.shellPageFileService.ensureDirectory(parentNode, segment).then(
+									function(newNode) {
+										writeFile(newNode, fileToWrite, pathSegments);
+									},
+									errorFn(self.shellPageFileService.computePathString(parentNode) + self.shellPageFileService.SEPARATOR + segment).callback
+								);
+							};
+							writeFile(directory, file, pathSegments);
+						});
+					},
+					errorFn(destination).callback
+				);
 			},
 
 			/**
@@ -46,21 +282,24 @@ define(["i18n!orion/shell/nls/messages", "orion/widgets/Shell", "orion/i18nUtil"
 			 * value's string representation.
 			 */
 			stringify: function(value) {
-				if (typeof(value) === "string") { //$NON-NLS-0$
-					return value;
-				}
-				return value.typedPath || this.shellPageFileService.computePathString(value);
+				return typeof(value) === "string" ? value : value.stringify(); //$NON-NLS-0$
 			},
 
 			/** @private */
 
-			_createCompletion: function(string, predictions, directory, file, exist) {
-				var message, status, value = null;
+			_createCompletion: function(string, predictions, typeSpec) {
 				if (!predictions) {
 					/* parent hierarchy is not valid */
-					status = mShell.CompletionStatus.ERROR;
-					message = i18nUtil.formatMessage(messages["'${0}' is not valid"], string);
-				} else {
+					return {
+						value: null,
+						status: mShell.CompletionStatus.ERROR,
+						message: i18nUtil.formatMessage(messages["'${0}' is not valid"], string),
+						predictions: null
+					};
+				}
+
+				var message, status, value = null;
+				if (typeSpec.exist === false) {
 					var exactMatch;
 					for (var i = 0; i < predictions.length; i++) {
 						var current = predictions[i];
@@ -69,42 +308,59 @@ define(["i18n!orion/shell/nls/messages", "orion/widgets/Shell", "orion/i18nUtil"
 							break;
 						}
 					}
-
-					if (exist === false) {
-						predictions = [];
-						if (exactMatch) {
-							status = mShell.CompletionStatus.ERROR;
-							message = i18nUtil.formatMessage(messages["'${0}' already exists"], string);
-						} else {
-							// TODO verify filename?
-							status = mShell.CompletionStatus.MATCH;
-							value = string;
-						}
+					if (exactMatch) {
+						status = mShell.CompletionStatus.ERROR;
+						message = i18nUtil.formatMessage(messages["'${0}' already exists"], string);
 					} else {
-						/* filter the predictions based on directory/file */
-						var temp = [];
+						// TODO validate filename?
+						status = mShell.CompletionStatus.MATCH;
+						value = new FileResult(string, typeSpec, string, this.shellPageFileService);
+					}
+					predictions = [];
+				} else {
+					/* filter the predictions based on directory/file */
+					var temp = [];
+					predictions.forEach(function(current) {
+						if (current.value.Directory || typeSpec.file) {
+							temp.push(current);
+						}
+					});
+					predictions = temp;
+
+					if (typeSpec.exist) {
+						var exactMatches = [];
+						var testString = string.replace(/[\-(){}\[\]+,.\\\^$|#]/g, "\\$&"); //$NON-NLS-0$
+						testString = testString.replace(/\?/g, "."); //$NON-NLS-0$
+						testString = testString.replace(/\*/g, ".*"); //$NON-NLS-0$
+						var regExp = new RegExp("^" + testString + "$"); //$NON-NLS-1$ //$NON-NLS-0$
 						predictions.forEach(function(current) {
-							if (current.value.Directory || file) {
-								temp.push(current);
+							if (current.name.match(regExp) && ((current.value.Directory && typeSpec.directory) || (!current.value.Directory && typeSpec.file))) {
+								exactMatches.push(current.value);
 							}
 						});
-						predictions = temp;
-
-						if (exist) {
-							if (exactMatch && ((exactMatch.value.Directory && directory) || (!exactMatch.value.Directory && file))) {
-								status = mShell.CompletionStatus.MATCH;
-								value = exactMatch.value;
-							} else if (predictions.length > 0) {
-								status = mShell.CompletionStatus.PARTIAL;
-							} else {
-								status = mShell.CompletionStatus.ERROR;
-								message = i18nUtil.formatMessage(messages["'${0}' is not valid"], string);
-							}
-						} else { /* exist is undefined */
-							// TODO verify filename?
+						if ((typeSpec.multiple && exactMatches.length) || exactMatches.length === 1) {
 							status = mShell.CompletionStatus.MATCH;
-							value = exactMatch ? exactMatch.value : string;
+							value = new FileResult(exactMatches, typeSpec, string, this.shellPageFileService);
+						} else if (predictions.length) {
+							status = mShell.CompletionStatus.PARTIAL;
+						} else {
+							status = mShell.CompletionStatus.ERROR;
+							message = i18nUtil.formatMessage(messages["'${0}' is not valid"], string);
 						}
+					} else { /* exist is undefined */
+						// TODO validate filename?
+						var exactMatch;
+						for (var i = 0; i < predictions.length; i++) {
+							var current = predictions[i];
+							if (current.name === string) {
+								exactMatch = current;
+								break;
+							}
+						}
+						status = mShell.CompletionStatus.MATCH;
+						value = exactMatch ?
+							new FileResult([exactMatch.value], typeSpec, string, this.shellPageFileService) :
+							new FileResult(string, typeSpec, string, this.shellPageFileService);
 					}
 				}
 
@@ -143,22 +399,23 @@ define(["i18n!orion/shell/nls/messages", "orion/widgets/Shell", "orion/i18nUtil"
 					var parentNode = this.shellPageFileService.getParent(directoryNode);
 					if (parentNode) {
 						name = directoriesSegment + ".."; //$NON-NLS-0$
-						parentNode.typedPath = name;
 						directoryPredictions.push({name: name, value: parentNode, incomplete: true});
 					}
 				}
 				
 				if (finalSegment.trim().length === 0 && directoriesSegment.length > 0) {
 					name = directoriesSegment;
-					directoryNode.typedPath = name;
 					directoryPredictions.push({name: name, value: directoryNode, incomplete: false});
 				}
+				var testString = finalSegment.replace(/[\-(){}\[\]+,.\\\^$|#]/g, "\\$&"); //$NON-NLS-0$
+				testString = testString.replace(/\?/g, "."); //$NON-NLS-0$
+				testString = testString.replace(/\*/g, ".*"); //$NON-NLS-0$
+				var regExp = new RegExp("^" + testString); //$NON-NLS-0$
 				for (var i = 0; i < childNodes.length; i++) {
 					var candidate = childNodes[i];
-					if (candidate.Name.indexOf(finalSegment) === 0) {
+					if (candidate.Name.match(regExp)) {
 						var complete = !candidate.Directory || (candidate.Children && candidate.Children.length === 0);
 						name = directoriesSegment + candidate.Name;
-						candidate.typedPath = name;
 						if (candidate.Directory) {
 							directoryPredictions.push({name: name, value: candidate, incomplete: !complete});
 						} else {
