474e0976674e968e406b80714c0b32ca122095cc
sfranklin <susan_franklin@us.ibm.com>
2013-01-22 17:18:46 -0500

    Bug 398519 - Mysterious keyboard shortcuts shown in Navigator

--- bundles/org.eclipse.orion.client.ui/web/orion/webui/littlelib.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/webui/littlelib.js
@@ -157,14 +157,21 @@ define(['require'], function(require) {
 	}
 	
 	var KEY = {
+		BKSPC: 8,
 		TAB: 9,
 		ENTER: 13,
 		ESCAPE: 27,
 		SPACE: 32,
+		PAGEUP: 33,
+		PAGEDOWN: 34,
+		END: 35,
+		HOME: 36,
 		LEFT: 37,
 		UP: 38,
 		RIGHT: 39,
-		DOWN: 40
+		DOWN: 40,
+		INSERT: 45,
+		DEL: 46
 	};
 		
 	//return module exports
