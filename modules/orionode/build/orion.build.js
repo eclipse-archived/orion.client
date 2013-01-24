a20c57a4605858cf6421285adf5df2c6e98a3577
Mark Macdonald <Mark_MacDonald@ca.ibm.com>
2013-01-24 14:32:12 -0500

    Bug 399010 - Remove dojo references from source code

--- modules/orionode/build/orion.build.js
+++ modules/orionode/build/orion.build.js
@@ -17,23 +17,7 @@
     baseUrl: '.',
 
     // set the paths to our library packages
-    // mamacdon: I hacked the dojo path to resolve --  dojo is just too gigantic to copy into a staging dir
-    packages: [{
-        name: 'dojo',
-        location: '../../node_modules/dojo',
-        main: 'lib/main-browser',
-        lib: '.'
-    }, {
-        name: 'dijit',
-        location: '../../node_modules/dijit',
-        main: 'lib/main',
-        lib: '.'
-    }, {
-        name: 'dojox',
-        location: '../../node_modules/dojox',
-        main: 'lib/main',
-        lib: '.'
-    }],
+    packages: [],
     paths: {
         text: 'requirejs/text',
         i18n: 'requirejs/i18n',
