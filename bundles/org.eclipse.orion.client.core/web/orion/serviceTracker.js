85444ea36e3a36e04d2ff57ed2009e663d2a08ec
Mark Macdonald <Mark_MacDonald@ca.ibm.com>
2013-05-01 11:21:06 -0400

    jsdoc service classes

--- bundles/org.eclipse.orion.client.core/web/orion/serviceTracker.js
+++ bundles/org.eclipse.orion.client.core/web/orion/serviceTracker.js
@@ -15,13 +15,17 @@ define([], function() {
 	/**
 	 * @name orion.ServiceTracker
 	 * @class Simplifies the use of services within a service registry.
-	 * @description A ServiceTracker tracks matching services in the given service registry. Matching services are those
-	 * whose <code>objectClass</code> property contains the given <code>serviceName</code>. The {@link #addingService} and 
-	 * {@link #removedService} methods can be overridden to customize the service objects being tracked.
+	 * @description Creates a <code>ServiceTracker</code> against the given service registry.
+	 * The returned <code>ServiceTracker</code> will track services whose <code>objectClass</code> property contains the
+	 * given <code>objectClass</code> parameter.
+	 *
+	 * <p>After creating a <code>ServiceTracker</code>, it can then be {@link #open}ed to begin tracking services.</p>
+	 * <p>The {@link #addingService} and {@link #removedService} methods can be overridden to customize the service objects
+	 * being tracked.</p>
 	 * @param {orion.serviceregistry.ServiceRegistry} serviceRegistry The service registry to track services of.
-	 * @param {String} serviceName The service name of services to be tracked.
+	 * @param {String} objectClass The objectClass of services to be tracked.
 	 */
-	function ServiceTracker(serviceRegistry, serviceName) {
+	function ServiceTracker(serviceRegistry, objectClass) {
 		this.serviceRegistry = serviceRegistry;
 		var refs = {};
 		var services = {};
@@ -44,7 +48,7 @@ define([], function() {
 			this.removedService(serviceRef, service);
 		}
 		function isTrackable(serviceRef) {
-			return serviceRef.getProperty('objectClass').indexOf(serviceName) !== -1; //$NON-NLS-0$
+			return serviceRef.getProperty('objectClass').indexOf(objectClass) !== -1; //$NON-NLS-0$
 		}
 
 		/**
@@ -109,7 +113,7 @@ define([], function() {
 			};
 			serviceRegistry.addEventListener('registered', addedListener); //$NON-NLS-0$
 			serviceRegistry.addEventListener('unregistering', removedListener); //$NON-NLS-0$
-			serviceRegistry.getServiceReferences(serviceName).forEach(function(serviceRef) {
+			serviceRegistry.getServiceReferences(objectClass).forEach(function(serviceRef) {
 				add.call(self, serviceRef);
 			});
 			if (typeof this.onOpen === 'function') {
@@ -117,13 +121,11 @@ define([], function() {
 			}
 		};
 	}
-	ServiceTracker.prototype = {
+	ServiceTracker.prototype = /** @lends orion.ServiceTracker.prototype */ {
 		/**
 		 * Called to customize a service object being added to this ServiceTracker. Subclasses may override this method.
 		 * The default implementation returns the result of calling {@link orion.serviceregistry.ServiceRegistry#getService}
 		 * passing the service reference.
-		 * @name orion.ServiceTracker#addingService
-		 * @function
 		 * @param {orion.serviceregistry.ServiceReference} serviceRef The reference to the service being added.
 		 * @returns {Object} The service object to be tracked for the given service reference. If <code>null</code> 
 		 * is returned, the service reference will not be tracked.
@@ -133,24 +135,21 @@ define([], function() {
 		},
 		/**
 		 * Called when this ServiceTracker has been opened. Subclasses can override this method.
-		 * @name orion.ServiceTracker#onOpen
 		 * @function
 		 */
 		onOpen: null,
 		/**
 		 * Called when this ServiceTracker has been closed. Subclasses can override this method.
-		 * @name orion.ServiceTracker#onClose
 		 * @function
 		 */
 		onClose: null,
 		/**
 		 * Called when a service is being added to this ServiceTracker. Subclasses can override this method to take part
 		 * in the service's <code>'serviceAdded'</code> phase.
-		 * @name orion.ServiceTracker#onServiceAdded
 		 * @function
-		 * @param {orion.serviceregistry.ServiceReference} serviceRef
-		 * @param {orion.serviceregistry.Service} service
-		 * @returns {Deferred|undefined} This method can optionally return a deferred. If it does, the returned deferred
+		 * @param {orion.serviceregistry.ServiceReference} serviceRef The service reference for the service that is being added.
+		 * @param {Object} service The service implementation object that is being added.
+		 * @returns {orion.Promise|undefined} This method can optionally return a deferred. If it does, the returned deferred
 		 * will be added to the service's <code>serviceAdded</code> listener queue; in other words, the returned deferred
 		 * must resolve before any calls to the service's methods can proceed.
 		 */
@@ -158,10 +157,9 @@ define([], function() {
 		/**
 		 * Called when a service has been removed from this ServiceTracker. Subclasses may override this method.
 		 * The default implementation does nothing.
-		 * @name orion.ServiceTracker#removedService
 		 * @function
 		 * @param {orion.serviceregistry.ServiceReference} serviceRef The reference to the removed service.
-		 * @param {orion.serviceregistry.Service} service The service object for the removed service.
+		 * @param {Object} service The service implementation object for the removed service.
 		 */
 		removedService: function(serviceRef, service) {
 		}
