0a73f3f4d50a9f746f5fc9e76c4910c9ecd37dd1
Anton McConville <antonm@ca.ibm.com>
2013-05-17 14:38:09 -0400

    Take 2 - removing 'Orion Content'. This seems to have a unique path in breadcrumb naming, that is difficult to understand. Removing the string for now, as a first step.

--- bundles/org.eclipse.orion.client.ui/web/orion/navigate/nls/root/messages.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/navigate/nls/root/messages.js
@@ -99,7 +99,7 @@ define({
 	"New": "New",
 	"Click on an item to make a selection, then use this menu to see what actions are available.": "Click on an item to make a selection, then use this menu to see what actions are available.",
 	"Actions": "Actions",
-	"Orion Content": "Orion Content",
+	"Orion Content": "",
 	" is not supported in this file system": " is not supported in this file system",
 	"Create new content" : "Create new content",
 	"Favorites" : "Favorites",
