65c5f1dc221e52acb463c5a603222624dd08af55
Gabriel Luong <gabriel.luong@gmail.com>
2013-06-07 15:50:41 -0400

    Bug 410210 - browserCompatibility should support Safari 5

--- bundles/org.eclipse.orion.client.ui/web/orion/browserCompatibility.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/browserCompatibility.js
@@ -18,7 +18,7 @@ define(function() {
 		var browserData = [	{name: 'Chrome/Chromium', regExp: /(?:chrome|crios|chromium)\/(\d+)/i, minVersion: 24},
 							{name: 'Firefox', regExp: /firefox\/(\d+)/i, minVersion: 17},
 							{name: 'MSIE', regExp: /msie\s(\d+)/i, minVersion: 10},
-							{name: 'Safari', regExp: /version\/(\d+).*?safari/i, minVersion: 6} ];
+							{name: 'Safari', regExp: /version\/(\d+).*?safari/i, minVersion: 5} ];
 
 		for (var i = 0; i < browserData.length; i++) {
 			var browser = browserData[i];
