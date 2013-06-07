edf04df204a835e4d45539adc7711a1538bf0ec0
Silenio Quarti <silenio_quarti@ca.ibm.com>
2013-06-07 11:16:56 -0400

    clean globals in linkedMode.js

--- bundles/org.eclipse.orion.client.editor/web/orion/editor/linkedMode.js
+++ bundles/org.eclipse.orion.client.editor/web/orion/editor/linkedMode.js
@@ -9,7 +9,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-/*global define prompt */
+/*global define */
 
 define("orion/editor/linkedMode", [ //$NON-NLS-0$
 	'i18n!orion/editor/nls/messages', //$NON-NLS-0$
