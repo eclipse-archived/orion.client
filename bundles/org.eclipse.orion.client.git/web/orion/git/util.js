3dbb8cea6547859e9ff43d3118ad0f71e6bac86c
Gabriel Luong <gabriel.luong@gmail.com>
2013-06-12 17:40:02 +0200

    Bug 410530 - Remove delay in rendering of the compare widget and stage/unstage buttons in git status and git commit explorer Conflicts:
    	bundles/org.eclipse.orion.client.git/web/orion/git/gitStatusExplorer.js

--- bundles/org.eclipse.orion.client.git/web/orion/git/util.js
+++ bundles/org.eclipse.orion.client.git/web/orion/git/util.js
@@ -68,10 +68,10 @@ define(['i18n!git/nls/gitmessages', 'orion/compare/compareCommands', 'orion/comp
 	 * Create an embedded toggleable compare widget inside a given DIV.
 	 * @param {Object} serviceRegistry The serviceRegistry.
 	 * @param {Object} commandService The commandService.
-	 * @param {Sting} resoure The URL string of the complex URL which will be resolved by a diff provider into two file URLs and the unified diff.
+	 * @param {String} resoure The URL string of the complex URL which will be resolved by a diff provider into two file URLs and the unified diff.
 	 * @param {Boolean} hasConflicts The flag to indicate if the compare contains conflicts.
-	 * @param {Sting} parentDivId The id of the DIV that holds the compare widget.
-	 * @param {Sting} commandSpanId The id of the DIV where all the commands of the compare view are rendered. "Open in compare page", "toggle", "navigate diff" commands will be rendered.
+	 * @param {Object} parentDivId The DIV node or string id of the DIV that holds the compare widget.
+	 * @param {String} commandSpanId The id of the DIV where all the commands of the compare view are rendered. "Open in compare page", "toggle", "navigate diff" commands will be rendered.
 	 * @param {Boolean} editableInComparePage The flag to indicate if opening compage will be editable on the left side. Default is false. Optional.
 	 * @param {Object} gridRenderer If all the commands have to be rendered as grids, especially inside a row of Orion explorer, this has to be provided. Optional.
 	 * @param {String} compareTo Optional. If the resource parameter is a simple file URL then this can be used as the second file URI to compare with.
