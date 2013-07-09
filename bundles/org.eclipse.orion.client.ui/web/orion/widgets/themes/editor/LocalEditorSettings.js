58983f7f043af7bde0f533a6a025acbcead47a05
Gabriel Luong <gabriel.luong@gmail.com>
2013-07-09 01:17:08 -0400

    Bug 412536 - IES43 TVT00090 labels are not externalized

--- bundles/org.eclipse.orion.client.ui/web/orion/widgets/themes/editor/LocalEditorSettings.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/widgets/themes/editor/LocalEditorSettings.js
@@ -22,25 +22,25 @@ define(['i18n!orion/settings/nls/messages', 'orion/widgets/input/Select', 'orion
 		LocalEditorSettings.prototype.template =
 			'<div id="themeContainer">' + //$NON-NLS-0$
 				'<span class="settingsPanelLabel">' + //$NON-NLS-0$
-					'Theme:' +
+					messages["Theme:"] +
 				'</span>' + //$NON-NLS-0$
 				'<div class="settingsPanelValue" id="themepicker"></div>' + //$NON-NLS-0$
 			'</div>' + //$NON-NLS-0$
 			'<div id="sizecontainer">' + //$NON-NLS-0$
 				'<span class="settingsPanelLabel">' + //$NON-NLS-0$
-					'Font Size:' +
+					messages["Font Size:"] +
 				'</span>' + //$NON-NLS-0$
 				'<div class="settingsPanelValue" id="fontsizepicker"></div>' + //$NON-NLS-0$
 			'</div>' + //$NON-NLS-0$
 			'<div id="keyscontainer">' + //$NON-NLS-0$
 				'<span class="settingsPanelLabel">' + //$NON-NLS-0$
-					'Key Bindings:' +
+					messages["Key Bindings:"] +
 				'</span>' + //$NON-NLS-0$
 				'<div class="settingsPanelValue" id="keyspicker"></div>' + //$NON-NLS-0$
 			'</div>' + //$NON-NLS-0$
 			'<div id="autoSaveContainer" style="display:none;">' + //$NON-NLS-0$
 				'<span class="settingsPanelLabel">' + //$NON-NLS-0$
-					'Auto Save:' +
+					messages["Auto Save:"] +
 				'</span>' + //$NON-NLS-0$
 				'<div class="settingsPanelValue" id="autosavecheck"></div>' + //$NON-NLS-0$
 			'</div>'; //$NON-NLS-0$
