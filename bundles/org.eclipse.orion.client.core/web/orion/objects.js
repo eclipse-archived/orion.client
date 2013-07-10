a6c03074a50038de881b41fd899b6c01278a37f3
Mark Macdonald <Mark_MacDonald@ca.ibm.com>
2013-07-10 15:12:51 -0400

    [Bug 412622] CTRL+H search needs to render the file link with find parameter

--- bundles/org.eclipse.orion.client.core/web/orion/objects.js
+++ bundles/org.eclipse.orion.client.core/web/orion/objects.js
@@ -10,11 +10,38 @@
  ******************************************************************************/
 /*global define*/
 define([], function() {
+	function mixin(target/*, source..*/) {
+		Array.prototype.slice.call(arguments, 1).forEach(function(source) {
+			var keys = Object.keys(source);
+			for (var i=0; i < keys.length; i++) {
+				var key = keys[i];
+				target[key] = source[key];
+			}
+		});
+	}
+
 	/**
 	 * @name orion.objects
 	 * @class Object-oriented helpers.
 	 */
 	return {
+		/**
+		 * Creates a shallow clone of the given <code>object</code>.
+		 * @name orion.objects.clone
+		 * @function
+		 * @static
+		 * @param {Object|Array} object The object to clone. Must be a "normal" Object or Array. Other built-ins,
+		 * host objects, primitives, etc, will not work.
+		 * @returns {Object|Array} A clone of <code>object</code>.
+		 */
+		clone: function(object) {
+			if (Array.isArray(object)) {
+				return Array.prototype.slice.call(object);
+			}
+			var clone = Object.create(Object.getPrototypeOf(object));
+			mixin(clone, object);
+			return clone;
+		},
 		/**
 		 * Mixes all <code>source</code>'s own enumerable properties into <code>target</code>. Multiple source objects
 		 * can be passed as varags.
@@ -24,14 +51,6 @@ define([], function() {
 		 * @param {Object} target
 		 * @param {Object} source
 		 */
-		mixin: function(target/**, source..*/) {
-			Array.prototype.slice.call(arguments, 1).forEach(function(source) {
-				var keys = Object.keys(source);
-				for (var i=0; i < keys.length; i++) {
-					var key = keys[i];
-					target[key] = source[key];
-				}
-			});
-		}
+		mixin: mixin
 	};
 });
