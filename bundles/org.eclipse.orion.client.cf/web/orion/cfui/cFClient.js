f40bb1ccf6d103a9424d2be399dfe2b5349a6532
Szymon Brandys <Szymon.Brandys@pl.ibm.com>
2013-11-19 12:52:31 +0100

    Bug 422047 - Add Cloud Foundry integration to Orion (tweaks)

--- bundles/org.eclipse.orion.client.cf/web/orion/cfui/cFClient.js
+++ bundles/org.eclipse.orion.client.cf/web/orion/cfui/cFClient.js
@@ -20,196 +20,72 @@ define(['require', 'orion/xhr', 'orion/Deferred', 'orion/operation'], function(r
 		 * Creates a new CF service.
 		 * 
 		 * @class Provides operations for interacting with Cloud Foundry
-		 * @name com.ibm.cloudoe.cf.CFService
+		 * @name org.eclipse.orion.client.cf.CFService
 		 */
 		function CFService(serviceRegistry) {
 			if (serviceRegistry) {
 				this._serviceRegistry = serviceRegistry;
 				this._serviceRegistration = serviceRegistry.registerService(
-						"com.ibm.cloudoe.cf.service", this);
+						"orion.cf.service", this);
 			}
 		}
 	
-		CFService.prototype = /** @lends com.ibm.cloudoe.cf.CFService.prototype */
+		CFService.prototype = /** @lends org.eclipse.orion.client.cf.CFService.prototype */
 		{	
-			_getServiceResponse: function(deferred, result){
-				var self = this;
-				var response =  result.response ? JSON.parse(result.response) : null;
-				if (response && response instanceof Array) {
-					response = self._parseNestedJson(response);
-				}
+			_getServiceResponse : function(deferred, result) {
+				var response = result.response ? JSON.parse(result.response) : null;
+
 				if (result.xhr && result.xhr.status === 202) {
 					var def = operation.handle(response.Location);
-					def.then(
-						deferred.resolve, 
-						function(data) {
-							data.failedOperation = response.Location;
-							deferred.reject(data);
-						}, 
-						deferred.progress
-					);
-					deferred.then(null, function(error){def.reject(error);});
+					def.then(function(data) {
+						try {
+							deferred.resolve(JSON.parse(data));
+						} catch (e) {
+							deferred.resolve(data);
+						}
+					}, function(data) {
+						data.failedOperation = response.Location;
+						deferred.reject(data);
+					}, deferred.progress);
+					deferred.then(null, function(error) {
+						def.reject(error);
+					});
 					return;
 				}
 				deferred.resolve(response);
 				return;
 			},
-			
+				
 			_handleServiceResponseError: function(deferred, error){
 				deferred.reject(error);
 			},
-			
-			_xhr: function(method, url, data, timeout){
+
+			_xhr : function(method, url) {
 				var self = this;
-				var d = new Deferred();
-				
-				xhr(method, url, { 
-					headers : { 
-						"Orion-Version" : "1"
-					},
-					data : data ? JSON.stringify(data) : null,
-					timeout : timeout ? timeout : 15000,
-					handleAs : "json"
+				var clientDeferred = new Deferred();
+
+				xhr(method, url, { headers : { "Orion-Version" : "1",
+				"Content-Type" : contentType
+				},
+				timeout : 15000,
+				handleAs : "json"
 				}).then(function(result) {
-					self._getServiceResponse(d, result);
-				}, function(error){
-					self._handleServiceResponseError(d, error);
+					self._getServiceResponse(clientDeferred, result);
+				}, function(error) {
+					self._handleServiceResponseError(clientDeferred, error);
 				});
-	
-				return d;
+
+				return clientDeferred;
 			},
-			
-			_parseNestedJson: function(arr) {
-				if (!arr) {
-					return null;
-				}
-				var result = [];
-				if (arr.every(function (item) {
-					if (typeof item === "string") {
-						try {
-							result.push(JSON.parse(item));
-							return true;
-						} catch(e){}
-					}
-					return false;
-				})) {
-					return result;
-				}
-				return arr;
-			},	
 		
-			getTarget: function() {
-				var self = this;
-				return self._xhr("GET", require.toUrl("cf/target"), null, 120000);
+			getTarget: function(targetName) {
+				var targetLocation = require.toUrl("jazz/Project");
+				return this._xhrV2("GET", targetLocation + (targetName ? "/" + targetName : ""));
 			},
 			
-			setTarget: function(url, token) {
-				var self = this;
-				return self._xhr("POST", require.toUrl("cf/target"), {URL: url, 'token': token}, 240000);
-			},
-			
-//			register: function(user, password) {
+//			setTarget: function(url, token) {
 //				var self = this;
-//				return self._xhr("POST", require.toUrl("cf/users"), {email: user, 'password': password});
-//			},
-//			login: function(email, password, organization, space) {
-//				var self = this;
-//				return self._xhr("POST", require.toUrl("cf/login"), 
-//					{'email': email, 'password': password, 'organization': organization, 'space': space},
-//					15000);
-//			},
-//			loginUsingToken: function(token) {
-//				var self = this;
-//				return self._xhr("POST", require.toUrl("cf/login"), 
-//					{'token': token},
-//					15000);
-//			},
-//			logout: function() {
-//				var self = this;
-//				return self._xhr("POST", require.toUrl("cf/logout"));
-//			},
-//			getInfo: function() {
-//				var self = this;
-//				return self._xhr("GET", require.toUrl("cf/info"));
-//			},
-//			getApplications: function() {
-//				var self = this;
-//				return self._xhr("GET", require.toUrl("cf/apps"));
-//			},
-//			getApplication: function(application, dir) {
-//				var self = this;
-//				var url = require.toUrl("cf/app");
-//				if (application) {
-//					url += "/" + application;
-//				}
-//				if (dir) {
-//					url += "?location=" + dir;
-//				}
-//				return self._xhr("GET", url);
-//			},
-//			getLogs: function(application, dir) {
-//				var self = this;
-//				var url = require.toUrl("cf/logs");
-//				if (application) {
-//		           url += "?app=" + application;
-//				} else if (dir) {
-//		           url += "?location=" + dir;
-//		        }
-//				return self._xhr("GET", url, null, 30000);
-//			},
-//			startApplication: function(application, dir) {
-//				var self = this;
-//				return self._xhr("POST", require.toUrl("cf/start"), {app: application, location: dir}, 240000);
-//			},
-//			stopApplication: function(application, dir) {
-//				var self = this;
-//				return self._xhr("POST", require.toUrl("cf/stop"), {app: application, location: dir}, 240000);
-//			},
-//			restartApplication: function(application, dir) {
-//				var self = this;
-//				return self._xhr("POST", require.toUrl("cf/restart"), {app: application, location: dir}, 240000);
-//			},
-//			pushApplication: function(application, dir) {
-//				var self = this;
-//				return self._xhr("POST", require.toUrl("cf/push"), {app: application, location: dir}, 240000);
-//			},
-//			deleteApplication: function(application, dir) {
-//				var self = this;
-//				return self._xhr("POST", require.toUrl("cf/delete"), {app: application, location: dir}, 240000);
-//			},
-//			changePassword: function(newPassword) {
-//				var self = this;
-//				return self._xhr("PUT", require.toUrl("cf/users"), {password: newPassword}, 240000);
-//			},
-//			getServices: function() {
-//				var self = this;
-//				return self._xhr("GET", require.toUrl("cf/services"));
-//			},
-//			getService: function(instanceName) {
-//				var self = this;
-//				return self._xhr("GET", require.toUrl("cf/services/") + instanceName);
-//			},
-//			createService: function(instanceName, service, version) {
-//				var self = this;
-//				return self._xhr("POST", require.toUrl("cf/services"), {name: instanceName, 'service': service, 'version': version});
-//			},
-//			deleteService: function(instanceName) {
-//				var self = this;
-//				return self._xhr("DELETE", require.toUrl("cf/services/") + instanceName);
-//			},
-//			bindService: function(instanceName, application) {
-//				var self = this;
-//				var url = require.toUrl("cf/app/") + application + "/services";
-//				return self._xhr("POST", url, {service: instanceName});
-//			},
-//			unbindService: function(instanceName, application) {
-//				var self = this;
-//				var url = require.toUrl("cf/app/") + application + "/services/" + instanceName;
-//				return self._xhr("DELETE", url);
-//			},
-//			getSpaces: function() {
-//				var self = this;
-//				return self._xhr("GET", require.toUrl("cf/spaces"));
+//				return self._xhr("POST", require.toUrl("cf/target"), {URL: url, 'token': token}, 240000);
 //			}
 		};
 		
