50608fe4c61fb3f5cc34373be0a1393ce7288fd1
Bogdan Gheorghe <gheorghe@ca.ibm.com>
2013-11-26 14:56:25 -0500

    Send some start/stop events

--- bundles/org.eclipse.orion.client.ui/web/dockerShell/dockerShell.js
+++ bundles/org.eclipse.orion.client.ui/web/dockerShell/dockerShell.js
@@ -16,45 +16,72 @@ define(["require", "orion/browserCompatibility", "orion/bootstrap", "orion/xhr",
 	function(require, mBrowserCompatibility, mBootstrap, xhr, Deferred) {
 	var pluginRegistry, serviceRegistry, preferences;
 
-	var userName = "bog";
-
+	var connected = false;
 	mBootstrap.startup().then(function(core) {
 		pluginRegistry = core.pluginRegistry;
 		serviceRegistry = core.serviceRegistry;
 		preferences = core.preferences;
-
-		var output = document.getElementById("dockerPage-output"); //$NON-NLS-0$
-		var input = document.getElementById("dockerCmd"); //$NON-NLS-0$
 		
 		function startContainer() {
-			return xhr("GET", "/docker", {  //$NON-NLS-1$ //$NON-NLS-0$
+			return xhr("POST", "/docker", {  //$NON-NLS-1$ //$NON-NLS-0$
 				headers: {
 					"Orion-Version": "1", //$NON-NLS-1$ //$NON-NLS-0$
 					"Content-Type": "application/json; charset=UTF-8" //$NON-NLS-1$ //$NON-NLS-0$
 				},
+				data: JSON.stringify({
+					"dockerCmd": "start"
+				}),
 				timeout: 15000,
 				handleAs: "json" //$NON-NLS-0$
 			});
 		}
-		
-		function execute() {
-			window.console.log("send cmd to docker servlet: \"" + input.value + "\"");
-			input.value = "";
-		}
-		
-		function _keyDown(e){
-			if (e.keyCode === 13) {
-				execute();
-				e.preventDefault();
-			}
-		}
-		startContainer().then(function(result) {
-			window.console.log(result);
-			input.addEventListener("keydown", function (e) { //$NON-NLS-0$
-				_keyDown(e);
+			
+		function stopContainer() {
+			return xhr("POST", "/docker", {  //$NON-NLS-1$ //$NON-NLS-0$
+				headers: {
+					"Orion-Version": "1", //$NON-NLS-1$ //$NON-NLS-0$
+					"Content-Type": "application/json; charset=UTF-8" //$NON-NLS-1$ //$NON-NLS-0$
+				},
+				data: JSON.stringify({
+					"dockerCmd": "stop"
+				}),
+				timeout: 15000,
+				handleAs: "json" //$NON-NLS-0$
 			});
-		}, function(error){
-			window.console.log(error);
+		}
+
+		var output = document.getElementById("dockerPage-output"); //$NON-NLS-0$
+		var input = document.getElementById("dockerCmd"); //$NON-NLS-0$
+		var button = document.getElementById("dockerConnect"); //$NON-NLS-0$
+		button.textContent="Connect";
+		button.addEventListener("click", function(e) {
+			if (!connected) {
+				//connect
+				startContainer().then(function(result) {
+					window.console.log(result);
+					input.addEventListener("keydown", function (e) { //$NON-NLS-0$
+						if (e.keyCode === 13) {
+							window.console.log("send cmd to docker servlet: \"" + input.value + "\"");
+							input.value = "";
+							e.preventDefault();
+						}
+					});
+					button.textContent="Disconnect";
+					connected = !connected;
+					input.disabled = !connected;
+				}, function(error){
+					window.console.log(error);
+				});
+			} else {
+				stopContainer().then(function(result) {
+					button.textContent="Connect";
+					connected = !connected;
+					input.disabled = !connected;
+				}, function(error){
+					window.console.log(error);
+				});
+			}
+			
 		});
 	});
 });
