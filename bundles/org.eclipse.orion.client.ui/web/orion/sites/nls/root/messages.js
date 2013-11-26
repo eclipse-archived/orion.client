2a78429c1ee58498f381dde7804640f6d9387a9b
Curtis Windatt <cwindatt@ca.ibm.com>
2013-11-26 16:31:14 -0600

    Bug 404403 - Difficult to determine how to delete site Bug 421368 - [sites] Sites pages looks goofy when no sites exist Refreshes the look and feel of the sites explorer for consistency. Actions are more discoverable.
    Change-Id: I253f5b45a0f03420e7fc53877cba71657d87c49b
    Signed-off-by: Curtis Windatt <cwindatt@ca.ibm.com>

--- bundles/org.eclipse.orion.client.ui/web/orion/sites/nls/root/messages.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/sites/nls/root/messages.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * @license
- * Copyright (c) 2012 IBM Corporation and others.
+ * Copyright (c) 2012, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials are made 
  * available under the terms of the Eclipse Public License v1.0 
  * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
@@ -73,5 +73,8 @@ define({
 	"Stopped" : "Stopped",
 	"Started at " : "Started at ",
 	"Sites": "Sites",
-	"Mappings": "Mappings"
+	"Mappings": "Mappings",
+	// The create button will be inserted between the pre and post text
+	"SitesExplorer.NoSitesPreText" : "You have no sites. Click",
+	"SitesExplorer.NoSitesPostText" : "to create a site."
 });
