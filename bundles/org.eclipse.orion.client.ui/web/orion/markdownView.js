7bfd10231c49d21ee0b96765eaaa358a88a508b6
Silenio Quarti <silenio_quarti@ca.ibm.com>
2013-12-02 12:27:43 -0500

    fix no contents in editor if markdown viewer opens first

--- bundles/org.eclipse.orion.client.ui/web/orion/markdownView.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/markdownView.js
@@ -112,6 +112,9 @@ define([
 			this._contentDiv.innerHTML = createMarked(this.getModel().getText());
 		},
 		setInput: function(title, message, contents, contentsSaved) {
+			if (!message && !contentsSaved) {
+				this._model.setText(contents);
+			}
 			this._contentDiv.innerHTML = createMarked(contents);
 			BaseEditor.prototype.setInput.call(this, title, message, contents, contentsSaved);
 		},
