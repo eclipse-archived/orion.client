c7ccf55484d27bdbae86267de792c137b1df1315
Mark Macdonald <mamacdon@gmail.com>
2013-12-09 14:37:09 -0500

    Bug 423624 - [eslint] Typed array globals should be recognized as built-in

--- bundles/org.eclipse.orion.client.javascript/web/eslint/tests/lib/rules/no-undef.js
+++ bundles/org.eclipse.orion.client.javascript/web/eslint/tests/lib/rules/no-undef.js
@@ -355,6 +355,16 @@ describe(RULE_ID, function() {
             assert.equal(messages[0].message, "'Array' is read only.");
             assert.include(messages[0].node.type, "Identifier");
         });
+
+        it("should not report a violation when Typed Array globals are used", function() {
+            var topic = "ArrayBuffer; DataView; Uint32Array;";
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+            assert.equal(messages.length, 0);
+        });
     });
 
 });
