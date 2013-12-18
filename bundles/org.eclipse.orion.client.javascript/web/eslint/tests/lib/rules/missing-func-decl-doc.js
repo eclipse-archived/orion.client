b6a884035afe3919cae3653076e5a7bf9b9a9a8d
Mike Rennie <Michael_Rennie@ca.ibm.com>
2013-12-18 14:22:02 -0600

    Bug 424128 - Provide missing JSDoc rule for eslint - member expression support and tests
    Change-Id: I1a88c112a4f7bcaf157336836ae2e54037563c44
    Signed-off-by: Mike Rennie <Michael_Rennie@ca.ibm.com>

---
+++ bundles/org.eclipse.orion.client.javascript/web/eslint/tests/lib/rules/missing-func-decl-doc.js
@@ -0,0 +1,145 @@
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html).
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*global describe it module require*/
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var assert = require("assert"),
+	eslint = require("../../../lib/eslint");
+
+//------------------------------------------------------------------------------
+// Constants
+//------------------------------------------------------------------------------
+
+var RULE_ID = "missing-func-decl-doc";
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+describe(RULE_ID, function() {
+	it("Should not flag missing function doc", function() {
+		var topic = "var v;\n/**foo*/function f() {};";
+
+		var config = { rules: {} };
+		config.rules[RULE_ID] = [1, 'decl'];
+
+		var messages = eslint.verify(topic, config);
+		assert.equal(messages[0].length, 0);
+	});
+	it("Should not flag missing function doc for excessive white space", function() {
+		var topic = "var v;\n/**foo*/\n\n\nfunction f() {};";
+
+		var config = { rules: {} };
+		config.rules[RULE_ID] = [1, 'decl'];
+
+		var messages = eslint.verify(topic, config);
+		assert.equal(messages[0].length, 0);
+	});
+	it("Should not flag missing fuction doc for line comment", function() {
+		var topic = "var v;\n//foo\nfunction f() {};";
+
+		var config = { rules: {} };
+		config.rules[RULE_ID] = [1, 'decl'];
+
+		var messages = eslint.verify(topic, config);
+		assert.equal(messages.length, 0);
+	});
+	it("Should not flag missing functioon doc for excessive space with line comment", function() {
+		var topic = "var v;\n//foo\n\n\n\nfunction f() {};";
+
+		var config = { rules: {} };
+		config.rules[RULE_ID] = [1, 'decl'];
+
+		var messages = eslint.verify(topic, config);
+		assert.equal(messages.length, 0);
+	});
+	it("Should not flag missing functioon doc for inner block comment", function() {
+		var topic = "var v;\n/***/function o() {/***/function f() {};};";
+
+		var config = { rules: {} };
+		config.rules[RULE_ID] = [1, 'decl'];
+
+		var messages = eslint.verify(topic, config);
+		assert.equal(messages.length, 0);
+	});
+	it("Should not flag missing functioon doc for excessive space with inner block comment", function() {
+		var topic = "var v;\n/***/function o() {/***/\n\n\n\nfunction f() {};};";
+
+		var config = { rules: {} };
+		config.rules[RULE_ID] = [1, 'decl'];
+
+		var messages = eslint.verify(topic, config);
+		assert.equal(messages.length, 0);
+	});
+	it("Should not flag missing functioon doc for inner line comment", function() {
+		var topic = "var v;\n/***/function o() {//foo\nfunction f() {};};";
+
+		var config = { rules: {} };
+		config.rules[RULE_ID] = [1, 'decl'];
+
+		var messages = eslint.verify(topic, config);
+		assert.equal(messages.length, 0);
+	});
+	it("Should not flag missing functioon doc for excessive space with inner line comment", function() {
+		var topic = "var v;\n/***/function o() {//foo\n\n\n\nfunction f() {};};";
+
+		var config = { rules: {} };
+		config.rules[RULE_ID] = [1, 'decl'];
+
+		var messages = eslint.verify(topic, config);
+		assert.equal(messages.length, 0);
+	});
+	describe("Found missing documentation problems", function() {
+		it("Should flag missing documentation for function f", function() {
+			var topic = "var foo;\nfunctionf() {};";
+
+			var config = { rules: {} };
+			config.rules[RULE_ID] = [1, 'decl'];
+
+			var messages = eslint.verify(topic, config);
+			assert.equal(messages.length, 1);
+			assert.equal(messages[0].ruleId, RULE_ID);
+			assert.equal(messages[0].message, "Missing documentation for function \'f\'");
+			assert.equal(messages[0].node.type, "Identifier");
+		});
+		it("Should find missing documentation problem for inner function", function() {
+			var topic = "var foo;\n/***/\nfunction o() {\nfunction f() {}; };";
+
+			var config = { rules: {} };
+			config.rules[RULE_ID] = [1, 'decl'];
+
+			var messages = eslint.verify(topic, config);
+			assert.equal(messages.length, 1);
+			assert.equal(messages[0].ruleId, RULE_ID);
+			assert.equal(messages[0].message, "Missing documentation for function \'f\'");
+			assert.equal(messages[0].node.type, "Identifier");
+		});
+		it("Should find missing doc problem for root node", function() {
+			/**
+			 * This test covers the Estraverse bug:
+			 * https://github.com/Constellation/estraverse/issues/20
+			 */
+			var topic = "/***/function f() {};";
+
+			var config = { rules: {} };
+			config.rules[RULE_ID] = [1, 'decl'];
+
+			var messages = eslint.verify(topic, config);
+			assert.equal(messages.length, 1);
+			assert.equal(messages[0].ruleId, RULE_ID);
+			assert.equal(messages[0].message, "'Missing documentation for function \'f\'");
+			assert.equal(messages[0].node.type, "Identifier");
+		});
+	});
+});
