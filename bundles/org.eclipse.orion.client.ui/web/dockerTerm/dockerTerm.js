3bcb8be50be733e2ce86fad983ab39fd3af1f2c2
ahunter <anthonyh@ca.ibm.com>
2014-01-09 15:25:06 -0500

    Bug 422288 Correct terminal constructor and format

--- bundles/org.eclipse.orion.client.ui/web/dockerTerm/dockerTerm.js
+++ bundles/org.eclipse.orion.client.ui/web/dockerTerm/dockerTerm.js
@@ -13,94 +13,94 @@
 /*global define window document*/
 
 define(["require", "orion/browserCompatibility", "orion/bootstrap", "orion/xhr", "orion/Deferred",
-    "orion/commandRegistry", "orion/fileClient", "orion/searchClient", "orion/globalCommands",
-    "orion/status", "orion/progress", "orion/operationsClient", "dockerTerm/term"],
+	"orion/commandRegistry", "orion/fileClient", "orion/searchClient", "orion/globalCommands",
+	"orion/status", "orion/progress", "orion/operationsClient", "dockerTerm/term"],
 
 function(require, mBrowserCompatibility, mBootstrap, xhr, Deferred, mCommandRegistry, mFileClient,
 mSearchClient, mGlobalCommands, mStatus, mProgress, mOperationsClient, terminal) {
 
-    var dockerTerminal = {
-        connect: function() {
-            return xhr("POST", "/docker/connect", { //$NON-NLS-1$ //$NON-NLS-0$
-                headers: {
-                    "Orion-Version": "1", //$NON-NLS-1$ //$NON-NLS-0$
-                },
-                timeout: 15000,
-                responseType: "text",
-                handleAs: "json" //$NON-NLS-0$
-            });
-        },
-        disconnect: function() {
-            return xhr("POST", "/docker/disconnect", { //$NON-NLS-1$ //$NON-NLS-0$
-                headers: {
-                    "Orion-Version": "1", //$NON-NLS-1$ //$NON-NLS-0$
-                },
-                timeout: 15000
-            });
-        }
-    };
+	var dockerTerminal = {
+		connect: function() {
+			return xhr("POST", "/docker/connect", { //$NON-NLS-1$ //$NON-NLS-0$
+				headers: {
+					"Orion-Version": "1", //$NON-NLS-1$ //$NON-NLS-0$
+				},
+				timeout: 15000,
+				responseType: "text",
+				handleAs: "json" //$NON-NLS-0$
+			});
+		},
+		disconnect: function() {
+			return xhr("POST", "/docker/disconnect", { //$NON-NLS-1$ //$NON-NLS-0$
+				headers: {
+					"Orion-Version": "1", //$NON-NLS-1$ //$NON-NLS-0$
+				},
+				timeout: 15000
+			});
+		}
+	};
 
-    mBootstrap.startup().then(function(core) {
-        var serviceRegistry = core.serviceRegistry;
-        var preferences = core.preferences;
+	mBootstrap.startup().then(function(core) {
+		var serviceRegistry = core.serviceRegistry;
+		var preferences = core.preferences;
 
-        var commandRegistry = new mCommandRegistry.CommandRegistry({});
-        var fileClient = new mFileClient.FileClient(serviceRegistry);
-        var searcher = new mSearchClient.Searcher({
-            serviceRegistry: serviceRegistry,
-            commandService: commandRegistry,
-            fileService: fileClient
-        });
-        var operationsClient = new mOperationsClient.OperationsClient(serviceRegistry);
-        var statusService = new mStatus.StatusReportingService(serviceRegistry, operationsClient, "statusPane", "notifications", "notificationArea"); //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
-        new mProgress.ProgressService(serviceRegistry, operationsClient, commandRegistry);
-        mGlobalCommands.generateBanner("orion-dockerTerminalPage", serviceRegistry, commandRegistry, preferences, searcher); //$NON-NLS-0$
-        mGlobalCommands.setPageTarget({
-            task: "Docker Terminal",
-            serviceRegistry: serviceRegistry,
-            commandService: commandRegistry
-        });
+		var commandRegistry = new mCommandRegistry.CommandRegistry({});
+		var fileClient = new mFileClient.FileClient(serviceRegistry);
+		var searcher = new mSearchClient.Searcher({
+			serviceRegistry: serviceRegistry,
+			commandService: commandRegistry,
+			fileService: fileClient
+		});
+		var operationsClient = new mOperationsClient.OperationsClient(serviceRegistry);
+		var statusService = new mStatus.StatusReportingService(serviceRegistry, operationsClient, "statusPane", "notifications", "notificationArea"); //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		new mProgress.ProgressService(serviceRegistry, operationsClient, commandRegistry);
+		mGlobalCommands.generateBanner("orion-dockerTerminalPage", serviceRegistry, commandRegistry, preferences, searcher); //$NON-NLS-0$
+		mGlobalCommands.setPageTarget({
+			task: "Docker Terminal",
+			serviceRegistry: serviceRegistry,
+			commandService: commandRegistry
+		});
 
-        var term;
-        var websocket;
+		var term;
+		var websocket;
 
-        var connect = function() {
-            dockerTerminal.connect().then(function(result) {
-                var jsonObject = JSON.parse(result.responseText);
-                var attachWsURI = jsonObject.attachWsURI;
-                term.reset();
-                websocket = new WebSocket(attachWsURI);
-                websocket.onopen = function(evt) {
-                    onOpen(evt);
-                };
-                websocket.onclose = function(evt) {
-                    onClose(evt);
-                };
-                websocket.onmessage = function(evt) {
-                    onMessage(evt);
-                };
-                websocket.onerror = function(evt) {
-                    onError(evt);
-                };
-                term.on('data', function(data) {
-                    websocket.send(data);
-                });
+		var connect = function() {
+			dockerTerminal.connect().then(function(result) {
+				var jsonObject = JSON.parse(result.responseText);
+				var attachWsURI = jsonObject.attachWsURI;
+				term.reset();
+				websocket = new WebSocket(attachWsURI);
+				websocket.onopen = function(evt) {
+					onOpen(evt);
+				};
+				websocket.onclose = function(evt) {
+					onClose(evt);
+				};
+				websocket.onmessage = function(evt) {
+					onMessage(evt);
+				};
+				websocket.onerror = function(evt) {
+					onError(evt);
+				};
+				term.on('data', function(data) {
+					websocket.send(data);
+				});
 
-                function onOpen(evt) {
-                    websocket.send("\r");
-                }
+				function onOpen(evt) {
+					websocket.send("\r");
+				}
 
-                function onClose(evt) {}
+				function onClose(evt) {}
 
-                function onMessage(evt) {
-                    term.write(evt.data);
-                }
+				function onMessage(evt) {
+					term.write(evt.data);
+				}
 
-                function onError(evt) {
-                    window.console.log(evt.data);
-                }
-                term.focus();
-            }, function(error) {
+				function onError(evt) {
+					window.console.log(evt.data);
+				}
+				term.focus();
+			}, function(error) {
 				var display = {};
 				display.Severity = "Error"; //$NON-NLS-0$
 				display.HTML = false;
@@ -111,34 +111,39 @@ mSearchClient, mGlobalCommands, mStatus, mProgress, mOperationsClient, terminal)
 					display.Message = error.message;
 				}
 				statusService.setProgressResult(display); //$NON-NLS-0$
-            });
-        };
+			});
+		};
 
-        var disconnect = function() {
-            dockerTerminal.disconnect().then(function(result) {
-                websocket.close();
-                term.reset();
-            }, function(error) {
-                window.console.log(error);
-            });
-        };
+		var disconnect = function() {
+			dockerTerminal.disconnect().then(function(result) {
+				websocket.close();
+				term.reset();
+			}, function(error) {
+				window.console.log(error);
+			});
+		};
 
-        var onHashChange = function() {
-            connect();
-        };
-        window.addEventListener("hashchange", onHashChange);
+		var onHashChange = function() {
+			connect();
+		};
+		window.addEventListener("hashchange", onHashChange);
 
-        window.onbeforeunload = function() {
-            disconnect();
-        };
+		window.onbeforeunload = function() {
+			disconnect();
+		};
 
-        // Initialize
-        (function() {
-            term = new Terminal();
-            term.open(document.getElementById("terminal"));
+		// Initialize
+		(function() {
+			term = new Terminal({
+				cols: 80,
+				rows: 24,
+				useStyle: true,
+				screenKeys: true
+			});
+			term.open(document.getElementById("terminal"));
 
-            onHashChange();
-        }());
+			onHashChange();
+		}());
 
-    });
+	});
 });
