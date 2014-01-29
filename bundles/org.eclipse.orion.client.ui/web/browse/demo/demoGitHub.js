7205274363dc3d6fdbc58c41e7cbc796d339ca43
libing wang <libingw@ca.ibm.com>
2014-01-29 11:37:12 -0500

    Bug 426917 - Readonly file widget: Need tag css classes for markdown viewer.

--- bundles/org.eclipse.orion.client.ui/web/browse/demo/demoGitHub.js
+++ bundles/org.eclipse.orion.client.ui/web/browse/demo/demoGitHub.js
@@ -10,21 +10,6 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 /*global URL define window console eclipse orion*/
-/*
-define([
-	'orion/bootstrap', 
-	'orion/fileClient',
-	'orion/widgets/browse/fileBrowser'
-], function(mBootstrap, mFileClient, mFileBrowser) {
-	mBootstrap.startup().then(function(core) {
-		var fBrowser = new mFileBrowser.FileBrowser({
-			parent: "fileBrowser", 
-			//maxEditorHeight: 800,
-			fileClient: new mFileClient.FileClient(core.serviceRegistry)
-		}); 
-	});
-});
-*/
 define(['orion/widgets/browse/fileBrowser', 'orion/serviceregistry', 'orion/pluginregistry', 'orion/URL-shim'],
 function(mFileBrowser, mServiceRegistry, mPluginRegistry) {
 	var pluginURL = new URL("../../plugins/GitHubFilePlugin.html?repo=https://github.com/eclipse/orion.client.git", window.location.href);
