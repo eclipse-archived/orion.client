fc7b93dad19454d45bd6aee9d00fb8ad84cfd9df
Mark Macdonald <mamacdon@gmail.com>
2014-02-18 10:46:21 -0500

    [Bug 428446] 'All Git Repositories' link should always be shown

--- bundles/org.eclipse.orion.client.git/web/git/plugins/gitPlugin.js
+++ bundles/org.eclipse.orion.client.git/web/git/plugins/gitPlugin.js
@@ -42,7 +42,6 @@ function(PluginProvider, xhr, mServiceregistry, mGitClient, mSshTools, i18nUtil,
 		id: "orion.git.repositories",
 		nls: "git/nls/gitmessages",
 		category: "git",
-		"default": true,
 		order: 110, // Bottom
 		uriTemplate: "{+OrionHome}/git/git-repository.html#"
 	});
