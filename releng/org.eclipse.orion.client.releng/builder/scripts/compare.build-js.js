c85c1e153c65aad3a16c9241a9939229925bb6b4
Libing Wang <libingw@ca.ibm.com>
2014-03-19 17:11:12 -0400

    Bug 430734 - Orion build script to build compare widget automatically.

---
+++ releng/org.eclipse.orion.client.releng/builder/scripts/compare.build-js.js
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+ 
+// optimization script to concat/minify the Orion editor javascript code
+ 
+({
+    baseUrl: '.',
+	closure: {
+		CompilerOptions: {
+			languageIn: Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode.valueOf(Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode, "ECMASCRIPT5")
+		},
+		CompilationLevel: 'SIMPLE_OPTIMIZATIONS',
+		loggingLevel: 'WARNING'
+	},
+	paths: {
+        i18n: 'requirejs/i18n',
+        text: 'requirejs/text',
+        "orion/i18n": "compare/builder/i18n"
+ 	},
+	name: "compare/builder/compare",
+	preserveLicenseComments: false,
+	uglify: {
+		ascii_only: true
+	},
+	wrap: {
+		start: "/* orion compare */ ", //start cannot be empty
+		end: " define(['compare/builder/compare'], function(compare) {return compare;});"
+	}
+})
