10b301f3be88d14c03a06476f2c5927258a581f1
Mark Macdonald <mamacdon@gmail.com>
2014-04-03 15:52:20 -0400

    Node: use automatic prepublish hook to build the Node server
    * Minification now happens automatically during `npm publish` or `npm pack`.
    * Move build to Grunt. Remove crummy build scripts build.js, make-publish.sh
    * Generate source maps.
    * Separate .npmignore from .gitignore

---
+++ modules/orionode/Gruntfile.js
@@ -0,0 +1,168 @@
+/*jslint node:true*/
+var _path = require("path"),
+    utilFactory = require("./build/utils");
+module.exports = function(grunt) {
+	var util = utilFactory(grunt),
+	    BADDIR = "This Gruntfile must be run from the modules/orionode folder in the Orion client repository.",
+	    SOURCE_GLOB = ["**", "!**/node_modules/**", "!**/built/**", "!**/builder/**", "!**/target/**"],
+	    // All paths here are relative to Gruntfile.js
+	    configPath = "../../releng/org.eclipse.orion.client.releng/builder/scripts/orion.build.js",
+	    clientPath = "../../",
+	    staging = "build/.temp/",
+	    optimized = "build/.temp-optimized/";
+
+	var orionBuildConfig = util.loadBuildConfig(configPath),
+	    bundles = util.parseBundles(orionBuildConfig, {
+			buildDirectory: staging,
+			orionClient: clientPath
+		});
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+		clientPath: clientPath,
+		configPath: configPath,
+		staging: staging,
+		optimized: optimized,
+		nodeBuildConfig: util.filterBuildConfig(orionBuildConfig, "<% requirejsExcludeModules %>"),
+		checkDirs: {
+			orion: {
+				src: ["<%= clientPath %>/bundles", "<%= configPath %>"],
+				options: {
+					expand: true
+				}
+			}
+		},
+		copy: {
+			// Copy each client {bundle} to lib/orion.client/bundles/{bundle}
+			orionclient: {
+				files: bundles.map(function(bundle) {
+					return {
+						expand: true,
+						cwd: bundle.path,
+						src: SOURCE_GLOB,
+						dest: "lib/orion.client/bundles/" + bundle.name + "/"
+					};
+				})
+			},
+			// Copy each client bundle's web folder to staging dir for optimization
+			stage: {
+				files: bundles.map(function(bundle) {
+					return {
+						expand: true,
+						cwd: bundle.web,
+						src: SOURCE_GLOB,
+						dest: staging
+					};
+				}).concat([
+					// Copy orionode.client last since it must overwrite some files
+					{
+						expand: true,
+						cwd: "lib/orionode.client/**",
+						src: SOURCE_GLOB,
+						dest: staging
+					}
+				])
+			},
+			// Copy optimized js files, source maps, and css, back to the bundles they originated from in lib/
+			unstage: {
+				files: [{
+					expand: true,
+					cwd: optimized,
+					dest: "lib/",
+					src: ["**/*.js", "**/*.css", "**/*.html", "**/*.map"], // includes .src.js
+					rename: function(dest, src) {
+						// Determine the actual destination. This is either a bundle in lib/orion.client/bundles/
+						// or the Orionode client code lib/orionode.client/.
+						var bundlefile, match;
+						if ((match = /(.*)(\.src\.js|\.map)$/.exec(src)) != null) {
+							// Source map file; use the associated .js file to decide where we go
+							bundlefile = match[1];
+						} else {
+							bundlefile = src;
+						}
+						var newDest;
+						bundles.some(function(bundle) {
+							if (grunt.file.exists(_path.join(bundle.web, bundlefile))) {
+								// This file originated from bundle
+								newDest = _path.join(dest, "orion.client/bundles/", bundle.name, "web/", src);
+								return true;
+							}
+						});
+						// Check orionode.client last, since it overrides orion.client bundles
+						if (grunt.file.exists(_path.join("lib/orionode.client/", bundlefile)))
+							newDest = _path.join(dest, "orionode.client/", src);
+
+						if (!newDest)
+							grunt.verbose.write("Could not determine originating bundle for " + src);
+						return newDest;
+					}
+				}]
+			}
+		},
+		requirejs: {
+			compile: {} // .options is set later
+		},
+		"string-replace": {
+			// Ensure optimized files use the minified copy of requirejs
+			requiremin: {
+				files: [{
+					expand: true,
+					cwd: optimized,
+					dest: optimized,
+					src: ["**/*.js", "!**/*.src.js", "**/*.html", "!**/node_modules/**"]
+				}],
+				options: {
+					replacements: [{
+						pattern: "requirejs/require.js",
+						replacement: "requirejs/require.min.js"
+					}]
+				}
+			},
+			orionclient: {
+				files: {
+					"index.js": "index.js"
+				},
+				options: {
+					replacements: [{
+						pattern: /(ORION_CLIENT.+)['""]\.\.\/\.\.\/['"]/,
+						replacement: "$1'./lib/orion.client/'"
+					}]
+				}
+			}
+		},
+		simplemocha: {
+			options: {
+				reporter: "dot"
+			},
+			all: { src: "test/*.js" }
+		}
+	});
+
+	// Dynamic configuration
+	grunt.config("requirejs.compile.options", util.mixin(grunt.config("nodeBuildConfig"), {
+		optimize: "uglify2",
+		appDir: staging,
+		baseUrl: "./",
+		dir: optimized // TODO <% optimized %> ?
+	}));
+
+	// Task definitions
+	grunt.loadNpmTasks("grunt-contrib-copy");
+	grunt.loadNpmTasks("grunt-contrib-requirejs");
+	grunt.loadNpmTasks("grunt-simple-mocha");
+	grunt.loadNpmTasks("grunt-string-replace");
+
+	grunt.registerMultiTask("checkDirs", "Check files/dirs exist", function() {
+		this.filesSrc.forEach(function(filepath) {
+			grunt.verbose.write("Checking existence of " + filepath + "...");
+			if (grunt.file.exists(filepath))
+				grunt.verbose.ok();
+			else grunt.fatal(BADDIR);
+		});
+	});
+
+	grunt.registerTask("test", ["simplemocha"]);
+	grunt.registerTask("optimize", ["copy:stage", "requirejs", "string-replace", "copy:unstage"]);
+	grunt.registerTask("default", ["checkDirs", "copy:orionclient", "optimize", "test"]);
+	grunt.registerTask("nomin",   ["checkDirs", "copy:orionclient", "string-replace:orionclient", "test"]);
+};
---
+++ modules/orionode/build/utils.js
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2014 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ * 
+ * Contributors: IBM Corporation - initial API and implementation
+ ******************************************************************************/
+/*
+ * Helpers for the grunt build script.
+ */
+/*jslint node:true*/
+var _path = require("path");
+module.exports = function(grunt) {
+	var self = {};
+
+	function replaceProperties(str, props) {
+		props = props || {};
+		return str.replace(/\$\{([^}]*)\}/g, function(match, propertyName) {
+			if (props[propertyName] === undefined)
+				throw new Error("Property not found, cannot replace: " + propertyName)
+			return props[propertyName];
+		});
+	}
+
+	self.loadBuildConfig = function(configPath) {
+		return (new Function("var bc = " + grunt.file.read(configPath) + "; return bc;"))();
+	};
+
+	/**
+	 * @returns {Bundle[]} where Bundle is {{ name: string, path: string, web: string }}
+	 */
+	self.parseBundles = function(buildConfig, replacements) {
+		return buildConfig.bundles.map(function(path) {
+			path = replaceProperties(path, replacements);
+			return {
+				name: _path.basename(path),
+				path: path,
+				web: _path.join(path, "web")
+			};
+		});
+	};
+
+	/**
+	 * @returns {Object} A copy of `config` with the `excludeModules` removed from its `modules` section.
+	 */
+	self.filterBuildConfig = function(config, excludeModules) {
+		excludeModules = excludeModules || [];
+		var clone = JSON.parse(JSON.stringify(config));
+		clone.modules = clone.modules && clone.modules.filter(function(module) {
+			var baseName = module.name.split("/").pop();
+			return excludeModules.indexOf(baseName) === -1;
+		});
+		return clone;
+	};
+
+	self.mixin = function(target/*, source..*/) {
+		if (!target || typeof target !== "object")
+			throw new Error("target is required");
+		for (var j = 1; j < arguments.length; j++) {
+			var source = arguments[j];
+			for (var key in source)
+				if (Object.prototype.hasOwnProperty.call(source, key))
+					target[key] = source[key];
+		}
+		return target;
+	};
+
+	return self;
+};
