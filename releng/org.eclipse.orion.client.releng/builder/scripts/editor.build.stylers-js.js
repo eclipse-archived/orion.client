2feaf2b3fcabe2430139cb54d3b77ffeb5ce90ae
Silenio Quarti <silenio_quarti@ca.ibm.com>
2014-04-10 14:13:09 -0400

    building YAML styler

---
+++ releng/org.eclipse.orion.client.releng/builder/scripts/editor.build.stylers-js.js
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+ 
+// optimization script to concat/minify the Orion editor javascript code
+ 
+({
+	baseUrl: '.',
+	closure: {
+		CompilerOptions: {
+			languageIn: Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode.valueOf(Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode, "ECMASCRIPT5")
+		},
+		CompilationLevel: 'SIMPLE_OPTIMIZATIONS',
+		loggingLevel: 'WARNING'
+	},
+	preserveLicenseComments: false,
+	uglify: {
+		ascii_only: true
+	}
+})
