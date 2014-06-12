bd5d0000c30942bfbef8f2e8997443c4b274aedf
Aidan Redpath <aidanr@ca.ibm.com>
2014-06-12 10:14:08 -0400

    Bug 400479 Externalized "Linked Account String"
    "Linked Account" string is externalized. I created a new messages file so
    that all other strings in mixloginstatic can be externalized too in the
    same location.
    
    Change-Id: I847a8599d77ae41a26a2084059e6e6ba59e756fc
    Signed-off-by: Aidan Redpath <aidanr@ca.ibm.com>

---
+++ bundles/org.eclipse.orion.client.ui/web/mixloginstatic/nls/root/messages.js
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2012, 2014 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ * 
+ ******************************************************************************/
+
+//NLS_CHARSET=UTF-8
+
+define({
+	"AddExternalAccount" : "Add external account:"	
+});
