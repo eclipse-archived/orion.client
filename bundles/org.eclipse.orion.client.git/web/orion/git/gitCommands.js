ffffc4a9cee9979648db617b69ae2a3e7666677e
Bartosz Grabski <bartosz.grabski@pl.ibm.com>
2014-06-12 18:15:21 +0200

    Bug 436548 - Git Status Fetch button should report unauthorized post (Please login again), but does nothing
    Change-Id: Ib663695366ce12f0e8d98abef9b0151c3bba27cf
    Signed-off-by: Bartosz Grabski <bartosz.grabski@pl.ibm.com>

--- bundles/org.eclipse.orion.client.git/web/orion/git/gitCommands.js
+++ bundles/org.eclipse.orion.client.git/web/orion/git/gitCommands.js
@@ -14,9 +14,9 @@
 
 define(['i18n!git/nls/gitmessages', 'require', 'orion/Deferred', 'orion/i18nUtil', 'orion/webui/littlelib', 'orion/commands', 'orion/commandRegistry', 'orion/git/util', 'orion/compare/compareUtils', 'orion/git/gitPreferenceStorage', 'orion/git/gitConfigPreference',
         'orion/git/widgets/ConfirmPushDialog', 'orion/git/widgets/RemotePrompterDialog', 'orion/git/widgets/ReviewRequestDialog', 'orion/git/widgets/CloneGitRepositoryDialog', 
-        'orion/git/widgets/GitCredentialsDialog', 'orion/git/widgets/OpenCommitDialog', 'orion/git/widgets/CommitDialog', 'orion/git/widgets/ApplyPatchDialog', 'orion/URL-shim', 'orion/PageLinks', 'orion/URITemplate','orion/git/logic/gitPush','orion/git/logic/gitCommit', 'orion/objects'], 
+        'orion/git/widgets/GitCredentialsDialog', 'orion/git/widgets/OpenCommitDialog', 'orion/git/widgets/CommitDialog', 'orion/git/widgets/ApplyPatchDialog', 'orion/URL-shim', 'orion/PageLinks', 'orion/URITemplate','orion/git/logic/gitPush','orion/git/logic/gitCommit', 'orion/objects', 'orion/git/logic/gitCommon'], 
         function(messages, require, Deferred, i18nUtil, lib, mCommands, mCommandRegistry, mGitUtil, mCompareUtils, GitPreferenceStorage, GitConfigPreference, mConfirmPush, mRemotePrompter,
-        mReviewRequest, mCloneGitRepository, mGitCredentials, mOpenCommit, mCommit, mApplyPatch, _, PageLinks, URITemplate, mGitPushLogic, mGitCommitLogic, objects) {
+        mReviewRequest, mCloneGitRepository, mGitCredentials, mOpenCommit, mCommit, mApplyPatch, _, PageLinks, URITemplate, mGitPushLogic, mGitCommitLogic, objects, mGitCommonLogic) {
 
 /**
  * @namespace The global container for eclipse APIs.
@@ -908,7 +908,7 @@ var exports = {};
 						statusService.createProgressMonitor(deferred, messages["Fetching remote: "] + name);
 						deferred.then(
 							function(jsonData) {
-								exports.handleGitServiceResponse(jsonData, serviceRegistry, 
+								mGitCommonLogic.handleGitServiceResponse(jsonData, serviceRegistry, 
 									function() {
 										d.resolve();
 									}, function (jsonData) {
@@ -916,7 +916,7 @@ var exports = {};
 									}
 								);
 							}, function(jsonData) {
-								exports.handleGitServiceResponse(jsonData, serviceRegistry, 
+								mGitCommonLogic.handleGitServiceResponse(jsonData, serviceRegistry, 
 									function() {
 										d.resolve();
 									}, function (jsonData) {
@@ -1036,13 +1036,16 @@ var exports = {};
 						}else{
 							display.Message = result.error.message;
 						}
-						display.HTML = true;
-						display.Message ="<span>" + display.Message +"</span>"; //$NON-NLS-1$ //$NON-NLS-0$
+						displayErrorOnStatus(result); //$NON-NLS-1$ //$NON-NLS-0$
 					}
 
 					progressService.setProgressResult(display);
 					explorer.changedItem(item);
 				}, function (error, ioArgs) {
+					if (!ioArgs) {
+						displayErrorOnStatus(error);
+						return;
+					}
 					var display = {};
 					display.Severity = "Error"; //$NON-NLS-0$
 					display.HTML = true;
@@ -1405,11 +1408,7 @@ var exports = {};
 						explorer.changedItem(item);
 						progressService.setProgressResult(display);
 					}, function (error){
-						var display = {};
-						display.Severity = "Error"; //$NON-NLS-0$
-						display.HTML = false;
-						display.Message = error.message;
-						progressService.setProgressResult(display);
+						displayErrorOnStatus(error);
 					}
 				);
 			}
