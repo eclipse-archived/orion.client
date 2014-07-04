9bba8a8fa13a9264aeac614138aacb3bad77d1cd
Mark Macdonald <mamacdon@gmail.com>
2014-07-03 22:46:43 -0400

    CSSlint path missing from requirejs build config

--- releng/org.eclipse.orion.client.releng/builder/scripts/orion.build.js
+++ releng/org.eclipse.orion.client.releng/builder/scripts/orion.build.js
@@ -1,9 +1,9 @@
 /*******************************************************************************
  * Copyright (c) 2013, 2014 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials are made 
- * available under the terms of the Eclipse Public License v1.0 
- * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
- * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html).
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
@@ -18,7 +18,7 @@
         CompilerOptions: {
             // Need this check to avoid TypeError when eval'ing this buildfile without the Closure jars loaded, or from Node.
             languageIn: (typeof Packages === "object" && typeof Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode.valueOf === "function")
-                ? Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode.valueOf(Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode, "ECMASCRIPT5") 
+                ? Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode.valueOf(Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode, "ECMASCRIPT5")
                 : "ECMASCRIPT5"
         },
         CompilationLevel: 'SIMPLE_OPTIMIZATIONS',
@@ -43,6 +43,7 @@
         esprima: 'esprima/esprima',
         estraverse: 'estraverse/estraverse',
         escope: 'escope/escope',
+        csslint: 'csslint/csslint'
     },
     packages: [
         {
