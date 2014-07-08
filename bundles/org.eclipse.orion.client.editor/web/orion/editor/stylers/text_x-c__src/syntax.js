f01cb0888666eb52577197c2de80de1c7b42182f
Grant Gayed <grant_gayed@ca.ibm.com>
2014-07-08 13:22:14 -0400

    implement syntax styling for Objective-C

--- bundles/org.eclipse.orion.client.editor/web/orion/editor/stylers/text_x-c__src/syntax.js
+++ bundles/org.eclipse.orion.client.editor/web/orion/editor/stylers/text_x-c__src/syntax.js
@@ -35,7 +35,8 @@ define("orion/editor/stylers/text_x-c__src/syntax", ["orion/editor/stylers/lib/s
 		"union", "unsigned", "using", //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
 		"virtual", "void", "volatile", //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
 		"wchar_t", "while", //$NON-NLS-1$ //$NON-NLS-0$
-		"xor_eq", "xor" //$NON-NLS-1$ //$NON-NLS-0$
+		"xor_eq", "xor", //$NON-NLS-1$ //$NON-NLS-0$
+		"_Bool", "_Complex", "_Imaginary" //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
 	];
 
 	var directives = [
--- bundles/org.eclipse.orion.client.editor/web/orion/editor/stylers/text_x-csrc/syntax.js
+++ bundles/org.eclipse.orion.client.editor/web/orion/editor/stylers/text_x-csrc/syntax.js
@@ -20,14 +20,15 @@ define("orion/editor/stylers/text_x-csrc/syntax", ["orion/editor/stylers/lib/syn
 		"else", "enum", "extern", //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
 		"float", "for", //$NON-NLS-1$ //$NON-NLS-0$
 		"goto", //$NON-NLS-0$
-		"if", "int", //$NON-NLS-1$ //$NON-NLS-0$
+		"if", "inline", "int", //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
 		"long", //$NON-NLS-0$
 		"register", "return", //$NON-NLS-1$ //$NON-NLS-0$
 		"short", "signed", "sizeof", "static", "struct", "switch", //$NON-NLS-5$ //$NON-NLS-4$ //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
 		"typedef", //$NON-NLS-0$
 		"union", "unsigned", //$NON-NLS-1$ //$NON-NLS-0$
 		"void", "volatile", //$NON-NLS-1$ //$NON-NLS-0$
-		"while" //$NON-NLS-0$
+		"while", //$NON-NLS-0$
+		"_Bool", "_Complex", "_Imaginary" //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
 	];
 
 	var directives = [
