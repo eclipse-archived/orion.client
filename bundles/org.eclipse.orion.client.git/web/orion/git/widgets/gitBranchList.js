b82b6f1629898ac2ff911b023a3de81c7c9cb917
Silenio Quarti <silenio_quarti@ca.ibm.com>
2014-07-10 16:39:58 -0400

    do not show description for tags

--- bundles/org.eclipse.orion.client.git/web/orion/git/widgets/gitBranchList.js
+++ bundles/org.eclipse.orion.client.git/web/orion/git/widgets/gitBranchList.js
@@ -274,8 +274,8 @@ define([
 					} else if (item.parent.Type === "TagRoot") { //$NON-NLS-0$
 						createExpand();
 						commit = item.Commit.Children[0];
-						description = util.trimCommitMessage(commit.Message);
-						subDescription = i18nUtil.formatMessage(messages["authored by 0 (1) on 2"], commit.AuthorName, commit.AuthorEmail, new Date(commit.Time).toLocaleString()); //$NON-NLS-0$
+//						description = util.trimCommitMessage(commit.Message);
+//						subDescription = i18nUtil.formatMessage(messages["authored by 0 (1) on 2"], commit.AuthorName, commit.AuthorEmail, new Date(commit.Time).toLocaleString()); //$NON-NLS-0$
 						actionsID = "tagActionsArea"; //$NON-NLS-0$
 					} else if (item.parent.Type === "RemoteRoot") { //$NON-NLS-0$
 						createExpand();
