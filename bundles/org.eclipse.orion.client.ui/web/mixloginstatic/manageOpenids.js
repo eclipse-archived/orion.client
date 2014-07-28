95723917989706d0a83cfb6d0bf51ab082e7c4cd
Aidan Redpath <aidanr@ca.ibm.com>
2014-07-28 14:35:34 -0400

    Added icons to use test oauth changes
    Created a new icon for google plus as well as adding some new javascript
    functions to properly handle oauth requests and responses.
    
    Signed-off-by: Aidan Redpath <aidanr@ca.ibm.com>

--- bundles/org.eclipse.orion.client.ui/web/mixloginstatic/manageOpenids.js
+++ bundles/org.eclipse.orion.client.ui/web/mixloginstatic/manageOpenids.js
@@ -40,6 +40,30 @@ define(["i18n!orion/mixloginstatic/nls/messages", "orion/xhr", "orion/webui/litt
 			});
 		}
 	}
+	function removeOAuth(oauth) {
+		if (confirm("Are you sure you want to remove " + oauth + " from the list of your external accounts?")) {
+			var oauths = jsonData.properties.oauth.split('\n');
+			var newoauths = "";
+			for (var i = 0; i < oauths.length; i++) {
+				if (oauths[i] !== oauth) {
+					newoauths += (oauths[i] + '\n');
+				}
+			}
+			jsonData.properties.oauth = newoauths;
+
+			xhr("PUT", jsonData.Location, { //$NON-NLS-0$
+				data: JSON.stringify(jsonData),
+				headers: {
+					"Orion-Version": "1"
+				},
+				timeout: 15000
+			}).then(function(xhrResult) {
+				loadUserData(jsonData.Location);
+			}, function(xhrResult) {
+				console.error(xhrResult.error);
+			});
+		}
+	}
 	
 	loadAttachedOpenIds = function(userjsonData) {
 		jsonData = userjsonData;
@@ -51,16 +75,22 @@ define(["i18n!orion/mixloginstatic/nls/messages", "orion/xhr", "orion/webui/litt
 		var table = document.createElement("table"); //$NON-NLS-0$
 		table.classList.add("manageOpenIdsTable"); //$NON-NLS-0$
 		list.appendChild(table); //$NON-NLS-0$
-		if (jsonData.properties && jsonData.properties.openid) {
+		if (jsonData.properties && (jsonData.properties.openid || jsonData.properties.oauth)) {
 	
 			var openids = jsonData.properties.openid ? jsonData.properties.openid.split('\n') : []; //$NON-NLS-0$
+			var oauths = jsonData.properties.oauth ? jsonData.properties.oauth.split('\n') : []; //$NON-NLS-0$
 			for (var i = openids.length - 1; i >= 0; i--) {
 				if (openids[i] === "") {
 					openids.splice(i, 1);
 				}
 			}
+			for (var i = oauths.length - 1; i >= 0; i--) {
+				if (oauths[i] === "") {
+					oauths.splice(i, 1);
+				}
+			}
 
-			if (openids.length) {
+			if (openids.length || oauths.length) {
 				var thead = document.createElement("thead"); //$NON-NLS-0$
 				thead.classList.add("navTableHeading"); //$NON-NLS-0$
 				table.appendChild(thead);
@@ -75,38 +105,46 @@ define(["i18n!orion/mixloginstatic/nls/messages", "orion/xhr", "orion/webui/litt
 
 			for (var i = 0; i < openids.length; i++) {
 				var openid = openids[i];
-				var tr = document.createElement("tr"); //$NON-NLS-0$
-				tr.classList.add(i % 2 === 0 ? "lightTreeTableRow" : "darkTreeTableRow");  //$NON-NLS-1$ //$NON-NLS-0$
-				tr.classList.add("manageOpenIdRow"); //$NON-NLS-0$
-				tr.style.verticalAlign = "baseline"; //$NON-NLS-0$
-				table.appendChild(tr);
-
-				var td = document.createElement("td"); //$NON-NLS-0$
-				td.classList.add("navColumn"); //$NON-NLS-0$
-				tr.appendChild(td);
-				var span = document.createElement("span"); //$NON-NLS-0$
-				span.title = openid;
-				td.appendChild(span);
-				var textNode = document.createTextNode(openid.length > 70 ? (openid.substring(0, 65) + "...") : openid);
-				span.appendChild(textNode);
-
-				td = document.createElement("td"); //$NON-NLS-0$
-				td.classList.add("navColumn"); //$NON-NLS-0$
-				tr.appendChild(td);
-				var removeLink = document.createElement("a"); //$NON-NLS-0$
-				removeLink.classList.add("removeOpenId"); //$NON-NLS-0$
-				removeLink.id = "remlink" + i; //$NON-NLS-0$
-				removeLink.innerHTML = "Remove";
-				removeLink.style.visibility = "hidden"; //$NON-NLS-0$
-				removeLink.title = "Remove " + openid;
-				td.appendChild(removeLink);
-
-				removeLink.addEventListener("click", function(openid) { //$NON-NLS-0$
-					removeOpenId(openid);
-				}.bind(this, openid));
+				addAuthenticationEntry(openid, removeOpenId);
+			}
+			for (var i = 0; i < oauths.length; i++) {
+				var oauth = oauths[i];
+				addAuthenticationEntry(oauth, i, table, removeOAuth);
 			}
 		}
 	};
+	
+	function addAuthenticationEntry(openid, i, table, removeFunction){
+		var tr = document.createElement("tr"); //$NON-NLS-0$
+		tr.classList.add(i % 2 === 0 ? "lightTreeTableRow" : "darkTreeTableRow");  //$NON-NLS-1$ //$NON-NLS-0$
+		tr.classList.add("manageOpenIdRow"); //$NON-NLS-0$
+		tr.style.verticalAlign = "baseline"; //$NON-NLS-0$
+		table.appendChild(tr);
+
+		var td = document.createElement("td"); //$NON-NLS-0$
+		td.classList.add("navColumn"); //$NON-NLS-0$
+		tr.appendChild(td);
+		var span = document.createElement("span"); //$NON-NLS-0$
+		span.title = openid;
+		td.appendChild(span);
+		var textNode = document.createTextNode(openid.length > 70 ? (openid.substring(0, 65) + "...") : openid);
+		span.appendChild(textNode);
+
+		td = document.createElement("td"); //$NON-NLS-0$
+		td.classList.add("navColumn"); //$NON-NLS-0$
+		tr.appendChild(td);
+		var removeLink = document.createElement("a"); //$NON-NLS-0$
+		removeLink.classList.add("removeOpenId"); //$NON-NLS-0$
+		removeLink.id = "remlink" + i; //$NON-NLS-0$
+		removeLink.innerHTML = "Remove";
+		removeLink.style.visibility = "hidden"; //$NON-NLS-0$
+		removeLink.title = "Remove " + openid;
+		td.appendChild(removeLink);
+
+		removeLink.addEventListener("click", function(openid) { //$NON-NLS-0$
+			removeFunction(openid);
+		}.bind(this, openid));
+	}
 	
 	loadUserData = function(userLocation){
 		xhr("GET", userLocation, { //$NON-NLS-0$
@@ -160,9 +198,42 @@ define(["i18n!orion/mixloginstatic/nls/messages", "orion/xhr", "orion/webui/litt
 		});
 	};
 	
+	window.handleOAuthResponse = function(oauthid) {
+		var oauthids = jsonData.properties.oauth ? jsonData.properties.oauth.split('\n') : [];
+		for (var i = 0; i < oauthids.length; i++) {
+			if (oauthids[i] === oauthid) {
+				return;
+			}
+		}
+	
+		if (!jsonData.properties.oauth) {
+			jsonData.properties.oauth = oauthid;
+		} else {
+			jsonData.properties.oauth += '\n' + oauthid;
+		}
+	
+		xhr("PUT", jsonData.Location, { //$NON-NLS-0$
+			data: JSON.stringify(jsonData),
+			headers: {
+				"Orion-Version": "1"
+			},
+			timeout: 15000
+		}).then(function(xhrResult) {
+			loadUserData(jsonData.Location);
+		}, function(xhrResult) {
+			console.error(xhrResult.error);
+		});
+	};
+	
 	function confirmOpenId(openid) {
 		if (openid !== "" && openid !== null) {
 			window.open("../mixlogin/manageopenids/openid?openid=" + encodeURIComponent(openid), "openid_popup", "width=790,height=580");
+		}
+	}
+	
+	function confirmOAuth(oauth) {
+		if (oauth !== "" && oauth !== null) {
+			window.open("../mixlogin/manageopenids/oauth?oauth=" + encodeURIComponent(oauth), "oauth_popup", "width=790,height=580");
 		}
 	}
 
@@ -204,9 +275,14 @@ define(["i18n!orion/mixloginstatic/nls/messages", "orion/xhr", "orion/webui/litt
 			var providerElements = providers.map(function(provider) {
 				return createProviderLink(provider.Name, provider.Image, confirmOpenId.bind(null, provider.Url));
 			});
+			
+			providerElements.push(createProviderLink("Google OAuth", "../mixloginstatic/images/googleplus.png", confirmOAuth.bind(null, "google")));
+			
 			providerElements.push(createProviderLink("Mozilla Persona", "../mixloginstatic/images/persona.png",
 				alert.bind(null, "To link your account with a Persona, set your Orion email address above to match your Persona email address.")));
  
+			
+			
 			var openIdContainer = document.getElementById("newOpenId");
 						
 			providerElements.forEach(function(provider) {
