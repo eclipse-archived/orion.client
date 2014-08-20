18a681cc6efb95b7cbe8ebbcb917f58e1e13fb1e
Mike Rennie <Michael_Rennie@ca.ibm.com>
2014-08-20 15:22:15 -0400

    [nobug] remove CLI code from eslint environment

--- bundles/org.eclipse.orion.client.javascript/web/eslint/conf/environments.js
+++ bundles/org.eclipse.orion.client.javascript/web/eslint/conf/environments.js
@@ -1,18 +1,7 @@
 /* global define exports module require */
-(function(root, factory) {
-    if(typeof exports === 'object') {
-        module.exports = factory(require, exports, module);
-    }
-    else if(typeof define === 'function' && define.amd) {
-        define(['require', 'exports', 'module'], factory);
-    }
-    else {
-        var req = function(id) {return root[id];},
-            exp = root,
-            mod = {exports: exp};
-        root.conf.environments = factory(req, exp, mod);
-    }
-}(this, function(require, exports, module) {
+define([
+    'module'
+], function(module) {
 module.exports = {
     "builtin": {
         "NaN": false,
@@ -393,4 +382,4 @@ module.exports = {
 };
 
     return module.exports;
-}));
+});
