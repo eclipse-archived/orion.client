f5d5c4306e4a2ddb0950e99252f1042aade8b980
Mark Macdonald <mamacdon@gmail.com>
2014-09-09 09:56:40 -0400

    Bug 441482 - "Set up your cloud" not externalized

--- bundles/org.eclipse.orion.client.cf/web/cfui/cfUtil.js
+++ bundles/org.eclipse.orion.client.cf/web/cfui/cfUtil.js
@@ -1,51 +1,53 @@
 /*******************************************************************************
  * @license
- * Licensed Materials - Property of IBM
- * (c) Copyright IBM Corporation 2014. All Rights Reserved. 
+ * Copyright (c) 2013, 2014 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
  * 
- * Note to U.S. Government Users Restricted Rights:  Use, 
- * duplication or disclosure restricted by GSA ADP Schedule 
- * Contract with IBM Corp.
- *******************************************************************************/
- /*global define*/
-define(['orion/Deferred', 'orion/URITemplate', 'orion/PageLinks'], function(Deferred, URITemplate, PageLinks){
-	
+ * Contributors: IBM Corporation - initial API and implementation
+ ******************************************************************************/
+/*eslint-env browser,amd*/
+define([
+	'i18n!cfui/nls/messages',
+	'orion/Deferred',
+	'orion/i18nUtil',
+	'orion/URITemplate',
+	'orion/PageLinks',
+], function(messages, Deferred, i18nUtil, URITemplate, PageLinks){
+
+	function handleNoCloud(error) {
+		error = error || {};
+		var cloudSettingsPageUrl = new URITemplate("{+OrionHome}/settings/settings.html#,category=cloud").expand({
+			OrionHome : PageLinks.getOrionHome()
+		});
+		error.Message = i18nUtil.formatMessage(messages["deploy.setUpYourCloud"], cloudSettingsPageUrl);
+		error.Severity = "Warning";
+		return new Deferred().reject(error);
+	}
+
 	return {
-	// make sure target is set and it matches the url in settings
-	getTarget : function(preferences) {
-		var deferred = new Deferred();
-			preferences.getPreferences('/cm/configurations').then(
-				function(settings){
-					var cloud = settings.get("org.eclipse.orion.client.cf.settings");
-					if (cloud && cloud.targetUrl){
-						var target = {};
-						target.Url = cloud.targetUrl;
-						if (cloud.manageUrl)
-							target.ManageUrl = cloud.manageUrl;
-						if (cloud.org)
-							target.Org = cloud.org;
-						if (cloud.space)
-							target.Space = cloud.space;
-						deferred.resolve(target);
-						return;
-					} else {
-						var error = {};
-						var cloudSettingsPageUrl = new URITemplate("{+OrionHome}/settings/settings.html#,category=cloud").expand({OrionHome : PageLinks.getOrionHome()});
-						error.Message = "Set up your Cloud. Go to [Settings](" + cloudSettingsPageUrl + ")."; 
-						error.Severity = "Warning";
-						deferred.reject(error);
-					}
-				}, function(error){
-					var cloudSettingsPageUrl = new URITemplate("{+OrionHome}/settings/settings.html#,category=cloud").expand({OrionHome : PageLinks.getOrionHome()});
-					error.Message = "Set up your Cloud. Go to [Settings](" + cloudSettingsPageUrl + ")."; 
-					error.Severity = "Warning";
-					deferred.reject(error);
+		// make sure target is set and it matches the url in settings
+		getTarget : function(preferences) {
+			return preferences.getPreferences('/cm/configurations').then(function(settings){
+				var cloud = settings.get("org.eclipse.orion.client.cf.settings");
+				if (cloud && cloud.targetUrl){
+					var target = {};
+					target.Url = cloud.targetUrl;
+					if (cloud.manageUrl)
+						target.ManageUrl = cloud.manageUrl;
+					if (cloud.org)
+						target.Org = cloud.org;
+					if (cloud.space)
+						target.Space = cloud.space;
+					return target;
 				}
-			);
-			return deferred;
+				return handleNoCloud();
+			}, handleNoCloud);
 		},
-	getLoginMessage: function(manageUrl){
-			return "Please enter your IBM id below to authorize deployment to Cloud Foundry.";
+		getLoginMessage: function(/*manageUrl*/){
+			return messages["deploy.enterCredentials"];
 		}
 	};
 });
