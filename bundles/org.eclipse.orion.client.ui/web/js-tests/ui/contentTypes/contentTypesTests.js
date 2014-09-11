cce9056091d1fee26b2c2f6e4e417a5c0f004d00
libing wang <libingw@ca.ibm.com>
2014-09-11 16:42:10 -0400

    JS unit test: Convert contents types tests into Mocha style.

--- bundles/org.eclipse.orion.client.ui/web/js-tests/ui/contentTypes/contentTypesTests.js
+++ bundles/org.eclipse.orion.client.ui/web/js-tests/ui/contentTypes/contentTypesTests.js
@@ -8,7 +8,7 @@
  * 
  * Contributors: IBM Corporation - initial API and implementation
  ******************************************************************************/
-/*eslint-env browser, amd*/
+/*eslint-env browser, amd, mocha*/
 define([
 	'chai/chai',
 	'orion/contentTypes',
@@ -78,190 +78,188 @@ define([
 		contentTypeService = new ContentTypeRegistry(dataSource);
 		testbody(serviceRegistry, contentTypeService, basicTypes);
 	}
-
+	var tests = {};
 	/**
 	 * Constructs a "with serviceRegistry" and "without serviceRegistry" test.
 	 * @param {Function} testBody should call `this.withTestData` to perform its setup, or check `this.useServiceRegistry`
 	 * to see what kind of test it should be running.
 	 */
 	function makeTests(tests, basename, testBody) {
-		tests["test_" + basename] = testBody.bind({
+		it("test_" + basename, testBody.bind({
 			useServiceRegistry: true,
 			withTestData: _withTestData.bind(null, true)
-		});
-		tests["test_" + basename + "_no_serviceRegistry"] = testBody.bind({
+		}));
+		it("test_" + basename + "_no_serviceRegistry", testBody.bind({
 			useServiceRegistry: false,
 			withTestData: _withTestData.bind(null, false)
-		});
+		}));
 	}
 
-	var tests = {};
-
-	tests.test_new_ContentTypeRegistry = function() {
-		assert.throws(function() { new ContentTypeRegistry(); });
-		assert.throws(function() { new ContentTypeRegistry(null); });
-		assert.throws(function() { new ContentTypeRegistry("zzzz"); });
-	};
-
-	makeTests(tests, "getContentTypes", function() {
-		this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
-			contentTypeService.getContentTypes().every(function(type, i) {
-				assertContentTypesEqual(basicTypes[i], type);
+	describe("Content Types Test", function() {
+		it("new_ContentTypeRegistry", function() {
+			assert.throws(function() { new ContentTypeRegistry(); });
+			assert.throws(function() { new ContentTypeRegistry(null); });
+			assert.throws(function() { new ContentTypeRegistry("zzzz"); });
+		});
+	
+		makeTests(tests, "getContentTypes", function() {
+			this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
+				contentTypeService.getContentTypes().every(function(type, i) {
+					assertContentTypesEqual(basicTypes[i], type);
+				});
 			});
 		});
-	});
-
-	makeTests(tests, "getContentTypesMap", function() {
-		this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
-			var map = contentTypeService.getContentTypesMap();
-			Object.keys(map).forEach(function(contentTypeId) {
-				var type = null;
-				basicTypes.some(function(elem, i) {
-					if (elem.id === contentTypeId) {
-						type = elem;
+	
+		makeTests(tests, "getContentTypesMap", function() {
+			this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
+				var map = contentTypeService.getContentTypesMap();
+				Object.keys(map).forEach(function(contentTypeId) {
+					var type = null;
+					basicTypes.some(function(elem, i) {
+						if (elem.id === contentTypeId) {
+							type = elem;
+							return true;
+						}
+					});
+					assert.notEqual(type, null);
+					assertContentTypesEqual(type, map[contentTypeId]);
+				});
+			});
+		});
+	
+		makeTests(tests, "getContentType", function() {
+			this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
+				basicTypes.forEach(function(contentType) {
+					assertContentTypesEqual(contentType, contentTypeService.getContentType(contentType.id));
+				});
+			});
+		});
+	
+		makeTests(tests, "getFileContentType", function() {
+			this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
+				var fileMetadata1 = {
+					Name: "aaaaaaaaaaa"
+				},
+				fileMetadata2 = {
+					Name: "test.file.xml"
+				},
+				fileMetadata3 = {
+					Name: "test.file.txt"
+				},
+				fileMetadata4 = {
+					Name: "build.xml"
+				};
+				assertContentTypesEqual(contentTypeService.getFileContentType(fileMetadata1), null, "No content type for unrecognized file");
+				assertContentTypesEqual(contentTypeService.getFileContentType(fileMetadata2), basicTypes[2]);
+				assertContentTypesEqual(contentTypeService.getFileContentType(fileMetadata3), basicTypes[2]);
+				assertContentTypesEqual(contentTypeService.getFileContentType(fileMetadata4), basicTypes[3], "filename match beats extension match");
+			});
+		});
+	
+		makeTests(tests, "getFilenameContentType", function() {
+			this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
+				assertContentTypesEqual(contentTypeService.getFilenameContentType("aaaaaaa"), null, "No content type for unrecognized file");
+				assertContentTypesEqual(contentTypeService.getFilenameContentType("test.file.xml"), basicTypes[2]);
+				assertContentTypesEqual(contentTypeService.getFilenameContentType("test.file.txt"), basicTypes[2]);
+				assertContentTypesEqual(contentTypeService.getFilenameContentType("build.xml"), basicTypes[3], "filename match beats extension match");
+			});
+		});
+	
+		makeTests(tests, "isExtensionOf", function() {
+			this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
+				function assertIsExtensionOf(typeA, typeB, expected, msg) {
+					// Test calling both by ContentType object and by String ID:
+					assert.equal(contentTypeService.isExtensionOf(typeA, typeB), expected, msg);
+					assert.equal(contentTypeService.isExtensionOf(typeA.id, typeB.id), expected, msg);
+				}
+				assertIsExtensionOf(basicTypes[0], basicTypes[0], true, "Basic 0 extends itself");
+				assertIsExtensionOf(basicTypes[1], basicTypes[0], true, "Basic 1 extends Basic 0");
+				assertIsExtensionOf(basicTypes[2], basicTypes[0], false);
+				assertIsExtensionOf(basicTypes[3], basicTypes[0], false);
+	
+				assertIsExtensionOf(basicTypes[0], basicTypes[1], false);
+				assertIsExtensionOf(basicTypes[1], basicTypes[1], true);
+				assertIsExtensionOf(basicTypes[2], basicTypes[1], false);
+				assertIsExtensionOf(basicTypes[3], basicTypes[1], false);
+	
+				assertIsExtensionOf(basicTypes[0], basicTypes[2], false);
+				assertIsExtensionOf(basicTypes[1], basicTypes[2], false);
+				assertIsExtensionOf(basicTypes[2], basicTypes[2], true);
+				assertIsExtensionOf(basicTypes[3], basicTypes[2], false);
+	
+				assertIsExtensionOf(basicTypes[0], basicTypes[3], false);
+				assertIsExtensionOf(basicTypes[1], basicTypes[3], false);
+				assertIsExtensionOf(basicTypes[2], basicTypes[3], false);
+				assertIsExtensionOf(basicTypes[3], basicTypes[3], true);
+			});
+		});
+	
+		// Note this one does not call `withTestData`
+		tests.test_isExtensionOf_bad = function() {
+			var bad = {
+				id: 'orion/test4',
+				name: 'Bad',
+				'extends': 'orion/test4'
+			};
+			var contentTypeService;
+			if (this.useServiceRegistry) {
+				var serviceRegistry = new ServiceRegistry();
+				serviceRegistry.registerService("orion.core.contenttype", {}, {
+					contentTypes: [ bad ]
+				});
+				contentTypeService = new ContentTypeRegistry(serviceRegistry);
+			} else {
+				contentTypeService = new ContentTypeRegistry([ bad ]);
+			}
+			Deferred.when(contentTypeService.isExtensionOf(bad, bad)).then(assert.fail);
+		};
+	
+		makeTests(tests, "isSomeExtensionOf", function() {
+			this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
+				function assertIsSomeExtensionOf(type, types, expected, msg) {
+					assert.equal(contentTypeService.isSomeExtensionOf(type, types), expected, msg);
+					var typeIds = types.map(function(type) { return type.id; });
+					assert.equal(contentTypeService.isSomeExtensionOf(type.id, typeIds), expected, msg);
+				}
+	
+				assertIsSomeExtensionOf(basicTypes[0], basicTypes, true);
+				assertIsSomeExtensionOf(basicTypes[1], basicTypes, true);
+				assertIsSomeExtensionOf(basicTypes[2], basicTypes, true);
+				assertIsSomeExtensionOf(basicTypes[3], basicTypes, true);
+	
+				assertIsSomeExtensionOf(basicTypes[0], [ basicTypes[0] ], true);
+				assertIsSomeExtensionOf(basicTypes[1], [ basicTypes[0] ], true);
+				assertIsSomeExtensionOf(basicTypes[2], [ basicTypes[0] ], false);
+				assertIsSomeExtensionOf(basicTypes[3], [ basicTypes[0] ], false);
+	
+				assertIsSomeExtensionOf(basicTypes[1], [ basicTypes[0], basicTypes[2] ], true);
+	
+				assertIsSomeExtensionOf(basicTypes[3], [], false);
+			});
+		});
+	
+		makeTests(tests, "extensionsCaseMismatch", function() {
+			this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
+				var type1 = contentTypeService.getFilenameContentType('test.TXT');
+				var type2 = contentTypeService.getFilenameContentType('test.txt');
+				var type3 = contentTypeService.getFilenameContentType('test.TxT');
+				assertContentTypesEqual(type1, type2);
+				assertContentTypesEqual(type2, type3);
+			});
+		});
+	
+		makeTests(tests, "image", function() {
+			this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
+				var type4;
+				contentTypeService.getContentTypes().some(function(ct) {
+					if (ct.id === 'orion/test4') {
+						type4 = ct;
 						return true;
 					}
 				});
-				assert.notEqual(type, null);
-				assertContentTypesEqual(type, map[contentTypeId]);
+				assert.equal(type4.image, 'http://example.org/foo.png');
+				assert.equal(type4.imageClass, 'imageFoo');
 			});
 		});
 	});
-
-	makeTests(tests, "getContentType", function() {
-		this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
-			basicTypes.forEach(function(contentType) {
-				assertContentTypesEqual(contentType, contentTypeService.getContentType(contentType.id));
-			});
-		});
-	});
-
-	makeTests(tests, "getFileContentType", function() {
-		this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
-			var fileMetadata1 = {
-				Name: "aaaaaaaaaaa"
-			},
-			fileMetadata2 = {
-				Name: "test.file.xml"
-			},
-			fileMetadata3 = {
-				Name: "test.file.txt"
-			},
-			fileMetadata4 = {
-				Name: "build.xml"
-			};
-			assertContentTypesEqual(contentTypeService.getFileContentType(fileMetadata1), null, "No content type for unrecognized file");
-			assertContentTypesEqual(contentTypeService.getFileContentType(fileMetadata2), basicTypes[2]);
-			assertContentTypesEqual(contentTypeService.getFileContentType(fileMetadata3), basicTypes[2]);
-			assertContentTypesEqual(contentTypeService.getFileContentType(fileMetadata4), basicTypes[3], "filename match beats extension match");
-		});
-	});
-
-	makeTests(tests, "getFilenameContentType", function() {
-		this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
-			assertContentTypesEqual(contentTypeService.getFilenameContentType("aaaaaaa"), null, "No content type for unrecognized file");
-			assertContentTypesEqual(contentTypeService.getFilenameContentType("test.file.xml"), basicTypes[2]);
-			assertContentTypesEqual(contentTypeService.getFilenameContentType("test.file.txt"), basicTypes[2]);
-			assertContentTypesEqual(contentTypeService.getFilenameContentType("build.xml"), basicTypes[3], "filename match beats extension match");
-		});
-	});
-
-	makeTests(tests, "isExtensionOf", function() {
-		this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
-			function assertIsExtensionOf(typeA, typeB, expected, msg) {
-				// Test calling both by ContentType object and by String ID:
-				assert.equal(contentTypeService.isExtensionOf(typeA, typeB), expected, msg);
-				assert.equal(contentTypeService.isExtensionOf(typeA.id, typeB.id), expected, msg);
-			}
-			assertIsExtensionOf(basicTypes[0], basicTypes[0], true, "Basic 0 extends itself");
-			assertIsExtensionOf(basicTypes[1], basicTypes[0], true, "Basic 1 extends Basic 0");
-			assertIsExtensionOf(basicTypes[2], basicTypes[0], false);
-			assertIsExtensionOf(basicTypes[3], basicTypes[0], false);
-
-			assertIsExtensionOf(basicTypes[0], basicTypes[1], false);
-			assertIsExtensionOf(basicTypes[1], basicTypes[1], true);
-			assertIsExtensionOf(basicTypes[2], basicTypes[1], false);
-			assertIsExtensionOf(basicTypes[3], basicTypes[1], false);
-
-			assertIsExtensionOf(basicTypes[0], basicTypes[2], false);
-			assertIsExtensionOf(basicTypes[1], basicTypes[2], false);
-			assertIsExtensionOf(basicTypes[2], basicTypes[2], true);
-			assertIsExtensionOf(basicTypes[3], basicTypes[2], false);
-
-			assertIsExtensionOf(basicTypes[0], basicTypes[3], false);
-			assertIsExtensionOf(basicTypes[1], basicTypes[3], false);
-			assertIsExtensionOf(basicTypes[2], basicTypes[3], false);
-			assertIsExtensionOf(basicTypes[3], basicTypes[3], true);
-		});
-	});
-
-	// Note this one does not call `withTestData`
-	tests.test_isExtensionOf_bad = function() {
-		var bad = {
-			id: 'orion/test4',
-			name: 'Bad',
-			'extends': 'orion/test4'
-		};
-		var contentTypeService;
-		if (this.useServiceRegistry) {
-			var serviceRegistry = new ServiceRegistry();
-			serviceRegistry.registerService("orion.core.contenttype", {}, {
-				contentTypes: [ bad ]
-			});
-			contentTypeService = new ContentTypeRegistry(serviceRegistry);
-		} else {
-			contentTypeService = new ContentTypeRegistry([ bad ]);
-		}
-		Deferred.when(contentTypeService.isExtensionOf(bad, bad)).then(assert.fail);
-	};
-
-	makeTests(tests, "isSomeExtensionOf", function() {
-		this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
-			function assertIsSomeExtensionOf(type, types, expected, msg) {
-				assert.equal(contentTypeService.isSomeExtensionOf(type, types), expected, msg);
-				var typeIds = types.map(function(type) { return type.id; });
-				assert.equal(contentTypeService.isSomeExtensionOf(type.id, typeIds), expected, msg);
-			}
-
-			assertIsSomeExtensionOf(basicTypes[0], basicTypes, true);
-			assertIsSomeExtensionOf(basicTypes[1], basicTypes, true);
-			assertIsSomeExtensionOf(basicTypes[2], basicTypes, true);
-			assertIsSomeExtensionOf(basicTypes[3], basicTypes, true);
-
-			assertIsSomeExtensionOf(basicTypes[0], [ basicTypes[0] ], true);
-			assertIsSomeExtensionOf(basicTypes[1], [ basicTypes[0] ], true);
-			assertIsSomeExtensionOf(basicTypes[2], [ basicTypes[0] ], false);
-			assertIsSomeExtensionOf(basicTypes[3], [ basicTypes[0] ], false);
-
-			assertIsSomeExtensionOf(basicTypes[1], [ basicTypes[0], basicTypes[2] ], true);
-
-			assertIsSomeExtensionOf(basicTypes[3], [], false);
-		});
-	});
-
-	makeTests(tests, "extensionsCaseMismatch", function() {
-		this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
-			var type1 = contentTypeService.getFilenameContentType('test.TXT');
-			var type2 = contentTypeService.getFilenameContentType('test.txt');
-			var type3 = contentTypeService.getFilenameContentType('test.TxT');
-			assertContentTypesEqual(type1, type2);
-			assertContentTypesEqual(type2, type3);
-		});
-	});
-
-	makeTests(tests, "image", function() {
-		this.withTestData(function(serviceRegistry, contentTypeService, basicTypes) {
-			var type4;
-			contentTypeService.getContentTypes().some(function(ct) {
-				if (ct.id === 'orion/test4') {
-					type4 = ct;
-					return true;
-				}
-			});
-			assert.equal(type4.image, 'http://example.org/foo.png');
-			assert.equal(type4.imageClass, 'imageFoo');
-		});
-	});
-
-	return tests;
 });
