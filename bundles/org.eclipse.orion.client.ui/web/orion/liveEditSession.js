6ca5a5c7dbd31bad740ddebe3e0657a8f6441693
Mark Macdonald <mamacdon@gmail.com>
2014-09-22 15:53:22 -0400

    Bug 437849 - Minor cleanup to Flux patch

--- bundles/org.eclipse.orion.client.ui/web/orion/liveEditSession.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/liveEditSession.js
@@ -8,20 +8,20 @@
  * 
  * Contributors: Pivotal Software Inc. - initial API and implementation 
  ******************************************************************************/
-
-/*global define window */
-
+/*eslint-env amd, browser*/
 define([
 	'orion/Deferred',
 	'orion/edit/liveEditorContext'
 ], function(Deferred, LiveEditorContext) {
-	
 
-var resource;
-var services = [];
-var previousEditSession;
+	var resource;
+	var services = [];
+//	var previousEditSession;
 
-var LiveEditSession = (function () {
+	function handleError(err) {
+		return err instanceof Error ? err : new Error(err);
+	}
+
 	/**
 	 * @name orion.LiveEditSession
 	 * @class Provides access to validation services registered with the service registry.
@@ -36,10 +36,10 @@ var LiveEditSession = (function () {
 		/**
 		 * Looks up applicable live edit sessions, calls them to pass editor context to services.
 		 */
-		start: function (contentType, title, contents) {
+		start: function (contentType, title) {
 			function getLiveEditors(registry, contentType) {
 				var contentTypeService = registry.getService("orion.core.contentTypeRegistry"); //$NON-NLS-0$
-				function getFilteredLiveEditSession(registry, liveEditSession, contentType) {
+				function getFilteredLiveEditSession(liveEditSession, contentType) {
 					var contentTypeIds = liveEditSession.getProperty("contentType"); //$NON-NLS-0$
 					return contentTypeService.isSomeExtensionOf(contentType, contentTypeIds).then(function(result) {
 						return result ? liveEditSession : null;
@@ -49,29 +49,23 @@ var LiveEditSession = (function () {
 				var filteredLiveEditSessions = [];
 				for (var i=0; i < liveEditSessions.length; i++) {
 					var serviceReference = liveEditSessions[i];
-					var pattern = serviceReference.getProperty("pattern"); // backwards compatibility //$NON-NLS-0$
 					if (serviceReference.getProperty("contentType")) { //$NON-NLS-0$
-						filteredLiveEditSessions.push(getFilteredLiveEditSession(registry, serviceReference, contentType));
-					} else if (pattern && new RegExp(pattern).test(title)) {
-						var d = new Deferred();
-						d.resolve(serviceReference);
-						filteredLiveEditSessions.push(d);
+						filteredLiveEditSessions.push(getFilteredLiveEditSession(serviceReference, contentType));
 					}
 				}
-				// Return a promise that gives the validators that aren't null
-				return Deferred.all(filteredLiveEditSessions, function(error) {return {_error: error}; }).then(
-					function(liveEditSessions) {
-						var capableLiveEditSessions = [];
-						for (var i=0; i < liveEditSessions.length; i++) {
-							var liveEdit = liveEditSessions[i];
-							if (liveEdit && !liveEdit._error) {
-								capableLiveEditSessions.push(liveEdit);
-							}
+				// Return a promise that gives the live edit sessions that aren't null
+				return Deferred.all(filteredLiveEditSessions, handleError).then(function(liveEditSessions) {
+					var capableLiveEditSessions = [];
+					for (var i=0; i < liveEditSessions.length; i++) {
+						var liveEdit = liveEditSessions[i];
+						if (liveEdit && !(liveEdit instanceof Error)) {
+							capableLiveEditSessions.push(liveEdit);
 						}
-						return capableLiveEditSessions;
-					});
+					}
+					return capableLiveEditSessions;
+				});
 			}
-			
+
 			if (resource !== title) {
 				if (services) {
 					services.forEach(function(service) {
@@ -83,12 +77,10 @@ var LiveEditSession = (function () {
 				if (!contentType) {
 					return;
 				}
-				var self = this;
 				var serviceRegistry = this.registry;
 				getLiveEditors(serviceRegistry, contentType).then(function(liveEditors) {
 					var editSessionPromises = liveEditors.map(function(liveEditor) {
 						var service = serviceRegistry.getService(liveEditor);
-						var promise;
 						if (service.startEdit) {
 							var context = {
 								contentType: contentType.id,
@@ -100,12 +92,11 @@ var LiveEditSession = (function () {
 						}
 					});
 						
-					previousEditSession = Deferred.all(editSessionPromises, function(error) {return {_error: error}; });
+					/*previousEditSession =*/ Deferred.all(editSessionPromises, handleError);
 				});
 			}
 		}
 	};
+
 	return LiveEditSession;
-}());
-return {LiveEditSession: LiveEditSession};
 });
