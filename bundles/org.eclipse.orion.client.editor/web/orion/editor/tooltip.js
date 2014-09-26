b5a8d6c3147f92dde33dacf2ac583729b94e1134
Eric Moffatt <emoffatt@ca.ibm.com>
2014-09-26 13:43:48 -0400

    Bug 445215 - Implement a pluggable service to support tooltips on editors
    Change-Id: I95f31e501d5db9c970d19ce764a9978481a06df7
    Signed-off-by: Eric Moffatt <emoffatt@ca.ibm.com>

--- bundles/org.eclipse.orion.client.editor/web/orion/editor/tooltip.js
+++ bundles/org.eclipse.orion.client.editor/web/orion/editor/tooltip.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * @license
- * Copyright (c) 2010, 2012 IBM Corporation and others.
+ * Copyright (c) 2010, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials are made 
  * available under the terms of the Eclipse Public License v1.0 
  * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
@@ -9,15 +9,15 @@
  * Contributors: IBM Corporation - initial API and implementation
  ******************************************************************************/
 
-/*eslint-env browser, amd*/
+/*eslint-env browser, amd, node*/
 define("orion/editor/tooltip", [ //$NON-NLS-0$
 	'i18n!orion/editor/nls/messages', //$NON-NLS-0$
 	'orion/editor/textView', //$NON-NLS-0$
-	'orion/editor/textModel', //$NON-NLS-0$
 	'orion/editor/projectionTextModel', //$NON-NLS-0$
 	'orion/editor/util', //$NON-NLS-0$
+	'marked/marked', //$NON-NLS-0$
 	'orion/util' //$NON-NLS-0$
-], function(messages, mTextView, mTextModel, mProjectionTextModel, textUtil, util) {
+], function(messages, mTextView, mProjectionTextModel, textUtil, Markdown, util) {
 
 	/** @private */
 	function Tooltip (view) {
@@ -187,14 +187,19 @@ define("orion/editor/tooltip", [ //$NON-NLS-0$
 		show: function(autoHide) {
 			if (!this._target) { return; }
 			var info = this._target.getTooltipInfo();
+
 			if (!info) { return; }
 			var tooltipDiv = this._tooltipDiv, tooltipContents = this._tooltipContents;
 			tooltipDiv.style.left = tooltipDiv.style.right = tooltipDiv.style.width = tooltipDiv.style.height = 
 				tooltipContents.style.width = tooltipContents.style.height = "auto"; //$NON-NLS-0$
+			var tooltipDoc = tooltipDiv.ownerDocument;
+			var documentElement = tooltipDoc.documentElement;
+			
 			var contents = info.contents;
 			if (contents instanceof Array) {
-				contents = this._getAnnotationContents(contents);
+				contents = this._getAnnotationContents(contents);			
 			}
+			
 			if (typeof contents === "string") { //$NON-NLS-0$
 				tooltipContents.innerHTML = contents;
 			} else if (this._isNode(contents)) {
@@ -227,10 +232,57 @@ define("orion/editor/tooltip", [ //$NON-NLS-0$
 				tooltipContents.style.width = size.width + "px"; //$NON-NLS-0$
 				tooltipContents.style.height = size.height + "px"; //$NON-NLS-0$
 				contentsView.resize();
-			} else {
+			} else if (!info.deferredInfo) {
 				return;
 			}
-			var documentElement = tooltipDiv.ownerDocument.documentElement;
+			
+			var deferredInfo = info.deferredInfo;
+			if (deferredInfo) {
+				var self = this;
+				deferredInfo.forEach(function(tipSection) {
+					tipSection.promise.then(function (data) {
+						if (data) {
+						    var sectionDiv = null;
+						    var divResult = null;
+							switch(data.type) {
+								case 'markdown': {
+									sectionDiv = util.createElement(tooltipDoc, "div"); //$NON-NLS-0$
+									sectionDiv.innerHTML = (data.title ? Markdown(data.title) : tipSection.title);
+									tooltipContents.appendChild(sectionDiv);
+									divResult = util.createElement(tooltipDoc, "div"); //$NON-NLS-0$
+									divResult.innerHTML = Markdown(data.hover);
+									sectionDiv.appendChild(divResult);
+									break;
+								}
+								case 'proposal': {
+									sectionDiv = util.createElement(tooltipDoc, "button"); //$NON-NLS-0$
+									sectionDiv.textContent = (data.title ? data.title : tipSection.title);
+									tooltipContents.appendChild(sectionDiv);
+									
+									sectionDiv.addEventListener("click", function(e) {
+										self._view.setText(data.text, data.start, data.end);
+									});
+									break;
+								}
+								default: {
+									sectionDiv = util.createElement(tooltipDoc, "div"); //$NON-NLS-0$
+									var title = data.title ? data.title : tipSection.title;
+									sectionDiv.innerHTML = title;
+									tooltipContents.appendChild(sectionDiv);
+									divResult = util.createElement(tooltipDoc, "div"); //$NON-NLS-0$
+									divResult.appendChild(tooltipDoc.createTextNode(data.hover));
+									sectionDiv.appendChild(divResult);
+								}
+							}
+							// Ensure that the tooltip is visible
+							tooltipDiv.style.visibility = "visible"; //$NON-NLS-0$
+						}
+					}, function(err) {
+						console.error("Problem computing hover tooltip", err);
+					});
+				});
+			}
+			
 			if (info.anchor === "right") { //$NON-NLS-0$
 				var right = documentElement.clientWidth - info.x;
 				tooltipDiv.style.right = right + "px"; //$NON-NLS-0$
@@ -248,7 +300,12 @@ define("orion/editor/tooltip", [ //$NON-NLS-0$
 			tooltipDiv.style.top = top + "px"; //$NON-NLS-0$
 			tooltipDiv.style.maxHeight = (documentElement.clientHeight - top - 10) + "px"; //$NON-NLS-0$
 			tooltipDiv.style.opacity = "1"; //$NON-NLS-0$
-			tooltipDiv.style.visibility = "visible"; //$NON-NLS-0$
+			
+			// Delay the showing of a tootip with no 'static' contents
+			if (contents) {
+				tooltipDiv.style.visibility = "visible"; //$NON-NLS-0$
+			}
+			
 			if (autoHide) {
 				var self = this;
 				var window = this._getWindow();
