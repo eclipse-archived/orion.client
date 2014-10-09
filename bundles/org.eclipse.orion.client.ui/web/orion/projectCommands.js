fff815ce6ae35f72eb09341c7d360bef0d29db3c
Aidan Redpath <aidanr@ca.ibm.com>
2014-10-09 13:45:40 -0400

    Bug 437645 - Fixed Zip archive
    Changed zip archive to first create a basic project, wait for the project
    to be created then import the zip. This ensures the user is placed inside
    the project view
    
    Signed-off-by: Aidan Redpath <aidanr@ca.ibm.com>

--- bundles/org.eclipse.orion.client.ui/web/orion/projectCommands.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/projectCommands.js
@@ -1086,26 +1086,30 @@ define(['require', 'i18n!orion/navigate/nls/messages', 'orion/webui/littlelib', 
 				return !hasReadMe;
 			}
 		});
-		
+
 		commandService.addCommand(addReadmeCommand);
-		
+
+		function createProject (name, item){
+			fileClient.loadWorkspace(fileClient.fileServiceRootURL(item.Location)).then(function(workspace) {
+				progress.progress(projectClient.createProject(workspace.ChildrenLocation, {Name: name}), messages["Creating project "] + name).then(function(project){
+					dispatchNewProject(workspace, project);
+				});
+			});
+		}
+
 		var createBasicProjectCommand = new mCommands.Command({
 			name: messages["basic"],
 			tooltip: messages["createAnEmptyProject."],
 			id: "orion.project.create.basic",
 			parameters : new mCommandRegistry.ParametersDescription([new mCommandRegistry.CommandParameter("name", "text", messages["Name:"])]),
-			callback: function(data){
-					var name = data.parameters.valueFor("name");
-					if(!name){
-						return;
-					}
-					var item = forceSingleItem(data.items);
-					fileClient.loadWorkspace(fileClient.fileServiceRootURL(item.Location)).then(function(workspace) {
-						progress.progress(projectClient.createProject(workspace.ChildrenLocation, {Name: name}), messages["Creating project "] + name).then(function(project){
-							dispatchNewProject(workspace, project);
-						});
-					});
-				},
+			callback: function(data) {
+				var name = data.parameters.valueFor("name");
+				if(!name){
+					return;
+				}
+				var item = forceSingleItem(data.items);
+				createProject(name, data);
+			},
 			visibleWhen: function(item) {
 					if (!explorer || !explorer.isCommandsVisible()) {
 						return false;
@@ -1131,38 +1135,20 @@ define(['require', 'i18n!orion/navigate/nls/messages', 'orion/webui/littlelib', 
 					var projectNameDialog = new PromptDialog.PromptDialog({
 						title: messages["Enter project name:"] //$NON-NLS-0$
 					});
-					
 					// add listener which uses project name entered by user to create a new project
 					projectNameDialog.addEventListener("ok", function(event) { //$NON-NLS-0$
 						var projectName = event.value;
-						fileClient.loadWorkspace(fileClient.fileServiceRootURL(item.Location)).then(function(workspace) {
-							progress.progress(projectClient.createProject(workspace.ChildrenLocation, {Name: projectName}), messages["Creating project "] + projectName).then(function(projectInfo){ //$NON-NLS-0$
-								progress.progress(fileClient.read(projectInfo.ContentLocation, true)).then(function(projectMetadata){
-									
-									explorer.changedItem(workspace, true).then(function(newWorkspace){ //force workspace reload
-										var children = newWorkspace.Children;
-										// find the item that represents the newly created project
-										var projectItem = null;
-										children.some(function(child){
-											if (child.Location === projectMetadata.Location) {
-												projectItem = child;
-												return true;
-											}
-											return false;
-										});
-										// upload files selected by user into newly created project
-										if (projectItem) {
-											if (fileInput.files && fileInput.files.length > 0) {
-												for (var i = 0; i < fileInput.files.length; i++) {
-													explorer._uploadFile(projectItem, fileInput.files.item(i), true);
-												}
-											}
-										}
-									});
-									
-								}, errorHandler);
-							}, errorHandler);
-						}, errorHandler);
+						var handleOpen = function (event) {
+							if (projectName == event.item.Name && fileInput.files && fileInput.files.length > 0) {
+								for (var i = 0; i < fileInput.files.length; i++) {
+									explorer._uploadFile(event.item, fileInput.files.item(i), false);
+								}
+								explorer.sidebarNavInputManager.removeEventListener("projectOpened", handleOpen); //$NON-NLS-0$
+							}
+						};
+						// Add listener to wait for the project to open
+						explorer.sidebarNavInputManager.addEventListener("projectOpened", handleOpen); //$NON-NLS-0$
+						createProject(projectName, item);
 					});
 					
 					var changeListener = function(){
