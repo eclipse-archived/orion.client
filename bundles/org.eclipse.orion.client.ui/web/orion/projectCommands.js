c7a76dc38eb921d0cc2de791b21b67da819a0aaf
Malgorzata Janczarska <malgorzata.tomczyk@pl.ibm.com>
2014-10-22 14:19:26 +0200

    [nobug] Add hideAllDeployCommands property to project commands

--- bundles/org.eclipse.orion.client.ui/web/orion/projectCommands.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/projectCommands.js
@@ -368,6 +368,7 @@ define(['require', 'i18n!orion/navigate/nls/messages', 'orion/webui/littlelib', 
 					});
 				},
 				visibleWhen: function(items) {
+					if(projectCommandUtils.hideAllDeployCommands) return false;
 					if (!(command.showCommand == undefined || command.showCommand)) return false;
 					var item = forceSingleItem(items);
 					return(item.Project === treeRoot.Project);
@@ -1296,6 +1297,7 @@ define(['require', 'i18n!orion/navigate/nls/messages', 'orion/webui/littlelib', 
 
 					},
 					visibleWhen: function(item) {
+						if(projectCommandUtils.hideAllDeployCommands) return false;
 						if (!(command.showCommand == undefined || command.showCommand)) return false;
 						item = explorer.treeRoot;
 						if(!item.Project || !item.children || item.children.length === 0){
