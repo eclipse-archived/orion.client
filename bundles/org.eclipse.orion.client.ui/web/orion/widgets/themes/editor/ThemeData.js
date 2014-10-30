40831d9a45e106b0f426caf3278d3cb29add177a
earl93 <earl93@hotmail.com>
2014-10-30 11:36:16 -0400

    Parsing sublime/textmate themes & added seti theme
    Change-Id: I5274f52c97ebc4a103effa14b5def41b3a228f02
    Signed-off-by: Insoo Choo <earl93@hotmail.com>

--- bundles/org.eclipse.orion.client.ui/web/orion/widgets/themes/editor/ThemeData.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/widgets/themes/editor/ThemeData.js
@@ -46,6 +46,182 @@ define([
 		function ThemeData() {
 
 		this.styles = [];
+		
+		var seti = {
+		   "name":"Seti",  //$NON-NLS-1 //$NON-NLS-0$ 
+		   "className":"seti", //$NON-NLS-1 //$NON-NLS-0$ 
+		   "styles":{ //$NON-NLS-0$
+		      "annotationRange":{ //$NON-NLS-0$
+		          "matchingSearch":{ //$NON-NLS-0$
+		              "backgroundColor":"#2B333C" //$NON-NLS-1 //$NON-NLS-0$ 
+		          },
+		          "currentSearch":{ //$NON-NLS-0$
+		              "backgroundColor":"#2B333C", //$NON-NLS-1 //$NON-NLS-0$ 
+		              "text-decoration":"underline" //$NON-NLS-1 //$NON-NLS-0$ 
+		          }
+		      },
+		      "backgroundColor":"#151718", //$NON-NLS-1 //$NON-NLS-0$ 
+		      "color":"#d4d7d6", //$NON-NLS-1 //$NON-NLS-0$ 
+		      "fontFamily":"\"Consolas\", \"Monaco\", \"Vera Mono\", monospace", //$NON-NLS-1 //$NON-NLS-0$ 
+		      "fontSize":"16px", //$NON-NLS-1 //$NON-NLS-0$ 
+		      "textviewRightRuler":{ //$NON-NLS-0$
+		         "borderLeft":"1px solid #0e1112" //$NON-NLS-1 //$NON-NLS-0$ 
+		      },
+		      "textviewLeftRuler":{ //$NON-NLS-0$
+		         "borderRight":"1px solid #0e1112" //$NON-NLS-1 //$NON-NLS-0$ 
+		      },
+		      "ruler":{ //$NON-NLS-0$
+		         "backgroundColor":"#0e1112", //$NON-NLS-1 //$NON-NLS-0$ 
+		         "overview":{ //$NON-NLS-0$
+		            "backgroundColor":"#0e1112" //$NON-NLS-1 //$NON-NLS-0$ 
+		         },
+		         "annotations":{ //$NON-NLS-0$
+		            "backgroundColor":"#0e1112" //$NON-NLS-1 //$NON-NLS-0$ 
+		         }
+		      },
+		      "rulerLines":{ //$NON-NLS-0$
+		         "color":"#404b53", //$NON-NLS-1 //$NON-NLS-0$ 
+		         "odd":{ //$NON-NLS-0$
+		            "color":"#404b53" //$NON-NLS-1 //$NON-NLS-0$ 
+		         },
+		         "even":{ //$NON-NLS-0$
+		            "color":"#404b53" //$NON-NLS-1 //$NON-NLS-0$ 
+		         }
+		      },
+		      "annotationLine":{ //$NON-NLS-0$
+		         "currentLine":{ //$NON-NLS-0$
+		            "backgroundColor":"#101112" //$NON-NLS-1 //$NON-NLS-0$ 
+		         }
+		      },
+		      "comment":{ //$NON-NLS-0$
+		         "color":"#41535b", //$NON-NLS-1 //$NON-NLS-0$ 
+		         "block":{ //$NON-NLS-0$
+		            "color":"#41535b" //$NON-NLS-1 //$NON-NLS-0$ 
+		         },
+		         "line":{ //$NON-NLS-0$
+		            "color":"#41535b" //$NON-NLS-1 //$NON-NLS-0$ 
+		         }
+		      },
+		      "constant":{ //$NON-NLS-0$
+		         "color":"#cd3f45", //$NON-NLS-1 //$NON-NLS-0$ 
+		         "numeric":{ //$NON-NLS-0$
+		            "color":"#cd3f45", //$NON-NLS-1 //$NON-NLS-0$ 
+		            "hex":{ //$NON-NLS-0$
+		               "color":"#cd3f45" //$NON-NLS-1 //$NON-NLS-0$ 
+		            }
+		         }
+		      },
+		      "entity":{ //$NON-NLS-0$
+		         "name":{ //$NON-NLS-0$
+		            "color":"#55b5db", //$NON-NLS-1 //$NON-NLS-0$ 
+		            "function":{ //$NON-NLS-0$
+		               "fontWeight":"normal", //$NON-NLS-1 //$NON-NLS-0$ 
+		               "color":"#55b5db" //$NON-NLS-1 //$NON-NLS-0$ 
+		            }
+		         },
+		         "other":{ //$NON-NLS-0$
+		            "attribute-name":{ //$NON-NLS-0$
+		               "color":"cadetBlue" //$NON-NLS-1 //$NON-NLS-0$ 
+		            }
+		         }
+		      },
+		      "keyword":{ //$NON-NLS-0$
+		         "control":{ //$NON-NLS-0$
+		            "color":"#9fca56", //$NON-NLS-1 //$NON-NLS-0$ 
+		            "fontWeight":"normal" //$NON-NLS-1 //$NON-NLS-0$ 
+		         },
+		         "operator":{ //$NON-NLS-0$
+		            "color":"#e6cd69", //$NON-NLS-1 //$NON-NLS-0$ 
+		            "fontWeight":"normal" //$NON-NLS-1 //$NON-NLS-0$ 
+		         },
+		         "other":{ //$NON-NLS-0$
+		            "documentation":{ //$NON-NLS-0$
+		               "color":"#7F9FBF" //$NON-NLS-1 //$NON-NLS-0$ 
+		            }
+		         }
+		      },
+		      "markup":{ //$NON-NLS-0$
+		         "bold":{ //$NON-NLS-0$
+		            "fontWeight":"bold" //$NON-NLS-1 //$NON-NLS-0$ 
+		         },
+		         "heading":{ //$NON-NLS-0$
+		            "color":"blue" //$NON-NLS-1 //$NON-NLS-0$ 
+		         },
+		         "italic":{ //$NON-NLS-0$
+		            "fontStyle":"italic" //$NON-NLS-1 //$NON-NLS-0$ 
+		         },
+		         "list":{ //$NON-NLS-0$
+		            "color":"#CC4C07" //$NON-NLS-1 //$NON-NLS-0$ 
+		         },
+		         "other":{ //$NON-NLS-0$
+		            "separator":{ //$NON-NLS-0$
+		               "color":"#00008F" //$NON-NLS-1 //$NON-NLS-0$ 
+		            },
+		            "strikethrough":{ //$NON-NLS-0$
+		               "textDecoration":"line-through" //$NON-NLS-1 //$NON-NLS-0$ 
+		            },
+		            "table":{ //$NON-NLS-0$
+		               "color":"#3C802C" //$NON-NLS-1 //$NON-NLS-0$ 
+		            }
+		         },
+		         "quote":{ //$NON-NLS-0$
+		            "color":"#55b5db" //$NON-NLS-1 //$NON-NLS-0$ 
+		         },
+		         "raw":{ //$NON-NLS-0$
+		            "fontFamily":"monospace" //$NON-NLS-1 //$NON-NLS-0$ 
+		         },
+		         "underline":{ //$NON-NLS-0$
+		            "link":{ //$NON-NLS-0$
+		               "textDecoration":"underline" //$NON-NLS-1 //$NON-NLS-0$ 
+		            }
+		         }
+		      },
+		      "meta":{ //$NON-NLS-0$
+		         "documentation":{ //$NON-NLS-0$
+		            "annotation":{ //$NON-NLS-0$
+		               "color":"#7F9FBF" //$NON-NLS-1 //$NON-NLS-0$ 
+		            },
+		            "tag":{ //$NON-NLS-0$
+		               "color":"#7F7F9F" //$NON-NLS-1 //$NON-NLS-0$ 
+		            }
+		         },
+		         "tag":{ //$NON-NLS-0$
+		            "color":"#CC4C07" //$NON-NLS-1 //$NON-NLS-0$ 
+		         }
+		      },
+		      "string":{ //$NON-NLS-0$
+		         "color":"#55b5db", //$NON-NLS-1 //$NON-NLS-0$ 
+		         "quoted":{ //$NON-NLS-0$
+		            "single":{ //$NON-NLS-0$
+		               "color":"#55b5db" //$NON-NLS-1 //$NON-NLS-0$ 
+		            },
+		            "double":{ //$NON-NLS-0$
+		               "color":"#55b5db" //$NON-NLS-1 //$NON-NLS-0$ 
+		            }
+		         }
+		      },
+		      "support":{ //$NON-NLS-0$
+		         "type":{ //$NON-NLS-0$
+		            "propertyName":{ //$NON-NLS-0$
+		               "color":"#9F4177" //$NON-NLS-1 //$NON-NLS-0$ 
+		            }
+		         }
+		      },
+		      "variable":{ //$NON-NLS-0$
+		         "language":{ //$NON-NLS-0$
+		            "color":"#9fca56", //$NON-NLS-1 //$NON-NLS-0$ 
+		            "fontWeight":"normal" //$NON-NLS-1 //$NON-NLS-0$ 
+		         },
+		         "other":{ //$NON-NLS-0$
+		            "color":"#E038AD" //$NON-NLS-1 //$NON-NLS-0$ 
+		         },
+		         "parameter":{ //$NON-NLS-0$
+		            "color":"#55b5db" //$NON-NLS-1 //$NON-NLS-0$ 
+		         }
+		      }
+		   }
+		};
+		this.styles.push(seti);
 
 		var eclipse = {
 			name: messages["eclipseThemeName"],
@@ -1846,10 +2022,220 @@ define([
 		
 		ThemeData.prototype.selectFontSize = selectFontSize;
 		
+		// Changes XML to JSON
+		function xmlToJson(xml) {
+			// Create the return object
+			var obj = {};
+			if (xml.nodeType == 1) { // element
+				// do attributes
+				if (xml.attributes.length > 0) {
+					for (var j = 0; j < xml.attributes.length; j++) {
+						var attribute = xml.attributes.item(j);
+						obj[attribute.nodeName] = attribute.nodeValue;
+					}
+				}
+			} else if (xml.nodeType == 3) { // text
+				obj = xml.nodeValue.trim(); // add trim here
+			}
+			// do children
+			if (xml.hasChildNodes()) {
+				for(var i = 0; i < xml.childNodes.length; i++) {
+					var item = xml.childNodes.item(i);
+					var nodeName = item.nodeName;
+					if (typeof(obj[nodeName]) == "undefined") { //$NON-NLS-0$
+						var tmp = xmlToJson(item);
+						if(tmp != "") // if not empty string
+							obj[nodeName] = tmp;
+					} else {
+						if (typeof(obj[nodeName].push) == "undefined") { //$NON-NLS-0$
+							var old = obj[nodeName];
+							obj[nodeName] = [];
+							obj[nodeName].push(old);
+						}
+						var tmp = xmlToJson(item);
+						if(tmp != "") // if not empty string
+							obj[nodeName].push(tmp);
+					}
+				}
+			}
+			return obj;
+		}		
+		
 		function importTheme(data) {
 			var body = data.parameters.valueFor("name"); //$NON-NLS-0$
 			var xml = this.parseToXML(body);
-			if (xml) {
+			
+			if(xml.children[0].tagName === "plist"){ //$NON-NLS-0$ //assume it uses tmTheme structure
+				var themeJson = xmlToJson(xml); //convert to Json
+				var newStyle = new StyleSet(); //sets the default styling
+				newStyle = {"name":"default","className":"default","styles":{"annotationRange":{"matchingSearch":{"backgroundColor":""}, "currentSearch":{"backgroundColor":"","text-decoration":"underline"}},"backgroundColor":"","color":"","fontFamily":"\"Consolas\", \"Monaco\", \"Vera Mono\", monospace","fontSize":"16px","textviewRightRuler":{"borderLeft":"1px solid rgba(131, 131, 131, 0.05)"},"textviewLeftRuler":{"borderRight":"1px solid rgba(131, 131, 131, 0.05)"},"ruler":{"backgroundColor":"","overview":{"backgroundColor":""},"annotations":{"backgroundColor":""}},"rulerLines":{"color":"","odd":{"color":""},"even":{"color":""}},"annotationLine":{"currentLine":{"backgroundColor":"rgba(255, 255, 255, 0.05)"}},"comment":{"color":"","block":{"color":""},"line":{"color":""}},"constant":{"color":"","numeric":{"color":"","hex":{"color":""}}},"entity":{"name":{"color":"","function":{"fontWeight":"normal","color":""}},"other":{"attribute-name":{"color":""}}},"keyword":{"control":{"color":"","fontWeight":"normal"},"operator":{"color":"","fontWeight":"normal"},"other":{"documentation":{"color":""}}},"markup":{"bold":{"fontWeight":"bold"},"heading":{"color":"blue"},"italic":{"fontStyle":"italic"},"list":{"color":"#CC4C07"},"other":{"separator":{"color":"#00008F"},"strikethrough":{"textDecoration":"line-through"},"table":{"color":"#3C802C"}},"quote":{"color":"#55b5db"},"raw":{"fontFamily":"monospace"},"underline":{"link":{"textDecoration":"underline"}}},"meta":{"documentation":{"annotation":{"color":""},"tag":{"color":""}},"tag":{"color":""}},"string":{"color":"","quoted":{"single":{"color":""},"double":{"color":""}}},"support":{"type":{"propertyName":{"color":"#9F4177"}}},"variable":{"language":{"color":"","fontWeight":"normal"},"other":{"color":""},"parameter":{"color":""}}}				} //$NON-NLS-0$
+				//finds the name tag
+				for(var i = 0; i < themeJson.plist[1].dict.key.length; i++){
+					if(themeJson.plist[1].dict.key[i]["#text"] === "name"){ //$NON-NLS-0$
+						newStyle.name = themeJson.plist[1].dict.string[i]["#text"];
+						newStyle.className = newStyle.name.replace(/\s+/g, '');
+					}
+				}
+				var dictKey = themeJson.plist[1].dict.array.dict[0].dict.key;
+				var dictString = themeJson.plist[1].dict.array.dict[0].dict.string;
+				
+				//finds the general attributes
+				for(var i = 0; i<dictKey.length; i++){
+					if(dictKey[i]["#text"] === "background" && dictString[i]["#text"].length < 8){ //$NON-NLS-0$
+						newStyle.styles.backgroundColor = dictString[i]["#text"];
+					}
+					else if(dictKey[i]["#text"] === "foreground" && dictString[i]["#text"].length < 8){ //$NON-NLS-0$
+						newStyle.styles.color = dictString[i]["#text"];
+					}
+					else if(dictKey[i]["#text"] === "lineHighlight" && dictString[i]["#text"].length < 8){ //$NON-NLS-0$
+						newStyle.styles.annotationLine.currentLine.backgroundColor = dictString[i]["#text"];
+					}////annotationRange matchingSearch
+					else if(dictKey[i]["#text"] === "selection" && dictString[i]["#text"].length < 8){ //$NON-NLS-0$
+						newStyle.styles.annotationRange.matchingSearch.backgroundColor = dictString[i]["#text"];
+						newStyle.styles.annotationRange.currentSearch.backgroundColor = dictString[i]["#text"];
+					}
+				}
+				//finds the scope attributes
+				var restKey = themeJson.plist[1].dict.array.dict;
+				for(var i = 1; i< restKey.length; i++){
+					try{
+						var target = restKey[i].string[0]["#text"].split(",");
+						for (var k = 0; k < target.length; k++){
+							var found = false;
+							if(target[k].trim() === "Comment"){ //$NON-NLS-0$
+								found = true;
+								if (restKey[i].dict.key instanceof Array){
+									for(var l = 0; l< restKey[i].dict.key.length; l++){
+										if (restKey[i].dict.key[l]["#text"] === "foreground"){ //$NON-NLS-0$
+											newStyle.styles.comment.color = restKey[i].dict.string[l]["#text"];
+											newStyle.styles.comment.block.color = restKey[i].dict.string[l]["#text"];
+											newStyle.styles.comment.line.color = restKey[i].dict.string[l]["#text"];
+										}
+									}
+								}
+								else{
+									if (restKey[i].dict.key["#text"] === "foreground"){ //$NON-NLS-0$
+										newStyle.styles.comment.color = restKey[i].dict.string["#text"];
+										newStyle.styles.comment.block.color = restKey[i].dict.string["#text"];
+										newStyle.styles.comment.line.color = restKey[i].dict.string["#text"];
+									}
+								}
+							}
+							else if(target[k].trim() === "Keyword"){ //$NON-NLS-0$
+								found = true;
+								if (restKey[i].dict.key instanceof Array){
+									for(var l = 0; l< restKey[i].dict.key.length; l++){
+										if (restKey[i].dict.key[l]["#text"] === "foreground"){ //$NON-NLS-0$
+											newStyle.styles.keyword.control.color = restKey[i].dict.string[l]["#text"];
+										}
+									}
+								}
+								else{
+									if (restKey[i].dict.key["#text"] === "foreground"){ //$NON-NLS-0$
+										newStyle.styles.keyword.control.color = restKey[i].dict.string["#text"];
+									}
+								}
+							}
+							else if(target[k].trim() === "Variable" || target[k].trim() === "Function argument"){ //$NON-NLS-1$ //$NON-NLS-0$
+								found = true;
+								if (restKey[i].dict.key instanceof Array){
+									for(var l = 0; l< restKey[i].dict.key.length; l++){
+										if (restKey[i].dict.key[l]["#text"] === "foreground"){ //$NON-NLS-0$
+											newStyle.styles.variable.language.color = restKey[i].dict.string[l]["#text"];
+											newStyle.styles.variable.other.color = restKey[i].dict.string[l]["#text"];
+											newStyle.styles.variable.parameter.color = restKey[i].dict.string[l]["#text"];
+										}
+									}
+								}
+								else{
+									if (restKey[i].dict.key["#text"] === "foreground"){ //$NON-NLS-0$
+										newStyle.styles.variable.language.color = restKey[i].dict.string["#text"];
+										newStyle.styles.variable.other.color = restKey[i].dict.string["#text"];
+										newStyle.styles.variable.parameter.color = restKey[i].dict.string["#text"];
+									}
+								}
+							}
+							else if(target[k].trim() === "Constant" || target[k].trim() === "Number"){ //$NON-NLS-1$ //$NON-NLS-0$
+								found = true;
+								if (restKey[i].dict.key instanceof Array){
+									for(var l = 0; l< restKey[i].dict.key.length; l++){
+										if (restKey[i].dict.key[l]["#text"] === "foreground"){ //$NON-NLS-0$
+											newStyle.styles.constant.color = restKey[i].dict.string[l]["#text"];
+											newStyle.styles.constant.numeric.color = restKey[i].dict.string[l]["#text"];
+											newStyle.styles.constant.numeric.hex.color = restKey[i].dict.string[l]["#text"];
+										}
+									}
+								}
+								else{
+									if (restKey[i].dict.key["#text"] === "foreground"){ //$NON-NLS-0$
+										newStyle.styles.constant.color = restKey[i].dict.string["#text"];
+										newStyle.styles.constant.numeric.color = restKey[i].dict.string["#text"];
+										newStyle.styles.constant.numeric.hex.color = restKey[i].dict.string["#text"];
+									}
+								}
+							}
+							else if(target[k].trim() === "String"){ //$NON-NLS-0$
+								found = true;
+								if (restKey[i].dict.key instanceof Array){
+									for(var l = 0; l< restKey[i].dict.key.length; l++){
+										if (restKey[i].dict.key[l]["#text"] === "foreground"){ //$NON-NLS-0$
+											newStyle.styles.string.color = restKey[i].dict.string[l]["#text"];
+											newStyle.styles.string.quoted.single.color = restKey[i].dict.string[l]["#text"];
+											newStyle.styles.string.quoted.double.color = restKey[i].dict.string[l]["#text"];
+										}
+									}
+								}
+								else{
+									if (restKey[i].dict.key["#text"] === "foreground"){ //$NON-NLS-0$
+										newStyle.styles.string.color = restKey[i].dict.string["#text"];
+										newStyle.styles.string.quoted.single.color = restKey[i].dict.string["#text"];
+										newStyle.styles.string.quoted.double.color = restKey[i].dict.string["#text"];
+									}
+								}
+							}
+							else if(target[k].trim() === "Storage" || target[k].trim() === "Storage type"){ //$NON-NLS-1$ //$NON-NLS-0$
+								found = true;
+								if (restKey[i].dict.key instanceof Array){
+									for(var l = 0; l< restKey[i].dict.key.length; l++){
+										if (restKey[i].dict.key[l]["#text"] === "foreground"){ //$NON-NLS-0$
+											newStyle.styles.keyword.operator.color = restKey[i].dict.string[l]["#text"];
+										}
+									}
+								}
+								else{
+									if (restKey[i].dict.key["#text"] === "foreground"){ //$NON-NLS-0$
+										newStyle.styles.keyword.operator.color = restKey[i].dict.string["#text"];
+									}
+								}
+							}
+							else if(target[k].trim() === "Function" || target[k].trim() === "Entity" || target[k].trim() === "Function name"){ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+								found = true;
+								if (restKey[i].dict.key instanceof Array){
+									for(var l = 0; l< restKey[i].dict.key.length; l++){
+										if (restKey[i].dict.key[l]["#text"] === "foreground"){ //$NON-NLS-0$
+											newStyle.styles.entity.name.color = restKey[i].dict.string[l]["#text"];
+											newStyle.styles.entity.name["function"].color = restKey[i].dict.string[l]["#text"];
+										}
+									}
+								}
+								else{
+									if (restKey[i].dict.key["#text"] === "foreground"){ //$NON-NLS-0$
+										newStyle.styles.entity.name.color = restKey[i].dict.string["#text"];
+											newStyle.styles.entity.name["function"].color = restKey[i].dict.string["#text"];
+									}
+								}
+							}
+							
+							if(found === false)
+								console.log("Theme scope ignored : " + target[k].trim()); //$NON-NLS-0$
+						}
+					}
+					catch (e){
+						console.log("Exception : " + e); //$NON-NLS-0$
+					}
+				}
+			}
+			else if (xml) {
 				/* old-style theme definition */
 				var newStyle = new StyleSet();
 				
