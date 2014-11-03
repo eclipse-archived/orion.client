75b7d7d980fbe61ae27fcd00e5982cf78a2db55e
Grant Gayed <grant_gayed@ca.ibm.com>
2014-11-03 14:48:13 -0500

    don't track all editor commands

--- bundles/org.eclipse.orion.client.editor/web/orion/editor/textView.js
+++ bundles/org.eclipse.orion.client.editor/web/orion/editor/textView.js
@@ -2102,10 +2102,9 @@ define("orion/editor/textView", [  //$NON-NLS-0$
 			if (!this._clientDiv) { return; }
 			var action = this._actions[actionID];
 			if (action) {
-				var id = action.actionDescription ?
-					action.actionDescription.id || action.actionDescription.name + " (no id)" : //$NON-NLS-0$
-					"(unknown)"; //$NON-NLS-0$
-				mMetrics.logEvent("editor", "action", id); //$NON-NLS-1$ //$NON-NLS-0$
+				if (action.actionDescription && action.actionDescription.id) {
+					mMetrics.logEvent("editor", "action", action.actionDescription.id); //$NON-NLS-1$ //$NON-NLS-0$
+				}
 				if (!defaultAction && action.handler) {
 					if (action.handler(actionOptions)) {
 						return true;
