ba6aee308818b386571a9ce40f6f395374252830
Mike Rennie <Michael_Rennie@ca.ibm.com>
2014-11-20 11:46:31 -0500

    Bug 427299 - Add support to find the declaration of a node - hooks for define/require/importScripts

--- bundles/org.eclipse.orion.client.javascript/web/javascript/commands/openDeclaration.js
+++ bundles/org.eclipse.orion.client.javascript/web/javascript/commands/openDeclaration.js
@@ -9,7 +9,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
- /*eslint-env amd*/
+ /*eslint-env amd, browser*/
 define([
 'orion/objects',
 'javascript/finder',
@@ -20,16 +20,20 @@ define([
 	 * @description Creates a new open declaration command
 	 * @constructor
 	 * @public
+	 * @param {javascript.ASTManager} ASTManager The backing AST manager
+	 * @param {javascript.ScriptResolver} Resolver The backing script resolver 
 	 * @returns {javascript.commands.OpenDeclarationCommand} A new command
 	 * @since 8.0
 	 */
-	function OpenDeclarationCommand(ASTManager) {
+	function OpenDeclarationCommand(ASTManager, Resolver) {
 		this.astManager = ASTManager;
+		this.resolver = Resolver;
 	}
 	
 	Objects.mixin(OpenDeclarationCommand.prototype, {
 		/* override */
-		execute: function(editorContext, options) {
+		execute: function(editorContext/*, options*/) {
+		    var that = this;
 			return Deferred.all([
 				this.astManager.getAST(editorContext),
 				editorContext.getCaretOffset()
@@ -40,14 +44,32 @@ define([
 				    var parent = parents.pop();
 				    switch(parent.type) {
 				        case 'CallExpression': {
-				            if(parent.callee.type === 'Identifier') {
-    				            var decl = Finder.findDeclaration(results[1], results[0], {id: parent.callee.name, kind: Finder.SearchOptions.FUNCTION_DECLARATION});
-            					if(decl) {
-            					    return editorContext.setSelection(decl.id.range[0], decl.id.range[1]);
+				            if(node.type === 'Literal' && (parent.callee.name === 'require' || parent.callee.name === 'importScripts')) {
+				                that.resolver.getWorkspaceFile(node.value).then(function(files) {
+				                    // TODO uncomment when we get a file open strategy
+				                    //window.open(that.resolver.convertToURL(files[0]));
+				                });
+				            } else {
+    				            if(parent.callee.type === 'Identifier') {
+        				            var decl = Finder.findDeclaration(results[1], results[0], {id: parent.callee.name, kind: Finder.SearchOptions.FUNCTION_DECLARATION});
+                					if(decl) {
+                					    return editorContext.setSelection(decl.id.range[0], decl.id.range[1]);
+                					}
+            					} else if(parent.callee.type === 'MemberExpression') {
+            					    //TODO need the env to find the containing object expression / func expr
             					}
-        					} else if(parent.callee.type === 'MemberExpression') {
-        					    //TODO need the env to find the containing object expression / func expr
         					}
+        					break;
+				        }
+				        case 'ArrayExpression': {
+				            parent = parents.pop();
+				            if(parent.type === 'CallExpression' && parent.callee.name === 'define') {
+				                that.resolver.getWorkspaceFile(node.value).then(function(files) {
+				                    // TODO uncomment when we get a file open strategy
+				                    //window.open(that.resolver.convertToURL(files[0]));
+				                });
+				            }
+				            break;
 				        }
 				    }
 				}
