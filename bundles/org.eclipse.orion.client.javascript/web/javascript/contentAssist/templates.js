6a6f57a1616f1566d3f7f2d2f3c8b79e199bdc1b
Mike Rennie <Michael_Rennie@ca.ibm.com>
2014-12-10 15:09:36 -0500

    [nobug] update define template to use eslint-env rather than globals

--- bundles/org.eclipse.orion.client.javascript/web/javascript/contentAssist/templates.js
+++ bundles/org.eclipse.orion.client.javascript/web/javascript/contentAssist/templates.js
@@ -341,8 +341,8 @@ define([
 			name: "define", //$NON-NLS-0$
 			nodes: {top:true, member:false, prop:false},
 			description: " - define function call",  //$NON-NLS-0$
-			template: "/* global define */\n"+
-					  "define('${name}',[\n"+  //$NON-NLS-0$
+			template: "/* eslint-env amd */\n"+
+					  "define('${name}', [\n"+  //$NON-NLS-0$
 					  "'${import}'\n"+  //$NON-NLS-0$
 					  "], function(${importname}) {\n"+  //$NON-NLS-0$
 					  "\t${cursor}\n"+  //$NON-NLS-0$
