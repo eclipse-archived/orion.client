f6b3847336d566c1d98ef56f781293936fa3da0f
Anton McConville <antonm@ca.ibm.com>
2015-01-13 16:46:21 -0500

    Up-issue of theme version

--- bundles/org.eclipse.orion.client.ui/web/orion/widgets/themes/ThemeVersion.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/widgets/themes/ThemeVersion.js
@@ -14,7 +14,7 @@ define([], function() {
 	/**
 	 * Version string for theme data. Please update this string whenever you change the style of a themable element.
 	 */
-	var THEMES_VERSION = "6.57";
+	var THEMES_VERSION = "6.58";
 
 	return THEMES_VERSION;
 });
