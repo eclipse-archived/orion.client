eclipse-archived/orion.client

commit 016e38b2b8d6f31612b0b20280d18b7b43ca9c65

[nobug] Synthetic launch confs can not be deleted

bundles/org.eclipse.orion.client.ui/web/orion/widgets/projects/RunBar.js
@@ -152,8 +152,10 @@ define([
 							this._commandRegistry.registerCommandContribution(menuItem.id, "orion.launchConfiguration.delete", 1); //$NON-NLS-0$
 							domNodeWrapperList = [];
 							this._commandRegistry.renderCommands(menuItem.id, menuItem.firstChild, launchConfiguration, this, "tool", null, domNodeWrapperList); //$NON-NLS-0$
-							domNodeWrapperList[0].domNode.classList.remove("commandMargins"); //$NON-NLS-0$
-							domNodeWrapperList[0].domNode.classList.add("launchConfigurationDeleteButton"); //$NON-NLS-0$
+							if (domNodeWrapperList.lenght > 0){
+								domNodeWrapperList[0].domNode.classList.remove("commandMargins"); //$NON-NLS-0$
+								domNodeWrapperList[0].domNode.classList.add("launchConfigurationDeleteButton"); //$NON-NLS-0$
+							}
 							
 							this._menuItemsCache.push(menuItem);
 							

bundles/org.eclipse.orion.client.ui/web/orion/projectCommands.js
@@ -715,7 +715,7 @@ define(['require', 'i18n!orion/navigate/nls/messages', 'orion/webui/littlelib', 
 			},
 			visibleWhen: function(items) {
 				var item = forceSingleItem(items);
-				return item.ServiceId && item.Name;
+				return item.ServiceId && item.Name && item.Params && !item.Params.Synthetic;
 			}
 		});
 		commandService.addCommand(deleteLaunchConfigurationCommand);

commit 10bfce9da77651ad13e1a7fea4aff4c04ee6903f

Fixed typo introduced in commit 016e38b2b8d6f31612b0b20280d18b7b43ca9c65

bundles/org.eclipse.orion.client.ui/web/orion/widgets/projects/RunBar.js
@@ -152,7 +152,7 @@ define([
 							this._commandRegistry.registerCommandContribution(menuItem.id, "orion.launchConfiguration.delete", 1); //$NON-NLS-0$
 							domNodeWrapperList = [];
 							this._commandRegistry.renderCommands(menuItem.id, menuItem.firstChild, launchConfiguration, this, "tool", null, domNodeWrapperList); //$NON-NLS-0$
-							if (domNodeWrapperList.lenght > 0){
+							if (domNodeWrapperList.length > 0){
 								domNodeWrapperList[0].domNode.classList.remove("commandMargins"); //$NON-NLS-0$
 								domNodeWrapperList[0].domNode.classList.add("launchConfigurationDeleteButton"); //$NON-NLS-0$
 							}