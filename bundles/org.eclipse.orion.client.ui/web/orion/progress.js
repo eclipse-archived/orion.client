3e8d0af928901428b2a8c82bcae9d057d9abc39a
Paul Webster <pwebster@ca.ibm.com>
2015-02-27 10:24:10 -0500

    Bug 460694 - Merge status and operation notification system
    Have the task/operation module responsible for status operations.
    
    Change-Id: Ic33ed30e56aac7ccb9a23abb85992973224af540
    Signed-off-by: Paul Webster <pwebster@ca.ibm.com>

--- bundles/org.eclipse.orion.client.ui/web/orion/progress.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/progress.js
@@ -9,8 +9,8 @@
  * Contributors: IBM Corporation - initial API and implementation
  *******************************************************************************/
 /*eslint-env browser, amd*/
-define(['i18n!orion/nls/messages', 'orion/webui/littlelib', 'orion/webui/dialogs/OperationsDialog'], 
-function(messages, lib, mOperationsDialog) {
+define(['i18n!orion/nls/messages', 'orion/webui/littlelib', 'orion/webui/dialogs/OperationsDialog', 'orion/Deferred'], 
+function(messages, lib, mOperationsDialog, Deferred) {
 	
 	function ProgressMonitorTool(progressPane, commandRegistry){
 		if(this._progressPane){
@@ -138,12 +138,16 @@ function(messages, lib, mOperationsDialog) {
 		this._progressMonitorClass = progressMonitorClass;
 	}
 	
+
 	ProgressService.prototype = /** @lends orion.progress.ProgressService.prototype */ {
 			init: function(progressPane){
 				if(this._progressMonitorClass){
 					return; // we have an other progress monitor implementation, we don't need to initialize our UI
 				}
 				this._progressMonitorTool = new ProgressMonitorTool(progressPane, this._commandRegistry);
+			},
+			getNewoperationIndex: function() {
+				return this._operationsIndex++;
 			},
 			progress: function(deferred, operationName, progressMonitor){
 				var that = this;
@@ -228,6 +232,10 @@ function(messages, lib, mOperationsDialog) {
 			},
 			setProgressResult: function(result){
 				this._serviceRegistry.getService("orion.page.message").setProgressResult(result); //$NON-NLS-0$
+				var operation = this._operationsClient.createOperation(result.Message, "loadend", result);
+				var deferred = new Deferred();
+				deferred.resolve(result);
+				this.writeOperation(this._operationsIndex++, operation, deferred);
 			},
 			/**
 			 * Shows a progress message until the given promise is resolved. Returns a promise that resolves when
