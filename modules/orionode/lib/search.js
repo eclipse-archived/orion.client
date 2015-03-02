1d6c9e4fa4d7e844519a50f75d8010910c28d06f
Hyunjoon Song <rsong@mit.edu>
2015-03-02 01:19:17 -0500

    Bug 458884 - node.js implementation of fast grep search
    - Implemented SearchOptions class that parses necessary information from the request
    
    Signed-off-by: Hyunjoon Song <rsong@mit.edu>

--- modules/orionode/lib/search.js
+++ modules/orionode/lib/search.js
@@ -21,34 +21,72 @@ module.exports = function(options) {
 	var workspaceRoot = options.root;
 	var workspaceDir = options.workspaceDir;
 	if (!workspaceRoot) { throw 'options.root path required'; }
+	var workspaceId = 'orionode';
+	var workspaceName = 'Orionode Workspace';
+
+	var fieldNames = "Name,NameLower,Length,Directory,LastModified,Location,Path,RegEx,CaseSensitive";
+	var fieldList = fieldNames.split(",");
+
+	function isSearchField(term) {
+		for (var i = 0; i < fieldList.length; i++) {
+			if (term.lastIndexOf(fieldList[i] + ":", 0) === 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function searchOptions(req, res){
+		this.defaultLocation = null;
+		this.fileContentSearch = false;
+		this.filenamePattern = null;
+		this.filenamePatternCaseSensitive = false;
+		this.location = null;
+		this.regEx = false;
+		this.rows = 10000;
+		this.scopes = [];
+		this.searchTerm = null;
+		this.searchTermCaseSensitive = false;
+		this.username = null;
+
+		this.buildOptions = function() {
+			var queryObject = url.parse(req.url, true).query;
+			var terms = queryObject.q.split(" ");
+			for (var i = 0; i < terms.length; i++) {
+				term = terms[i];
+				if (isSearchField(term)) {
+					if (term.lastIndexOf("NameLower:", 0) === 0) {
+						this.filenamePatternCaseSensitive = false;
+						this.filenamePattern = term.substring(10);
+					} else if (term.lastIndexOf("Location:", 0) === 0) {
+						this.location = term.substring(9 + fileUtil.getContextPath(req).length);
+					} else if (term.lastIndexOf("Name:", 0) === 0) {
+						this.filenamePatternCaseSensitive = true;
+						this.filenamePattern = term.substring(5);
+					} else if (term.lastIndexOf("RegEx:", 0) === 0) {
+						this.regEx = true;
+					} else if (term.lastIndexOf("CaseSensitive:", 0) === 0) {
+						this.searchTermCaseSensitive = true;
+					}
+				} else {
+					this.searchTerm = term;
+					this.fileContentSearch = true;
+				}
+			}
+
+			this.defaultLocation = "/file/" + workspaceId;
+		}
+	};
+
 
 	return connect()
 	.use(connect.json())
 	.use(resource(workspaceRoot, {
 		GET: function(req, res, next, rest) {
-			var queryObject = url.parse(req.url, true).query;
+			var search = new searchOptions(req, res);
+			search.buildOptions();
 			var ws = JSON.stringify({
-					    "responseHeader": {
-					        "status": 0,
-					        "QTime": 1,
-					        "params": {
-					            "wt": "json",
-					            "fl": "Name,NameLower,Length,Directory,LastModified,Location,Path",
-					            "fq": [
-					                "Location:/file*",
-					                "UserName:rsong6"
-					            ],
-					            "rows": "10000",
-					            "start": "0",
-					            "sort": "Path asc",
-					            "q": ". Name:plugin.js Location:/file*"
-					        }
-					    },
-					    "response": {
-					        "numFound": 0,
-					        "start": 0,
-					        "docs": []
-					    }
+					    "response": search
 					});
 			res.setHeader('Content-Type', 'application/json');
 			res.end(ws);
