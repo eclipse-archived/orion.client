23d75ae8c16dddfbf97d16f691975668feb9f8b6
Mark Macdonald <mamacdon@gmail.com>
2015-03-12 17:36:19 -0400

    Package standalone HTML & CSS content assist in editor zip

---
+++ releng/org.eclipse.orion.client.releng/builder/scripts/contentassist.css.build.almond-js.js
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*global Packages*/
+// optimization script to concat/minify the Orion editor javascript code
+({
+	baseUrl: ".",
+	closure: {
+		CompilerOptions: {
+			languageIn: Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode.valueOf(Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode, "ECMASCRIPT5")
+		},
+		CompilationLevel: "SIMPLE_OPTIMIZATIONS",
+		loggingLevel: "WARNING"
+	},
+	paths: {
+		i18n: "requirejs/i18n",
+		csslint: "csslint/csslint",
+	},
+	name: "requirejs/almond",
+	include: ["webtools/cssContentAssist"],
+	preserveLicenseComments: false,
+	uglify: {
+		ascii_only: true
+	},
+	wrap: {
+		start: "\
+			(function (root, factory) {\
+				if (typeof define === 'function' && define.amd) {\
+					define([], factory);\
+				} else {\
+					root.orion = root.orion || {};\
+					root.orion.webtools = root.orion.webtools || {};\
+					root.orion.webtools.cssContentAssist = factory();\
+				}\
+			}(this, function () {\
+		",
+		end: "\
+				return require('webtools/cssContentAssist').CssContentAssistProvider;\
+			}));\
+		"
+	}
+})
---
+++ releng/org.eclipse.orion.client.releng/builder/scripts/contentassist.html.build.almond-js.js
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*global Packages*/
+// optimization script to concat/minify the Orion editor javascript code
+({
+	baseUrl: ".",
+	closure: {
+		CompilerOptions: {
+			languageIn: Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode.valueOf(Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode, "ECMASCRIPT5")
+		},
+		CompilationLevel: "SIMPLE_OPTIMIZATIONS",
+		loggingLevel: "WARNING"
+	},
+	paths: {
+		i18n: "requirejs/i18n",
+		csslint: "csslint/csslint",
+	},
+	name: "requirejs/almond",
+	include: ["webtools/htmlContentAssist"],
+	preserveLicenseComments: false,
+	uglify: {
+		ascii_only: true
+	},
+	// https://github.com/jrburke/almond#exporting-a-public-api
+	wrap: {
+		start: "\
+			(function (root, factory) {\
+				if (typeof define === 'function' && define.amd) {\
+					define([], factory);\
+				} else {\
+					root.orion = root.orion || {};\
+					root.orion.webtools = root.orion.webtools || {};\
+					root.orion.webtools.htmlContentAssist = factory();\
+				}\
+			}(this, function () {\
+		",
+		end: "\
+				return require('webtools/htmlContentAssist').HTMLContentAssistProvider;\
+			}));\
+		"
+	}
+})
