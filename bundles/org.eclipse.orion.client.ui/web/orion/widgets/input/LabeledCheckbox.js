1390801aebf07494ef43f0d85b888ea5f7a0d0ed
Umar Bolatov <ubolatov@ca.ibm.com>
2015-03-25 12:21:05 -0400

    Bug 459160 - "Show Code Map Ruler" label on "Local Editor Settings" popup truncated
    -Created new HTML templates for the "Local Editor Settings" popup
    -Changed the CSS do display "Local Editor Settings" popup settings as a table
    -Changed the CSS for the "text" input type to visually align it with a "select" input type
    
    Change-Id: I7e8c357a6423340c09671f209aba0f7dfd428bc1
    Signed-off-by: Umar Bolatov <ubolatov@ca.ibm.com>

--- bundles/org.eclipse.orion.client.ui/web/orion/widgets/input/LabeledCheckbox.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/widgets/input/LabeledCheckbox.js
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * @license
  * Copyright (c) 2013, 2014 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials are made 
- * available under the terms of the Eclipse Public License v1.0 
- * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
- * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
- * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html).
+ *
  * Contributors: IBM Corporation - initial API and implementation
  ******************************************************************************/
 /*eslint-env browser, amd*/
@@ -15,28 +15,40 @@ define(['orion/objects', 'orion/webui/littlelib', 'orion/widgets/input/Checkbox'
 	 * This is just an orion/widgets/input/Select with a label.
 	 */
 	function LabeledCheckbox( params, node ){
+		makeTemplate(params);
 		Checkbox.apply(this, arguments);
 		this.mylabel = lib.$(".setting-label", this.node); //$NON-NLS-0$
 	}
-	objects.mixin(LabeledCheckbox.prototype, Checkbox.prototype, {
-		templateString: '' +  //$NON-NLS-0$
+
+	function makeTemplate(params) {
+		var templateString = params.local
+			?
+			'<span class="setting-label"></span>' + //$NON-NLS-2$ //$NON-NLS-0$
+			'<input class="setting-control settingsCheckbox" type="checkbox"/>' //$NON-NLS-0$
+			:
+			'' +  //$NON-NLS-0$
 			'<label>' + //$NON-NLS-0$
 				'<span class="setting-label"></span>' + //$NON-NLS-2$ //$NON-NLS-0$
 				'<input class="setting-control settingsCheckbox" type="checkbox"/>' + //$NON-NLS-0$
-			'</label>',  //$NON-NLS-0$
+			'</label>'  //$NON-NLS-0$
 
-		postCreate: function() {
-			Checkbox.prototype.postCreate.call(this);
-            this.mylabel.textContent = this.fieldlabel;
-		},
+		objects.mixin(LabeledCheckbox.prototype, Checkbox.prototype, {
+			templateString: templateString,
 
-		destroy: function() {
-			Checkbox.prototype.destroy.call(this);
-			if (this.mylabel) {
-				this.mylabel = null;
+			postCreate: function() {
+				Checkbox.prototype.postCreate.call(this);
+				this.mylabel.textContent = this.fieldlabel;
+			},
+
+			destroy: function() {
+				Checkbox.prototype.destroy.call(this);
+				if (this.mylabel) {
+					this.mylabel = null;
+				}
 			}
-		}
-	});
+		});
+	}
+
 
 	return LabeledCheckbox;
 });
