4cce536e9ea1436d8df9dbecfae7c3ab24aaa3f8
Grant Gayed <Grant_Gayed@ca.ibm.com>
2015-04-06 14:25:36 -0400

    tweaks to fix for Bug 463712 - [HTML] Improve HTML syntax highlighting

--- bundles/org.eclipse.orion.client.editor/web/orion/editor/stylers/text_html/syntax.js
+++ bundles/org.eclipse.orion.client.editor/web/orion/editor/stylers/text_html/syntax.js
@@ -13,28 +13,31 @@
 define("orion/editor/stylers/text_html/syntax", ["orion/editor/stylers/application_javascript/syntax", "orion/editor/stylers/text_css/syntax", "orion/editor/stylers/application_xml/syntax"], //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
 	function(mJS, mCSS, mXML) {
 	var attributes = [
-		"accept-charset", "accept", "accesskey", "action", "align", "alt", "async", "autocomplete", "autoplay", "autosave",
-		"bgcolor", "border", "buffered",
-		"challenge", "charset", "checked", "cite", "class", "codebase", "code", "color", "colspan", "cols", "contenteditable", "content", "contextmenu", "controls", "coords",
-		"data-[A-Za-z]+", "data", "datetime", "default", "defer", "dirname", "dir", "disabled", "download", "draggable", "dropzone",
-		"enctype",
-		"formaction", "form", "for",
-		"headers", "height", "hidden", "high", "hreflang", "href", "http-equiv",
-		"icon", "id", "ismap", "itemprop",
-		"keytype", "kind",
-		"label", "language", "lang", "list", "loop", "low",
-		"manifest", "maxlength", "max", "media", "method", "min", "multiple",
-		"name", "novalidate",
-		"open", "optimum",
-		"pattern", "ping", "placeholder", "poster", "preload", "pubdate",
-		"radiogroup", "readonly", "rel", "required", "reversed", "rowspan", "rows",
-		"sandbox", "scope", "scoped", "seamless", "selected", "shape", "sizes", "size", "span", "spellcheck", "srcdoc", "srclang", "srcset", "src", "start", "step", "style", "summary",
-		"tabindex", "target", "title", "type",
-		"usemap",
-		"value",
-		"width",
-		"wrap"
-	]
+		"accept-charset", "accept", "accesskey", "action", "align", "alt", //$NON-NLS-5$ //$NON-NLS-4$ //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"async", "autocomplete", "autoplay", "autosave", //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"bgcolor", "border", "buffered", //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"challenge", "charset", "checked", "cite", "class", "codebase", "code", "color", //$NON-NLS-7$ //$NON-NLS-6$ //$NON-NLS-5$ //$NON-NLS-4$ //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"colspan", "cols", "contenteditable", "content", "contextmenu", "controls", "coords", //$NON-NLS-6$ //$NON-NLS-5$ //$NON-NLS-4$ //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"data-[A-Za-z_:][\\w.:-]*", "data", "datetime", "default", "defer", "dirname", "dir", //$NON-NLS-6$ //$NON-NLS-5$ //$NON-NLS-4$ //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"disabled", "download", "draggable", "dropzone", //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"enctype", //$NON-NLS-0$
+		"formaction", "form", "for", //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"headers", "height", "hidden", "high", "hreflang", "href", "http-equiv", //$NON-NLS-6$ //$NON-NLS-5$ //$NON-NLS-4$ //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"icon", "id", "ismap", "itemprop", //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"keytype", "kind", //$NON-NLS-1$ //$NON-NLS-0$
+		"label", "language", "lang", "list", "loop", "low", //$NON-NLS-5$ //$NON-NLS-4$ //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"manifest", "maxlength", "max", "media", "method", "min", "multiple", //$NON-NLS-6$ //$NON-NLS-5$ //$NON-NLS-4$ //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"name", "novalidate", //$NON-NLS-1$ //$NON-NLS-0$
+		"open", "optimum", //$NON-NLS-1$ //$NON-NLS-0$
+		"pattern", "ping", "placeholder", "poster", "preload", "pubdate", //$NON-NLS-5$ //$NON-NLS-4$ //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"radiogroup", "readonly", "rel", "required", "reversed", "rowspan", "rows", //$NON-NLS-6$ //$NON-NLS-5$ //$NON-NLS-4$ //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"sandbox", "scoped", "scope", "seamless", "selected", "shape", "sizes", "size", "span", "spellcheck", //$NON-NLS-9$ //$NON-NLS-8$ //$NON-NLS-7$ //$NON-NLS-6$ //$NON-NLS-5$ //$NON-NLS-4$ //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"srcdoc", "srclang","srcset", "src", "start", "step", "style", "summary", //$NON-NLS-7$ //$NON-NLS-6$ //$NON-NLS-5$ //$NON-NLS-4$ //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"tabindex", "target", "title", "type", //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+		"usemap", //$NON-NLS-0$
+		"value", //$NON-NLS-0$
+		"width", "wrap" //$NON-NLS-1$ //$NON-NLS-0$
+	];
 
 	var grammars = [];
 	grammars.push.apply(grammars, mJS.grammars);
@@ -65,14 +68,13 @@ define("orion/editor/stylers/text_html/syntax", ["orion/editor/stylers/applicati
 				patterns: [
 					{include: "orion.js"} //$NON-NLS-0$
 				]
-			},
-			{
+			}, {
 				begin: "</?[A-Za-z0-9]+", //$NON-NLS-0$
 				end: "/?>", //$NON-NLS-0$
 				captures: {
-					0: {name: "meta.tag.xml"}, //$NON-NLS-0$
+					0: {name: "meta.tag.html"}, //$NON-NLS-0$
 				},
-				name: "meta.tag.xml", //$NON-NLS-0$
+				name: "meta.tag.html", //$NON-NLS-0$
 				patterns: [
 					{include: "orion.xml#comment"}, //$NON-NLS-0$
 					{include: "orion.lib#string_doubleQuote"}, //$NON-NLS-0$
@@ -87,7 +89,7 @@ define("orion/editor/stylers/text_html/syntax", ["orion/editor/stylers/applicati
 		repository: {
 			attribute:{
 				match: "\\b(?:" + attributes.join("|") + ")\\b", //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
-				name: "meta.tag.html.attribute" //$NON-NLS-0$
+				name: "meta.tag.attribute.html" //$NON-NLS-0$
 			}
 		}
 	});
