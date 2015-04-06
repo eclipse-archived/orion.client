810cbffe82fcfeea1f72db573adf6583e6f4379d
Umar Bolatov <ubolatov@ca.ibm.com>
2015-04-06 10:22:06 -0400

    Bug 463712 - [HTML] Improve HTML syntax highlighting
    -Added matching for HTML attributes
    -Added default styling for HTML attributes
    -Added a LHS menu option
    -Changed wording a bit to conform to the set standard
    -Added a new exclusion item for CSS, Java and JS
    
    Change-Id: Icf187f66ed48b8ad90db35cf33b5e6e9b8a010e0
    Signed-off-by: Umar Bolatov <ubolatov@ca.ibm.com>

--- bundles/org.eclipse.orion.client.editor/web/orion/editor/stylers/text_html/syntax.js
+++ bundles/org.eclipse.orion.client.editor/web/orion/editor/stylers/text_html/syntax.js
@@ -12,6 +12,29 @@
 /*eslint-env browser, amd*/
 define("orion/editor/stylers/text_html/syntax", ["orion/editor/stylers/application_javascript/syntax", "orion/editor/stylers/text_css/syntax", "orion/editor/stylers/application_xml/syntax"], //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
 	function(mJS, mCSS, mXML) {
+	var attributes = [
+		"accept-charset", "accept", "accesskey", "action", "align", "alt", "async", "autocomplete", "autoplay", "autosave",
+		"bgcolor", "border", "buffered",
+		"challenge", "charset", "checked", "cite", "class", "codebase", "code", "color", "colspan", "cols", "contenteditable", "content", "contextmenu", "controls", "coords",
+		"data-[A-Za-z]+", "data", "datetime", "default", "defer", "dirname", "dir", "disabled", "download", "draggable", "dropzone",
+		"enctype",
+		"formaction", "form", "for",
+		"headers", "height", "hidden", "high", "hreflang", "href", "http-equiv",
+		"icon", "id", "ismap", "itemprop",
+		"keytype", "kind",
+		"label", "language", "lang", "list", "loop", "low",
+		"manifest", "maxlength", "max", "media", "method", "min", "multiple",
+		"name", "novalidate",
+		"open", "optimum",
+		"pattern", "ping", "placeholder", "poster", "preload", "pubdate",
+		"radiogroup", "readonly", "rel", "required", "reversed", "rowspan", "rows",
+		"sandbox", "scope", "scoped", "seamless", "selected", "shape", "sizes", "size", "span", "spellcheck", "srcdoc", "srclang", "srcset", "src", "start", "step", "style", "summary",
+		"tabindex", "target", "title", "type",
+		"usemap",
+		"value",
+		"width",
+		"wrap"
+	]
 
 	var grammars = [];
 	grammars.push.apply(grammars, mJS.grammars);
@@ -43,11 +66,30 @@ define("orion/editor/stylers/text_html/syntax", ["orion/editor/stylers/applicati
 					{include: "orion.js"} //$NON-NLS-0$
 				]
 			},
+			{
+				begin: "</?[A-Za-z0-9]+", //$NON-NLS-0$
+				end: "/?>", //$NON-NLS-0$
+				captures: {
+					0: {name: "meta.tag.xml"}, //$NON-NLS-0$
+				},
+				name: "meta.tag.xml", //$NON-NLS-0$
+				patterns: [
+					{include: "orion.xml#comment"}, //$NON-NLS-0$
+					{include: "orion.lib#string_doubleQuote"}, //$NON-NLS-0$
+					{include: "orion.lib#string_singleQuote"}, //$NON-NLS-0$
+					{include: "#attribute"} //$NON-NLS-0$
+				]
+			},
 			{include: "orion.xml#comment"}, //$NON-NLS-0$
 			{include: "orion.xml#doctype"}, //$NON-NLS-0$
-			{include: "orion.xml#tag"}, //$NON-NLS-0$
 			{include: "orion.xml#ampersandEscape"} //$NON-NLS-0$
-		]
+		],
+		repository: {
+			attribute:{
+				match: "\\b(?:" + attributes.join("|") + ")\\b", //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+				name: "meta.tag.html.attribute" //$NON-NLS-0$
+			}
+		}
 	});
 	return {
 		id: grammars[grammars.length - 1].id,
