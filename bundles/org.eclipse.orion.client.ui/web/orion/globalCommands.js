7b2b22017aa380b6dbdecac88bb194fd083d4957
Grant Gayed <Grant_Gayed@ca.ibm.com>
2015-04-10 13:25:40 -0400

    log uncaught error stack traces

--- bundles/org.eclipse.orion.client.ui/web/orion/globalCommands.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/globalCommands.js
@@ -580,6 +580,8 @@ define([
 			var index = e.filename.lastIndexOf("/"); //$NON-NLS-0$
 			var errorString = e.message + " (" + e.filename.substring(index + 1) + ": " + e.lineno + (e.colno ? ", " + e.colno : "") + ")"; //$NON-NLS-3$ //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
 			mMetrics.logEvent("runtime", "uncaughtError", errorString); //$NON-NLS-1$ //$NON-NLS-0$
+			var stackString = e.error.stack.replace(new RegExp(window.location.origin, "g"), ""); //$NON-NLS-0$
+			mMetrics.logEvent("runtime", "uncaughtErrorStack", stackString); //$NON-NLS-1$ //$NON-NLS-0$
 		});
 
 		new mThemePreferences.ThemePreferences(prefsService, new mThemeData.ThemeData()).apply();
