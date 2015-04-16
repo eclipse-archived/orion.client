3e096af80637fd290becaf711a80bd21410c6316
Mike Rennie <Michael_Rennie@ca.ibm.com>
2015-04-16 13:57:22 -0400

    Bug 432940 - Investigate using Tern to provide inferred content assist - provide plug-in stubs for our old indexes

--- bundles/org.eclipse.orion.client.javascript/web/tern/plugin/orion_requirejs.js
+++ bundles/org.eclipse.orion.client.javascript/web/tern/plugin/orion_requirejs.js
@@ -1,52 +1,95 @@
-/*eslint-env amd */
-define([
-'../lib/infer',
-'../lib/tern',
-], function(infer, tern) {
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2015 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*eslint-env node, amd*/
+/*globals infer tern walk*/
+(function(mod) {
+  if (typeof exports === "object" && typeof module === "object") // CommonJS
+    return mod(require("../lib/infer"), require("../lib/tern"), require("acorn/util/walk"));
+  if (typeof define === "function" && define.amd) // AMD
+    return define(["../lib/infer", "../lib/tern", "acorn/util/walk"], mod);
+  mod(infer, tern, walk);
+})(function(infer, tern, walk) {
   "use strict";
 
-  function getInterface(name, data, c) {
-    if (name === "require") {
-    	if (!data.require) {
-	      data.require = new infer.Fn("require", infer.ANull, [infer.cx().str], ["module"], new infer.AVal);
-	      data.require.computeRet = /* @callback */ function(_self, _args, argNodes) {
-	        if (argNodes.length && argNodes[0].type === "Literal" && typeof argNodes[0].value === "string") {
-	          return c(getInterface(argNodes[0].value, data, c));
-	        }
-	        return c(infer.ANull);
-	      };
-	    }
-	    return c(data.require);
+  function getRequire(data) {
+    if (!data.require) {
+      data.require = new infer.Fn("require", infer.ANull, [infer.cx().str], ["module"], new infer.AVal);
+      data.require.computeRet = function(_self, _args, argNodes) {
+        if (argNodes.length && argNodes[0].type === "Literal" && typeof argNodes[0].value === "string")
+          return getInterface(argNodes[0].value, data);
+        return infer.ANull;
+      };
     }
-    if (name === "module") {
-    	return c(infer.cx().definitions.requirejs.module);
-    }
+    return data.require;
+  }
+
+  function getInterface(name, data) {
+    if (name === "require") return getRequire(data);
+    if (name === "module") return infer.cx().definitions.requirejs.module;
+
     if (data.options.override && Object.prototype.hasOwnProperty.call(data.options.override, name)) {
       var over = data.options.override[name];
-      if (typeof over === "string" && over.charAt(0) === "=") {
-      	return c(infer.def.parsePath(over.slice(1)));
-      }
+      if (typeof over === "string" && over.charAt(0) === "=") return infer.def.parsePath(over.slice(1));
       if (typeof over === "object") {
         var known = getKnownModule(name, data);
-        if (known) {
-        	return c(known);
-        }
+        if (known) return known;
         var scope = data.interfaces[stripJSExt(name)] = new infer.Obj(null, stripJSExt(name));
         infer.def.load(over, scope);
-        return c(scope);
+        return scope;
       }
       name = over;
     }
-	data.server.options.getFile({logical:name}, /* @callback */ function(err, file) {
-  		var known = getKnownModule(file.file, data);
-	    if (!known) {
-	      known = getModule(file.file, data);
-	      data.server.addFile(file.file, null, data.currentFile);
+	var state = Object.create(null);
+	var done = false;
+	var file = null;
+	var callback = function(err, _file) {
+		file = _file;
+		done = true;
+		delete state[name];
+	};
+	asyncGetFile(data, name, state, callback);
+	if(done) {
+		if (!known) {
+	      known = getModule(file.name, data);
+	      data.server.addFile(file.name, file.contents, data.currentFile);
 	    }
-	    return c(known);
-  	});
+		return known;
+	}
+	return getInterface(name, data);
   }
-
+	
+  function asyncGetFile(data, name, state, callback) {
+	if(state[name] && (state[name].value || state[name].err)) {
+		callback(state[name].err, state[name].value);
+	}
+	if(typeof(state[name] === 'object')) {
+		return wait(data, name, state, callback);
+	}
+	state[name] = Object.create(null);
+	data.server.options.getFile(function(err, file) {
+		state[name].value = file;
+		state[name].err = err;
+	});
+  }
+	
+  function wait(data, name, state, callback) {
+	var timeout;
+	var f = function() {
+		clearTimeout(timeout);
+		asyncGetFile(data, name, state, callback);
+	};
+	timeout = setTimeout(f, 100);
+  }
+	
   function getKnownModule(name, data) {
     return data.interfaces[stripJSExt(name)];
   }
@@ -66,39 +109,18 @@ define([
     return f.replace(/\.js$/, '');
   }
 
-  infer.registerFunction("orionRequireJS", /* @callback */ function(_self, args, argNodes) {
+  infer.registerFunction("requireJS", function(_self, args, argNodes) {
     var server = infer.cx().parent, data = server && server._requireJS;
-    if (!data || !args.length) {
-    	return infer.ANull;
-    }
+    if (!data || !args.length) return infer.ANull;
+
     var name = data.currentFile;
     var out = getModule(name, data);
+
     var deps = [], fn;
-    var _set = function(_t) {
-		if(_t) {
- 			deps.push(_t);
- 			if (!fn) {
-		      fn = args[Math.min(args.length - 1, 2)];
-		      if (!fn.isEmpty() && !fn.getFunctionType()) {
-		      	fn = null;
-		      }
-		    }
-		
-		    if (fn) {
-		    	fn.propagate(new infer.IsCallee(infer.ANull, deps, null, out));
-		    }
-		    else if (args.length) {
-		    	args[0].propagate(out);
-			}
- 		}
- 		data.server.finishAsyncAction();
- 		return infer.ANull;
-	}
     if (argNodes && args.length > 1) {
       var node = argNodes[args.length === 2 ? 0 : 1];
       if (node.type === "Literal" && typeof node.value === "string") {
-      	data.server.startAsyncAction();
-        getInterface(node.value, data, _set);
+        deps.push(getInterface(node.value, data));
       } else if (node.type === "ArrayExpression") for (var i = 0; i < node.elements.length; ++i) {
         var elt = node.elements[i];
         if (elt.type === "Literal" && typeof elt.value === "string") {
@@ -107,20 +129,27 @@ define([
             deps.push(exports);
             out.addType(exports, EXPORT_OBJ_WEIGHT);
           } else {
-          	data.server.startAsyncAction();
-            getInterface(elt.value, data, _set);
+            deps.push(getInterface(elt.value, data));
           }
         }
       }
     } else if (argNodes && args.length === 1 && argNodes[0].type === "FunctionExpression" && argNodes[0].params.length) {
       // Simplified CommonJS call
       var exports = new infer.Obj(true);
-      data.server.startAsyncAction();
+      deps.push(getInterface("require", data), exports);
       out.addType(exports, EXPORT_OBJ_WEIGHT);
-      getInterface("require", data, _set);
-      deps.push(exports);
       fn = args[0];
     }
+
+    if (!fn) {
+      fn = args[Math.min(args.length - 1, 2)];
+      if (!fn.isEmpty() && !fn.getFunctionType()) fn = null;
+    }
+
+    if (fn) fn.propagate(new infer.IsCallee(infer.ANull, deps, null, out));
+    else if (args.length) args[0].propagate(out);
+
+    return infer.ANull;
   });
 
   // Parse simple ObjectExpression AST nodes to their corresponding JavaScript objects.
@@ -140,6 +169,24 @@ define([
     }
   }
 
+  infer.registerFunction("requireJSConfig", function(_self, _args, argNodes) {
+    var server = infer.cx().parent, data = server && server._requireJS;
+    if (data && argNodes && argNodes.length && argNodes[0].type === "ObjectExpression") {
+      var config = parseExprNode(argNodes[0]);
+      for (var key in config) if (config.hasOwnProperty(key)) {
+        var value = config[key], exists = data.options[key];
+        if (!exists) {
+          data.options[key] = value;
+        } else if (key === "paths") {
+          for (var path in value) if (value.hasOwnProperty(path) && !data.options.paths[path])
+            data.options.paths[path] = value[path];
+        }
+      }
+    }
+    return infer.ANull;
+  });
+
+
   function preCondenseReach(state) {
     var interfaces = infer.cx().parent._requireJS.interfaces;
     var rjs = state.roots["!requirejs"] = new infer.Obj(null);
@@ -153,10 +200,8 @@ define([
   function postLoadDef(data) {
     var cx = infer.cx(), interfaces = cx.definitions[data["!name"]]["!requirejs"];
     var data = cx.parent._requireJS;
-    if (interfaces) {
-    	for (var name in interfaces.props) {
-      		interfaces.props[name].propagate(getInterface(name, data));
-    	}
+    if (interfaces) for (var name in interfaces.props) {
+      interfaces.props[name].propagate(getInterface(name, data));
     }
   }
 
@@ -174,13 +219,14 @@ define([
     server.on("reset", function() {
       this._requireJS.interfaces = Object.create(null);
       this._requireJS.require = null;
+      this._requireJS.fileMap = Object.create(null);
     });
     return {
       defs: defs,
       passes: {
         preCondenseReach: preCondenseReach,
         postLoadDef: postLoadDef
-      },
+      }
     };
   });
 
@@ -195,15 +241,16 @@ define([
       }
     },
     requirejs: {
-      "!type": "fn(deps: [string], callback: fn(), errback: fn()) -> !custom:orionRequireJS",
+      "!type": "fn(deps: [string], callback: fn(), errback: fn()) -> !custom:requireJS",
       onError: "fn(err: +Error)",
       load: "fn(context: ?, moduleName: string, url: string)",
+      config: "fn(config: ?) -> !custom:requireJSConfig",
       version: "string",
       isBrowser: "bool"
     },
     require: "requirejs",
     define: {
-      "!type": "fn(deps: [string], callback: fn()) -> !custom:orionRequireJS",
+      "!type": "fn(deps: [string], callback: fn()) -> !custom:requireJS",
       amd: {
         jQuery: "bool"
       }
