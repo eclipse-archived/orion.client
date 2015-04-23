3f257bd5a880f48e7f2157663bdc6cac2402ae27
Filip Madej <filip.madej@pl.ibm.com>
2015-04-23 18:31:29 +0200

    [Bug 465291] Domains from different spaces/orgs are appended in deployment wizard
    Change-Id: I0674ddd045b2a1ccbf491371ac87fb73f4a26572
    Signed-off-by: Filip Madej <filip.madej@pl.ibm.com>

--- bundles/org.eclipse.orion.client.cf/web/cfui/plugins/wizards/common/corePageBuilder.js
+++ bundles/org.eclipse.orion.client.cf/web/cfui/plugins/wizards/common/corePageBuilder.js
@@ -92,6 +92,17 @@ define(['i18n!cfui/nls/messages', 'orion/selection', 'orion/widgets/input/ComboT
 			var self = this;
 			
 			self._showMessage(messages["loadingDeploymentSettings..."]);
+
+			if(self._cloudsDropdown){
+				self._cloudsDropdown.disabled = true;
+			}
+			if(self._orgsDropdown){
+				self._orgsDropdown.disabled = true;
+			}
+			if(self._spacesDropdown){
+				self._spacesDropdown.disabled = true;
+			}
+
 			self._cfService.getOrgs(target).then(
 				function(orgs){
 					lib.empty(self._orgsDropdown);
@@ -186,6 +197,17 @@ define(['i18n!cfui/nls/messages', 'orion/selection', 'orion/widgets/input/ComboT
 			    			self._domainsDropdown.classList.add("modifiedCell");
 			    		}
 			    	}
+
+					if(self._cloudsDropdown && self._cloudsDropdown.disabled == true){
+						self._cloudsDropdown.disabled = false;
+					}
+					if(self._orgsDropdown.disabled == true){
+						self._orgsDropdown.disabled = false;
+					}
+					if(self._spacesDropdown.disabled == true){
+						self._spacesDropdown.disabled = false;
+					}
+
 					self._setSelection();
 					self._hideMessage();
 				}
@@ -340,6 +362,11 @@ define(['i18n!cfui/nls/messages', 'orion/selection', 'orion/widgets/input/ComboT
 								lib.empty(self._orgsDropdown);
 								lib.empty(self._spacesDropdown);
 								lib.empty(self._domainsDropdown);
+								if(self._cloudsDropdown){
+									self._cloudsDropdown.disabled = true;
+								}
+								self._orgsDropdown.disabled = true;
+								self._spacesDropdown.disabled = true;
 								self._setSelection();
 								
 								var selectedCloud = self._clouds[event.target.selectedIndex];
@@ -360,6 +387,11 @@ define(['i18n!cfui/nls/messages', 'orion/selection', 'orion/widgets/input/ComboT
 						self._orgsDropdown = document.createElement("select"); //$NON-NLS-0$
 						self._orgsDropdown.onchange = function(event){
 							var selectedOrg = event.target.value;
+							if(self._cloudsDropdown){
+								self._cloudsDropdown.disabled = true;
+							}
+							self._orgsDropdown.disabled = true;
+							self._spacesDropdown.disabled = true;
 							self._loadSpaces(selectedOrg);
 	
 						};
@@ -375,6 +407,11 @@ define(['i18n!cfui/nls/messages', 'orion/selection', 'orion/widgets/input/ComboT
 						self._spacesDropdown.onchange = function(/*event*/){
 							self._setSelection();
 							var selection = self._selection.getSelection();
+							if(self._cloudsDropdown){
+								self._cloudsDropdown.disabled = true;
+							}
+							self._orgsDropdown.disabled = true;
+							self._spacesDropdown.disabled = true;
 							self._loadDomains(selection);
 							self._loadApplications(selection);
 							self._loadHosts(selection);
