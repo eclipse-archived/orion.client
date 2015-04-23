fe53679bc2436064db1c53a631ccac7c359c00db
Filip Madej <filip.madej@pl.ibm.com>
2015-04-23 11:29:24 +0200

    Cache
    Signed-off-by: Filip Madej <filip.madej@pl.ibm.com>

--- bundles/org.eclipse.orion.client.cf/web/cfui/plugins/wizards/common/corePageBuilder.js
+++ bundles/org.eclipse.orion.client.cf/web/cfui/plugins/wizards/common/corePageBuilder.js
@@ -19,6 +19,7 @@ define(['i18n!cfui/nls/messages', 'orion/selection', 'orion/widgets/input/ComboT
 	 */
 	
 	var rendered = false;
+	var plan;
 	
 	function CorePageBuilder(options){
 		options = options || {};
@@ -234,14 +235,22 @@ define(['i18n!cfui/nls/messages', 'orion/selection', 'orion/widgets/input/ComboT
 		},
 		
 		getPlan : function(){
-			var deferred = new Deferred();
-			var relativeFilePath = this._filePath + this.getManifestPath();
-			this._cfService.getDeploymentPlans(relativeFilePath).then(function(resp) {
-				var plans = resp.Children;
-				deferred.resolve(plans[0]);
-			});
-
-			return deferred;
+			
+			
+			if(this.getManifestPath() != this._initManifestPath){
+				var deferred = new Deferred();
+				var relativeFilePath = this._filePath + this.getManifestPath();
+				this._cfService.getDeploymentPlans(relativeFilePath).then(function(resp) {
+					var plans = resp.Children;
+					plan = deferred.resolve(plans[0]);
+					deferred.resolve(plans[0]);
+				});	
+				this._initManifestPath = this.getManifestPath();
+				return deferred;
+			}
+			
+			return plan;
+			
 		},
 		
 		build : function(){
@@ -384,7 +393,7 @@ define(['i18n!cfui/nls/messages', 'orion/selection', 'orion/widgets/input/ComboT
 						document.getElementById("manifestLabel").textContent = messages["manifestLabel"];
 						self._manifestInput = document.createElement("input"); //$NON-NLS-0$
 						self._manifestInput.value = (self._initManifestPath == "") ? "manifest.yml" : self._initManifestPath;
-						self._manifestInput.readOnly = true; // TODO should be editable
+						self._manifestInput.readOnly = false; // TODO should be editable
 						document.getElementById("manifest").appendChild(self._manifestInput); //$NON-NLS-0$
 						
 						self._manifestinput = document.getElementById("manifest").firstChild;
@@ -398,7 +407,6 @@ define(['i18n!cfui/nls/messages', 'orion/selection', 'orion/widgets/input/ComboT
 									lib.empty(self._domainsDropdown);
 									self._loadDomains(selection);
 								});
-								self._initManifestPath = self.getManifestPath();
 							}
 						};
 
