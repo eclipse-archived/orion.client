274891976b70726befbb245d1e4e7355ea684578
libing wang <libingw@ca.ibm.com>
2015-05-11 15:06:44 -0400

    Bug 465923 - Stand alone editor: consume-ability improvement.

---
+++ bundles/org.eclipse.orion.client.ui/web/embeddedEditor/builder/javascriptPlugin.build.almond-js.js
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+ 
+// optimization script to concat/minify the Orion editor javascript code
+ 
+({
+    baseUrl: '../../',
+	closure: {
+		CompilerOptions: {
+			languageIn: Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode.valueOf(Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode, "ECMASCRIPT5")
+		},
+		CompilationLevel: 'SIMPLE_OPTIMIZATIONS',
+		loggingLevel: 'WARNING'
+	},
+	paths: {
+        almond: 'requirejs/almond',
+        i18n: 'requirejs/i18n',
+        text: 'requirejs/text',
+        'orion/bootstrap': 'embeddedEditor/builder/buildFrom/bootstrap'
+	},
+	name: "almond",
+	//locales: ["ja", "zh", "zh-tw", "fr", "de", "it", "es", "pt-br"],						
+	include: "javascript/plugins/javascriptPlugin",
+	preserveLicenseComments: false,
+	uglify: {
+		ascii_only: true
+	},
+	wrap: {
+		start: "/* orion javascriptPlugin */ ", //start cannot be empty
+		end: "\
+		orion = this.orion || (this.orion = {});\n\
+		var javascriptPlugin = orion.javascriptPlugin || (orion.javascriptPlugin = {});\n\
+		javascriptPlugin.javascriptPlugin = require('javascript/plugins/javascriptPlugin');"
+	}
+})
