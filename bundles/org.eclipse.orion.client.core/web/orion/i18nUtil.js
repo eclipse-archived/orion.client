f94f509bda9be165537015628ff5d4163fc32856
Mike Rennie <Michael_Rennie@ca.ibm.com>
2015-05-21 16:05:05 -0400

    Bug 467917 - i18nUtil should not have dependencies on 'require' and 'Deferred'

--- bundles/org.eclipse.orion.client.core/web/orion/i18nUtil.js
+++ bundles/org.eclipse.orion.client.core/web/orion/i18nUtil.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * @license
- * Copyright (c) 2012 IBM Corporation and others.
+ * Copyright (c) 2012, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials are made 
  * available under the terms of the Eclipse Public License v1.0 
  * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
@@ -10,7 +10,7 @@
  *******************************************************************************/
 /*eslint-env browser, amd*/
 /*global requirejs*/
-define(['require', 'orion/Deferred'], function(require, Deferred) {
+define([], function() {
 	/**
 	 * Performs string substitution. Can be invoked in 2 ways:
 	 *
