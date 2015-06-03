a15a7687ee7de86fedc085d0b76fdc874a3000e8
Libing Wang <libingw@ca.ibm.com>
2015-06-03 19:37:44 -0400

    Bug 465923 - Stand alone editor: consume-ability improvement. -- Pushed the build script to orion client releng

---
+++ releng/org.eclipse.orion.client.releng/builder/scripts/embeddedToolingPlugin.build.js
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+ 
+// optimization script to concat/minify the Orion editor javascript code
+ 
+({
+    baseUrl: '.',
+	closure: {
+		CompilerOptions: {
+			languageIn: Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode.valueOf(Packages.com.google.javascript.jscomp.CompilerOptions.LanguageMode, "ECMASCRIPT5")
+		},
+		CompilationLevel: 'SIMPLE_OPTIMIZATIONS',
+		loggingLevel: 'WARNING'
+	},
+	paths: {
+        almond: 'requirejs/almond',
+  		text: 'requirejs/text',
+  		i18n: 'requirejs/i18n',
+  		domReady: 'requirejs/domReady'
+	},
+	packages: [
+	],
+	name: "almond",
+	//locales: ["ja", "zh", "zh-tw", "fr", "de", "it", "es", "pt-br"],						
+	include: "plugins/embeddedToolingPlugin",
+	preserveLicenseComments: false,
+	uglify: {
+		ascii_only: true
+	},
+	wrap: {
+		start: "\
+			(function (root, factory) {\
+				if (typeof define === 'function' && define.amd) {\
+					define([], factory);\
+				} else {\
+					root.orion = root.orion || {};\
+					root.orion.webtools = root.orion.webtools || {};\
+					root.orion.webtools.embeddedTooling = factory();\
+				}\
+			}(this, function () {\
+		",
+		end: "\
+				return require('plugins/embeddedToolingPlugin');\
+			}));\
+		"
+	}
+})
