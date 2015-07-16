1354250218f27e8be07c98cdc2e701daf4c9d440
Steve Northover <snorthov@ca.ibm.com>
2015-07-16 17:27:25 -0400

    Bug 471493 - Make project.json optional and more robust (don't fail when contents corrupt or not there)
    Fixed update bug and //TODO's
    
    Change-Id: I0c235deadfa5804e2af398fb184230b91bb1abe8
    Signed-off-by: Steve Northover <snorthov@ca.ibm.com>

--- bundles/org.eclipse.orion.client.ui/web/orion/widgets/nav/project-nav.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/widgets/nav/project-nav.js
@@ -27,6 +27,10 @@ define([
 	messages, mCommands, i18nUtil, objects, lib, mExplorer, mCommonNav, ProjectCommands,
 	PageUtil, URITemplate, Deferred, mFileUtils, mCustomGlobalCommands
 ) {
+	
+	//TODO - this should be a setting
+	var AUTOMATIC_VIEW_PROJECT = false;
+	
 	var CommonNavExplorer = mCommonNav.CommonNavExplorer;
 	var CommonNavRenderer = mCommonNav.CommonNavRenderer;
 	var FileModel = mExplorer.FileModel;
@@ -202,6 +206,7 @@ define([
 		},
 		changedItem: function(item, forceExpand){
 			if(!item || !this.model){
+				this.fileMetadata.children = null;
 				return this.display(this.fileMetadata, true);
 			}
 			if(item.Projects){
@@ -304,13 +309,13 @@ define([
 		var sidebar = this.sidebar;
 		// Switch to project view mode if a project is opened
 		function openProject(metadata){
-			function failed() {
+			function openDefaultMode() {
 				if (!sidebar.getActiveViewModeId()) {
-					sidebar.setViewMode(sidebar.getNavigationViewMode().id);
+					sidebar.setViewMode(sidebar.getDefaultViewModeId());
 				}
 			}
 			if (!metadata) {
-				failed();
+				openDefaultMode();
 				return;
 			}
 			if(_self.lastCheckedLocation === metadata.Location){
@@ -321,22 +326,29 @@ define([
 			}
 			_self.lastCheckedLocation = metadata.Location;
 			_self.getProject(metadata).then(function(project) {
-				_self.getProjectJson(project).then(function(json) {
-					_self.showViewMode(!!json);
-					if (json) {
-						if (sidebar.getActiveViewModeId() === _self.id) {
-							_self.explorer.display(project);
+				if (AUTOMATIC_VIEW_PROJECT) {
+					_self.getProjectJson(project).then(function(json) {
+						_self.showViewMode(!!json);
+						if (json) {
+							if (sidebar.getActiveViewModeId() === _self.id) {
+								_self.explorer.display(project);
+							} else {
+								_self.project = project;
+								sidebar.setViewMode(_self.id);
+							}
 						} else {
-							_self.project = project;
-							sidebar.setViewMode(_self.id);
+							if (!sidebar.getActiveViewModeId()) {
+								sidebar.setViewMode(sidebar.getNavigationViewMode().id);
+							}
 						}
-					} else {
-						if (!sidebar.getActiveViewModeId()) {
-							sidebar.setViewMode(sidebar.getNavigationViewMode().id);
-						}
-					}
-				}, failed);
-			}, failed);
+						
+					}, openDefaultMode);
+				} else {
+					_self.project = project;
+					_self.showViewMode(true);
+					openDefaultMode();
+				}
+			}, openDefaultMode);
 			var handleDisplay = function (event) {
 				if(event.item === metadata) {
 					sidebar.sidebarNavInputManager.removeEventListener("projectDisplayed", handleDisplay); //$NON-NLS-0$
@@ -355,13 +367,19 @@ define([
 			var item = event.selections && event.selections.length > 0 ? event.selections[0] : null;
 			if (item) {
 				_self.getProject(item).then(function(project) {
-					_self.getProjectJson(project).then(function(json) {
+					if (AUTOMATIC_VIEW_PROJECT) {
+						_self.getProjectJson(project).then(function(json) {
+							_self.project = project;
+							_self.showViewMode(!!json);
+						});
+					} else {
 						_self.project = project;
-						_self.showViewMode(!!json);
-					});
+					}
 				});
 			} else {
-				_self.showViewMode(false);
+				if (AUTOMATIC_VIEW_PROJECT) {
+					_self.showViewMode(false);
+				}
 			}
 		});
 	}
