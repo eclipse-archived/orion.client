ac13e0aae1e11ac431d5250387cba9d6f1a40073
Mark Macdonald <mamacdon@gmail.com>
2015-09-15 17:08:01 -0400

    Improve `run-tests-locally` script
    Install test npm dependencies if they're missing

--- releng/org.eclipse.orion.client.releng/run-tests-locally.sh
+++ releng/org.eclipse.orion.client.releng/run-tests-locally.sh
@@ -3,12 +3,13 @@
 #
 # Run the JS unit tests. Uses the Sauce Labs SSH tunnel to provide access to a webserver running
 # on your local machine.
-# 
+#
 # Requires: Node.js, grunt-cli
-# 
+#
+# TODO Dockerize this
 
 function die () {
-    echo "Error:" "$1"
+    echo "Error:" "$1" >&2
     exit 1
 }
 
@@ -19,6 +20,11 @@ test_dir="$script_dir/test"
 [[ -z "$SAUCE_USERNAME" || -z "$SAUCE_ACCESS_KEY" ]] && die "environment variables SAUCE_USERNAME and SAUCE_ACCESS_KEY must be set"
 
 which grunt >/dev/null || die "grunt not found. Run 'npm install -g grunt-cli', ensure global NPM modules are in your PATH, and retry."
+
+if [ ! -d "$test_dir/node_modules" ]; then
+    echo "Installing test dependencies, please wait..."
+    ( cd ${test_dir} && npm install . ) || die "Failed to install dependencies, consult the npm log to find out why."
+fi
 
 cd "$test_dir" && grunt --verbose
 
