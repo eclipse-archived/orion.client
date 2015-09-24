cd7f258975c8970438d56a03d461b000ae60571a
Silenio Quarti <Silenio_Quarti@ca.ibm.com>
2015-09-24 20:08:54 +0000

    Adding splash/progress to orion pages (cleanup implementation, add shadow to splash)

--- bundles/org.eclipse.orion.client.core/web/orion/splash.js
+++ bundles/org.eclipse.orion.client.core/web/orion/splash.js
@@ -11,239 +11,218 @@
 
 define([], function() {
 
-function step( order, description, type, barSpace, incrementCount ){
- 
-    this.TYPE_FILLER = 0;
-    this.TYPE_DRIVEN = 1;
-    
-    this.type = type;
-    
-    this.incrementCount = incrementCount;
-    
-    this.order = order;
-    
-    /* reserve this much space on the progress bar 
-       for this step */
-    
-    this.barSpace = barSpace;
-    
-    this.description = description;
-    
-    this.WAITING = 0;
-    this.HAPPENING = 1;
-    this.COMPLETED = 2;
-    
-    this.state = this.WAITING;
-    
-    this.template = "";
-    this.domNode;
-    
-    this.increments = 0;
+function step(description, type, total, incrementCount){
+	
+	this.TYPE_FILLER = 0;
+	this.TYPE_DRIVEN = 1;
+	
+	this.type = type;
+	this.incrementCount = incrementCount;
+	this.total = total;
+	this.description = description;
+	
+	this.WAITING = 0;
+	this.HAPPENING = 1;
+	this.COMPLETED = 2;
+	this.state = this.WAITING;
+	
+	this.template = "";
+	this.domNode;
+	
+	this.increments = 0;
 }
 
 step.prototype.complete = function(){
-    this.domNode.className = 'splashSuccessImage';  
+	this.state = this.COMPLETED;
+	this.domNode.className = 'splashSuccessImage';
 };
 
 step.prototype.spin = function(){
-    this.domNode.className = 'splashLoadingImage';   
+	this.state = this.HAPPENING;
+	this.domNode.className = 'splashLoadingImage';
 };
 
 step.prototype.getStepNode = function(){
-    
-    var stepNode = document.createElement( 'div' );
-    stepNode.className = 'splashStep';
-    stepNode.innerHTML =  '<div class="splashVisual">' + 
-                                '<div id="step' + this.order + '"></div>' +
-                            '</div>' +
-                            '<div class="splashVerbal">' + this.description + '</div>';
-    
-    this.domNode = stepNode.firstChild.firstChild;
-    
-    return stepNode;
+	
+	var stepNode = document.createElement( 'div' );
+	stepNode.className = 'splashStep';
+	stepNode.innerHTML =  '<div class="splashVisual">' + 
+								'<div id="step' + this.order + '"></div>' +
+							'</div>' +
+							'<div class="splashVerbal">' + this.description + '</div>';
+	
+	this.domNode = stepNode.firstChild.firstChild;
+	
+	if (this.state === this.HAPPENING) {
+		this.domNode.className = 'splashLoadingImage';
+	} else if (this.state === this.COMPLETED) {
+		this.domNode.className = 'splashSuccessImage';
+	}
+	
+	return stepNode;
 };
 
 step.prototype.transition = function(){
-    switch( this.state ){
-     
-        case this.WAITING:
-            this.state = this.HAPPENING;
-            /* add happening class */
-            break;
-            
-        case this.HAPPENING:
-            this.state = this.COMPLETED;
-            /* add completed class */
-            break;
-    }
+	switch( this.state ){
+	 
+		case this.WAITING:
+			this.state = this.HAPPENING;
+			/* add happening class */
+			break;
+			
+		case this.HAPPENING:
+			this.state = this.COMPLETED;
+			/* add completed class */
+			break;
+	}
 };
 
-function loader( domNode, subject, length ){
+function loader( domNode, subject ){
 
-    this.domNode = domNode;
-    
-    this.content;
-    
-    this.length = length;
-    
-    this.title = subject;
-    
-    this.steps = [];   
-    
-    this.PROGRESS_BAR_LENGTH = length;
-	
-	/* Let's say that the first 40px are reserved for initialization */
-	
-	this.PROGRESS_FIRST_SECTION_LENGTH = 40;
-	
+	this.domNode = domNode;
+	this.content;
+	this.title = subject;
+	this.steps = [];   
 	this.FILLER_TIMEOUT = 200;
-    
-    this.progressbar = 'progressbar';
-    
-    this.template = '<div class="splashLoader">' +
-                        '<div class="splashAbout">Setting up workspace</div>' +
-                        '<div class="splashProgressbar">' +
-                            '<progress id ="' + this.progressbar + '" max="' + this.length + '" value="0"></progress>' +
-                        '</div>' +
-                        '<div id="steps" class="splashSteps">' +
-                        '</div>' +
-                    '</div>';   
-    
-    this.initialize();
-    
-    this.interval;
-    
-    this.currentStep = 0;
+	this.progressbar = 'progressbar';
+	
+	this.template = '<div class="splashLoader">' +
+						'<div class="splashAbout">Setting up workspace</div>' +
+						'<div class="splashProgressbar">' +
+							'<progress id ="' + this.progressbar + '"></progress>' +
+						'</div>' +
+						'<div id="steps" class="splashSteps">' +
+						'</div>' +
+					'</div>';   
+	
+	this.initialize();
+	
+	this.interval;
+	
+	this.currentStep = -1;
 }
 
-/* want to fill time in a first section - to fake progress
-   while we wait for real progress indication to begin */
-
-loader.prototype.animateFirstSection = function(){
-    
-};
-	
 loader.prototype.showFillerProgress = function(){
 
-    var splashProgress = document.getElementById( this.progressbar );
-    
-    var cs = this.steps[ this.currentStep ];
-    
-    var barSpace = 0;
-    
-    for( var s = 1; s <= cs.order; s++ ){
-        barSpace = barSpace + this.steps[s].barSpace;
-    }
+	var splashProgress = this.splashProgress;
+	
+	var cs = this.steps[ this.currentStep ];
+	
+	var total = 0;
+	
+	for( var s = 0; s <= cs.order; s++ ){
+		total = total + this.steps[s].total;
+	}
 
-    if( splashProgress  ) {	
-        var increment = ( barSpace - splashProgress.value )/5;
-        splashProgress.value = splashProgress.value + increment;
-//        console.log( splashProgress.value );
-    }	
+	if( splashProgress  ) {	
+		var increment = ( total - splashProgress.value )/5;
+		splashProgress.value = splashProgress.value + increment;
+	}	
 };
 
 loader.prototype.startFillerProgress = function(){   
-    var myloader = this;
-    this.interval = setInterval( function(){ myloader.showFillerProgress(); }, this.FILLER_TIMEOUT );
+	var myloader = this;
+	this.interval = setInterval( function(){ myloader.showFillerProgress(); }, this.FILLER_TIMEOUT );
 };
 	
 loader.prototype.completeFillerProgress = function (){
-    clearInterval( this.interval );		
+	clearInterval( this.interval );		
 };
 
 loader.prototype.nextStep = function(){
-            
-    var myStep;
-    
-    /* complete current step */
-    
-    if( this.currentStep > 0  ){
-        
-        myStep = this.steps[this.currentStep];
-    
-        switch( myStep.type ){
+			
+	var myStep;
+	
+	/* complete current step */
+	
+	if( this.currentStep >= 0  ){
+		
+		myStep = this.steps[this.currentStep];
+	
+		switch( myStep.type ){
 
-            case myStep.TYPE_FILLER:
-                this.completeFillerProgress();
-                myStep.complete();
-                break;
+			case myStep.TYPE_FILLER:
+				this.completeFillerProgress();
+				myStep.complete();
+				break;
 
-            case myStep.TYPE_DRIVEN:
-                myStep.complete();
-                break;
-        }
-    }
-    
-    /* initaliate next step */
-    
-    this.currentStep = this.currentStep + 1;
-    
-    myStep = this.steps[this.currentStep];
-    
-    if (!myStep) return;
-    
-    switch( myStep.type ){
+			case myStep.TYPE_DRIVEN:
+				myStep.complete();
+				break;
+		}
+	}
+	
+	/* initaliate next step */
+	this.currentStep = this.currentStep + 1;
+	
+	myStep = this.steps[this.currentStep];
+	
+	if (!myStep) return;
+	
+	switch( myStep.type ){
 
-            case myStep.TYPE_FILLER:
-                this.startFillerProgress();
-                myStep.spin();
-                break;
+			case myStep.TYPE_FILLER:
+				this.startFillerProgress();
+				myStep.spin();
+				break;
 
-            case myStep.TYPE_DRIVEN:
-                 myStep.spin();
-                break;
-    }
+			case myStep.TYPE_DRIVEN:
+				 myStep.spin();
+				break;
+	}
 };
 
 loader.prototype.initialize = function(){
-    this.content = document.getElementById( this.domNode );
-    this.content.innerHTML = this.template;
-    var splashProgress = document.getElementById( this.progressbar );
-    splashProgress.value = 0;
-    //    this.startInitializationProgress();
+	this.content = document.getElementById( this.domNode );
+	this.content.innerHTML = this.template;
+	this.splashProgress = document.getElementById( this.progressbar );
+	this.splashProgress.value = 0;
 };
 
 loader.prototype.emptySteps = function(){
-    var stepsNode = document.getElementById( 'steps' );
-    while( stepsNode.firstChild ){
-        stepsNode.removeChild( stepsNode.firstChild ) ;
-    }   
+	var stepsNode = document.getElementById( 'steps' );
+	while( stepsNode.firstChild ){
+		stepsNode.removeChild( stepsNode.firstChild ) ;
+	}   
 };
 
 loader.prototype.drawSteps = function(){
-    
-    this.emptySteps();
-    
-    var stepsNode = document.getElementById( 'steps' );
-    
-    for( var s in this.steps ){
-        var nextStep = this.steps[s];
-        stepsNode.appendChild( nextStep.getStepNode() );
-    }
+	
+	this.emptySteps();
+	
+	var stepsNode = document.getElementById( 'steps' );
+	
+	for( var s in this.steps ){
+		var nextStep = this.steps[s];
+		stepsNode.appendChild( nextStep.getStepNode() );
+	}
 };
 
 /* Newly provided steps need to begin at 1 */
 
 loader.prototype.addStep = function( step ){
-    this.steps[step.order] = step;
-    
-    if( !this.currentStep ){
-        this.curentStep = step;   
-    }
-    
-    this.drawSteps();
+	step.order = this.steps.length;
+	this.steps.push(step);
+	
+	var max = 0;
+	for(var s = 0; s < this.steps.length; s++ ){
+		max = max + this.steps[s].total;
+	}
+	this.splashProgress.max = max;
+	
+	this.drawSteps();
 };
 
 loader.prototype.increment = function(){
-    var cs = this.steps[ this.currentStep ];
-    cs.increments = cs.increments + 1;
-    var splashProgress = document.getElementById( this.progressbar );
-    var increment = cs.barSpace / cs.incrementCount;		
-    splashProgress.value = splashProgress.value + increment;
-    
-    if( cs.increments === cs.incrementCount ){
-        this.nextStep();   
-    }
+	var cs = this.steps[ this.currentStep ];
+	cs.increments = cs.increments + 1;
+	var splashProgress = this.splashProgress;
+	var increment = cs.total / cs.incrementCount;		
+	splashProgress.value = splashProgress.value + increment;
+	
+	if( cs.increments === cs.incrementCount ){
+		this.nextStep();   
+	}
 };
 
 loader.prototype.takeDown = function() {
@@ -252,6 +231,10 @@ loader.prototype.takeDown = function() {
 	if (splash && splash.parentNode) {
 		splash.parentNode.removeChild(splash);
 	}
+};
+
+loader.prototype.createStep = function(description, type, total, incrementCount) {
+	return new step(description, type, total, incrementCount);
 };
 
 var pageLoader;
@@ -264,16 +247,16 @@ function start() {
 //	container.style.display = "none";
 	splash.appendChild(container);
 	
-	pageLoader = new loader('splashContainer', 'Setting up workspace', 100);
-	var initial = new step(1, 'Loading Page', 0, 50);
+	pageLoader = new loader('splashContainer', 'Setting up workspace');
+	var initial = new step('Loading Page', 0, 50);
 	pageLoader.addStep(initial);
 	
 	var pluginStep;
-	pluginStep = new step( 2, 'Loading Plugins', 0, 20);
-	pageLoader.addStep( pluginStep );
+	pluginStep = new step('Loading Plugins', 0, 20);
+	pageLoader.addStep(pluginStep );
 	
-	pluginStep = new step( 3, 'Loading Workspace', 0, 30);
-	pageLoader.addStep( pluginStep );
+	pluginStep = new step('Loading Workspace', 0, 30);
+	pageLoader.addStep(pluginStep);
 	
 	pageLoader.nextStep();
 	
