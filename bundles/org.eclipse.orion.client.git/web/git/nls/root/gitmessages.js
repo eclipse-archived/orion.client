8d02745ed4b5c93863d9621992e2db7f4108760b
xintong <xintong@ca.ibm.com>
2015-09-30 14:29:03 -0400

    submodule repo list view display show detached head - fixes
    Change-Id: I4c65163b67b28bc8197a6632ba1f59a3f47c393c
    Signed-off-by: xintong <xintong@ca.ibm.com>

--- bundles/org.eclipse.orion.client.git/web/git/nls/root/gitmessages.js
+++ bundles/org.eclipse.orion.client.git/web/git/nls/root/gitmessages.js
@@ -569,5 +569,8 @@ define({//Default message bundle
 	'Ssh Passphrase:' : 'Ssh Passphrase:', //$NON-NLS-0$  //$NON-NLS-1$
 	'confirmUnsavedChanges': 'There are unsaved changes. Do you want to save them?', //$NON-NLS-1$ //$NON-NLS-0$
 	'NewConfigurationEntryTooltip': "Add a new entry to the repository configuration", //$NON-NLS-1$ //$NON-NLS-0$
-	'Configurations' : "Configurations" //$NON-NLS-0$  //$NON-NLS-1$
+	'Configurations' : "Configurations", //$NON-NLS-0$  //$NON-NLS-1$
+	'DetachedHead ${0}' : "Detached HEAD @ ${0}", //$NON-NLS-1$ //$NON-NLS-0$
+	'SingleSubmodule' : "${0} (${1} submodule)", //$NON-NLS-1$ //$NON-NLS-0$
+	'PluralSubmodule' : "${0} (${1} submodules)" //$NON-NLS-1$ //$NON-NLS-0$
 });
