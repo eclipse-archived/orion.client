b00c6c04cadeea735e22e3eecdfd061f55342553
Silenio Quarti <Silenio_Quarti@ca.ibm.com>
2015-10-05 15:52:41 -0400

    Adding splash/progress to orion pages (separate bootstrap from page experiment)

---
+++ bundles/org.eclipse.orion.client.ui/web/edit/edit2.js
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2011, 2012 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*eslint-env browser, amd*/
+define(['orion/browserCompatibility', 'edit/setup'], function(mBrowserCompatibility, mSetup) {
+	require(['orion/bootstrap'], function(mBootstrap) {
+		mBootstrap.startup().then(function(core) {
+			var serviceRegistry = core.serviceRegistry;
+			var pluginRegistry = core.pluginRegistry;
+			var preferences = core.preferences;
+			mSetup.setUpEditor(serviceRegistry, pluginRegistry, preferences, false);  // not read only
+		});
+	})
+});
