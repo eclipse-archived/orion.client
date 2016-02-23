96c8fdca3fe415dc14b3f37334cde3c79dfee9c7
Silenio Quarti <Silenio_Quarti@ca.ibm.com>
2016-02-23 15:17:56 +0000

    Revert "Defect 54736 - Performance: WebIDE should clone repositories in parallel"
    This reverts commit b54b8012fc887f003e94a02b81fd4d3c8f1ec69f.

--- bundles/org.eclipse.orion.client.ui/web/orion/splash.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/splash.js
@@ -105,7 +105,7 @@ function loader( domNode, title ){
 						'</div>' +
 						'<div id="steps" class="splashSteps">' +
 						'</div>' +
-						'<div id="stepMessages" class="splashMessages">' +
+						'<div id="stepMessages" class="stepMessages">' +
 						'</div>' +
 						'</div>' +
 					'</div>';   
