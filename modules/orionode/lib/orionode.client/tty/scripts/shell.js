55737190a4dea340b091635af9be3af118c342e9
Silenio Quarti <Silenio_Quarti@ca.ibm.com>
2016-02-27 01:34:55 -0500

    node client: improve tty page

--- modules/orionode/lib/orionode.client/tty/scripts/shell.js
+++ modules/orionode/lib/orionode.client/tty/scripts/shell.js
@@ -1,180 +1,230 @@
-require(["/socket.io/socket.io.js", "scripts/term.js", "/requirejs/domReady.js", "orion/PageUtil", "orion/fileClient", "orion/bootstrap"], 
-    function(io, Terminal, onReady, PageUtil, FileClient, mBootstrap) {
-  // default settings:
-  
-  var serviceRegistry, fileClient;
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2016 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ * 
+ * Contributors: IBM Corporation - initial API and implementation
+ ******************************************************************************/
+/*eslint-env browser, amd*/
+define([
+	"/socket.io/socket.io.js",
+	"scripts/term.js",
+	"/requirejs/domReady.js",
+	'orion/widgets/input/DropDownMenu',
+	'orion/widgets/input/SettingsSelect',
+	'orion/commands',
+	"orion/PageUtil"
+], function(io, Terminal, onReady, DropDownMenu, SettingsSelect, mCommands, PageUtil) {
 
+	var term, serviceRegistry;
+	var colorScheme = "Dark";
 
-  onReady(function() {
-    var socket;
-    var resize = function(term, newWidth, newHeight) {
-      if (socket == null) return;
-      var el = term.element;
-      var size = {
-        x: el.clientWidth,
-        y: el.clientHeight,
-        cols: term.cols,
-        rows: term.rows
-      }
-      var newRows = newHeight / Math.floor(size.y / size.rows)-1;
-      var newCols = newWidth / Math.floor(size.x / size.cols)-1;
+	onReady(function() {
+		var socket, charWidth, charHeight, rows = 24, cols = 80;
+		var fitToDiv = function(term) {
+			if (socket === null) return;
+			var termContainer = document.getElementById("terminalBox"),
+				newWidth = termContainer.clientWidth,
+				newHeight = termContainer.clientHeight;
+			if (charWidth === undefined) {
+				var span = document.createElement("span");
+				span.textContent = "X";
+				termContainer.appendChild(span);
+				var rect = span.getBoundingClientRect();
+				charWidth = rect.right - rect.left;
+				charHeight = rect.bottom - rect.top + 2;
+				termContainer.removeChild(span);
+			}
+			var newRows = newHeight / (charHeight || 12);
+			var newCols = Math.max(80, newWidth / (charWidth || 12));
+			if (newRows === rows && newCols !== cols) return;
+			rows = newRows;
+			cols = newCols;
+			term.resize(Math.floor(newCols), Math.floor(newRows));
+			socket.emit('resize', Math.floor(newCols), Math.floor(newRows));
+		};
 
-      term.resize(Math.floor(newCols), Math.floor(newRows));
-      socket.emit('resize', Math.floor(newCols), Math.floor(newRows));
-    };       
-
-    var fitToDiv = function(term) {
-      var termContainer = document.getElementById("terminalBox"),
-        mainContainer = document.getElementById("mainNode"),
-        termControls  = document.getElementById("terminalControl"),
-        newWidth = mainContainer.clientWidth,
-        newHeight = mainContainer.clientHeight;
-
-      resize(term, newWidth, newHeight);
-    };
-
-    socket = io.connect('/tty');
-    socket.on('connect', function() {
-      socket.emit('start', getCWD());
-    });
-    socket.on('fail', function(error) {
-      console.log(error);
-    });
+		socket = io.connect('/tty');
+		socket.on('connect', function() {
+			socket.emit('start', getCWD());
+		});
+		socket.on('fail', function(error) {
+			console.log(error);
+		});
 	
-	var term;
-    socket.on('ready', function() {
-      term = new Terminal({
-        cols: 80,
-          rows: 24,
-          useStyle: true,
-          screenKeys: true,
-      });
-      
-      
-      term.on('data', function(data) {
-        socket.emit('data', data);
-      });
+		socket.on('ready', function() {
+			term = new Terminal({
+				cols: cols,
+				rows: rows,
+				useStyle: true,
+				screenKeys: true,
+			});
+			
+			term.on('data', function(data) {
+				socket.emit('data', data);
+			});
 
-      term.on('title', function(title) {
-        document.title = title;
-      });
-      var termContainer = document.getElementById("terminalBox");
-      term.open(termContainer);
-      
-      fitToDiv(term);
+			term.on('title', function(title) {
+				document.title = title;
+			});
+			var termContainer = document.getElementById("terminalBox");
+			term.open(termContainer);
+			
+			fitToDiv(term);
 
-      var oldResize = window.onresize;
-      var timeout;
-      window.onresize = function() {
-        if (oldResize) { oldResize(); }
-        clearTimeout(timeout);
-        timeout = setTimeout(fitToDiv, 500, term);
-      }
-      socket.on('data', function(data) {
-        term.write(data);
-      });
-      socket.on('disconnect', function() {
-        term.destroy();
-      });
-    });
-
-    var schemeButton = document.getElementById("color-scheme-dropdown");
-    if (!schemeButton) return;
-    schemeButton.onchange = changeScheme;
-
-    var wrenchButton = document.getElementById("menuWrench");
-    wrenchButton.onclick = function() {
-        var d = document.getElementById("dropdown");
-        if (d.style.display == "block")
-            d.style.display = "none";
-        else
-            d.style.display = "block";
-    };
-    
-    function __changeScheme(schemeName) {
-        if (term != null) {
-            switch(schemeName) {
-                case 'Dark': 
-                  var t = document.getElementsByClassName('terminal');
-                  t[0].style.backgroundColor = "#000000";
-                  t[0].style.color = "#fdf6e3";
-                    term.colors[0] = '#000000';
-                    term.colors[1] = '#dc322f';
-                    term.colors[2] = '#859900';
-                    term.colors[3] = '#b58900';
-                    term.colors[4] = '#268bd2';
-                    term.colors[5] = '#d33682';
-                    term.colors[6] = '#2aa198';
-                    term.colors[7] = '#eee8d5';
-                    term.colors[8] = '#002b36';
-                    term.colors[9] = '#cb4b16';
-                    term.colors[10] = '#8AE234'; //Linux Tango - user@machine, shell scripts
-                    term.colors[11] = '#FCE94F'; //Linux Tango - ~/location/
-                    term.colors[12] = '#729FCF'; //Linux Tango - (git-branch)
-                    term.colors[13] = '#AD7FA8'; //Linux Tango - images
-                    term.colors[14] = '#3465A4'; //Linux Tango - javascript comments
-                    term.colors[15] = '#fdf6e3';
-                    term.colors[256] = '#000000';
-                    term.colors[257] = '#fdf6e3';
-                  break;
-                case 'Light':
-                  var t = document.getElementsByClassName('terminal');
-                  t[0].style.backgroundColor = "#ffffff";
-                  t[0].style.color = "#000000";
-                    term.colors[0] = '#ffffff';
-                    term.colors[1] = '#dc322f';
-                    term.colors[2] = '#859900';
-                    term.colors[3] = '#b58900';
-                    term.colors[4] = '#268bd2';
-                    term.colors[5] = '#d33682';
-                    term.colors[6] = '#2aa198';
-                    term.colors[7] = '#000000';
-                    term.colors[8] = '#002b36';
-                    term.colors[9] = '#cb4b16';
-                    term.colors[10] = '#8AE234'; //Linux Tango - user@machine, shell scripts
-                    term.colors[11] = '#FCE94F'; //Linux Tango - ~/location/
-                    term.colors[12] = '#729FCF'; //Linux Tango - (git-branch)
-                    term.colors[13] = '#AD7FA8'; //Linux Tango - images
-                    term.colors[14] = '#3465A4'; //Linux Tango - javascript comments
-                    term.colors[15] = '#000000';
-                    term.colors[256] = '#ffffff';
-                    term.colors[257] = '#000000';
-                  break;
-                case 'Solarized':
-                    var t = document.getElementsByClassName('terminal');
-                    t[0].style.backgroundColor = "#002b36";
-                    t[0].style.color = "#839496";
-                    term.colors[0] = '#073642';
-                    term.colors[1] = '#dc322f'; //git - changes not staged for commit
-                    term.colors[2] = '#268bd2'; //git - changes staged for commit
-                    term.colors[3] = '#b58900';
-                    term.colors[4] = '#268bd2';
-                    term.colors[5] = '#d33682';
-                    term.colors[6] = '#2aa198';
-                    term.colors[7] = '#eee8d5';
-                    term.colors[8] = '#002b36';
-                    term.colors[9] = '#cb4b16';
-                    term.colors[10] = '#d33682'; //magenta user@machine
-                    term.colors[11] = '#268bd2';
-                    term.colors[12] = '#859900'; //green location
-                    term.colors[13] = '#6c71c4';
-                    term.colors[14] = '#2aa198';
-                    term.colors[15] = '#fdf6e3';
-                    break;
-            }
-        }
-    }
-
-    function changeScheme() {
-        var t = document.getElementsByClassName('terminal');
-        var schemeName = document.getElementById("color-scheme-dropdown").value;
-        __changeScheme(schemeName)
-    }
-  });
-  
+			var timeout;
+			window.addEventListener("resize", function() {
+				if (timeout) clearTimeout(timeout);
+				timeout = setTimeout(function() {
+					fitToDiv(term);
+					timeout = null;
+				}, 500);
+			});
+			socket.on('data', function(data) {
+				term.write(data);
+			});
+			socket.on('disconnect', function() {
+				term.destroy();
+			});
+			
+			changeScheme(colorScheme);
+		});
+		
+	});
+	
 	function getCWD() {
 		var result = PageUtil.matchResourceParameters(window.location.href).resource;
 		return result.length > 0 ? result : null;
 	}
 
+	function changeScheme(schemeName) {
+		var t;
+		if (term !== null) {
+			t = document.querySelector('.terminal');
+			switch(schemeName) {
+			case 'Dark': 
+				t.style.backgroundColor = t.style.borderColor = "#000000";
+				t.style.color = "#fdf6e3";
+				term.colors[0] = '#000000';
+				term.colors[1] = '#dc322f';
+				term.colors[2] = '#859900';
+				term.colors[3] = '#b58900';
+				term.colors[4] = '#268bd2';
+				term.colors[5] = '#d33682';
+				term.colors[6] = '#2aa198';
+				term.colors[7] = '#eee8d5';
+				term.colors[8] = '#002b36';
+				term.colors[9] = '#cb4b16';
+				term.colors[10] = '#8AE234'; //Linux Tango - user@machine, shell scripts
+				term.colors[11] = '#FCE94F'; //Linux Tango - ~/location/
+				term.colors[12] = '#729FCF'; //Linux Tango - (git-branch)
+				term.colors[13] = '#AD7FA8'; //Linux Tango - images
+				term.colors[14] = '#3465A4'; //Linux Tango - javascript comments
+				term.colors[15] = '#fdf6e3';
+				term.colors[256] = '#000000';
+				term.colors[257] = '#fdf6e3';
+				break;
+			case 'Light':
+				t.style.backgroundColor = t.style.borderColor = "#ffffff";
+				t.style.color = "#000000";
+				term.colors[0] = '#ffffff';
+				term.colors[1] = '#dc322f';
+				term.colors[2] = '#859900';
+				term.colors[3] = '#b58900';
+				term.colors[4] = '#268bd2';
+				term.colors[5] = '#d33682';
+				term.colors[6] = '#2aa198';
+				term.colors[7] = '#000000';
+				term.colors[8] = '#002b36';
+				term.colors[9] = '#cb4b16';
+				term.colors[10] = '#8AE234'; //Linux Tango - user@machine, shell scripts
+				term.colors[11] = '#FCE94F'; //Linux Tango - ~/location/
+				term.colors[12] = '#729FCF'; //Linux Tango - (git-branch)
+				term.colors[13] = '#AD7FA8'; //Linux Tango - images
+				term.colors[14] = '#3465A4'; //Linux Tango - javascript comments
+				term.colors[15] = '#000000';
+				term.colors[256] = '#ffffff';
+				term.colors[257] = '#000000';
+				break;
+			case 'Solarized':
+				t.style.backgroundColor = t.style.borderColor = "#002b36";
+				t.style.color = "#839496";
+				term.colors[0] = '#073642';
+				term.colors[1] = '#dc322f'; //git - changes not staged for commit
+				term.colors[2] = '#268bd2'; //git - changes staged for commit
+				term.colors[3] = '#b58900';
+				term.colors[4] = '#268bd2';
+				term.colors[5] = '#d33682';
+				term.colors[6] = '#2aa198';
+				term.colors[7] = '#eee8d5';
+				term.colors[8] = '#002b36';
+				term.colors[9] = '#cb4b16';
+				term.colors[10] = '#d33682'; //magenta user@machine
+				term.colors[11] = '#268bd2';
+				term.colors[12] = '#859900'; //green location
+				term.colors[13] = '#6c71c4';
+				term.colors[14] = '#2aa198';
+				term.colors[15] = '#fdf6e3';
+				break;
+			}
+		}
+		if (serviceRegistry) {
+			serviceRegistry.getService("orion.core.preference").put("/orion/console", {"colorScheme": schemeName});
+		}
+	}
+
+	function createCommands(registry, commandRegistry) {
+		serviceRegistry = registry;
+		serviceRegistry.getService("orion.core.preference").get("/orion/console").then(function(prefs) {
+			colorScheme = prefs.colorScheme || colorScheme;
+			changeScheme(colorScheme);
+		});
+		
+		var that = this;
+		var settingsCommand = new mCommands.Command({
+			imageClass: "core-sprite-wrench", //$NON-NLS-0$
+			tooltip: "Settings",
+			id: "orion.term.settings", //$NON-NLS-0$
+			visibleWhen: /** @callback */ function(items, data) {
+				return true;
+			},
+			callback: function(data) {
+				var dropDown = settingsCommand.settingsDropDown;
+				if (!dropDown || dropDown.isDestroyed()) {
+					dropDown = settingsCommand.settingsDropDown = new DropDownMenu(data.domNode.parentNode, data.domNode, {
+						noClick: true,
+						selectionClass: 'dropdownSelection', //$NON-NLS-0$
+						onShow: function() {
+							dropDown.focus();
+						},
+						onHide: function() {
+							that.editor.focus();
+						}
+					});
+					var menu = dropDown.getContentNode();
+					var select = new SettingsSelect({
+						options: ["Dark", "Light", "Solarized"].map(function(l) {
+							return {label: l, value: l, selected: colorScheme === l};
+						}),
+						fieldlabel: "Color Scheme:",
+						local: true
+					});
+					menu.appendChild(select.node);
+					select.postChange = changeScheme;
+					select.show();
+					menu.tabIndex = menu.style.marginTop = 0;
+				}
+				dropDown.click();
+			}
+		});
+		commandRegistry.addCommand(settingsCommand);
+		commandRegistry.registerCommandContribution("settingsActions", "orion.term.settings", 1, null, false, null, null, this); //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$
+	}
+
+	return {
+		createCommands: createCommands
+	};
 });
--- modules/orionode/lib/orionode.client/tty/scripts/ttyPage.js
+++ modules/orionode/lib/orionode.client/tty/scripts/ttyPage.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * @license
- * Copyright (c) 2013 IBM Corporation and others.
+ * Copyright (c) 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials are made 
  * available under the terms of the Eclipse Public License v1.0 
  * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
@@ -16,8 +16,10 @@ define([
 	"orion/commandRegistry",
 	"orion/fileClient",
 	"orion/searchClient",
-	"orion/globalCommands"
-], function(messages, mBootstrap, mCommandRegistry, mFileClient, mSearchClient, mGlobalCommands) {
+	"orion/globalCommands",
+	'orion/webui/littlelib',
+	"tty/scripts/shell"
+], function(messages, mBootstrap, mCommandRegistry, mFileClient, mSearchClient, mGlobalCommands, lib, shell) {
 
 	mBootstrap.startup().then(function(core) {
 		var serviceRegistry = core.serviceRegistry;
@@ -28,5 +30,15 @@ define([
 		var searcher = new mSearchClient.Searcher({serviceRegistry: serviceRegistry, commandService: commandRegistry, fileService: fileClient});
 		mGlobalCommands.generateBanner("orion-shellPage", serviceRegistry, commandRegistry, preferences, searcher); //$NON-NLS-0$
 		mGlobalCommands.setPageTarget({task: messages.Shell, serviceRegistry: serviceRegistry, commandService: commandRegistry});
+		
+		shell.createCommands(serviceRegistry, commandRegistry, preferences);
+		
+		["pageActions", "pageNavigationActions", "settingsActions"].forEach(function(id) { //$NON-NLS-2$ //$NON-NLS-1$ //$NON-NLS-0$ //$NON-NLS-3$
+			var toolbar = lib.node(id);
+			if (toolbar) {
+				commandRegistry.destroy(toolbar);
+				commandRegistry.renderCommands(toolbar.id, toolbar, this, this, "button"); //$NON-NLS-0$
+			}
+		});
 	});
 });
