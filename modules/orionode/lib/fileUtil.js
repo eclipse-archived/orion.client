eclipse-archived/orion.client

commit 9c2b027c9fcb17648234fb82e7960bff44c2a1b3

Fix: error in fileUtil.getChildren causes response to hang
Change-Id: Ieb5c11988ed84d6183bcc074eecfdf2ef3de9a90

modules/orionode/lib/fileUtil.js
@@ -26,50 +26,58 @@ var fs = Promise.promisifyAll(require('fs'));
 /**
  * Builds up an array of a directory's children, as File objects.
  * @param {String} parentLocation Parent location in the file api for child items (ugh)
- * @param {Array} [exclude] Filenames of children to hide. If omitted, everything is included.
- * @param {Function} callback
- * @returns {Array}
+ * @param {Array} [exclude] Filenames of children to hide. If `null`, everything is included.
+ * @param {Function} callback Invoked as func(error?, children)
+ * @returns A promise
  */
 // TODO depth
 var getChildren = exports.getChildren = function(directory, parentLocation, excludes, callback) {
-	// If 'excludes' is omitted, the callback can be given as the 3rd argument
-	callback = excludes || callback;
-	fs.readdirAsync(directory).then(function(files) {
-		// stat each file to find if it's a Directory -- ugh
-		var childStatPromises = files.map(function(file) {
+	return fs.readdirAsync(directory)
+	.then(function(files) {
+		return Promise.map(files, function(file) {
 			if (Array.isArray(excludes) && excludes.indexOf(file) !== -1) {
 				return null; // omit
 			}
 			var filepath = path.join(directory, file);
-			return fs.statAsync(filepath).then(function(stat) {
+			return fs.statAsync(filepath)
+			.then(function(stat) {
 				return [filepath, stat];
+			})
+			.catch(function(err) {
+				return err; // suppress rejection
 			});
-		}).filter(function(f) { return f; }); // skip omitted stuff
-		Promise.all(childStatPromises).then(function(childStats) {
-			var children = childStats.map(function(cs) {
-				var childname = path.basename(cs[0]);
-				var isDirectory = cs[1].isDirectory();
-				var timeStamp = cs[1].mtime.getTime();
-				var size = cs[1].size;
-				var location = api.join(parentLocation, encodeURIComponent(childname));
-				if(isDirectory && location[location.length-1] !== "/"){
-					location = location +"/";
-				}
-				var child = {
-					Name: childname,
-					Id: childname,
-					Length: size,
-					LocalTimeStamp: timeStamp,
-					Directory: isDirectory,
-					Location: location
-				};
-				if (isDirectory)
-					child.ChildrenLocation = api.join(parentLocation, childname) + '?depth=1';
-				return child;
-			});
-			callback(children); //yay
 		});
-	});
+	})
+	.then(function(childStats) {
+		var results = [];
+		childStats.forEach(function(cs) { // cs is [filepath, stat] or Error
+			if (cs instanceof Error) {
+				// TODO return failures to caller via some side channel
+				return;
+			}
+			var childname = path.basename(cs[0]);
+			var isDirectory = cs[1].isDirectory();
+			var timeStamp = cs[1].mtime.getTime();
+			var size = cs[1].size;
+			var location = api.join(parentLocation, encodeURIComponent(childname));
+			if(isDirectory && location[location.length-1] !== "/"){
+				location = location +"/";
+			}
+			var child = {
+				Name: childname,
+				Id: childname,
+				Length: size,
+				LocalTimeStamp: timeStamp,
+				Directory: isDirectory,
+				Location: location
+			};
+			if (isDirectory)
+				child.ChildrenLocation = api.join(parentLocation, childname) + '?depth=1';
+			results.push(child);
+		});
+		return results;
+	})
+	.asCallback(callback);
 };
 
 /**

modules/orionode/lib/search.js
@@ -217,7 +217,10 @@ module.exports = function(options) {
 		var searchScope = req.user.workspaceDir + searchOpt.location.substring(endOfFileRootIndex, searchOpt.location.length - 1);
 		if (searchScope.charAt(searchScope.length - 1) !== "/") searchScope = searchScope + "/";
 
-		fileUtil.getChildren(searchScope, parentFileLocation, function(children) {
+		fileUtil.getChildren(searchScope, parentFileLocation, null, function(err, children) {
+			if (err) {
+				// ignore
+			}
 			var results = [];
 
 			Promise.map(children, function(child) {

modules/orionode/lib/workspace.js
@@ -76,7 +76,10 @@ module.exports = function(options) {
 		} else if (rest === workspaceId) {
 			// http://wiki.eclipse.org/Orion/Server_API/Workspace_API#Getting_workspace_metadata
 			var parentFileLocation = originalFileRoot(req);
-			fileUtil.getChildren(req.user.workspaceDir, parentFileLocation, function(children) {
+			fileUtil.getChildren(req.user.workspaceDir, parentFileLocation, null, function(err, children) {
+				if (err) {
+					return res.status(500).json({Message: err.message });
+				}
 				// TODO this is basically a File object with 1 more field. Should unify the JSON between workspace.js and file.js
 				var ws = JSON.stringify({
 					Directory: true,

commit c88ae057203a7352411eb30d33b0eb2c44185b4e

Node: fix code I broke in 9c2b027c9f
Change-Id: I8f1909479cadc53ce2063318fea8ac6fb0d3a733

modules/orionode/lib/fileUtil.js
@@ -353,7 +353,10 @@ var writeFileMetadata = exports.writeFileMetadata = function(fileRoot, res, wwwp
 		if (!includeChildren) {
 			api.write(null, res, null, metaObj);
 		} else {
-			getChildren(filepath, api.join(fileRoot, wwwpath)/*this dir*/, null/*omit nothing*/, function(children) {
+			getChildren(filepath, api.join(fileRoot, wwwpath)/*this dir*/, null/*omit nothing*/, function(err, children) {
+				if (err) {
+					return api.writeError(500, res, err);
+				}
 				var name = path.basename(filepath);
 				if (name[name.length-1] === path.sep) {
 					name = name.substring(0, name.length-1);