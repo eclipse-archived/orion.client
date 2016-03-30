bab53b676845af9916c7163ce82c09e485b805b7
Curtis Windatt <cwindatt@ca.ibm.com>
2016-03-30 20:38:30 +0000

    [nobug] - Fix self hosting of the WebIDE Bad define and other lint problems

--- bundles/org.eclipse.orion.client.ui/web/plugins/site/selfHostingRules.js
+++ bundles/org.eclipse.orion.client.ui/web/plugins/site/selfHostingRules.js
@@ -10,7 +10,9 @@
  * Contributors: IBM Corporation - initial API and implementation
  ******************************************************************************/
 /*eslint-env browser, amd*/
-define([], function() {
+define([
+'i18n!orion/nls/messages'
+], function(messages) {
 
 	/*
 	 * FILE: targetPattern represents a workspace path
@@ -19,42 +21,42 @@ define([], function() {
 	var FILE = 0, API = 1;
 	// This is kind of clumsy because API paths aren't followed by / but FILE paths are..
 	var SELF_HOSTING_TEMPLATE = [
-		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.ui/web/index.html" },
-		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.ui/web" },
-		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.help/web" },
-		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.users/web" },
-		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.core/web" },
-		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.editor/web" },
-		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.cf/web" },
-		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.git/web" },
-		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.javascript/web" },
-		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.webtools/web" },
-		{ type: API, source: "/file", targetPattern: "${0}file" },
-		{ type: API, source: "/prefs", targetPattern: "${0}prefs" },
-		{ type: API, source: "/workspace", targetPattern: "${0}workspace" },
-		{ type: API, source: "/users", targetPattern: "${0}users" },
-		{ type: API, source: "/authenticationPlugin.html", targetPattern: "${0}authenticationPlugin.html" },
-		{ type: API, source: "/login", targetPattern: "${0}login" },
-		{ type: API, source: "/loginstatic", targetPattern: "${0}loginstatic" },
-		{ type: API, source: "/useremailconfirmation", targetPattern: "${0}useremailconfirmation" },
-		{ type: API, source: "/site", targetPattern: "${0}site" },
-		{ type: API, source: "/gitapi", targetPattern: "${0}gitapi" },
-		{ type: API, source: "/xfer", targetPattern: "${0}xfer" },
-		{ type: API, source: "/filesearch", targetPattern: "${0}filesearch" },
-		{ type: API, source: "/index.jsp", targetPattern: "${0}index.jsp" },
-		{ type: API, source: "/plugins/git", targetPattern: "${0}plugins/git" },
-		{ type: API, source: "/plugins/user", targetPattern: "${0}plugins/user" },
-		{ type: API, source: "/logout", targetPattern: "${0}logout" },
-		{ type: API, source: "/task", targetPattern: "${0}task" },
-		{ type: API, source: "/cfapi", targetPattern: "${0}cfapi" },
-		{ type: API, source: "/metrics", targetPattern: "${0}metrics" },
+		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.ui/web/index.html" }, //$NON-NLS-1$
+		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.ui/web" }, //$NON-NLS-1$
+		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.help/web" }, //$NON-NLS-1$
+		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.users/web" }, //$NON-NLS-1$
+		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.core/web" }, //$NON-NLS-1$
+		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.editor/web" }, //$NON-NLS-1$
+		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.cf/web" }, //$NON-NLS-1$
+		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.git/web" }, //$NON-NLS-1$
+		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.javascript/web" }, //$NON-NLS-1$
+		{ type: FILE, source: "/", targetPattern: "${0}/bundles/org.eclipse.orion.client.webtools/web" }, //$NON-NLS-1$
+		{ type: API, source: "/file", targetPattern: "${0}file" }, //$NON-NLS-1$ //$NON-NLS-2$
+		{ type: API, source: "/prefs", targetPattern: "${0}prefs" }, //$NON-NLS-2$ //$NON-NLS-1$
+		{ type: API, source: "/workspace", targetPattern: "${0}workspace" }, //$NON-NLS-2$ //$NON-NLS-1$
+		{ type: API, source: "/users", targetPattern: "${0}users" }, //$NON-NLS-1$ //$NON-NLS-2$
+		{ type: API, source: "/authenticationPlugin.html", targetPattern: "${0}authenticationPlugin.html" }, //$NON-NLS-1$ //$NON-NLS-2$
+		{ type: API, source: "/login", targetPattern: "${0}login" }, //$NON-NLS-1$ //$NON-NLS-2$
+		{ type: API, source: "/loginstatic", targetPattern: "${0}loginstatic" }, //$NON-NLS-1$ //$NON-NLS-2$
+		{ type: API, source: "/useremailconfirmation", targetPattern: "${0}useremailconfirmation" }, //$NON-NLS-1$ //$NON-NLS-2$
+		{ type: API, source: "/site", targetPattern: "${0}site" }, //$NON-NLS-1$ //$NON-NLS-2$
+		{ type: API, source: "/gitapi", targetPattern: "${0}gitapi" }, //$NON-NLS-1$ //$NON-NLS-2$
+		{ type: API, source: "/xfer", targetPattern: "${0}xfer" }, //$NON-NLS-1$ //$NON-NLS-2$
+		{ type: API, source: "/filesearch", targetPattern: "${0}filesearch" }, //$NON-NLS-1$ //$NON-NLS-2$
+		{ type: API, source: "/index.jsp", targetPattern: "${0}index.jsp" }, //$NON-NLS-1$ //$NON-NLS-2$
+		{ type: API, source: "/plugins/git", targetPattern: "${0}plugins/git" }, //$NON-NLS-1$ //$NON-NLS-2$
+		{ type: API, source: "/plugins/user", targetPattern: "${0}plugins/user" }, //$NON-NLS-1$ //$NON-NLS-2$
+		{ type: API, source: "/logout", targetPattern: "${0}logout" }, //$NON-NLS-1$ //$NON-NLS-2$
+		{ type: API, source: "/task", targetPattern: "${0}task" }, //$NON-NLS-1$ //$NON-NLS-2$
+		{ type: API, source: "/cfapi", targetPattern: "${0}cfapi" }, //$NON-NLS-1$ //$NON-NLS-2$
+		{ type: API, source: "/metrics", targetPattern: "${0}metrics" }, //$NON-NLS-1$ //$NON-NLS-2$
 	];
 
 	return {
 		Config: {
 			folders: [
 				{
-					name: "org.eclipse.orion.client",
+					name: "org.eclipse.orion.client", //$NON-NLS-1$
 					label: messages.orionClientLabel
 				}
 			]
