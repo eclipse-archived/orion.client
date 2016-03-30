b7c93cd5e66ba2ff7b0286385f97c8d98c482d91
Bogdan Gheorghe <gheorghe@ca.ibm.com>
2016-03-30 15:24:40 -0400

    [node] Add build script for orionode

---
+++ modules/orionode/orionode_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+npm config set proxy http://proxy.eclipse.org:9898
+npm config set https-proxy http://proxy.eclipse.org:9898
+npm install
+./node_modules/.bin/grunt
+rm -rf node_modules
+npm install --production
+rm -rf node_modules/nodegit
+rm -rf node_modules/pty.js
+cd ..
+DATE=`date +%Y%m%d%H%M%S`
+tar -czf "orionode_$DATE.tar.gz" orionode/
