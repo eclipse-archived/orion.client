a370f9cb492cd51a217163a3843a227df0732167
Bogdan Gheorghe <gheorghe@ca.ibm.com>
2016-03-31 11:27:25 -0400

    [node] Fix build script

--- modules/orionode/build/orionode_build.sh
+++ modules/orionode/build/orionode_build.sh
@@ -10,4 +10,4 @@ rm -rf target
 cd ../..
 tar -czf "orionode_$1.tar.gz" orionode/
 cp "orionode_$1.tar.gz" /home/data/httpd/download.eclipse.org/orion/orionode/ 
-ssh gheorghe@build.eclipse.org ./deploy.sh -archive 
+ssh gheorghe@build.eclipse.org ./deploy.sh -archive /home/data/httpd/download.eclipse.org/orion/orionode/"orionode_$1.tar.gz"
