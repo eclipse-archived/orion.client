da7c76c4351920306f6abe83b1543bfae6b35ab9
Bogdan Gheorghe <gheorghe@ca.ibm.com>
2016-03-31 15:28:56 -0400

    [node] Fix build script

--- modules/orionode/build/orionode_build.sh
+++ modules/orionode/build/orionode_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 npm install
 ../node_modules/.bin/grunt
-rm -rf node_modules
+rm -rf ../node_modules
 npm install --production
-rm -rf node_modules/pty.js
-rm -rf node_modules/nodegit/vendor
-rm -rf node_modules/nodegit/build/Release/obj.target
-rm -rf target
+rm -rf ../node_modules/pty.js
+rm -rf ../node_modules/nodegit/vendor
+rm -rf ../node_modules/nodegit/build/Release/obj.target
+rm -rf ../target
 cd ../..
 tar -czf "orionode_$1.tar.gz" orionode/
 cp "orionode_$1.tar.gz" /home/data/httpd/download.eclipse.org/orion/orionode/ 
