e0cbc372987edd7c610d73a8f114560a162a9618
Silenio Quarti <Silenio_Quarti@ca.ibm.com>
2016-04-11 16:57:58 -0400

    node: fix /filesearch endpoint no longer call

--- modules/orionode/index.js
+++ modules/orionode/index.js
@@ -62,10 +62,10 @@ function startServer(options) {
 		}
 		app.use('/site', checkAuthenticated, require('./lib/sites')(options));
 		app.use('/task', checkAuthenticated, require('./lib/tasks').router({ root: '/task' }));
+		app.use('/filesearch', checkAuthenticated, require('./lib/search')({ root: '/filesearch',  fileRoot: '/file' }));
 		app.use('/file*', checkAuthenticated, require('./lib/file')({ root: '/file', options: options }));
 		app.use('/workspace*', checkAuthenticated, require('./lib/workspace')({ root: '/workspace', fileRoot: '/file', options: options }));
 		app.use('/gitapi', checkAuthenticated, require('./lib/git')({ root: '/gitapi', fileRoot: '/file' }));
-		app.use('/filesearch', checkAuthenticated, require('./lib/search')({ root: '/filesearch',  fileRoot: '/file' }));
 		app.use('/prefs', checkAuthenticated, require('./lib/controllers/prefs')(options));
 		app.use('/xfer', checkAuthenticated, require('./lib/xfer')(options));
 		app.use('/metrics', require('./lib/metrics').router(options));
