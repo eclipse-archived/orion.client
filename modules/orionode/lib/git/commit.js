8952809b772abd398b03a7a4645432a3a9816a4d
Sidney Jiang <xinyij@ca.ibm.com>
2016-04-27 14:26:37 -0400

    Bug 492272 - generate change Id
    Change-Id: I38ac1a2ca1ba00aa8c1ae252881318cb4f8e98af
    Signed-off-by: Sidney Jiang <xinyij@ca.ibm.com>

--- modules/orionode/lib/git/commit.js
+++ modules/orionode/lib/git/commit.js
@@ -557,7 +557,7 @@ function merge(req, res, commitToMerge, squash) {
 	});
 }
 
-function createCommit(repo, committerName, committerEmail, authorName, authorEmail, message, amend){
+function createCommit(repo, committerName, committerEmail, authorName, authorEmail, message, amend, insertChangeid){
 	var index, oid, author, committer;
 	return repo.index()
 	.then(function(indexResult) {
@@ -584,6 +584,11 @@ function createCommit(repo, committerName, committerEmail, authorName, authorEma
 			committer = git.Signature.now(committerName, committerEmail);
 		} else {
 			committer = git.Signature.default(repo);
+		}
+		if(insertChangeid) {
+			var firstParentId = parent? parent.parentId(0) : "";
+			var hashId = generateChangeId(oid, firstParentId, author.toString(), committer.toString(), message);
+			message = insertChangeId(message,hashId);
 		}
 		if (amend) {
 			return parent.amend("HEAD",  author, committer, null, message, oid);
@@ -658,7 +663,6 @@ function postCommit(req, res) {
 		return;
 	}
 	
-	//TODO create commit -> change id
 	var commit = util.decodeURIComponent(req.params.commit);
 	if (commit !== "HEAD") {
 		writeError(404, res, "Needs to be HEAD");
@@ -670,6 +674,9 @@ function postCommit(req, res) {
 		return;
 	}
 
+	// Since ChangeId messagefoot is not showing in the box, so insert Message after empty Checking.
+	var isInsertChangeId = req.body.ChangeId;	
+
 	var theRepo, thisCommit;
 	var theDiffs = [], theParents;
 	
@@ -677,7 +684,7 @@ function postCommit(req, res) {
 	.then(function(repo) {
 		theRepo = repo;
 		fileDir = api.join(fileRoot, repo.workdir().substring(req.user.workspaceDir.length + 1));
-		return createCommit(repo, req.body.CommitterName, req.body.CommitterEmail, req.body.AuthorName, req.body.AuthorEmail, req.body.Message, req.body.Amend);
+		return createCommit(repo, req.body.CommitterName, req.body.CommitterEmail, req.body.AuthorName, req.body.AuthorEmail, req.body.Message, req.body.Amend, isInsertChangeId);
 	})
 	.then(function(commit) {
 		thisCommit = commit;
@@ -698,5 +705,88 @@ function postCommit(req, res) {
 	.done(function() {
 		res.status(200).json(commitJSON(thisCommit, fileDir, theDiffs, theParents));
 	});
+}
+
+function generateChangeId(oid, firstParentId, authorId, committerId, message){
+	var cleanMessage = clean(message);
+	function clean(fullMessage) {
+		return fullMessage.//
+				replace(/^#.*$\n?/g, "").// //$NON-NLS-1$
+				replace(/\n\n\n+/g, "\n").// //$NON-NLS-1$
+				replace(/\n*$/g, "").// //$NON-NLS-1$
+				replace(/\ndiff --git.*/g, "").// //$NON-NLS-1$
+				trim();
+	}
+	var mergedMessage = "tree ".concat(oid).concat("\n").concat("parent ").concat(firstParentId).concat("\n")
+		.concat("author ").concat(authorId).concat("\n").concat("committer ").concat(committerId).concat("\n\n").concat(cleanMessage);
+	var hash = crypto.createHash("sha1");
+	hash.update(mergedMessage);
+	return hash.digest('hex');
+}
+
+function insertChangeId(originalMessage,changeId){
+	var lines = originalMessage.split("\n"); //$NON-NLS-1$
+	// Has ChangeId do nothing, even if changeId is changed, do not replace it.
+	if(!existingOfChangeId(lines)) {
+		var footerFirstLine = indexOfFirstFooterLine(lines);
+		var insertAfter = footerFirstLine;
+		for (var i = footerFirstLine; i < lines.length; ++i) {
+			if (lines[i].match(/^(Bug|Issue)[a-zA-Z0-9-]*:.*$/)) {
+				insertAfter = i + 1;
+				continue;
+			}
+			break;
+		}
+		var changeIdString = "Change-Id: I" + changeId;
+		if ( lines.length === 1 ){
+			lines.splice(1, 0, "\n"+changeIdString);	
+		}else{
+			lines.splice(insertAfter, 0, changeIdString);
+		}
+		return lines.join("\n");
+ 	}
+ 	
+ 	function existingOfChangeId(alllinesArray){
+		if (alllinesArray.length === 0)
+			return false;
+		var inFooter = false;
+		for (var m = alllinesArray.length - 1; m >= 0; --m) {
+			if (!inFooter && isEmptyLine(alllinesArray[m])){
+				// Fine the last not-empty line, or keep finding.
+				continue;
+			}
+			inFooter = true;
+			if (alllinesArray[m].match(/^\s+Change-Id: *I[a-f0-9]{40}$/)) {
+				return true;
+			} else if (isEmptyLine(alllinesArray[m]) || m === 0)
+				// In case that the array length is not 0, but there are actually nothing inside.
+				return false;
+		}	
+ 	}
+ 	
+ 	function isEmptyLine(line){
+		return line.trim().length === 0;
+ 	}
+ 		
+ 	function indexOfFirstFooterLine(lines){
+ 		var footerFirstLineNum = lines.length;
+		for (var l = lines.length - 1; l > 1; --l) {
+			if (lines[l].match(/^[a-zA-Z0-9-]+:.*$/)) {
+				footerFirstLineNum = l;
+				continue;
+			}
+			if (footerFirstLineNum !== lines.length && lines[l].length === 0){
+				break;
+			}
+			if (footerFirstLineNum !== lines.length
+					&& lines[l].match(/^[ \\[].*$/)) {
+				footerFirstLineNum = l + 1;
+				continue;
+			}
+			footerFirstLineNum = lines.length;
+			break;
+		}
+		return footerFirstLineNum;
+ 	}
 }
 };
