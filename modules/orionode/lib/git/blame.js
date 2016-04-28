3acf715638f7cedd064cc89cb58e98b910bfb978
Sidney Jiang <xinyij@ca.ibm.com>
2016-04-28 17:49:46 -0400

    Bug 490817 - Implement /gitapi/blame
    Change-Id: I02877173437fa0ebb9f77a15a626a267a40f00fd
    Signed-off-by: Sidney Jiang <xinyij@ca.ibm.com>

--- modules/orionode/lib/git/blame.js
+++ modules/orionode/lib/git/blame.js
@@ -9,10 +9,12 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 /*eslint-env node */
+var api = require('../api'), writeError = api.writeError;
 var git = require('nodegit');
 var express = require('express');
 var bodyParser = require('body-parser');
 var clone = require('./clone');
+var commitm = require('./commit');
 
 module.exports = {};
 
@@ -22,15 +24,115 @@ module.exports.router = function(options) {
 
 	return express.Router()
 	.use(bodyParser.json())
-	.get('/file/*', getBlame);
+	.get('/:refName/file/*', getBlame);
 	
 function getBlame(req, res) {
+	var blamerepo,fileDir, fileRelativePath;
 	clone.getRepo(req)
 	.then(function(repo) {
-		git.Blame.file(repo, req.params["0"]).then(function(blame) {
-			res.status(200).json(blame);
-			return blame;
-		});
+		blamerepo = repo;
+		fileDir = api.join(fileRoot, repo.workdir().substring(req.user.workspaceDir.length + 1));
+		fileRelativePath = req.params["0"].split("/").slice(1).join("/");
+		return git.Blame.file(repo,fileRelativePath, git.Blame.FLAG.NORMAL);
+	})
+	.then(function(blame){
+		var hunkNum = blame.getHunkCount(); 
+		var hunks = new Array();
+		for(var i = 0; i < hunkNum; i++){
+			hunks.push(blame.getHunkByIndex(i));
+		}
+		
+		createhunkCommitArray(blamerepo,hunks).then(function(hunkCommitArray){
+			var hunkMergedArray = mergeChildren(hunkCommitArray);
+			var blamejason = createBlameJson(hunkMergedArray,fileDir);
+			var sendingBlamejason;
+			sendingBlamejason = {
+				"Children" : blamejason,
+				"CloneLocation": "/gitapi/clone" + fileDir ,
+				"Location": "/gitapi/blame/HEAD" + fileDir + fileRelativePath,
+				"Type" : "Blame"
+			};
+			res.status(200).json(sendingBlamejason);
+		}).catch(function(err){
+			console.log(err);
+			});
+	}).catch(function(err){
+		console.log(err);
 	});
+}
+
+function createhunkCommitArray(blamerepo, hunks){
+	return Promise.all(hunks.map(function(hunk){
+		return getHunkCommit(blamerepo,hunk);
+	}));
+}
+
+function getHunkCommit(blamerepo,hunk){
+	return git.Commit.lookup(blamerepo, hunk.finalCommitId())
+			.then(function(commit){
+				return {
+					commit:commit,
+					hunk: hunk
+				};
+			});
+}	
+
+function hunklines(hunk){
+	var starttemp = hunk.finalStartLineNumber();
+	var lines = hunk.linesInHunk();
+	return {
+        "End": starttemp + lines - 1,
+		"Start": starttemp
+	};
+}	
+
+function mergeChildren(hunkArray){
+	// Coming object in the array {commit:XX,hunk:XX}
+	var hunkArraywithChildren = hunkArray.map(function(hunkcommit){
+		return {
+			Commit: hunkcommit.commit,
+			Children: [hunklines(hunkcommit.hunk)]
+		};
+	});
+	
+	var result = [];
+	for(var i = 0; i < hunkArraywithChildren.length; i++) {
+		var indexInResult = arrayContains(result,hunkArraywithChildren[i]);
+        if( indexInResult === -1 ) {
+            result.push(hunkArraywithChildren[i]);
+        }else{
+        	// Merge Children
+        	result[indexInResult].Children = result[indexInResult].Children.concat(hunkArraywithChildren[i].Children);
+        }
+    }
+    // Going object int he array {commit:XX ,children:[XX,XX]}
+    return 	result;
+	
+	function arrayContains(array , element){
+	  	for(var j = 0; j < array.length; j++) {
+	        if(array[j].Commit.sha() === element.Commit.sha()) 
+	        	return j;
+	    }
+	    return -1;
+	}
+}
+
+function createBlameJson(hunkMergedArray, fileDir){
+	// Coming object in the array {commit: ,children:}
+	return hunkMergedArray.map(function(hunkCommit){
+		var hunkCommitJson = commitm.commitJSON(hunkCommit.Commit,fileDir);
+		delete hunkCommitJson.Branches;
+		delete hunkCommitJson.CloneLocation;
+		delete hunkCommitJson.ContentLocation;
+	    delete hunkCommitJson.DiffLocation;
+	    delete hunkCommitJson.CloneLocation;
+	    delete hunkCommitJson.Diffs;
+	    delete hunkCommitJson.Parents;
+	    delete hunkCommitJson.Id;
+	    delete hunkCommitJson.Type;
+		hunkCommitJson.Children = hunkCommit.Children;
+		return hunkCommitJson;
+	});
+	// Going object in the array {authorname:,committer:.....,children:} 
 }
 };
