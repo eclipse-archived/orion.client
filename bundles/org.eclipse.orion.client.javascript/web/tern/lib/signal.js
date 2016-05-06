3cdcd39715d99531e6eae984362b40acfa924ee2
Mike Rennie <Michael_Rennie@ca.ibm.com>
2016-05-06 11:38:55 -0400

    Bug 493144 - HTML Tooling is broken - signal

--- bundles/org.eclipse.orion.client.javascript/web/tern/lib/signal.js
+++ bundles/org.eclipse.orion.client.javascript/web/tern/lib/signal.js
@@ -32,7 +32,7 @@
     var arr = getHandlers(this, type)
     for (var i = 0; i < arr.length; ++i) {
       var result = arr[i].call(this, a1, a2, a3, a4)
-      if (result) return result
+      if (result !== null && result !== undefined) return result
     }
   }
 
