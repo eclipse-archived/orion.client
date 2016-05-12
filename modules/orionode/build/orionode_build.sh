c2124bbe785cbb666d43605a51bb28c4dfdbab1a
Silenio Quarti <Silenio_Quarti@ca.ibm.com>
2016-05-12 16:19:06 -0400

    node build: replace nodegit.node with pre-compiled library (which has SSH memory credentials support)

--- modules/orionode/build/orionode_build.sh
+++ modules/orionode/build/orionode_build.sh
@@ -6,9 +6,11 @@ function die () {
 }
 
 (npm install --no-optional) || die "Failed to install dependencies, consult the npm log to find out why."
+cp /home/data/httpd/download.eclipse.org/orion/orionode/nodegit.node ../node_modules/nodegit/build/Release
 (../node_modules/.bin/grunt) || die "Failed to minify client code."
 rm -rf ../node_modules
 (npm install --production --no-optional) || die "Failed to install dependencies, consult the npm log to find out why."
+cp /home/data/httpd/download.eclipse.org/orion/orionode/nodegit.node ../node_modules/nodegit/build/Release
 rm -rf ../node_modules/pty.js
 rm -rf ../node_modules/nodegit/vendor
 rm -rf ../node_modules/nodegit/build/Release/obj.target
