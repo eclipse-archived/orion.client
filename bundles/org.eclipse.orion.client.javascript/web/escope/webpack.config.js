278917749f3d0fe2c027e34b495d346b316ceec4
Mike Rennie <Michael_Rennie@ca.ibm.com>
2016-05-13 14:46:12 -0400

    [nobug] updated webpack build for escope, with instructions

--- bundles/org.eclipse.orion.client.javascript/web/escope/webpack.config.js
+++ bundles/org.eclipse.orion.client.javascript/web/escope/webpack.config.js
@@ -1,5 +1,26 @@
+/**
+ * THe follow steps describe how to use this build file
+ *
+ * 1. Clone the escope repo: https://github.com/estools/escope.git
+ * 2. Change to the repo directory
+ * 3. Install the node modules needed for building
+ *  > npm i babel babel-core babel-loader babel-preset-es2015
+ *  > npm i webpack
+ *  > npm i json-loader
+ * 4. Open the package.json file and remove the dependencies for es6-map and es6-weakmap
+ * 5. Install the dependencies
+ *  > npm install
+ * 6. Edit the following files:
+ *  - remove 'assert' use in referencer.js
+ *  - remove 'es6-map' and 'assert' use in scope.js
+ *  - remove 'es6-weak-map' and 'assert' use in scope-manager.js
+ *  - remove the package.json/version import in index.js and put in the version directly
+ * 7. Run 
+ *  > webpack -p
+ *  there should be no errors reported
+ */
 module.exports = {
-    entry: './lib/index.js',
+    entry: './src/index.js',
     output: {
     	libraryTarget: 'amd',
         filename: './built-js/escope.js',
@@ -10,9 +31,13 @@ module.exports = {
             {
                 test: /\.json$/,
                 loader: "json-loader" 
-            }
+            },
+            {
+                test: /\.js$/,
+                loaders: ['babel']
+              }
         ]
-    },
+    }
     //debug: true,
-    devtool: "source-map"
+    //devtool: "source-map"
 };
