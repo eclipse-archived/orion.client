cba0a24d409ed0664d56558e414fa99467e6a6ba
Mike Rennie <Michael_Rennie@ca.ibm.com>
2016-05-24 13:47:32 -0400

    Bug 494467 - Tern is getting hammered with occurrences requests

--- bundles/org.eclipse.orion.client.javascript/web/javascript/occurrences.js
+++ bundles/org.eclipse.orion.client.javascript/web/javascript/occurrences.js
@@ -1,6 +1,6 @@
  /*******************************************************************************
  * @license
- * Copyright (c) 2013, 2015 IBM Corporation and others.
+ * Copyright (c) 2013, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials are made 
  * available under the terms of the Eclipse Public License v1.0 
  * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
@@ -12,8 +12,9 @@
 /*eslint-env amd*/
 define([
 'orion/objects',
-'orion/Deferred'
-], function(Objects, Deferred) {
+'orion/Deferred',
+'javascript/finder'
+], function(Objects, Deferred, Finder) {
 	
 	/**
 	 * @name javascript.JavaScriptOccurrences
@@ -41,14 +42,19 @@ define([
 			var deferred = new Deferred();
 			editorContext.getFileMetadata().then(function(meta) {
 				editorContext.getText().then(function(text) {
-					var files = [{type: "full", name: meta.location, text: text}]; //$NON-NLS-1$
-					this.ternworker.postMessage({request: "occurrences", args: {params: {offset: ctxt.selection.end}, files: files, meta: {location: meta.location}}}, function(response, error) { //$NON-NLS-1$
-					if(response.occurrences) {
-						deferred.resolve(response.occurrences);
-					} else if(error) {
-						deferred.reject(error);
+					var word = Finder.findWord(text, ctxt.selection.end);
+					if(word) {
+						var files = [{type: "full", name: meta.location, text: text}]; //$NON-NLS-1$
+						this.ternworker.postMessage({request: "occurrences", args: {params: {offset: ctxt.selection.end}, files: files, meta: {location: meta.location}}}, function(response, error) { //$NON-NLS-1$
+							if(response.occurrences) {
+								deferred.resolve(response.occurrences);
+							} else if(error) {
+								deferred.reject(error);
+							}
+						});
+					} else {
+						deferred.resolve([]);
 					}
-				});
 				}.bind(this));
 				
 			}.bind(this));
