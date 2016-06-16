ff60313d3ac50ffe75a89dbd319514034f834eaa
Silenio Quarti <Silenio_Quarti@ca.ibm.com>
2016-06-16 17:14:20 -0400

    electron build - make electron version configurable (pre-compiled nodegit)

--- modules/orionode/build/electron_build.sh
+++ modules/orionode/build/electron_build.sh
@@ -82,6 +82,7 @@ cleanup_nodemodules() {
 # update orion.conf and package.json
 update_config_files() {
 	electron_version=$(cat orionode/package.json | jsawk 'return this.build.electronVersion')
+	nodegit_version=$(cat orionode/package.json | jsawk 'return this.dependencies.nodegit')
 	pkg_version=$(grep -m1 "version" orionode/package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 	old_version=${pkg_version}
 	pkg_version=`echo ${pkg_version} | sed 's/.0$/.'"${BUILD_NUMBER}"'/'`
@@ -120,7 +121,7 @@ cleanup_nodemodules
 update_config_files
 
 # copy over nodegit binary to workaround in memory ssh limitation
-cp ~/downloads/orion/orionode/nodegit/v0.13.0/electron/${electron_version}/mac/nodegit.node ./orionode/node_modules/nodegit/build/Release
+cp ~/downloads/orion/orionode/nodegit/v${nodegit_version}/electron/v${electron_version}/mac/nodegit.node ./orionode/node_modules/nodegit/build/Release
 
 pushd orionode
 
@@ -143,7 +144,7 @@ cleanup_nodemodules
 update_config_files
 
 # copy over nodegit binary to workaround in memory ssh limitation
-cp ~/downloads/orion/orionode/nodegit/v0.13.0/electron/${electron_version}/windows/nodegit.node orionode/node_modules/nodegit/build/Release
+cp ~/downloads/orion/orionode/nodegit/v${nodegit_version}/electron/v${electron_version}/windows/nodegit.node orionode/node_modules/nodegit/build/Release
 
 pushd orionode
 update_remote_releases
@@ -173,7 +174,7 @@ cleanup_nodemodules
 update_config_files
 
 # copy over nodegit binary to workaround in memory ssh limitation
-cp ~/downloads/orion/orionode/nodegit/v0.13.0/electron/${electron_version}/linux/nodegit.node orionode/node_modules/nodegit/build/Release
+cp ~/downloads/orion/orionode/nodegit/v${nodegit_version}/electron/v${electron_version}/linux/nodegit.node orionode/node_modules/nodegit/build/Release
 
 pushd orionode
 
