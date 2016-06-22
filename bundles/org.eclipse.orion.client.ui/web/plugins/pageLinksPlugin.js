bf85367ce5bdb0eb4c3003bd0f4e7e8f0e7cdcb5
Silenio Quarti <Silenio_Quarti@ca.ibm.com>
2016-06-22 17:14:35 -0400

    Bug 496593 - Remove Help menu item because it is outdated

--- bundles/org.eclipse.orion.client.ui/web/plugins/pageLinksPlugin.js
+++ bundles/org.eclipse.orion.client.ui/web/plugins/pageLinksPlugin.js
@@ -178,16 +178,16 @@ define([
 				uriTemplate: "{+OrionHome}/settings/settings.html#,category=userSettings",
 				category: "user.0"
 			});
-		}
 	
-		provider.registerService("orion.page.link.user", null, {
-			id: "orion.help",
-			order: 20,
-			name: widgetMessages["Help"],
-			nls: "orion/widgets/nls/messages",
-			uriTemplate: "{+OrionHome}/help/help.html",
-			category: "user.0"
-		});
+			provider.registerService("orion.page.link.user", null, {
+				id: "orion.help",
+				order: 20,
+				name: widgetMessages["Help"],
+				nls: "orion/widgets/nls/messages",
+				uriTemplate: "{+OrionHome}/help/help.html",
+				category: "user.0"
+			});
+		}
 		
 		provider.registerService("orion.page.link.user", null, {
 			id: "orion.report.bug",
