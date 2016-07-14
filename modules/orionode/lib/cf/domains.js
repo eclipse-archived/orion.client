8e58c52198315aa2279dc0516098916bb718359d
Sidney <xinyij@ca.ibm.com>
2016-07-14 10:47:12 -0400

    Bug 497124 - RunBar(Cloud Foundation) implementation
    Change-Id: I6862be28bfc3c2713d191ec5aa4f3ec98ceb4ae7
    Signed-off-by: Sidney <xinyij@ca.ibm.com>

---
+++ modules/orionode/lib/cf/domains.js
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ * Copyright (c) 2016 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*eslint-env node */
+var express = require('express');
+var bodyParser = require('body-parser');
+var orgs = require('./orgs');
+var target = require('./target');
+var request = require('request');
+var tasks = require('../tasks');
+
+module.exports.router = function(options) {
+	
+	module.exports.getCFdomains = getCFdomains;
+	
+	return express.Router()
+	.use(bodyParser.json())
+	.get('*', getDomain);
+	
+	function getDomain(req, res){
+		var task = new tasks.Task(res,false,false,0,false);
+		var targetRequest = JSON.parse(req.query.Target);
+		var defaultDomainMode = req.query.Default; // TODO This seems not place to get the value or Java code wield!
+		var domainName = req.query.Name;  // TODO Need to check java code to see when this is needed.
+		getCFdomains(targetRequest, domainName,defaultDomainMode)		
+		.then(function(domainArray){
+			var resp = {};
+			if(!domainArray){
+				task.done({
+					HttpCode: 404,
+					Code: 0,
+					DetailedMessage: "Domain can not be found",
+					JsonData: resp,
+					Message: "Domain can not be found",
+					Severity: "Error"
+				});
+			}else{
+				resp = {
+					"Domains" : domainArray
+				};
+				task.done({
+					HttpCode: 200,
+					Code: 0,
+					DetailedMessage: "Ok",
+					JsonData: resp,
+					Message: "Ok",
+					Severity: "Ok"
+				});
+			}
+		});
+	}
+	
+	// This method can not accept req, because the req something has a body instead of a query.
+	function getCFdomains(targetRequest,domainName,defaultDomainMode){
+		var cloudAccessToken = target.getAccessToken();
+		var domainArray = [];
+		return new Promise(function(fulfill) {
+			if(defaultDomainMode){
+				var privateDomainURL = orgs.getOrg(targetRequest.Org).entity.private_domains_url;
+				var domainHeader = {
+					url: targetRequest.Url + privateDomainURL,
+					headers: {'Authorization': cloudAccessToken}
+					// TODO check domain name specified or not.
+				};
+				request(domainHeader, function (error, response, body) {
+					fulfill(target.parsebody(body));
+				});
+			}else{
+				domainHeader = {
+					url: targetRequest.Url + '/v2/shared_domains',
+					headers: {'Authorization': cloudAccessToken},
+					qs :{'page':'1','results-per-page':'1'}
+					// TODO check domain name specified or not.
+				};
+				request(domainHeader, function (error, response, body) {
+					fulfill(target.parsebody(body));
+				});
+			}
+		}).then(function(result){
+			var domainResources = result.resources;
+			for(var k = 0; k < domainResources.length; k++){
+				var domainJson = {
+					'Guid': domainResources[k].metadata.guid,
+					'DomainName': domainResources[k].entity.name,
+					'Type':	'Domain'
+				};
+				domainArray.push(domainJson);
+			}
+			return domainArray;
+		})
+	}
+};
---
+++ modules/orionode/lib/cf/routes.js
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ * Copyright (c) 2016 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*eslint-env node */
+var express = require('express');
+var bodyParser = require('body-parser');
+var domains = require('./domains');
+var orgs = require('./orgs');
+var target = require('./target');
+var request = require('request');
+var tasks = require('../tasks');
+
+module.exports.router = function(options) {
+
+	return express.Router()
+	.use(bodyParser.json())
+	.get('*', getroutes)
+	.put('*', putroutes);
+	
+function getroutes(req, res){
+	var task = new tasks.Task(res,false,false,0,false);
+	var targetRequest = JSON.parse(req.query.Target);
+	var RouteJsonRequest = req.query.Route && JSON.parse(req.query.Route);
+	var globalCheck = req.query.GlobalCheck;
+	var targetURL = targetRequest.Url;
+	var spaceGuid = targetRequest.SpaceId;
+	var cloudAccessToken = target.getAccessToken();
+	var routesArray = [];
+	var orphaned = false; // TODO this is hard coded for now!
+	new Promise(function(fulfill) {
+		if(RouteJsonRequest){
+			if(globalCheck && globalCheck === "true"){
+				// TODO special case to handle
+			}
+			// TODO special case to handle
+		}
+		var routesUrl = orgs.getSpace(spaceGuid).entity.routes_url;
+		var orgsHeader = {
+			url: targetURL + routesUrl,
+			headers: {'Authorization': cloudAccessToken},
+			qs :{'inline-relations-depth':'1'}
+		};
+		request(orgsHeader, function (error, response, body) {
+			fulfill(target.parsebody(body));
+		});
+	}).then(function(result){
+		var routesResources = result.resources;
+		for(var k = 0; k < routesResources.length; k++){
+			if(!orphaned || routesResources.entity.apps.length === 0 ){
+				var appArray = [];
+				var apps = routesResources[k].entity.apps;
+				for(var j = 0; j < apps.length ; j ++){
+					appArray.push({
+						"Guid":apps[j].metadata.guid,
+						"Name":apps[j].entity.name,
+						"State":apps[j].entity.state,
+						"Type" : "App"
+					});
+				}
+				var routeJson = {
+					'Apps' : appArray,
+					'DomainName': routesResources[k].entity.domain.entity.name,
+					'Guid': routesResources[k].metadata.guid,
+					'Host': routesResources[k].entity.host,
+					'Type':	'Route'
+				};
+				routesArray.push(routeJson);
+			}
+		}
+	}).then(function(){
+		var resp = {
+			"Routes" : routesArray
+		};
+		task.done({
+			HttpCode: 200,
+			Code: 0,
+			DetailedMessage: "OK",
+			JsonData: resp,
+			Message: "OK",
+			Severity: "Ok"
+		});
+});
+}
+	
+function putroutes(){
+	
+}
+};
