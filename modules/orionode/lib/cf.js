32f5019b32fb00e4daca53834162993ab2fd7536
Sidney <xinyij@ca.ibm.com>
2016-07-18 11:10:22 -0400

    Bug 497124 - RunBar(Cloud Foundation) implementation
    Change-Id: I6862be28bfc3c2713d191ec5aa4f3ec98ceb4ae7
    Signed-off-by: Sidney <xinyij@ca.ibm.com>

---
+++ modules/orionode/lib/cf.js
@@ -0,0 +1,40 @@
+/*eslint-env node */
+/*******************************************************************************
+ * Copyright (c) 2016 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+var express = require('express');
+
+var apps = require('./cf/apps');
+var domains = require('./cf/domains');
+var info = require('./cf/info');
+var logz = require('./cf/logz');
+var manifests = require('./cf/manifests');
+var orgs = require('./cf/orgs_spaces');
+var plans = require('./cf/plans');
+var routes = require('./cf/routes');
+var services = require('./cf/services');
+var target = require('./cf/target');
+
+module.exports = CF;
+function CF(options) {
+	var router = express.Router();
+	
+	router.use("/apps", apps.router(options));
+	router.use("/domains", domains.router(options));
+	router.use("/info", info.router(options));
+	router.use("/logz", logz.router(options));
+	router.use("/manifests", manifests.router(options));
+	router.use("/orgs", orgs.router(options));
+	router.use("/plans", plans.router(options));
+	router.use("/routes", routes.router(options));
+	router.use("/services", services.router(options));
+	router.use("/target", target.router(options));
+	return router;
+}
