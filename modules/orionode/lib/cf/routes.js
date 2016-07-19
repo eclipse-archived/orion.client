31de817ace13f6768400ec77c4fc7d25b5ac5189
Sidney <xinyij@ca.ibm.com>
2016-07-19 14:37:19 -0400

    Bug 497124 - RunBar(Cloud Foundation) implementation
    Change-Id: I6862be28bfc3c2713d191ec5aa4f3ec98ceb4ae7
    Signed-off-by: Sidney <xinyij@ca.ibm.com>

---
+++ modules/orionode/lib/cf/routes.js
@@ -0,0 +1,151 @@
+/*******************************************************************************
+ * Copyright (c) 2016 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*eslint-env node */
+var express = require('express');
+var bodyParser = require('body-parser');
+var domains = require('./domains');
+var target = require('./target');
+var request = require('request');
+var tasks = require('../tasks');
+var util = require('../git/util');
+
+module.exports.router = function() {
+
+	return express.Router()
+		.use(bodyParser.json())
+		.get('*', getroutes)
+		.put('*', putroutes);
+
+	function getroutes(req, res) {
+		var task = new tasks.Task(res, false, false, 0, false);
+		var targetRequest = JSON.parse(req.query.Target);
+		var RouteJsonRequest = req.query.Route && JSON.parse(req.query.Route);
+		var globalCheck = req.query.GlobalCheck;
+		target.computeTarget(req.user.username, targetRequest)
+	 	.then(function(appTarget){
+	 		var waitForGetRouteJson;
+			if (RouteJsonRequest) {
+				if (globalCheck && globalCheck === "true") {
+					// TODO need to test the following two cases.
+					waitForGetRouteJson = checkRouteRequest(appTarget, req.user.username,targetRequest, RouteJsonRequest.DomainName, RouteJsonRequest.Host);
+				}
+				waitForGetRouteJson = getRouteRequest(appTarget, req.user.username, targetRequest, RouteJsonRequest.DomainName, RouteJsonRequest.Host);
+			}
+			waitForGetRouteJson = getRouteRequest(appTarget, req.user.username, targetRequest, false);
+			return Promise.resolve(waitForGetRouteJson)
+				.then(function(routes) {
+					var resp = {
+						"Routes": routes
+					};
+					task.done({
+						HttpCode: 200,
+						Code: 0,
+						DetailedMessage: "OK",
+						JsonData: resp,
+						Message: "OK",
+						Severity: "Ok"
+					});
+				});
+		 	});
+	}
+	function checkRouteRequest(appTarget, userId, targetRequest, domainName, host) {
+		var cloudAccessToken = target.getAccessToken(userId);
+		return domains.getCFdomains(appTarget, userId, targetRequest.Url, targetRequest.Org, domainName)
+		.then(function(domains) {
+			return new Promise(function(fulfill) {
+				var domainGuid = domains[0].metadata.guid;
+				var checkRouteHeader = {
+					url: targetRequest.Url + '/v2/routes/reserved/domain/' + domainGuid + '/host/' + host,
+					headers: {
+						'Authorization': cloudAccessToken
+					}
+				};
+				request(checkRouteHeader, function(error, response, body) {
+					fulfill(target.parsebody(body));
+				});
+			});
+		});
+	}
+	function getRouteRequest(appTarget, userId, targetRequest) {
+		var cloudAccessToken = target.getAccessToken(userId);
+		var routesArray = [];
+		if (arguments.length === 5) {
+			var domainName = arguments[3];
+			var host = arguments[4];
+		} else if (arguments.length === 5) {
+			var orphaned = arguments[3];
+		}
+		return new Promise(function(fulfill) {
+				if (domainName && host) {
+					return domains.getCFdomains(appTarget, userId, targetRequest.Url, targetRequest.Org, domainName)
+						.then(function(domains) {
+							var domainGuid = domains[0].metadata.guid;
+							fulfill({
+								url: targetRequest.Url + '/v2/routes',
+								headers: {
+									'Authorization': cloudAccessToken
+								},
+								qs: {
+									'inline-relations-depth': '1',
+									'q': util.encodeURIComponent('host:' + host + ';domain_guid' + domainGuid)
+								}
+							});
+						});
+				}
+				var routesUrl = appTarget.Space.entity.routes_url;
+				fulfill({
+					url: targetRequest.Url + routesUrl,
+					headers: {
+						'Authorization': cloudAccessToken
+					},
+					qs: {
+						'inline-relations-depth': '1'
+					}
+				});
+			})
+			.then(function(getRouteHeader) {
+				return new Promise(function(fulfill) {
+					request(getRouteHeader, function(error, response, body) {
+						fulfill(target.parsebody(body));
+					});
+				});
+			}).then(function(result) {
+				var routesResources = result.resources;
+				for (var k = 0; k < routesResources.length; k++) {
+					if (!orphaned || routesResources.entity.apps.length === 0) {
+						var appArray = [];
+						var apps = routesResources[k].entity.apps;
+						for (var j = 0; j < apps.length; j++) {
+							appArray.push({
+								"Guid": apps[j].metadata.guid,
+								"Name": apps[j].entity.name,
+								"State": apps[j].entity.state,
+								"Type": "App"
+							});
+						}
+						var routeJson = {
+							'Apps': appArray,
+							'DomainName': routesResources[k].entity.domain.entity.name,
+							'Guid': routesResources[k].metadata.guid,
+							'Host': routesResources[k].entity.host,
+							'Type': 'Route'
+						};
+						routesArray.push(routeJson);
+					}
+				}
+				return routesArray;
+			});
+	}
+
+	function putroutes() {
+
+	}
+};
---
+++ modules/orionode/lib/cf/services.js
@@ -0,0 +1,119 @@
+/*******************************************************************************
+ * Copyright (c) 2016 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*eslint-env node */
+var express = require('express');
+var bodyParser = require('body-parser');
+var target = require('./target');
+var request = require('request');
+var tasks = require('../tasks');
+var async = require('async');
+
+module.exports.router = function() {
+	
+	return express.Router()
+	.use(bodyParser.json())
+	.get('*', getService);
+	
+function getService(req, res){
+	var task = new tasks.Task(res,false,false,0,false);
+	var targetRequest = JSON.parse(req.query.Target);
+	var targetURL = targetRequest.Url;
+	var spaceGuid = targetRequest.SpaceId;
+	var cloudAccessToken = target.getAccessToken(req.user.username, task);
+	new Promise(function(fulfill) {
+		var serviceUrl = '/v2/spaces/' + spaceGuid + '/service_instances';
+		var fullService = [];
+		function promiseWhile(doRequest, value) {
+			return Promise.resolve(value).then(doRequest).then(function(result) {
+				return collectServiceInfo(result)
+				.then(function(){
+					if(result.next_url){
+						serviceUrl = result.next_url;				
+						return promiseWhile(doRequest, serviceUrl);
+					}
+					return null;
+				});
+			});
+		}
+		function doRequest(serviceUrl){
+			var servicesHeader = {
+				url: targetURL + serviceUrl,
+				headers: {'Authorization': cloudAccessToken},
+				qs :{'inline-relations-depth':'1','return_user_provided_service_instances':'true'}
+			};
+			return new Promise(function(fulfill) {
+				request(servicesHeader, function (error, response, body) {
+					fulfill(target.parsebody(body));
+				});
+			});
+		}
+		function collectServiceInfo(result){
+			var serviceResources = result.resources;
+			return new Promise(function(fulfill , reject){
+				async.each(serviceResources, function(resource, cb) {
+					var isBindable = true;
+					if(resource.entity.service_plan){
+						var serviceEntity = resource.entity.service_plan.entity;
+						var serviceGuid = serviceEntity.service_guid;
+						return getCFService(req.user.username, targetURL,serviceGuid)
+						.then(function(singleServiceresult){
+							isBindable = singleServiceresult.entity.bindable;				
+						}).then(function(){
+							if(isBindable){
+								var ServiceJson = {
+									'Name': resource.entity.name,
+									'Type':	'Service'
+								};
+								fullService.push(ServiceJson);
+							}
+							cb();
+						});
+					}
+				}, function(err) {
+					if(err){
+						return reject(err);
+					}
+					fulfill();
+				});
+			});
+		}
+		return promiseWhile(doRequest,serviceUrl)
+		.then(function(){
+			fulfill(fullService);
+		});
+	}).then(function(fullService){
+		var resp = {
+			"Children" : fullService
+		};
+		task.done({
+			HttpCode: 200,
+			Code: 0,
+			DetailedMessage: "OK",
+			JsonData: resp,
+			Message: "OK",
+			Severity: "Ok"
+		});
+	});
+}
+
+function getCFService(userId, targetURL,serviceGuid){
+	var cloudAccessToken = target.getAccessToken(userId);
+	return new Promise(function(fulfill) {
+		var serviceHeader = {
+			url: targetURL + '/v2/services/' + serviceGuid,
+			headers: {'Authorization': cloudAccessToken}
+		};
+		request(serviceHeader, function (error, response, body) {
+			fulfill(target.parsebody(body));
+		});
+	});
+}
+};
