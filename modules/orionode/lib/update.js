94642532fd6d0dd405ad9b85140049c2f3bc4df8
Muhammad Mousa <mmousa@ca.ibm.com>
2016-07-19 13:28:00 -0400

    Bug 497985 - [Electron app] Add about button in help menu with update channel support and current version
    Change-Id: Ie927b9f8c2187221f329f7b0738937951a4dfeb2
    Signed-off-by: Muhammad Mousa <mmousa@ca.ibm.com>

---
+++ modules/orionode/lib/update.js
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2016 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *	 IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*eslint-env node */
+
+var express = require('express'),
+	electron = require('electron'),
+	autoUpdater = electron.autoUpdater,
+	bodyParser = require('body-parser'),
+	os = require('os'),
+	prefs = require('./controllers/prefs');
+
+module.exports = {};
+module.exports.router = function(options) {
+	return express.Router()
+	.use(bodyParser.json())
+	.put('*', /* @callback */ function (req, res) {
+		var configParams = options.configParams;
+		if (!configParams) {
+			console.log('options.configParams is required');
+		} else {
+			var feedURL = configParams["orion.autoUpdater.url"];
+			if (!feedURL) { 
+				console.log('autoUpdater feedURL is required');
+			} else {
+				var platform = os.platform() + '_' + os.arch(),
+					version = electron.app.getVersion(),
+					allPrefs = prefs.readPrefs();
+			
+				allPrefs.user.updateChannel = req.body.updateChannel;
+				prefs.writePrefs(allPrefs);
+				autoUpdater.setFeedURL(feedURL + '/channel/' + req.body.updateChannel + '/' + platform + '/' + version);
+				autoUpdater.checkForUpdates();
+			}
+		}		
+	});	
+};
