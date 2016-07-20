95eb909bb65c6a5aacf2d9f423bba4b3a913abc8
Sidney <xinyij@ca.ibm.com>
2016-07-20 17:23:43 -0400

    Bug 498169 - Implement close other tabs context menu
    Change-Id: Ie78c81288a422824b1caa631ccc09c9f4bae12f6
    Signed-off-by: Sidney <xinyij@ca.ibm.com>

--- modules/orionode/lib/main.js
+++ modules/orionode/lib/main.js
@@ -11,6 +11,7 @@
 /*eslint-env browser, node*/
 var electron = require('electron');
 var dragSrcEl = null;
+var contextSrcEl = null;
 
 function redrawButtons() {
 	var bar = document.querySelector("#bar");
@@ -18,23 +19,25 @@ function redrawButtons() {
 	buttons.classList.add("tabButtons");
 	var back = document.createElement("button");
 	back.textContent = "<";
-	back.classList.add("tabButton");	
-	function historyBack(){
+	back.classList.add("tabButton");
+
+	function historyBack() {
 		var active = getActiveTab();
 		if (!active) return;
 		active.contentWindow.history.back();
 	}
-	function historyForward(){
+
+	function historyForward() {
 		var active = getActiveTab();
 		if (!active) return;
 		active.contentWindow.history.forward();
-	}	
+	}
 	back.addEventListener("click", historyBack);
 	buttons.appendChild(back);
 	var forward = document.createElement("button");
 	forward.textContent = ">";
 	forward.classList.add("tabButton");
-	forward.addEventListener("click",historyForward);
+	forward.addEventListener("click", historyForward);
 	buttons.appendChild(forward);
 	var refresh = document.createElement("button");
 	refresh.textContent = "\u27F2";
@@ -46,24 +49,24 @@ function redrawButtons() {
 	});
 	buttons.appendChild(refresh);
 	bar.appendChild(buttons);
-	
-	var _globalShortcut = electron.remote.globalShortcut;	
+
+	var _globalShortcut = electron.remote.globalShortcut;
 	_globalShortcut.unregister('Alt+Right');
 	_globalShortcut.unregister('Alt+Left');
-	_globalShortcut.register('Alt+Right',historyForward);	
-	_globalShortcut.register('Alt+Left',historyBack);
+	_globalShortcut.register('Alt+Right', historyForward);
+	_globalShortcut.register('Alt+Left', historyBack);
 }
 
-function addNewTab(id , iframe){
+function addNewTab(id, iframe) {
 	var bar = document.querySelector("#bar");
-	var tabParent = bar.querySelector("ul");	
+	var tabParent = bar.querySelector("ul");
 	if (!tabParent) {
 		tabParent = document.createElement("ul");
 		bar.appendChild(tabParent);
 	}
 	var tab = document.createElement("li");
 	var title = iframe.contentDocument.title;
-	tab.setAttribute('id', "tab"+id);
+	tab.setAttribute('id', "tab" + id);
 	tab.setAttribute('draggable', true);
 	tab.classList.add("tabItem");
 	var text = document.createElement("span");
@@ -71,7 +74,7 @@ function addNewTab(id , iframe){
 	text.textContent = title;
 	tab.appendChild(text);
 	tab.title = title;
-	
+
 	var closeBt = document.createElement("span");
 	closeBt.classList.add("close");
 	closeBt.textContent = "x";
@@ -83,25 +86,27 @@ function addNewTab(id , iframe){
 		evt.preventDefault();
 		evt.stopPropagation();
 	});
-	
+
 	function setActive() {
-		var currentTabcontents = document.querySelectorAll(".tabContent"); 
-		var tabbuttons = document.querySelectorAll("li");
-		for (var j=0; j<tabbuttons.length; j++) {
+		var currentTabcontents = document.querySelectorAll(".tabContent");
+		var tabbuttons = document.querySelectorAll(".tabItem");
+		for (var j = 0; j < tabbuttons.length; j++) {
 			tabbuttons[j].classList.remove("active");
 			currentTabcontents[j].classList.remove("active");
 		}
 		tab.classList.add("active");
 		iframe.classList.add("active");
 	}
-	
+
 	tab.addEventListener("click", function(evt) {
 		setActive();
 		evt.preventDefault();
 		evt.stopPropagation();
+		var menu = document.querySelector("#context-menu");
+		menu.classList.remove('context-menu-items-open');
 	});
-	
-	tab.addEventListener('dragstart', function(evt){
+
+	tab.addEventListener('dragstart', function(evt) {
 		setActive();
 		dragSrcEl = tab;
 		var crt = this.cloneNode(true);
@@ -109,16 +114,16 @@ function addNewTab(id , iframe){
 		evt.dataTransfer.setDragImage(crt, 0, 0);
 		evt.dataTransfer.effectAllowed = "move";
 	});
-	tab.addEventListener('dragenter', function(evt){
+	tab.addEventListener('dragenter', function(evt) {
 		if (evt.preventDefault) {
 			evt.preventDefault(); // Necessary. Allows us to drop.
 		}
-		if(dragSrcEl){
-			if(tab.isSameNode(dragSrcEl.previousSibling)){
+		if (dragSrcEl) {
+			if (tab.isSameNode(dragSrcEl.previousSibling)) {
 				// drag forward
 				var replacedTab1 = tabParent.replaceChild(dragSrcEl, tab);
 				tabParent.insertBefore(replacedTab1, dragSrcEl.nextSibling);
-			}else{
+			} else {
 				// drag back
 				var replacedTab2 = tabParent.replaceChild(dragSrcEl, tab);
 				tabParent.insertBefore(replacedTab2, dragSrcEl);
@@ -126,25 +131,33 @@ function addNewTab(id , iframe){
 		}
 		return false;
 	});
-	tab.addEventListener('dragover', function(evt){
+	tab.addEventListener('dragover', function(evt) {
 		if (evt.preventDefault) {
 			evt.preventDefault(); // Necessary. Allows us to drop.
 		}
 		return false;
 	});
-	tab.addEventListener('drop', function(evt){
+	tab.addEventListener('drop', function(evt) {
 		if (evt.stopPropagation) {
 			evt.stopPropagation(); // stops the browser from redirecting.
 		}
 		return false;
 	});
+	tab.addEventListener("contextmenu", function(evt) {
+		var menu = document.querySelector("#context-menu");
+		var activeClassName = "context-menu-items-open";
+		contextSrcEl = tab;
+		evt.preventDefault();
+		menu.classList.remove(activeClassName); // Close old one 
+		positionMenu(evt);	// calculate new pisition
+		menu.classList.add(activeClassName); // Open new One
+	});
 	tabParent.appendChild(tab);
-	
 	update();
 }
 
-function setTabLabel(id, str){
-	var tab = document.getElementById("tab"+id);
+function setTabLabel(id, str) {
+	var tab = document.getElementById("tab" + id);
 	var text = tab.querySelector(".tabLabel");
 	tab.title = text.textContent = str;
 }
@@ -191,6 +204,7 @@ function load() {
 			update();
 		}, 50);
 	});
+	registerContextMenu();
 }
 
 function createTab(url) {
@@ -199,23 +213,126 @@ function createTab(url) {
 	iframe.classList.add("tabContent");
 	iframe.src = url;
 	var id = Date.now();
-	iframe.id = "iframe"+ id;
+	iframe.id = "iframe" + id;
 	iframe.addEventListener("load", function() {
 		iframe.contentWindow.confirm = window.confirm;
 		iframe.contentWindow.alert = window.alert;
 		iframe.contentWindow.__electron = electron;
-		
+
 		var target = iframe.contentDocument.querySelector('head > title');
 		if (target) {
 			var observer = new window.WebKitMutationObserver(function(mutations) {
-				if(mutations){
+				if (mutations) {
 					setTabLabel(id, iframe.contentDocument.title);
 				}
 			});
-			observer.observe(target, { subtree: true, characterData: true, childList: true });
+			observer.observe(target, {
+				subtree: true,
+				characterData: true,
+				childList: true
+			});
 		}
 		setTabLabel(id, iframe.contentDocument.title);
+		iframe.contentWindow.addEventListener("click", function() {
+			var menu = document.querySelector("#context-menu");
+			var activeClassName = "context-menu-items-open";
+			menu.classList.remove(activeClassName);
+		});
 	});
 	document.body.appendChild(iframe);
 	addNewTab(id, iframe);
+}
+
+function registerContextMenu() {
+	var menu = document.querySelector("#context-menu");
+	var menuitem = document.querySelector(".context-menu-item");
+	menuitem.addEventListener("mouseover", function(){
+		menuitem.classList.add('context-menu-item-selected');
+	});
+	menuitem.addEventListener("mouseleave", function(){
+		menuitem.classList.remove('context-menu-item-selected');
+	});
+	// Register on document to make sure click on empty spaces after the tab buttons can close the context menu as well.
+	document.addEventListener("click", function(evt) { 
+		if (clickInsideElement(evt, 'context-menu-item')) {
+			var correspondingiframeId = 'iframe' + contextSrcEl.id.substr(3);
+			var corrspondingiframe = document.querySelector('#'+correspondingiframeId);
+			// Remove all the other tab buttons first
+			Array.prototype.forEach.call(contextSrcEl.parentNode.childNodes, function(eachOne) {
+				if(eachOne.id !== contextSrcEl.id){
+					 contextSrcEl.parentNode.removeChild(eachOne);
+				}
+			});
+			Array.prototype.forEach.call(corrspondingiframe.parentNode.childNodes, function(eachOne) {
+				if(eachOne.nodeName === 'IFRAME' && eachOne.id !== correspondingiframeId){
+					 corrspondingiframe.parentNode.removeChild(eachOne);
+				}
+			});
+			update();
+			evt.preventDefault();
+			evt.stopPropagation();
+		}
+		menu.classList.remove('context-menu-items-open');
+	});
+}
+function clickInsideElement(e, className) {
+	var el = e.srcElement || e.target;
+	if (el.classList.contains(className)) {
+		return el;
+	}
+	while (el) {
+		if (el.classList && el.classList.contains(className)) {
+			return el;
+		}
+		el = el.parentNode;
+	}
+	return false;
+}
+
+function positionMenu(e) {
+	var menu = document.querySelector("#context-menu");
+	var menuWidth;
+	var menuHeight;
+	var windowWidth;
+	var windowHeight;
+	var clickCoords;
+	var clickCoordsX;
+	var clickCoordsY;
+	clickCoords = getPosition(e);
+	clickCoordsX = clickCoords.x;
+	clickCoordsY = clickCoords.y;
+
+	menuWidth = menu.offsetWidth + 4;
+	menuHeight = menu.offsetHeight + 4;
+	windowWidth = window.innerWidth;
+	windowHeight = window.innerHeight;
+
+	if ((windowWidth - clickCoordsX) < menuWidth) {
+		menu.style.left = windowWidth - menuWidth + "px";
+	} else {
+		menu.style.left = clickCoordsX + "px";
+	}
+	if ((windowHeight - clickCoordsY) < menuHeight) {
+		menu.style.top = windowHeight - menuHeight + "px";
+	} else {
+		menu.style.top = clickCoordsY + "px";
+	}
+}
+function getPosition(e) {
+	var posx = 0;
+	var posy = 0;
+	if (!e) var e = window.event;
+	if (e.pageX || e.pageY) {
+		posx = e.pageX;
+		posy = e.pageY;
+	} else if (e.clientX || e.clientY) {
+		posx = e.clientX + document.body.scrollLeft +
+			document.documentElement.scrollLeft;
+		posy = e.clientY + document.body.scrollTop +
+			document.documentElement.scrollTop;
+	}
+	return {
+		x: posx,
+		y: posy
+	};
 }
