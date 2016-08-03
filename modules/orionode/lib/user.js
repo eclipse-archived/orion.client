a45307972d20663132058d4f34f43be510afa84d
Mark Macdonald <mamacdon@gmail.com>
2016-08-03 15:47:42 -0400

    Extract email functionality into separate file
    Change-Id: I5c6c6ed6fe21fddb8b3ca2a10116f0b2b133aad1

--- modules/orionode/lib/user.js
+++ modules/orionode/lib/user.js
@@ -17,10 +17,9 @@ var crypto = require('crypto'),
 	GithubStrategy = require('passport-github2').Strategy,
 	cookieParser = require('cookie-parser'),
 	bodyParser = require('body-parser'),
-	nodemailer = require('nodemailer'),
-	fs = require('fs'),
 	args = require('./args'),
-	generator = require('generate-password');
+	generator = require('generate-password'),
+	sendMail = require('./mail');
 
 var AUTH_TOKEN_BYTES = 48;
 
@@ -44,56 +43,6 @@ function userJSON(user) {
 		DiskUsageTimestamp: user.disk_usage_timestamp ? user.disk_usage_timestamp.getTime() : 0,
 		DiskUsage: user.disk_usage || 0 
 	};
-}
-
-function sendMail(opt){
-	var read = function(cb) {
-		var emailText;
-		fs.readFile(opt.template, 'utf8', function (err,data) {
-			if (err) {
-				return cb(err);
-			}
-			var authUrl = opt.req.protocol + '://' + opt.req.get('host') + opt.auth + opt.user.authToken;
-			emailText  = data.replace(/<EMAIL>/g, opt.user.email || "");
-			emailText  = emailText.replace(/<USER>/g, opt.user.username || "");
-			emailText  = emailText.replace(/<URL>/g, authUrl);
-			emailText  = emailText.replace(/<PASSWORD>/g, opt.pwd || "");
-			var subjLineIndex = emailText.indexOf("\n");
-			var subject = emailText.substr(0, subjLineIndex);
-			var body = emailText.substr(subjLineIndex);
-			cb(null, subject, body);
-		});
-	};
-	read(function(err, subject, body){
-		var smtpConfig = {
-			host: opt.options.configParams["mail.smtp.host"],
-			port: opt.options.configParams["mail.smtp.port"],
-			secure: true,
-			auth: {
-				user: opt.options.configParams["mail.smtp.user"],
-				pass: opt.options.configParams["mail.smtp.password"]
-			}
-		};
-
-		if (opt.options.configParams["mail.from"]) {
-			var transport = nodemailer.createTransport(smtpConfig);
-			var mailOptions = {
-				from: opt.options.configParams["mail.from"],
-				to: opt.user.email,
-				subject: subject,
-				text: body, 
-			};
-			transport.sendMail(mailOptions, function(error, info){
-				if (error){
-					return console.log(error + " " + info);
-				}
-				//console.log('Message sent: ' + info.response);
-			});
-		} else {
-			// dev
-			console.log(body);
-		}
-	});
 }
 
 function metastore(req) {
