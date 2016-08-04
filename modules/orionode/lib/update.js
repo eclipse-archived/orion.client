8b98163d4b75e3f16013e63ebd9991f0acbc74bd
Muhammad Mousa <mmousa@ca.ibm.com>
2016-08-04 16:26:13 -0400

    Bug 499170 - [Electron app] API call to update.js does not end
    Change-Id: Ie16bb028cad31708f86e23720d5cc93670255201
    Signed-off-by: Muhammad Mousa <mmousa@ca.ibm.com>

--- modules/orionode/lib/update.js
+++ modules/orionode/lib/update.js
@@ -10,33 +10,34 @@
  *******************************************************************************/
 /*eslint-env node */
 
-var express = require('express'),
-	electron = require('electron'),
+var express = require("express"),
+	electron = require("electron"),
 	autoUpdater = electron.autoUpdater,
-	bodyParser = require('body-parser'),
-	os = require('os'),
-	prefs = require('./controllers/prefs');
+	bodyParser = require("body-parser"),
+	os = require("os"),
+	prefs = require("./controllers/prefs");
 
 module.exports = {};
 module.exports.router = function(options) {
 	var configParams = options.configParams;
-	if (!configParams) { throw new Error('options.configParams is required'); }
+	if (!configParams) { throw new Error("options.configParams is required"); }
 	
 	return express.Router()
 	.use(bodyParser.json())
-	.put('*', /* @callback */ function (req, res) {
+	.put("*", /* @callback */ function (req, res) {
 		var feedURL = configParams["orion.autoUpdater.url"];
 		if (!feedURL) { 
-			console.log('autoUpdater feedURL is required');
+			console.log("autoUpdater feedURL is required");
 		} else {
-			var platform = os.platform() + '_' + os.arch(),
+			var platform = os.platform() + "_" + os.arch(),
 				version = electron.app.getVersion(),
 				allPrefs = prefs.readPrefs();
 		
 			allPrefs.user.updateChannel = req.body.updateChannel;
 			prefs.writePrefs(allPrefs);
-			autoUpdater.setFeedURL(feedURL + '/channel/' + req.body.updateChannel + '/' + platform + '/' + version);
+			autoUpdater.setFeedURL(feedURL + "/channel/" + req.body.updateChannel + "/" + platform + "/" + version);
 			autoUpdater.checkForUpdates();
 		}
+		res.status(200).end();
 	});	
 };
