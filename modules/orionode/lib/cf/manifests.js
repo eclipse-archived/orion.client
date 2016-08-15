b90e3ed67211b8b83cdf9eac4c74de06700b5670
Sidney <xinyij@ca.ibm.com>
2016-08-15 16:07:00 -0400

    Bug 499578 - CF endpoint bug fix
    Change-Id: I02a43ab913e40075595e0894390e27a4dad437d8
    Signed-off-by: Sidney <xinyij@ca.ibm.com>

--- modules/orionode/lib/cf/manifests.js
+++ modules/orionode/lib/cf/manifests.js
@@ -15,6 +15,8 @@ var api = require("../api"), writeError = api.writeError;
 var fs = require("fs");
 var path = require("path");
 var yaml = require("js-yaml");
+var yamlAstParser = require("yaml-ast-parser");
+var tasks = require("../tasks");
 
 module.exports.router = function() {
 	
@@ -43,17 +45,245 @@ function retrieveManifestFile(req, res, manifestAbsuluteLocation){
 		if(filePath.indexOf("manifest.yml") === -1){
 			filePath += "manifest.yml";
 		}
-		try {
-			var manifest = yaml.safeLoad(fs.readFileSync(filePath, "utf8"));
-		} catch (err) {
-			writeError(404, res, err.message);
-		}
-		fulfill(manifest);
+		fs.readFile(filePath, "utf8", function(err, fileContent){
+			if(err){
+				writeError(404, res, err.message);
+			}
+			fulfill(fileContent);
+		});
+	}).then(function(fileContent){
+		var manifest = yaml.safeLoad(fileContent);
+		var manifestAST = yamlAstParser.load(fileContent);
+		transformManifest(manifest);
+		// TODO when meet the case where need to do symbolResolver (as in JAVA) then implement.
+		analizeManifest(manifest, res, manifestAST, fileContent);
+		setDefaultManifestProperties(req,manifest);
+		return manifest;
 	});
 }
 
+function setDefaultManifestProperties(req,manifest){
+	function getDefaultName(rawProjectName){
+		var nameParts = rawProjectName.split(" --- ", 2);
+		return nameParts.length > 1 ? nameParts[1] : nameParts[0];
+	}
+	function getDefaultHost(rawProjectName){
+		return slugify(rawProjectName);
+	}
+	var rawContentLocationData = req.params[0].split("/");
+	var rawDefaultProjectName = rawContentLocationData[rawContentLocationData.length - 2];
+	rawDefaultProjectName = rawDefaultProjectName.replace(/\|/, " --- ");
+	var MUST_HAVE_PROPERTITIES ={
+		name : getDefaultName(rawDefaultProjectName),
+		host : getDefaultHost(rawDefaultProjectName),
+		memory : "512M",
+		instances : "1",
+		path: "."
+	};
+	Object.keys(MUST_HAVE_PROPERTITIES).forEach(function(key){
+		if(!manifest.applications[0].hasOwnProperty(key)){
+			manifest.applications[0][key] = MUST_HAVE_PROPERTITIES[key];
+		}					
+	});
+}
+function transformManifest(manifest){
+	if(!manifest.applications){
+		return;  // Do nothing
+	}
+	var globals = [];
+	var APPLICATION_PROPERTIES = ["name", "memory", "host", "buildpack", "command", //   //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$//$NON-NLS-4$ //$NON-NLS-5$
+			"domain", "instances", "path", "timeout", "no-route", "services"]; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
+	Object.keys(manifest).forEach(function(key){
+		if(APPLICATION_PROPERTIES.indexOf(key) !== -1){
+			globals.push(key);
+		}
+	});
+	if(!globals){
+		return; // nothing to do
+	}
+	manifest.applications.forEach(function(application){
+		for( var k = 0; k < globals.length ; k++){
+			if(!application.hasOwnProperty(globals[k])){
+				application[globals[k]] = manifest[globals[k]];
+			}
+		}
+	});
+}
+function analizeManifest(manifest, res, manifestAST, fileContent){
+	if(!manifest.applications){
+		return; // Do nothing
+	}
+	var fileContentArray = fileContent.split("\r\n")	;
+	var lineNumbers = [];
+	var valueWithParent = [];
+	var currentLineFirstCharAt = 0;
+	for(var i = 0; i < fileContentArray.length ; i++){
+		var lineLength = fileContentArray[i].length;
+		lineNumbers.push([currentLineFirstCharAt, currentLineFirstCharAt + lineLength ]);
+		currentLineFirstCharAt = currentLineFirstCharAt + lineLength + 2;
+	}
+	nullCheckOfManifest(manifestAST);
+	specificFieldCheck(valueWithParent);
+	
+	function nullCheckOfManifest(manifestAST){
+		if(!manifestAST){
+			sendInvalidResult("Empty Node", -1, res);	
+		}
+		if(manifestAST.hasOwnProperty("mappings")){
+			manifestAST.mappings.forEach(function(each){
+				nullCheckOfManifest(each);		
+			});
+		}else if(manifestAST.hasOwnProperty("items")){
+			manifestAST.items.forEach(function(each){
+				nullCheckOfManifest(each);		
+			});
+		}else if(manifestAST.hasOwnProperty("value")){
+			if(!manifestAST.value){   // null case
+				// find null
+				sendInvalidResult("Empty Propety",getLineNumber(manifestAST.startPosition, manifestAST.endPosition, lineNumbers),res);	
+			}
+			if(typeof manifestAST.value === "string"){
+				// record the node with real yaml values here. to save from another after null check. aka, do both together.
+				valueWithParent.push(manifestAST);
+			}
+			nullCheckOfManifest(manifestAST.value);
+		}
+	}
+	function specificFieldCheck(valueWithParent){
+		for(var m = 0; m < valueWithParent.length; m++){
+			var realParentNode = {nodeKey : "", node:{}};
+			findclosestKeyName(valueWithParent[m], realParentNode);
+			switch(realParentNode.nodeKey){
+				case "services":
+					if(isStringProperty(realParentNode.node)){
+						sendInvalidResult("Invalid services declaration. Expected a list of service names.", getLineNumber(realParentNode.node.startPosition, realParentNode.node.endPosition, lineNumbers), res);	
+					}
+					break;
+				case "buildpack":
+					if(!isStringProperty(realParentNode.node)){
+						sendInvalidResult("Invalid \"buildpack\" value. Expected a string literal.", getLineNumber(realParentNode.node.startPosition, realParentNode.node.endPosition, lineNumbers), res);	
+					}
+					break;
+				case "command":
+					if(!isStringProperty(realParentNode.node)){
+						sendInvalidResult("Invalid \"command\" value. Expected a string literal.", getLineNumber(realParentNode.node.startPosition, realParentNode.node.endPosition, lineNumbers), res);	
+					}
+					break;
+				case "domain":
+					if(!isStringProperty(realParentNode.node)){
+						sendInvalidResult("Invalid \"domain\" value. Expected a string literal.", getLineNumber(realParentNode.node.startPosition, realParentNode.node.endPosition, lineNumbers), res);	
+					}
+					break;
+				case "host":
+					if(!isStringProperty(realParentNode.node)){
+						sendInvalidResult("Invalid \"host\" value. Expected a string literal.", getLineNumber(realParentNode.node.startPosition, realParentNode.node.endPosition, lineNumbers), res);	
+					}
+					break;
+				case "path":
+					if(!isStringProperty(realParentNode.node)){
+						sendInvalidResult("Invalid \"path\" value. Expected a string literal.", getLineNumber(realParentNode.node.startPosition, realParentNode.node.endPosition, lineNumbers), res);	
+					}
+					break;
+				case "memory":
+					if(!isStringProperty(realParentNode.node)){
+						sendInvalidResult("Invalid \"memory\" value. Expected a memory limit.", getLineNumber(realParentNode.node.startPosition, realParentNode.node.endPosition, lineNumbers), res);	
+					}
+					if(!isValidMemoryProperty(valueWithParent[m].value)){
+						sendInvalidResult("Invalid \"memory\" limit; Supported measurement units are M/MB, G/GB.", getLineNumber(valueWithParent[m].startPosition, valueWithParent[m].endPosition, lineNumbers), res);	
+					}
+					break;
+				case "instances":
+					if(!isStringProperty(realParentNode.node)){
+						sendInvalidResult("Invalid \"instances\" value. Expected a non-negative integer value.", getLineNumber(realParentNode.node.startPosition, realParentNode.node.endPosition, lineNumbers), res);	
+					}
+					if(!isValidNonNegativeProperty(valueWithParent[m].value)){
+						sendInvalidResult("Invalid \"instances\" value. Expected a non-negative integer value.", getLineNumber(valueWithParent[m].startPosition, valueWithParent[m].endPosition, lineNumbers), res);	
+					}
+					break;
+				case "timeout":
+					if(!isStringProperty(realParentNode.node)){
+						sendInvalidResult("Invalid \"timeout\" value. Expected a non-negative integer value.", getLineNumber(realParentNode.node.startPosition, realParentNode.node.endPosition, lineNumbers), res);	
+					}
+					if(!isValidNonNegativeProperty(valueWithParent[m].value)){
+						sendInvalidResult("Invalid \"timeout\" value. Expected a non-negative integer value.", getLineNumber(valueWithParent[m].startPosition, valueWithParent[m].endPosition, lineNumbers), res);	
+					}
+					break;
+				case "no-route":
+					if(!isStringProperty(realParentNode.node)){
+						sendInvalidResult("Invalid \"no-route\" value. Expected a string literal \"true\".", getLineNumber(realParentNode.node.startPosition, realParentNode.node.endPosition, lineNumbers), res);	
+					}
+					if(valueWithParent[m].value !== "true"){
+						sendInvalidResult("Invalid \"no-route\" value. Expected a string literal \"true\".", getLineNumber(valueWithParent[m].startPosition, valueWithParent[m].endPosition, lineNumbers), res);	
+					}
+					break;
+			}
+		}
+	}
+	function isStringProperty(parentNode){
+		if(parentNode.value.hasOwnProperty("value") && typeof parentNode.value.value === "string"){
+			return true;
+		}
+		return false;
+	}
+	function isValidNonNegativeProperty(value){
+		if(/^[1-9][0-9]*/.test(value)){
+			return true;
+		}
+		return false;
+	}
+	function isValidMemoryProperty(value){
+		if(/^[1-9][0-9]*(M|MB|G|GB|m|mb|g|gb)/.test(value)){
+			return true;
+		}
+		return false;
+	}
+	function findclosestKeyName(valueNode, realParentNode){
+		if(valueNode.hasOwnProperty("key")){
+			realParentNode.nodeKey =  valueNode.key.value;
+			realParentNode.node =  valueNode;
+			return;
+		}
+		findclosestKeyName(valueNode.parent, realParentNode);
+	}
+	function getLineNumber(start, end, lineNumberArray){
+		if(start && end){
+			for(var j = 0; j < lineNumberArray.length ; j++){
+				if(start >= lineNumberArray[j][0] && end <= lineNumberArray[j][1]){
+					return j + 1;
+				}
+			}
+		}
+		return -1;
+	}
+	function sendInvalidResult(message, lineNumber, res){
+		var task = new tasks.Task(res, false, false, 0, false);
+		var resultJson = {
+			"Severity":"Warning",
+			"Message": message
+		};
+		if(lineNumber !== -1){
+			resultJson.Line = lineNumber;
+		}
+		task.done({
+			HttpCode: 400,
+			Code: 0,
+			DetailedMessage: message,
+			JsonData: resultJson,
+			Message: message,
+			Severity: "Error"
+		});
+	}
+}
 function retrieveProjectFilePath(req){
 	var projectPath = req.params[0];
 	return path.join(req.user.workspaceDir, projectPath);
+}
+function slugify(inputString){
+	return inputString.toString().toLowerCase()
+	.replace(/\s+/g, "-")		// Replace spaces with -
+	.replace(/[^\w\-]+/g, "")	// Remove all non-word chars
+	.replace(/\-\-+/g, "-")		// Replace multiple - with single -
+	.replace(/^-+/, "")			// Trim - from start of text
+	.replace(/-+$/, "");			// Trim - from end of text
 }
 };
--- modules/orionode/lib/cf/services.js
+++ modules/orionode/lib/cf/services.js
@@ -16,96 +16,112 @@ var tasks = require("../tasks");
 var async = require("async");
 
 module.exports.router = function() {
-	
+
 	return express.Router()
-	.use(bodyParser.json())
-	.get("*", getService);
-	
-function getService(req, res){
-	var task = new tasks.Task(res,false,false,0,false);
-	var targetRequest = JSON.parse(req.query.Target);
-	var targetURL = targetRequest.Url;
-	var spaceGuid = targetRequest.SpaceId;
-	new Promise(function(fulfill, reject) {
-		var serviceUrl = "/v2/spaces/" + spaceGuid + "/service_instances";
-		var fullService = [];
-		function promiseWhile(doRequest, value) {
-			return Promise.resolve(value).then(doRequest).then(function(result) {
-				return collectServiceInfo(result)
-				.then(function(){
-					if(result && result.next_url){
-						serviceUrl = result.next_url;				
-						return promiseWhile(doRequest, serviceUrl);
+		.use(bodyParser.json())
+		.get("*", getService);
+
+	function getService(req, res) {
+		var task = new tasks.Task(res, false, false, 0, false);
+		var targetRequest = JSON.parse(req.query.Target);
+		var targetURL = targetRequest.Url;
+		var spaceGuid = targetRequest.SpaceId;
+		if (spaceGuid && targetURL) {
+			new Promise(function(fulfill, reject) {
+				var serviceUrl = "/v2/spaces/" + spaceGuid + "/service_instances";
+				var fullService = [];
+				function promiseWhile(doRequest, value) {
+					return Promise.resolve(value).then(doRequest).then(function(result) {
+						return collectServiceInfo(result)
+							.then(function() {
+								if (result && result.next_url) {
+									serviceUrl = result.next_url;
+									return promiseWhile(doRequest, serviceUrl);
+								}
+								return null;
+							});
+					});
+				}
+
+				function doRequest(serviceUrl) {
+					return target.cfRequest("GET", req.user.username, task, targetURL + serviceUrl, {
+						"inline-relations-depth": "1",
+						"return_user_provided_service_instances": "true"
+					});
+				}
+
+				function collectServiceInfo(result) {
+					var serviceResources = result && result.resources;
+					if (serviceResources) {
+						return new Promise(function(fulfill, reject) {
+							async.each(serviceResources, function(resource, cb) {
+								var isBindable = true;
+								if (resource.entity.service_plan) {
+									var serviceEntity = resource.entity.service_plan.entity;
+									var serviceGuid = serviceEntity.service_guid;
+									return getCFService(req.user.username, targetURL, serviceGuid)
+										.then(function(singleServiceresult) {
+											isBindable = singleServiceresult.entity.bindable;
+										}).then(function() {
+											if (isBindable) {
+												var ServiceJson = {
+													"Name": resource.entity.name,
+													"Type": "Service"
+												};
+												fullService.push(ServiceJson);
+											}
+											cb();
+										});
+								}
+							}, function(err) {
+								if (err) {
+									return reject(err);
+								}
+								fulfill();
+							});
+						});
 					}
-					return null;
+					return Promise.resolve();
+				}
+				return promiseWhile(doRequest, serviceUrl)
+					.then(function() {
+						fulfill(fullService);
+					});
+			}).then(function(fullService) {
+				var resp = {
+					"Children": fullService
+				};
+				task.done({
+					HttpCode: 200,
+					Code: 0,
+					DetailedMessage: "OK",
+					JsonData: resp,
+					Message: "OK",
+					Severity: "Ok"
+				});
+			}).catch(function(err) {
+				task.done({
+					HttpCode: 401,
+					Code: 0,
+					JsonData: {},
+					DetailedMessage: err,
+					Message: err,
+					Severity: "Error"
 				});
 			});
+		}else{
+			task.done({
+				HttpCode: 400,
+				Code: 0,
+				JsonData: {},
+				DetailedMessage: "No-Target",
+				Message: "No-Target",
+				Severity: "Error"
+			});
 		}
-		function doRequest(serviceUrl){
-			return target.cfRequest("GET", req.user.username, task, targetURL + serviceUrl,{"inline-relations-depth":"1","return_user_provided_service_instances":"true"});
-		}
-		function collectServiceInfo(result){
-			var serviceResources = result && result.resources;
-			if(serviceResources){
-				return new Promise(function(fulfill , reject){
-					async.each(serviceResources, function(resource, cb) {
-						var isBindable = true;
-						if(resource.entity.service_plan){
-							var serviceEntity = resource.entity.service_plan.entity;
-							var serviceGuid = serviceEntity.service_guid;
-							return getCFService(req.user.username, targetURL,serviceGuid)
-							.then(function(singleServiceresult){
-								isBindable = singleServiceresult.entity.bindable;				
-							}).then(function(){
-								if(isBindable){
-									var ServiceJson = {
-										"Name": resource.entity.name,
-										"Type":	"Service"
-									};
-									fullService.push(ServiceJson);
-								}
-								cb();
-							});
-						}
-					}, function(err) {
-						if(err){
-							return reject(err);
-						}
-						fulfill();
-					});
-				});
-			}
-			return Promise.resolve();
-		}
-		return promiseWhile(doRequest,serviceUrl)
-		.then(function(){
-			fulfill(fullService);
-		});
-	}).then(function(fullService){
-		var resp = {
-			"Children" : fullService
-		};
-		task.done({
-			HttpCode: 200,
-			Code: 0,
-			DetailedMessage: "OK",
-			JsonData: resp,
-			Message: "OK",
-			Severity: "Ok"
-		});
-	}).catch(function(err){
-		task.done({
-			HttpCode: 401,
-			Code: 0,
-			JsonData: {},
-			DetailedMessage: err,
-			Message: err,
-			Severity: "Error"
-		});
-	});
-}
+	}
 
-function getCFService(userId, targetURL,serviceGuid){
-	return target.cfRequest("GET", userId, null , targetURL + "/v2/services/" + serviceGuid);
-}
+	function getCFService(userId, targetURL, serviceGuid) {
+		return target.cfRequest("GET", userId, null, targetURL + "/v2/services/" + serviceGuid);
+	}
 };
