d2082f90a4d92463af880c6162192e97dd567d12
Jenny Han <jhan@ca.ibm.com>
2016-08-18 15:18:57 -0400

    Bug 499790 - Improve error message for invalid CF user login info
    Change-Id: Iee532bb479fcdc2aa43c20eff01dafce6d6526dc
    Signed-off-by: Jenny Han <jhan@ca.ibm.com>

--- bundles/org.eclipse.orion.client.cf/web/cfui/nls/root/messages.js
+++ bundles/org.eclipse.orion.client.cf/web/cfui/nls/root/messages.js
@@ -52,7 +52,7 @@ define({//Default message bundle
 	"password:": "Password:",
 	"Cancelled": "Cancelled",
 	"Could not find the launch configuration manifest": "Could not find the launch configuration manifest nor use the top-level project manifest. Please restore it or provide a project manifest.",
-	"CouldNotFindManifestInProvidedPath" : "Could not find manifest in provided path. The launch conifguration will use default values.",
+	"CouldNotFindManifestInProvidedPath" : "Could not find manifest in provided path. The launch configuration will use default values.",
 	"hostNamesCannotHaveUnderscore" : "Hostnames cannot have an underscore",
 	"Would you like to use the top-level project manifest": "Could not find the launch configuration manifest. Would you like to use the top-level project manifest instead and bind it to the launch configuration?",
 	"iD:": "ID:",
