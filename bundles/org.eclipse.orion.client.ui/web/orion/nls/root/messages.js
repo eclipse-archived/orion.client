156ab4146fdfde5eae7b89ca49fc524550686bc5
Carolyn MacLeod <Carolyn_MacLeod@ca.ibm.com>
2016-09-19 18:10:44 -0400

    Bug 501779 - Orion "switch" command needs to be accessible

--- bundles/org.eclipse.orion.client.ui/web/orion/nls/root/messages.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/nls/root/messages.js
@@ -129,6 +129,7 @@ define({//Default message bundle
 	"title (optional)": "title (optional)",
 	"url": "url",
 	"workedProgress": "${0} (${1}/${2})",
+	"VerticalPaneOrientation": "Vertical pane orientation",
 	"TogglePaneOrientationTooltip": "Toggle split pane orientation",
 	"WarningDuplicateLinkId": "Duplicate link ID: ${0} (link IDs are not case-sensitive)",
 	"WarningHeaderTooDeep": "Header level cannot exceed 6",
