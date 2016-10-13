2afcf84a8f44a593851333783a841c20ffd75795
Sidney <xinyij@ca.ibm.com>
2016-10-13 13:52:33 -0400

    Bug 501531 - try using fast search for filename and content search
    Change-Id: I826ef8276998fb77056f3bed36e08da71d9e3e92
    Signed-off-by: Sidney <xinyij@ca.ibm.com>

--- modules/orionode/lib/file.js
+++ modules/orionode/lib/file.js
@@ -25,6 +25,10 @@ function getParam(req, paramName) {
 module.exports = function(options) {
 	var fileRoot = options.root;
 	if (!fileRoot) { throw new Error('options.root is required'); }
+	if(!options.options.configParams.isElectron){
+		var indexWorker = require('./indexWorker');
+		var Indexer = indexWorker.getIndexer(options.options.indexDir);
+	}
 
 	var writeFileMetadata = function() {
 		var args = Array.prototype.slice.call(arguments, 0);
@@ -230,7 +234,10 @@ module.exports = function(options) {
 		}
 
 		var filepath = getSafeFilePath(req, nodePath.join(rest, name));
-		fileUtil.handleFilePOST(fileRoot, req, res, filepath);
+		fileUtil.handleFilePOST(fileRoot, req, res, filepath)
+		.then(function(){
+			Indexer && Indexer.updateIndexEntry({location:req.body.Location,filepath:filepath,name:name}, req.user.workspaceDir, req.user.username);
+		});
 	});
 
 	// DELETE - no request body
@@ -250,6 +257,7 @@ module.exports = function(options) {
 					return;
 				}
 				res.sendStatus(204);
+				Indexer && Indexer.indexAfterAllDone(Indexer.SHORT_WAITING, req.user.workspaceDir, req.user.username);
 			};
 			if (stats.isDirectory()) {
 				fileUtil.rumRuff(filepath, callback);
