cd7d2bcee8df135b4448ca8626120be2ebe8ce3a
Mike Rennie <Michael_Rennie@ca.ibm.com>
2016-10-31 10:45:58 -0400

    Bug 506734 - [tern-project] Including .json at the end of a custom index results in a JavaScript error

--- bundles/org.eclipse.orion.client.javascript/web/javascript/plugins/ternWorkerCore.js
+++ bundles/org.eclipse.orion.client.javascript/web/javascript/plugins/ternWorkerCore.js
@@ -788,7 +788,7 @@ function(Tern, defaultOptions, Deferred, Objects, Serialize, Messages, i18nUtil)
 		if(projectLoc) {
 			loc = projectLoc + loc;
 		}
-		if(!/$.json/i.test(def)) {
+		if(!/\.json$/i.test(def)) {
 			loc = loc + '.json'; //$NON-NLS-1$
 		}
 		var deferred = new Deferred();
