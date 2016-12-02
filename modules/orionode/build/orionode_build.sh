2eef881299631477e16fb557f7e763ef0ebeb1e9
Silenio Quarti <Silenio_Quarti@ca.ibm.com>
2016-12-02 11:27:25 -0500

    Bug 508550 - Update to the latest version of Electron

--- modules/orionode/build/orionode_build.sh
+++ modules/orionode/build/orionode_build.sh
@@ -12,7 +12,7 @@ cp ${DOWNLOADS}/orion/orionode/nodegit/v0.13.0/linux/nodegit.node ../node_module
 (../node_modules/.bin/grunt ${GRUNT_TASK}) || die "Failed to minify client code."
 rm -rf ../node_modules
 (npm install --production --no-optional) || die "Failed to install dependencies, consult the npm log to find out why."
-cp ${DOWNLOADS}/orion/orionode/nodegit/v0.13.0/linux/nodegit.node ../node_modules/nodegit/build/Release
+cp ${DOWNLOADS}/orion/orionode/nodegit/v0.16.0/linux/nodegit.node ../node_modules/nodegit/build/Release
 rm -rf ../node_modules/pty.js
 rm -rf ../node_modules/nodegit/vendor
 rm -rf ../node_modules/nodegit/build/Release/obj.target
