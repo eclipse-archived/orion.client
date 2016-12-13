911098ab43cecd827c12f2b941322726bff6d225
Casey Flynn <caseyflynn@google.com>
2016-12-13 15:12:17 -0500

    Bug 507788 - [theme] The UI Themes preferences have been removed
    Add dynamic container theme styling
    Refactor editor theme UI
    Add container theme UI
    Add container theme preview
    Resolve conflicts with lightPage.css
    
    Change-Id: I843a9693124e387d35d4fe10cd19308ffbb4c9ec
    Signed-off-by: Casey Flynn <caseyflynn@google.com>

--- bundles/org.eclipse.orion.client.ui/web/orion/widgets/themes/ThemeVersion.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/widgets/themes/ThemeVersion.js
@@ -7,6 +7,7 @@
  * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
  * 
  * Contributors: Anton McConville - IBM Corporation - initial API and implementation
+ * 				 Casey Flynn - Google Inc.
  ******************************************************************************/
 /*eslint-env browser, amd*/
 define([], function() {
@@ -14,7 +15,7 @@ define([], function() {
 	/**
 	 * Version string for theme data. Please update this string whenever you change the style of a themable element.
 	 */
-	var THEMES_VERSION = "6.88";
+	var THEMES_VERSION = "6.9";
 
 	return THEMES_VERSION;
 });
