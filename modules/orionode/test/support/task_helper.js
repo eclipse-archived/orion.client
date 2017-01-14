f75a30e4b42de2066bee10edceddcda79dc19366
Remy Suen <remy.suen@gmail.com>
2017-01-14 21:09:04 +0900

    Bug 507940 - Tasks service's DELETE is unimplemented for the all tasks case
    Implement support for when the user clicks on the 'Remove Completed'
    button in the operations page. Any completed task that the current user
    had created should be deleted when this button is clicked on.
    
    Change-Id: I408dca3c9fe054881eb0dcec33cb4dd31d2fbd86
    Signed-off-by: Remy Suen <remy.suen@gmail.com>

---
+++ modules/orionode/test/support/task_helper.js
@@ -0,0 +1,49 @@
+/******************************************************************************
+ * Copyright (c) 2017 Remy Suen and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     Remy Suen - initial API and implementation
+ *****************************************************************************/
+/*eslint-env node*/
+var express = require('express');
+var bodyParser = require('body-parser');
+var api = require('../../lib/api');
+var tasks = require('../../lib/tasks');
+
+var taskList = {};
+
+function router(options) {
+	var root = options.root;
+	if (!root) { throw new Error('options.root path required'); }
+
+	return express.Router()
+	.use(bodyParser.json())
+	.post('*', doDelete)
+	.put('*', doPut);
+}
+
+function doDelete(req, res) {
+	var id = req.url.substr(req.url.lastIndexOf('/') + 1);
+	taskList[id].done({
+		HttpCode: 200,
+		Code: 0,
+		DetailedMessage: "OK",
+		JsonData: {},
+		Message: "OK",
+		Severity: "Ok"
+	});
+	res.status(200).json({});
+}
+
+function doPut(req, res) {
+	var task = new tasks.Task(res, false, false, 0, false);
+	taskList[task.id] = task;
+}
+
+module.exports = {
+	router: router
+};
