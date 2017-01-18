b8dbbfe9eaf048f7f6eefe7eaa30ccd7d21f77cd
Sidney <xinyij@ca.ibm.com>
2017-01-18 15:53:07 -0500

    Bug 501531 - try using fast search for filename and content search
    Change-Id: I826ef8276998fb77056f3bed36e08da71d9e3e92
    Signed-off-by: Sidney <xinyij@ca.ibm.com>

--- modules/orionode/lib/search.js
+++ modules/orionode/lib/search.js
@@ -8,17 +8,21 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-/*eslint-env node*/
+/*eslint-env node, worker*/
 var api = require('./api');
 var path = require('path');
 var bodyParser = require('body-parser');
 var express = require('express');
+var prefs = require('./controllers/prefs');
+var indexWorker;
+var isIndexForCurrentWorkspaceExist = true;
+var INDEX_USE_WORKERS = true; // Need to change the same flag in indexWorker.js to debug without worker.
 
 module.exports = function(options) {
 	var USE_WORKERS = options.configParams.isElectron, search;
 	if (USE_WORKERS) {
 		var requests = {};
-		var WORKER_COUNT = 5;
+		var WORKER_COUNT = 1;
 		var searchWorkers = [];
 		var id = 0, lastWorker = 0;
 		var Worker = require("tiny-worker");
@@ -34,21 +38,35 @@ module.exports = function(options) {
 			};
 			searchWorkers.push(searchWorker);
 		}
-		search = function(originalUrl, workspaceDir, contextPath) {
+		search = function(originalUrl, workspaceDir, contextPath, userId, indexDir, isElectron, usingIndex, isIndexForCurrentWorkspaceExsit, excludeNames) {
 			return new Promise(function(fullfil, reject) {
 				id++;
 				requests[id] = {fullfil: fullfil, reject: reject};
 				var worker = searchWorkers[lastWorker++ % searchWorkers.length];
-				worker.postMessage({id: id, originalUrl: originalUrl, workspaceDir: workspaceDir, contextPath: contextPath});
+				worker.postMessage({id: id, originalUrl: originalUrl, workspaceDir: workspaceDir, contextPath: contextPath, userId: userId, indexDir: indexDir, isElectron: isElectron, usingIndex: usingIndex, 
+				isIndexForCurrentWorkspaceExsit:isIndexForCurrentWorkspaceExsit, excludeNames:excludeNames});
 			});
 		};
+		 if(INDEX_USE_WORKERS){
+			 indexWorker = new Worker(path.join(__dirname, "indexWorker.js"));
+			 indexWorker.onmessage = function (event) {
+				isIndexForCurrentWorkspaceExist = event.data.isIndexOfCurrentWorkSpaceExsit;
+			 };
+			 indexWorker.postMessage({type:"startIndex", workspaceDir: options.workspaceDir, inverval:options.configParams["filename.indexing.interval"],indexDir:options.indexDir, userId:"anonymous"});
+		 }else{
+		 	 require('./indexWorker').scheduleIndex(options.workspaceDir, options.configParams["filename.indexing.interval"], options.indexDir, "anonymous");
+		 }
 	} else {
-		search = require('./searchWorker');
+		search = require('./searchWorker').search;
 	}
+		
 	return express.Router()
 	.use(bodyParser.json())
 	.get('*', function(req, res) {
-		search(req.originalUrl, req.user.workspaceDir, req.contextPath)
+		var generalSetting = prefs.readPrefs(options.workspaceDir, !options.configParams['orion.single.user']).user.general;
+		var usingIndex = (generalSetting ? generalSetting.settings.generalSettings.filenameSearchPolicy : false) || false;
+		var excludeNames = (generalSetting ? generalSetting.settings.generalSettings.indexExcludeFileNames : ["node_modules"]) || ["node_modules"];
+		search(req.originalUrl, req.user.workspaceDir, req.contextPath, req.user.username, options.indexDir, options.configParams.isElectron, usingIndex, isIndexForCurrentWorkspaceExist, excludeNames)
 		.then(function(result) {
 			res.json(result);
 		})
@@ -57,3 +75,15 @@ module.exports = function(options) {
 		});
 	});
 };
+
+module.exports.changeIndexWorkDir = function(workspaceDir) {
+	isIndexForCurrentWorkspaceExist = false;
+	if(INDEX_USE_WORKERS){
+		indexWorker && indexWorker.postMessage({type:"workspaceDirChange", workspaceDir: workspaceDir});
+	}else{
+		require('./indexWorker').handleWorkspaceDirChanged(workspaceDir);
+	}
+};
+module.exports.updateIsIndexOfCurrentWorkSpaceExist = function(isExsit){
+	isIndexForCurrentWorkspaceExist = isExsit;
+}
