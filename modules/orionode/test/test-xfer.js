28cdff63b77b95b3f68abc3868d931d0eef50fc2
Remy Suen <remy.suen@gmail.com>
2017-02-11 20:39:32 +0900

    Bug 511513 - Export non-existing folder leaks server path
    The ability to export a folder obviously depends on the folder
    actually existing on the server. If the file does not exist, the
    native error is to of course report the full path to the client.
    However, this doesn't make sense in a server setting as this would
    leak the internal folder structure of the Orion workspace to the
    client. We should check if the export failed because the folder
    doesn't exist and to then hide the workspace's path from the user in
    the returned error message if that is the case.
    
    Signed-off-by: Remy Suen <remy.suen@gmail.com>

---
+++ modules/orionode/test/test-xfer.js
@@ -0,0 +1,52 @@
+/******************************************************************************
+ * Copyright (c) 2017 Remy Suen and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html).
+ *
+ * Contributors:
+ *     Remy Suen - initial API and implementation
+ *****************************************************************************/
+/*eslint-env node, mocha, assert, express*/
+var path = require('path');
+var fs = require('fs');
+var assert = require('assert');
+var express = require('express');
+var supertest = require('supertest');
+var xfer = require('../lib/xfer');
+
+var CONTEXT_PATH = '';
+var WORKSPACE = path.join(__dirname, '.test_workspace');
+
+var app = express()
+.use(function(req, res, next) {
+	req.user = { workspaceDir: WORKSPACE };
+	next();
+})
+.use(CONTEXT_PATH + '/xfer', require("../lib/xfer")({ fileRoot: CONTEXT_PATH + "/xfer" }));
+
+var request = supertest.bind(null, app);
+
+describe("Xfer API", function() {
+	beforeEach(function() {
+	});
+
+	describe('Export', function(done) {
+		// Bug 511513 - Export non-existing folder leaks server path
+		it('bug 511513', function(finished) {
+			// make sure the folder doesn't actually exist
+			assert.equal(fs.existsSync(WORKSPACE + "/donotexist"), false);
+			// ask the server to export the non-existent folder
+			request()
+			.get(CONTEXT_PATH + "/xfer/export/donotexist.zip")
+			.expect(404)
+			.end(function(err, res) {
+				assert.ifError(err);
+				// message body doesn't include the path
+				assert.equal(res.body.Message.indexOf(WORKSPACE), -1);
+				finished();
+			});
+		});
+	}); // describe("Export")
+}); // describe("Xfer API")
