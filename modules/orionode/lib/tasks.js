9d13e3222baf14fe984a283e31fb6b9df1c8354b
Grant Gayed <Grant_Gayed@ca.ibm.com>
2017-02-20 22:48:11 -0500

    Bug 511584 - persist long-running tasks on node server such that they're available to multiple server instances

--- modules/orionode/lib/tasks.js
+++ modules/orionode/lib/tasks.js
@@ -15,12 +15,137 @@ var api = require('./api');
 var crypto = require('crypto');
 var writeError = api.writeError;
 
+var MS_EXPIRATION = 86400 * 1000 * 7; /* 7 days */  // TODO should be settable per task by client
+
+var TaskStoreInMemory = function() {
+	this._taskList = {};
+};
+
+TaskStoreInMemory.prototype = {
+	createTask: function(taskObj, callback) {
+		this._taskList[taskObj.id] = taskObj;
+		callback(null);
+	},
+	deleteTask: function(id, callback) {
+		delete this._taskList[id];
+		callback(null);
+	},
+	getTask: function(id, callback) {
+		callback(null, this._taskList[id]);
+	},
+	getTasksForUser: function(username, callback) {
+		var result = [];
+		Object.keys(this._taskList).forEach(function(id) {
+			if (this._taskList[id].username === username) {
+				result.push(this._taskList[id]);
+			}
+		}.bind(this));
+		callback(null, result);
+	},
+	updateTask: function(taskObj, callback) {
+		callback(null);
+	}
+};
+
+var TaskStoreMongoDB = function() {
+	this._mongoose = require("mongoose");
+	var taskSchema = new this._mongoose.Schema({
+		id: {
+			type: String,
+			required: true,
+			unique: true
+		},
+		username: {
+			type: String,
+			required: true
+		},
+		keep: {
+			type: Boolean,
+			required: true
+		},
+		lengthComputable: {
+			type: Boolean,
+			required: true
+		},
+		type: {
+			type: String,
+			required: true
+		},
+		loaded: Number,
+		total: Number,
+		message: String,
+		result: this._mongoose.Schema.Types.Mixed,
+		timestamp: Number,
+		expires: Number,
+		cancelable: Boolean
+		// uriUnqualStrategy: String // TODO needed?
+	});
+	
+	/* depends on the user module to establish the db connection */
+	this._orionTask = this._mongoose.model("orionTask", taskSchema);
+};
+
+TaskStoreMongoDB.prototype = {
+	createTask: function(taskObj, callback) {
+		taskObj._mongooseTask = new this._orionTask();
+		this.updateTask(taskObj, callback);
+	},
+	deleteTask: function(id, callback) {
+		this._orionTask.remove({id: id}, callback);
+	},
+	getTask: function(id, callback) {
+		this._orionTask.find(
+			{id: id},
+			function(err, mongooseTasks) {
+				if (err) {
+					callback(err);
+				} else {
+					callback(null, mongooseTasks[0]);
+				}
+			});
+	},
+	getTasksForUser: function(username, callback) {
+		this._orionTask.find({username: username}, callback);
+	},	
+	updateTask: function(taskObj, callback) {
+		var mongooseTask = taskObj._mongooseTask;
+		mongooseTask.id = taskObj.id;
+		mongooseTask.username = taskObj.username;
+		mongooseTask.keep = Boolean(taskObj.keep);
+		mongooseTask.lengthComputable = Boolean(taskObj.lengthComputable);
+		mongooseTask.type = taskObj.type;
+
+		mongooseTask.loaded = taskObj.loaded;
+		mongooseTask.total = taskObj.total;
+		mongooseTask.message = taskObj.message;
+
+		mongooseTask.result = taskObj.result;
+		mongooseTask.timestamp = taskObj.timestamp;
+		if (taskObj.expires) {
+			mongooseTask.expires = taskObj.expires;
+		}
+		mongooseTask.cancelable = Boolean(taskObj.cancelable);
+//		if (taskObj.uriUnequalStrategy) {
+//			mongooseTask.uriUnequalStrategy = taskObj.uriUnequalStrategy; // TODO needed?
+//		}
+
+		mongooseTask.save(callback);
+	}
+};
+
+var taskCount = 0;
+var taskStore;
 var taskRoot = "/task";
-var taskList = {};
 
 function orionTasksAPI(options) {
 	taskRoot = options.taskRoot;
 	if (!taskRoot) { throw new Error('options.taskRoot is required'); }
+
+	if (options.options.configParams["orion.single.user"]) {
+		taskStore = new TaskStoreInMemory();
+	} else {
+		taskStore = new TaskStoreMongoDB();
+	}
 
 	return express.Router()
 	.use(bodyParser.json())
@@ -29,33 +154,47 @@ function orionTasksAPI(options) {
 		next();
 	})
 	.get('/id/:id', function(req, res/*, next*/) {
-		if (!checkAccess(req.id, req)) return writeError(404, res);
-		var id = req.id;
-		res.json(taskList[id].toJSON(true));
+		taskStore.getTask(req.id, function(err, task) {
+			if (err) {
+				return writeError(500, res, err.toString());
+			}
+			if (!task || task.username !== req.user.username) {
+				return writeError(404, res);
+			}
+			res.json(toJSON(task, true));
+		});
 	})
 	.delete('', deleteAllOperations)
 	.delete('/id/:id', deleteOperation)
 	.get('/temp/:id', function(req, res/*, next*/) {
-		if (!checkAccess(req.id, req)) return writeError(404, res);
-		var id = req.id;
-		res.json(taskList[id].toJSON(false));
+		taskStore.getTask(req.id, function(err, task) {
+			if (err) {
+				return writeError(500, res, err.toString());
+			}
+			if (!task || task.username !== req.user.username) {
+				return writeError(404, res);
+			}
+			res.json(toJSON(task, false));
+		});
 	})
 	.delete('/temp/:id', deleteOperation)
 	.get('/count', function(req, res/*, next*/) {
-		res.json({"count": -1}); // TODO
+		res.json({"count": taskCount});
 	});
 }
 
 function Task(res, cancelable, lengthComputable, wait, keep) {
 	this.timestamp = Date.now();
+	this.expires = this.timestamp + MS_EXPIRATION;
 	this.id = crypto.randomBytes(5).toString('hex') + this.timestamp;
-	this.cancelable = !!cancelable;
-	this.lengthComputable = !!lengthComputable;
-	this.keep = !!keep; 
+	this.cancelable = Boolean(cancelable);
+	this.lengthComputable = Boolean(lengthComputable);
+	this.keep = Boolean(keep);
 	this.total = this.loaded = 0;
 	this.type = "loadstart";
 	this.username = res.req.user.username;
 	this.res = res;
+
 	//TODO temporarily disabled timeout to work around client bug.
 	if (true || wait === 0) {
 		this.start();
@@ -69,18 +208,24 @@ function Task(res, cancelable, lengthComputable, wait, keep) {
 Task.prototype = {
 	start: function() {
 		if (!this.isRunning()) return;
+		taskCount++;
 		this.started = true;
-		taskList[this.id] = this;
-		var resp = JSON.stringify(this.toJSON(true));
-		var res = this.res;
-		res.statusCode = 202;
-		res.setHeader('Content-Type', 'application/json');
-		res.setHeader('Content-Length', resp.length);
-		res.end(resp);
+		taskStore.createTask(this, function(err) {
+			if (err) {
+				return writeError(500, res, err.toString());
+			}
+			var resp = JSON.stringify(toJSON(this, true));
+			this.res.statusCode = 202;
+			this.res.setHeader('Content-Type', 'application/json');
+			this.res.setHeader('Content-Length', resp.length);
+			this.res.end(resp);
+		}.bind(this));
 	},
 	done: function(result) {
 		if (this.result) return;
 		this.result = result;
+		taskCount--;
+
 		switch (result.Severity) {
 			case "Ok":
 			case "Info":
@@ -98,19 +243,29 @@ Task.prototype = {
 				clearTimeout(this.timeout);
 				delete this.timeout;
 			}
-			var res = this.res;
-			if (this.result.JsonData) {
-				res.statusCode = this.result.HttpCode;
-				var resp = JSON.stringify(this.result.JsonData);
-				res.setHeader('Content-Type', 'application/json');
-				res.setHeader('Content-Length', resp.length);
-				res.end(resp);
-			} else if (this.type === "error") {
-				res.writeHead(this.result.HttpCode, this.result.Message || "");
-				res.end();
-			}
+			taskStore.updateTask(this, function(err) {
+				if (err) {
+					return writeError(500, this.res, err.toString());
+				}
+				if (this.result.JsonData) {
+					res.statusCode = this.result.HttpCode;
+					var resp = JSON.stringify(this.result.JsonData);
+					this.res.setHeader('Content-Type', 'application/json');
+					this.res.setHeader('Content-Length', resp.length);
+					this.res.end(resp);
+				} else if (this.type === "error") {
+					this.res.writeHead(this.result.HttpCode, this.result.Message || "");
+					this.res.end();
+				}
+			}.bind(this));
+		} else {
+			delete this.res;
+			taskStore.updateTask(this, function(err) {
+				if (err) {
+					return writeError(500, this.res, err.toString());
+				}
+			}.bind(this));
 		}
-		delete this.res;
 	},
 	isRunning: function() {
 		return this.type !== "error" && this.type !== "abort";
@@ -121,73 +276,82 @@ Task.prototype = {
 		if (typeof message === "string") this.message = message;
 		if (typeof loaded === "number") this.loaded = loaded;
 		if (typeof total === "number") this.total = total;
+		taskStore.updateTask(this, function(err) {
+			if (err) {
+				return writeError(500, this.res, err.toString());
+			}
+		}.bind(this));
 	},
-	toJSON: function(isWriteLocation) {
-		var result = {
-			lengthComputable: this.lengthComputable,
-			cancelable: this.cancelable,
-			timestamp: this.timestamp,
-			type: this.type
-		};
-		if (this.lengthComputable) {
-			result.loaded = this.loaded;
-			result.total = this.total;
-			result.message = this.message;
-		}
-		if (this.result) {
-			result.Result = this.result;
-		} 
-		
-		if(this.keep && isWriteLocation){
-			// Do not set location so that tasks is deleted
-			result.Location = taskRoot + "/id/" + this.id;
-		}else if(isWriteLocation){
-			result.Location = taskRoot + "/temp/" + this.id;
-		}
-		
-		return result;
-	}
 };
 
-function checkAccess(taskId, req) {
-	// check that the task exists and that the user id of the request
-	// matches the user id of the task originator, even if the id is
-	// correct, if the user ids don't match, then we want to disallow
-	// access to the task
-	return taskList[taskId] && taskList[taskId].username === req.user.username;
-}
-
 function deleteOperation(req, res/*, next*/){
-	var id = req.id;
-	if (checkAccess(id, req)) {
-		delete taskList[id];
-		res.status(200).json({});
-	} else {
-		writeError(404, res, "Task does not exist: " + id);
-	}
+	taskStore.getTask(req.id, function(err, task) {
+		if (err) {
+			return writeError(500, res, err.toString());
+		}
+		if (!task || task.username !== req.user.username) {
+			return writeError(404, res, "Task does not exist: " + req.id);
+		}
+		taskStore.deleteTask(req.id, function(err) {
+			if (err) {
+				return writeError(500, res, err.toString());
+			}
+			res.status(200).json({});
+		});
+	});
 }
 
 /**
  * Deletes all completed operations that the user sending the request had created.
  */
 function deleteAllOperations(req, res) {
-	var locations = [];
-	// iterate all the tasks
-	Object.keys(taskList).forEach(function(id) {
-		if (checkAccess(id, req)) {
-			if (taskList[id].result) {
-				// if the task is finished, delete it
-				delete taskList[id];
-			} else {
-				// otherwise store its location
-				locations.push(taskList[id].toJSON(true).Location);
-			}
+	taskStore.getTasksForUser(req.user.username, function(err, tasks) {
+		if (err) {
+			return writeError(500, res, err.toString());
 		}
+		var locations = [];
+		var doneCount = 0;
+		var done = function() {
+			if (++doneCount === tasks.length) {
+				res.status(200).json(locations);
+			}
+		};
+		tasks.forEach(function(task) {
+			if (task.result) {
+				taskStore.deleteTask(task.id, done); /* task is completed */
+			} else {
+				locations.push(toJSON(task, true).Location);
+				done();
+			}
+		});
 	});
-	// respond back to the user with the locations of all incomplete tasks
-	res.status(200).json(locations);
 }
 
+function toJSON(task, isWriteLocation) {
+	var result = {
+		lengthComputable: task.lengthComputable,
+		cancelable: task.cancelable,
+		expires: task.expires,
+		timestamp: task.timestamp,
+		type: task.type
+	};
+	if (task.lengthComputable) {
+		result.loaded = task.loaded;
+		result.total = task.total;
+		result.message = task.message;
+	}
+	if (task.result) {
+		result.Result = task.result;
+	}
+	if (task.keep && isWriteLocation) {
+		// Do not set location so that tasks is deleted
+		result.Location = taskRoot + "/id/" + task.id;
+	} else if (isWriteLocation) {
+		result.Location = taskRoot + "/temp/" + task.id;
+	}
+	return result;
+}
+	
 module.exports = {
 	router: orionTasksAPI,
 	Task: Task,
