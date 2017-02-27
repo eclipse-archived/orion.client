11c56c8098b0abddc5b29919d9d320f9c21e767c
Alston Lin <alston@ca.ibm.com>
2017-02-27 16:38:35 -0500

    Websockets now also check if it needs to listen to contextPath or not
    Signed-off-by: Alston Lin <alston@ca.ibm.com>

--- modules/orionode/server.js
+++ modules/orionode/server.js
@@ -92,7 +92,7 @@ function startServer(cb) {
 				configParams: configParams,
 				maxAge: dev ? 0 : undefined,
 			}));
-			var io = socketio.listen(server, { 'log level': 1, path: contextPath + '/socket.io' });
+			var io = socketio.listen(server, { 'log level': 1, path: (listenContextPath ? contextPath : '' ) + '/socket.io' });
 			ttyShell.install({ io: io, app: app, fileRoot: contextPath + '/file', workspaceDir: workspaceDir });
 
 			server.on('listening', function() {
