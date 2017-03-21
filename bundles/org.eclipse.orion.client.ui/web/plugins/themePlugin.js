c6216ded4a50409a4322d1ba6e2c2c53abeb71f4
Casey Flynn <caseyflynn@google.com>
2017-03-21 13:55:49 -0400

    Bug 513767 - Theme Plugin
    Create simple theme plugin that provides default values.
    The intent of this plugin is to be replaced by users that wish to provide custom style JSON to users.
    
    Change-Id: I4a0c35e65834117a9207ceb1fcb5b3289462f69a
    Signed-off-by: Casey Flynn <caseyflynn@google.com>

---
+++ bundles/org.eclipse.orion.client.ui/web/plugins/themePlugin.js
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ * 
+ * Contributors: Casey Flynn - Google Inc - initial API and implementation
+ ******************************************************************************/
+/*eslint-env browser, amd*/
+define([
+	'orion/plugin',
+	'orion/widgets/themes/ThemeVersion',
+	'orion/widgets/themes/container/LightPage',
+	'orion/widgets/themes/container/OrionPage',
+	"domReady!"
+],
+function(PluginProvider, ThemeVersion, LightPage, OrionPage) {
+	function ThemesProvider() {
+	}
+	
+		/*
+	 * Plugin Stub for Theme Data.
+	 * To provide custom theme data, change the _themeData object to return
+	 * custom values.
+	 */
+	
+	var _themeData = {
+		"styles": [LightPage, OrionPage],
+		"version": ThemeVersion,
+		"protectedThemes": [LightPage.name, OrionPage.name],
+		"defaultTheme": LightPage.name
+	};
+	
+	ThemesProvider.prototype = {
+		getThemeValue: function(key) {
+			if (_themeData.hasOwnProperty(key)) {
+				return _themeData[key];
+			}
+			throw new Error("Theme Data Not Available.");
+		},
+		getThemeVersion: function() {
+			return this.getThemeValue("version");
+		},
+		getStyles: function() {
+			return this.getThemeValue("styles");
+		},
+		getProtectedThemes: function() {
+			return this.getThemeValue("protectedThemes");
+		},
+		getDefaultTheme: function() {
+			this.getThemeValue("defaultTheme");
+		}
+	};
+	
+	function connect() {
+		var login = new URL("../mixloginstatic/LoginWindow.html", self.location.href).href;
+		var headers = {
+			name: "Orion Container Theme Provider",
+			version: "1.0",
+			description: "This plugin provides IDE theme data.",
+			login: login
+		};
+		var pluginProvider = new PluginProvider(headers);
+		registerServiceProviders(pluginProvider);
+		pluginProvider.connect();
+	}
+
+	function registerServiceProviders(provider) {
+		var service = new ThemesProvider();
+		provider.registerService("orion.core.container.themes.provider", service, {});
+	}
+
+	return {
+		connect: connect,
+		registerServiceProviders: registerServiceProviders
+	};
+});
