cdd4b31425f8014a68c70955cd15a19f8f38cc71
Xin Chen <jiudechenxin@hotmail.com>
2017-04-24 11:26:42 -0500

    Bug 478732  - Fix the warning in the org.eclipse.orion.client repo

--- bundles/org.eclipse.orion.client.git/web/orion/git/logic/gitStash.js
+++ bundles/org.eclipse.orion.client.git/web/orion/git/logic/gitStash.js
@@ -1,6 +1,6 @@
 /******************************************************************************* 
  * @license
- * Copyright (c) 2014 IBM Corporation and others.
+ * Copyright (c) 2014, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials are made 
  * available under the terms of the Eclipse Public License v1.0 
  * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
@@ -95,7 +95,7 @@ define([
 			var item = data.items.status || data.handler.status || data.items;
 			var gitStashLocation = item.Clone.StashLocation;
 
-			gitService.doStashPop(gitStashLocation).then(function(resp){
+			gitService.doStashPop(gitStashLocation).then(/* @callback */function(resp){
 				d.resolve(data);
 			}, function(error){
 				d.reject(error);
