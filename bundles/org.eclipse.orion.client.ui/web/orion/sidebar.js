a5c725c75b89da04010cac89fe31c14cac489236
libing wang <libingw@ca.ibm.com>
2017-05-01 15:07:06 -0400

    Bug 516002 - CTRL+SHIFT+H should not hide the search panel if there is selection in the editor.

--- bundles/org.eclipse.orion.client.ui/web/orion/sidebar.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/sidebar.js
@@ -372,7 +372,13 @@ define(['orion/objects', 'orion/commands', 'orion/outliner', 'orion/webui/little
 				},
 				callback: function (data) {
 					if (this._inlineSearchPane.isVisible()) {
-						this._inlineSearchPane.hide();
+						var searchText = getSearchText();
+						if(searchText) {
+							this._inlineSearchPane.setSearchText(searchText);
+							this._inlineSearchPane.showSearchOptions();
+						} else {
+							this._inlineSearchPane.hide();
+						}
 					} else {
 						this._inlineSearchPane.updateSearchScopeFromSelection(data.items.length > 0 ? (typeof data.items[0] === 'string' ? this.editorInputManager.inputManager.getFileMetadata() : data.items[0])
 							: data.items);
