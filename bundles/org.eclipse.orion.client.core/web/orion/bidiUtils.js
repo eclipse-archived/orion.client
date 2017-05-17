6353333e246fe558e49964dab386510c9377f5dc
Moshe Wajnberg <wajnberg@il.ibm.com>
2017-05-17 16:31:58 +0300

    Fix for eclipse bug 512109
    Signed-off-by: Moshe Wajnberg <wajnberg@il.ibm.com>

--- bundles/org.eclipse.orion.client.core/web/orion/bidiUtils.js
+++ bundles/org.eclipse.orion.client.core/web/orion/bidiUtils.js
@@ -12,12 +12,6 @@ define ([
 ],
 function(util) { /* BDL */
 	
-	var bidiEnabledStorage = "/orion/preferences/bidi/bidiEnabled"; //$NON-NLS-0$
-	var bidiLayoutStorage = "/orion/preferences/bidi/bidiLayout"; //$NON-NLS-0$	
-	var LRE = "\u202A";	//$NON-NLS-0$
-	var PDF = "\u202C"; //$NON-NLS-0$
-	var RLE = "\u202B"; //$NON-NLS-0$
-		
 	function setBrowserLangDirection() {
 		
 		var lang;
@@ -29,7 +23,7 @@ function(util) { /* BDL */
 		}
 		var isBidi = lang && "ar iw he".indexOf(lang.substring(0, 2)) !== - 1;
 
-		if (isBidi && isBidiEnabled()) {
+		if (isBidi) {
 			var htmlElement = document.getElementsByTagName("html")[0];
 			if (htmlElement){ //should be always true
 				htmlElement.setAttribute ("dir", "rtl");
@@ -39,6 +33,12 @@ function(util) { /* BDL */
 	
 	setBrowserLangDirection();
 	
+	var bidiEnabledStorage = "/orion/preferences/bidi/bidiEnabled"; //$NON-NLS-0$
+	var bidiLayoutStorage = "/orion/preferences/bidi/bidiLayout"; //$NON-NLS-0$	
+	var LRE = "\u202A";	//$NON-NLS-0$
+	var PDF = "\u202C"; //$NON-NLS-0$
+	var RLE = "\u202B"; //$NON-NLS-0$
+		
 	var bidiLayout = getBidiLayout();
 
 	/**
@@ -146,7 +146,16 @@ function(util) { /* BDL */
 		}
 	}
 	
-	function enforceTextDir(range) {
+	function createNewStyle ( oldStyle, textDir ) {
+		var newStyle = oldStyle;
+		if (typeof newStyle.attributes === "undefined") {
+			newStyle.attributes = {};
+		}
+		newStyle.attributes.dir = textDir;
+		return newStyle;
+	}
+	
+	function enforceTextDir( range, textDir ) {
 		var comments = [{name:"comment block"}, 
 		                {name:"comment line double-slash"},
 		                {name:"comment block documentation"},
@@ -163,13 +172,17 @@ function(util) { /* BDL */
 					var newStyle = style;
 					if (typeof newStyle.attributes === "undefined") {
 						newStyle.attributes = {};
-					}
-					newStyle.attributes.dir = getTextDirection(text);
-					range.style = newStyle;		
+					}							
+					range.style = createNewStyle( style, getTextDirection(text) );
 					return range;
 				}
 			}
 		}
+		else if (style && textDir && textDir.length > 0) {
+			range.style = createNewStyle( style, textDir );
+			return range;
+		}
+		
 		return range;
 	}
 
--- bundles/org.eclipse.orion.client.ui/web/orion/markdownEditor.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/markdownEditor.js
@@ -791,6 +791,25 @@ define([
 				this._computeAnnotations(annotationModel, baseModel, current, start, end, _add);
 			}.bind(this));
 		},
+		_enforceTextDir: function(block, tokens) {
+			var lineIndex = this.model.getLineAtOffset(block.start);
+			var LRE = "\u202A";	//$NON-NLS-0$			
+			var RLE = "\u202B"; //$NON-NLS-0$
+			var textDir = this.model.getLineTextDir(lineIndex);
+			var charToAdd = "";		
+			if (textDir === "ltr") {
+				charToAdd = LRE;
+			} else if (textDir === "rtl") {
+				charToAdd = RLE;
+			}
+			if (charToAdd.length > 0) {
+				tokens.forEach(function(current) {
+					if (current.text) {
+						current.text = charToAdd + current.text;
+					}
+				});
+			}
+		},
 		_generateHTML: function(rootElement, block) {
 			if (!block.startToken && !block.tokens) {
 				return;	 /* likely a block with only tokens for its parent */
@@ -806,6 +825,7 @@ define([
 				 * removes each token from the array as it is processed
 				 */
 				var parseTokens = block.tokens.slice();
+				this._enforceTextDir(block, parseTokens);
 				parseTokens.links = this._linkDefs;
 				rootElement.innerHTML = marked.Parser.parse(parseTokens, markedOptions);
 				return;
@@ -818,6 +838,7 @@ define([
 			/* a container block (a list, list item or blockquote) */
 
 			parseTokens = [block.startToken, block.endToken];
+			this._enforceTextDir(block, parseTokens);
 			parseTokens.links = this._linkDefs;
 			rootElement.innerHTML = marked.Parser.parse(parseTokens, markedOptions);
 
@@ -827,6 +848,7 @@ define([
 				 * context to marked) and the tokens being contributed from child blocks.
 				 */
 				parseTokens = [block.startToken].concat(parentTokens).concat(block.endToken);
+				this._enforceTextDir(block, parseTokens);
 				parseTokens.links = this._linkDefs;
 				var tempElement = document.createElement("div"); //$NON-NLS-0$
 				tempElement.innerHTML = marked.Parser.parse(parseTokens, markedOptions);
