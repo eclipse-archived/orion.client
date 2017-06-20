ffb6ab70c259c228f1269ec0bd3871da6326dfd7
Sidney <xinyij@ca.ibm.com>
2017-06-20 12:35:56 -0400

    more cleanup before merge
    Signed-off-by: Sidney <xinyij@ca.ibm.com>

--- modules/orionode/lib/orion_static.js
+++ modules/orionode/lib/orion_static.js
@@ -54,8 +54,7 @@ exports = module.exports = function(options) {
 		'./bundles/org.eclipse.orion.client.git/web',
 		'./bundles/org.eclipse.orion.client.webtools/web',
 		'./bundles/org.eclipse.orion.client.users/web',
-		'./bundles/org.eclipse.orion.client.cf/web',
-		'./bundles/org.eclipse.orion.client.collab/web'
+		'./bundles/org.eclipse.orion.client.cf/web'
 	];
 	var fullStaticAssets = options.prependStaticAssets.concat(originalStaticAssets).concat(options.appendStaticAssets);
 	fullStaticAssets = fullStaticAssets.forEach(function(bundlePath) {
