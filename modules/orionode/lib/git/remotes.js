8acb94c054e7a4b46dbd595d65843f442ecf74b6
Sidney <xinyij@ca.ibm.com>
2017-07-19 12:03:51 -0400

    Bug 519905 - [Node]FetchRemote might never returns this is not a fix, but to log the process to catch the error
    Signed-off-by: Sidney <xinyij@ca.ibm.com>

--- modules/orionode/lib/git/remotes.js
+++ modules/orionode/lib/git/remotes.js
@@ -19,6 +19,8 @@ var clone = require('./clone');
 var mConfig = require('./config');
 var express = require('express');
 var bodyParser = require('body-parser');
+var log4js = require('log4js');
+var logger = log4js.getLogger("git");
 var util = require('./util');
 
 module.exports = {};
@@ -274,7 +276,7 @@ function fetchRemote(req, res, remote, branch, force) {
 			refSpec = "refs/heads/" + remoteBranch + ":refs/remotes/" + remoteObj.name() + "/" + branch;
 			if (force) refSpec = "+" + refSpec;
 		}
-		
+		logger.debug("fetchRemote ", remote," Starting");
 		return remoteObj.fetch(
 			refSpec ? [refSpec] : null,
 			{
@@ -285,6 +287,7 @@ function fetchRemote(req, res, remote, branch, force) {
 		);
 	})
 	.then(function(err) {
+		logger.debug("err in fetchRemote is", err);
 		if (!err) {
 			task.done({
 				HttpCode: 200,
