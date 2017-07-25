a039ef68e3b1d9f53ab0f69de5e89c6e529919e1
Sidney <xinyij@ca.ibm.com>
2017-07-25 17:14:00 -0400

    electron build fix attemp
    Signed-off-by: Sidney <xinyij@ca.ibm.com>

--- modules/orionode/build/electron_build.sh
+++ modules/orionode/build/electron_build.sh
@@ -90,8 +90,8 @@ find node_modules/nodegit/build/Release/ -mindepth 1 ! -name '*.node' -exec rm -
 rm -rf target
 
 # Install electron builder and electron-builder-squirrel-windows for Squirrel.Window app
-npm install electron-builder@~19.16.0
-npm install electron-builder-squirrel-windows@19.16.0
+npm install -g electron-builder@~19.16.0
+npm install -g electron-builder-squirrel-windows@19.16.0
 
 # Build mac dmg, etc
 nodegit_lib=${NODEGIT_DIR}/v${nodegit_version}/electron/v${electron_version}/mac/nodegit.node
