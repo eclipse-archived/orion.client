afa918b2918a68571aa43e2e39b7c0cdf9305948
Michael Rennie <Michael_Rennie@ca.ibm.com>
2017-09-19 11:39:14 -0400

    Bug 516574 - Exception in orion.eclipse.org (Error: Forbidden at getRepoByPath )

--- modules/orionode/lib/git/util.js
+++ modules/orionode/lib/git/util.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 IBM Corporation and others.
+ * Copyright (c) 2016, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials are made 
  * available under the terms of the Eclipse Public License v1.0 
  * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
@@ -16,6 +16,13 @@ module.exports.encodeURIComponent = function(path) {
 	return encodeURIComponent(encodeURIComponent(path));
 };
 
+/**
+ * @name module.exports.decodeURIComponent
+ * @description Helper to properly decode a path.
+ * @function
+ * @param {string} path The path to decode
+ * @returns {string} The decoded path
+ */
 module.exports.decodeURIComponent = function(path) {
 	var result = path;
 	try {
