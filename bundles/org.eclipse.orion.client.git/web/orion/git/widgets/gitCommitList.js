ade93aaeb157dd21ab1e8428dab9b252918a915b
Carolyn MacLeod <Carolyn_MacLeod@ca.ibm.com>
2017-11-07 18:20:36 -0500

    Bug 526961 - Git filter dropdown input elements need semantic label connections (#271)

--- bundles/org.eclipse.orion.client.git/web/orion/git/widgets/gitCommitList.js
+++ bundles/org.eclipse.orion.client.git/web/orion/git/widgets/gitCommitList.js
@@ -601,6 +601,7 @@ define([
 				});
 				section.domNode.classList.add("gitFilterBox"); //$NON-NLS-0$
 				var filter = document.createElement("input"); //$NON-NLS-0$
+				filter.setAttribute("aria-labelledby", title + "commitFilterSectionTitle"); //$NON-NLS-1$ //$NON-NLS-0$
 				filter.className = "gitFilterInput"; //$NON-NLS-0$
 				filter.placeholder = messages["Filter " + query.key];
 				section.query = query;
