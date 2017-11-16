2cbac9f111e50f91a2aa592bb2146c6f6dcae9df
Casey Flynn <caseyflynn@google.com>
2017-11-15 16:32:23 -0800

    Add flag to allow newly created tabs to be permanent.
    Signed-off-by: Casey Flynn <caseyflynn@google.com>

--- bundles/org.eclipse.orion.client.ui/web/edit/setup.js
+++ bundles/org.eclipse.orion.client.ui/web/edit/setup.js
@@ -1899,6 +1899,8 @@ objects.mixin(EditorSetup.prototype, {
 	 * 			'replace': replaces the current editor's content
 	 * 			    'tab': opens the file in a new tab
 	 * 			  'split': Splits the editor (if needed) and shows the new content in the non-active editor
+	 *   name - (string) Name used to determine the default editor to be used.
+	 *   forcePermanent - (boolean) If specified, and editor tabs are enabled, the newly created tab will be specified as non-transient.
 	 * splitHint - (string) If the mode is 'split' and the editor has not yet been split this determines the
 	 *             initial splitter mode. Can be one of 'horizontal', 'vertical' or 'picInPic'.
 	 * 
@@ -1926,8 +1928,13 @@ objects.mixin(EditorSetup.prototype, {
 				}
 
 				var inputManager = this.activeEditorViewer.inputManager;
+				var activeEditorViewer = this.activeEditorViewer;
 				var inputChangeListener = function() {
 					inputManager.removeEventListener("InputChanged", inputChangeListener);
+					if (options.forcePermanent) {
+						activeEditorViewer.tabWidget.potentialTransientHref = href;
+						activeEditorViewer.tabWidget.transientToPermanent(href);
+					}
 					openEditorPromise.resolve();
 				};
 				inputManager.addEventListener("InputChanged", inputChangeListener);
