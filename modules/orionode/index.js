436b9505bfc5bf4b3ecd8e796525ce7747c40176
Silenio Quarti <Silenio_Quarti@ca.ibm.com>
2017-11-23 19:53:59 -0500

    cleanup dead code

--- modules/orionode/index.js
+++ modules/orionode/index.js
@@ -17,10 +17,8 @@ var express = require('express'),
 	fs = require('fs'),
 	api = require('./lib/api'),
 	checkRights = require('./lib/accessRights').checkRights,
-	log4js = require('log4js'),
 	socketio = require('socket.io'),
 	ttyShell = require('./lib/tty_shell'),
-	logger = log4js.getLogger("response"),
 	responseTime = require('response-time');
 
 var LIBS = path.normalize(path.join(__dirname, 'lib/')),
@@ -54,10 +52,6 @@ var _24_HOURS = "public, max-age=86400, must-revalidate",
 		".html": _NO_CACHE, 
 	};
 
-function handleError(err) {
-	throw err;
-}
-
 function startServer(options) {
 	options = options || {};
 	options.configParams = options.configParams || require("nconf");
@@ -75,146 +69,142 @@ function startServer(options) {
 		throw new Error("workspaceDir is required");
 	}
 	
-	try {
-		var app = express();
+	var app = express();
 
-		options.app = app;
-		
-		app.use(bodyParser.json({limit:"10mb"}));
-		app.use(bodyParser.urlencoded({ extended: false, limit:"10mb" }));
-		app.use(responseTime({digits: 2, header: "X-Total-Response-Time", suffix: true}));
-		
-		Object.assign(options, {
-			sharedWorkspaceFileRoot: contextPath + '/sharedWorkspace/tree/file',
-			taskRoot: contextPath + '/task',
-			workspaceRoot: contextPath + '/workspace',
-			fileRoot: contextPath + '/file',
-			gitRoot: contextPath + '/gitapi'
-		});
-		
-		function checkAuthenticated(req, res, next) {
-			if (!req.user) {
-				api.writeError(401, res, "Not authenticated");
-			} else {
-				req.user.workspaceDir = options.workspaceDir + (req.user.workspace ? "/" + req.user.workspace : "");
-				req.user.checkRights = checkRights;
-				next();
-			}
-		}
-		
-		function checkAccessRights(req, res, next) {
-			var uri = (typeof req.contextPath === "string" && req.baseUrl.substring(req.contextPath.length)) || req.baseUrl;
-			req.user.checkRights(req.user.username, uri, req, res, next);
-		}
-		
-		var additionalEndpoints = options.configParams.get("additional.endpoint") ? require(options.configParams.get("additional.endpoint")) : [];
-		function loadEndpoints(authenticated) {
-			additionalEndpoints.forEach(function(additionalEndpoint) {
-				if (authenticated !== Boolean(additionalEndpoint.authenticated)) return;
-				if (additionalEndpoint.endpoint){
-					additionalEndpoint.authenticated ? 
-						app.use(additionalEndpoint.endpoint, options.authenticate, checkAuthenticated, require(additionalEndpoint.module).router(options))	 : 
-						app.use(additionalEndpoint.endpoint, require(additionalEndpoint.module).router(options));
-				} else {
-					var extraModule = require(additionalEndpoint.module);
-					var middleware = extraModule.router ? extraModule.router(options) : extraModule(options);
-					if (middleware)	app.use(middleware); // use this module as a middleware 
-				}
-			});
-		}
-
-		// Configure metastore
-		var metastoreFactory;
-		if (!options.configParams.get("orion.single.user") && options.configParams.get("orion.metastore.useMongo") !== false) {
-			metastoreFactory = require('./lib/metastore/mongodb/store');
+	options.app = app;
+	
+	app.use(bodyParser.json({limit:"10mb"}));
+	app.use(bodyParser.urlencoded({ extended: false, limit:"10mb" }));
+	app.use(responseTime({digits: 2, header: "X-Total-Response-Time", suffix: true}));
+	
+	Object.assign(options, {
+		sharedWorkspaceFileRoot: contextPath + '/sharedWorkspace/tree/file',
+		taskRoot: contextPath + '/task',
+		workspaceRoot: contextPath + '/workspace',
+		fileRoot: contextPath + '/file',
+		gitRoot: contextPath + '/gitapi'
+	});
+	
+	function checkAuthenticated(req, res, next) {
+		if (!req.user) {
+			api.writeError(401, res, "Not authenticated");
 		} else {
-			metastoreFactory = require('./lib/metastore/fs/store');
+			req.user.workspaceDir = options.workspaceDir + (req.user.workspace ? "/" + req.user.workspace : "");
+			req.user.checkRights = checkRights;
+			next();
 		}
-		options.metastore = app.locals.metastore = metastoreFactory(options);
-		app.locals.metastore.setup(options);
-		options.authenticate = [
-			expressSession({
-				resave: false,
-				saveUninitialized: false,
-				secret: 'keyboard cat',
-				store: options.sessionStore // TODO by default MemoryStore is not designed for a production environment, as it will leak memory, and will not scale past a single process.
-			}),
-			passport.initialize(),
-			passport.session()
-		].concat(options.authenticate || []);
-		
-		loadEndpoints(false);
-		let CloudFoundry = require('./lib/cf').CloudFoundry;
-		app.use('/metrics', require('./lib/metrics').router(options));
-		app.use('/version', require('./lib/version').router(options));
-		let About = require('./lib/about').About;
-		app.use('/about', new About().createRouter(options));
-		loadEndpoints(true);
-		app.use(require('./lib/user').router(options));
-		app.use('/site', options.authenticate, checkAuthenticated, checkAccessRights, require('./lib/sites')(options));
-		app.use('/task', options.authenticate, checkAuthenticated, require('./lib/tasks').router(options));
-		app.use('/filesearch', options.authenticate, checkAuthenticated, require('./lib/search')(options));
-		app.use('/file*', options.authenticate, checkAuthenticated, checkAccessRights, require('./lib/file')(options));
-		app.use('/workspace*', options.authenticate, checkAuthenticated, checkAccessRights, require('./lib/workspace')(options));
-		app.use('/gitapi', options.authenticate, checkAuthenticated, require('./lib/git')(options));
-		app.use('/cfapi', options.authenticate, checkAuthenticated, new CloudFoundry().createRouter(options));
-		app.use('/prefs', options.authenticate, checkAuthenticated, require('./lib/prefs').router(options));
-		app.use('/xfer', options.authenticate, checkAuthenticated, require('./lib/xfer').router(options));
-		if (options.configParams.get("isElectron")) {
-			app.use('/update', options.authenticate, checkAuthenticated, require('./lib/update').router(options));
-		}
-		if(options.configParams.get("orion.collab.enabled")) {
-			app.use('/sharedWorkspace', options.authenticate, checkAuthenticated, require('./lib/sharedWorkspace').router(options));
-		}
-		
-		var io = socketio.listen(options.server, { 'log level': 1, path: (listenContextPath ? contextPath : '' ) + '/socket.io' });
-		ttyShell.install(options, io);
-		if (options.configParams.get("orion.debug.enabled")) {
-			var debugServer = require(options.configParams.get("debug.server.module"));
-			debugServer.install(options, io);
-		}
-		
-		// Static files
-		app.use('/xterm', express.static(path.join(__dirname, 'node_modules', 'xterm', 'dist')));
-		
-		var staticCacheOption;
-		if(typeof options.maxAge !== "undefined" ) {
-			// It's dev time
-			staticCacheOption = {
-				maxAge: options.maxAge
-			};
-		} else {
-			staticCacheOption = {
-				/**
-				 * @callback
-				 */
-				setHeaders: function(res, urlPath, stat){
-					var ext = path.extname(urlPath);
-					if(path.basename(path.dirname(urlPath)) === "requirejs"){
-						res.setHeader("Cache-Control",_24_HOURS);
-					}else if(EXT_CACHE_MAPPING[ext]){
-						res.setHeader("Cache-Control", EXT_CACHE_MAPPING[ext] );
-					}else{
-						res.setHeader("Cache-Control", _24_HOURS);
-					}
-				}
-			};
-		}
-		if (fs.existsSync(MINIFIED_ORION_CLIENT)) {
-			app.use(express.static(MINIFIED_ORION_CLIENT, Object.assign({dotfiles: 'allow'}, staticCacheOption)));
-		} else {
-			var prependStaticAssets = (options.configParams.get("prepend.static.assets") || "").split(",");
-			var appendStaticAssets = (options.configParams.get("append.static.assets") || "").split(",");
-			var orionode_static = path.normalize(path.join(LIBS, 'orionode.client/'));
-			if(options.configParams.get("orion.collab.enabled")) {
-				appendStaticAssets.push('./bundles/org.eclipse.orion.client.collab/web');
-			}
-			app.use(require('./lib/orion_static')(Object.assign({orionClientRoot: ORION_CLIENT, orionode_static: orionode_static, prependStaticAssets: prependStaticAssets, appendStaticAssets: appendStaticAssets}, staticCacheOption)));
-		}
-		return app;
-	} catch (e) {
-		handleError(e);
 	}
+	
+	function checkAccessRights(req, res, next) {
+		var uri = (typeof req.contextPath === "string" && req.baseUrl.substring(req.contextPath.length)) || req.baseUrl;
+		req.user.checkRights(req.user.username, uri, req, res, next);
+	}
+	
+	var additionalEndpoints = options.configParams.get("additional.endpoint") ? require(options.configParams.get("additional.endpoint")) : [];
+	function loadEndpoints(authenticated) {
+		additionalEndpoints.forEach(function(additionalEndpoint) {
+			if (authenticated !== Boolean(additionalEndpoint.authenticated)) return;
+			if (additionalEndpoint.endpoint){
+				additionalEndpoint.authenticated ? 
+					app.use(additionalEndpoint.endpoint, options.authenticate, checkAuthenticated, require(additionalEndpoint.module).router(options))	 : 
+					app.use(additionalEndpoint.endpoint, require(additionalEndpoint.module).router(options));
+			} else {
+				var extraModule = require(additionalEndpoint.module);
+				var middleware = extraModule.router ? extraModule.router(options) : extraModule(options);
+				if (middleware)	app.use(middleware); // use this module as a middleware 
+			}
+		});
+	}
+
+	// Configure metastore
+	var metastoreFactory;
+	if (!options.configParams.get("orion.single.user") && options.configParams.get("orion.metastore.useMongo") !== false) {
+		metastoreFactory = require('./lib/metastore/mongodb/store');
+	} else {
+		metastoreFactory = require('./lib/metastore/fs/store');
+	}
+	options.metastore = app.locals.metastore = metastoreFactory(options);
+	app.locals.metastore.setup(options);
+	options.authenticate = [
+		expressSession({
+			resave: false,
+			saveUninitialized: false,
+			secret: 'keyboard cat',
+			store: options.sessionStore // TODO by default MemoryStore is not designed for a production environment, as it will leak memory, and will not scale past a single process.
+		}),
+		passport.initialize(),
+		passport.session()
+	].concat(options.authenticate || []);
+	
+	loadEndpoints(false);
+	let CloudFoundry = require('./lib/cf').CloudFoundry;
+	app.use('/metrics', require('./lib/metrics').router(options));
+	app.use('/version', require('./lib/version').router(options));
+	let About = require('./lib/about').About;
+	app.use('/about', new About().createRouter(options));
+	loadEndpoints(true);
+	app.use(require('./lib/user').router(options));
+	app.use('/site', options.authenticate, checkAuthenticated, checkAccessRights, require('./lib/sites')(options));
+	app.use('/task', options.authenticate, checkAuthenticated, require('./lib/tasks').router(options));
+	app.use('/filesearch', options.authenticate, checkAuthenticated, require('./lib/search')(options));
+	app.use('/file*', options.authenticate, checkAuthenticated, checkAccessRights, require('./lib/file')(options));
+	app.use('/workspace*', options.authenticate, checkAuthenticated, checkAccessRights, require('./lib/workspace')(options));
+	app.use('/gitapi', options.authenticate, checkAuthenticated, require('./lib/git')(options));
+	app.use('/cfapi', options.authenticate, checkAuthenticated, new CloudFoundry().createRouter(options));
+	app.use('/prefs', options.authenticate, checkAuthenticated, require('./lib/prefs').router(options));
+	app.use('/xfer', options.authenticate, checkAuthenticated, require('./lib/xfer').router(options));
+	if (options.configParams.get("isElectron")) {
+		app.use('/update', options.authenticate, checkAuthenticated, require('./lib/update').router(options));
+	}
+	if(options.configParams.get("orion.collab.enabled")) {
+		app.use('/sharedWorkspace', options.authenticate, checkAuthenticated, require('./lib/sharedWorkspace').router(options));
+	}
+	
+	var io = socketio.listen(options.server, { 'log level': 1, path: (listenContextPath ? contextPath : '' ) + '/socket.io' });
+	ttyShell.install(options, io);
+	if (options.configParams.get("orion.debug.enabled")) {
+		var debugServer = require(options.configParams.get("debug.server.module"));
+		debugServer.install(options, io);
+	}
+	
+	// Static files
+	app.use('/xterm', express.static(path.join(__dirname, 'node_modules', 'xterm', 'dist')));
+	
+	var staticCacheOption;
+	if(typeof options.maxAge !== "undefined" ) {
+		// It's dev time
+		staticCacheOption = {
+			maxAge: options.maxAge
+		};
+	} else {
+		staticCacheOption = {
+			/**
+			 * @callback
+			 */
+			setHeaders: function(res, urlPath, stat){
+				var ext = path.extname(urlPath);
+				if(path.basename(path.dirname(urlPath)) === "requirejs"){
+					res.setHeader("Cache-Control",_24_HOURS);
+				}else if(EXT_CACHE_MAPPING[ext]){
+					res.setHeader("Cache-Control", EXT_CACHE_MAPPING[ext] );
+				}else{
+					res.setHeader("Cache-Control", _24_HOURS);
+				}
+			}
+		};
+	}
+	if (fs.existsSync(MINIFIED_ORION_CLIENT)) {
+		app.use(express.static(MINIFIED_ORION_CLIENT, Object.assign({dotfiles: 'allow'}, staticCacheOption)));
+	} else {
+		var prependStaticAssets = (options.configParams.get("prepend.static.assets") || "").split(",");
+		var appendStaticAssets = (options.configParams.get("append.static.assets") || "").split(",");
+		var orionode_static = path.normalize(path.join(LIBS, 'orionode.client/'));
+		if(options.configParams.get("orion.collab.enabled")) {
+			appendStaticAssets.push('./bundles/org.eclipse.orion.client.collab/web');
+		}
+		app.use(require('./lib/orion_static')(Object.assign({orionClientRoot: ORION_CLIENT, orionode_static: orionode_static, prependStaticAssets: prependStaticAssets, appendStaticAssets: appendStaticAssets}, staticCacheOption)));
+	}
+	return app;
 }
 
 module.exports = startServer;
