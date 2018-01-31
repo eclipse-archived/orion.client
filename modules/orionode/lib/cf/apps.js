1d50d26fd3ed92d31d79b47264fbd1a4f801738d
Silenio Quarti <Silenio_Quarti@ca.ibm.com>
2018-01-31 17:16:24 -0500

    London - Unable to deploy from Orion org-ids/roadmap#4940

--- modules/orionode/lib/cf/apps.js
+++ modules/orionode/lib/cf/apps.js
@@ -340,6 +340,8 @@ function putapps(req, res){
 		target.caughtErrorHandler(task, err);
 	});
 	
+var DEFAULT_TIMEOUT = 180;
+	
 function respondAppPutRequest(task,status){
 	var resp;
 	var appJson = {
@@ -347,7 +349,6 @@ function respondAppPutRequest(task,status){
 		metadata:theApp.appMetadata	
 	};
 	if(status === "RUNNING"){
-		var DEFAULT_TIMEOUT = 60;
 		resp = {
 			"App":appJson,
 			"DeployedPackage":theApp.appPackageType || "unknown",
@@ -375,7 +376,6 @@ function respondAppPutRequest(task,status){
 function startApp(userId, userTimeout ,appTarget){
 	expireAppCache(appTarget, theApp.appName);
 	
-	var DEFAULT_TIMEOUT = 60;
 	var MAX_TIMEOUT = 180;
 	var body = {"console":true, "state":"STARTED"};
 	logger.debug("Starting application=" + theApp.appName);
