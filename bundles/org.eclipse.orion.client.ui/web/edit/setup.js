2cc747b535b1ef76c49f8a9a08f4dffc80465ffb
Michael Rennie <Michael_Rennie@ca.ibm.com>
2018-01-31 09:29:48 -0500

    [nobug] Fix bad increment

--- bundles/org.eclipse.orion.client.ui/web/edit/setup.js
+++ bundles/org.eclipse.orion.client.ui/web/edit/setup.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * @license
- * Copyright (c) 2010, 2017 IBM Corporation and others.
+ * Copyright (c) 2010, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0
  * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution
@@ -1485,7 +1485,7 @@ objects.mixin(EditorViewer.prototype, {
 		// Remove duplicate events
 		for (var i = 0; i < pendingEvents.length; i++) {
 			var duplicate = false;
-			for (var j = 0; j < i; i++) {
+			for (var j = 0; j < i; j++) {
 				if (pendingEvents[i].textModelChangedEvent === pendingEvents[j].textModelChangedEvent) {
 					duplicate = true;
 				}
