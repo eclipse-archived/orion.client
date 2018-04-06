c1bf1a0fdbb800168aebe0d6e85023dcdd8603a8
Michael Rennie <Michael_Rennie@ca.ibm.com>
2018-04-06 11:19:06 -0400

    Allow version to come from a build metadata file

--- modules/orionode/lib/version.js
+++ modules/orionode/lib/version.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2017 IBM Corporation and others.
+ * Copyright (c) 2016, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials are made 
  * available under the terms of the Eclipse Public License v1.0 
  * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
@@ -11,27 +11,30 @@
 /*eslint-env node */
 var express = require('express'),
 	api = require('./api'),
-	responseTime = require('response-time'),
-	pjson = require('../package.json');
+	responseTime = require('response-time');
 
-module.exports = {};
+const BUILD_ID = "unknown";
 
-var BUILD_ID = "unknown";
-
-module.exports.router = function(options) {
+module.exports.router = function versionRouter(options) {
 	var configParams = options.configParams;
-	if (!configParams) { throw new Error('options.configParams is required'); }
-
+	if (!configParams) {
+		throw new Error('options.configParams is required'); 
+	}
+	var buildID = configParams.get("orion.buildId") || BUILD_ID;
+	const pjson = require('../package.json');
+	if(buildID === "unknown" && pjson && typeof pjson.version === 'string') {
+		//for the NPM case we want to return the version published to NPM (from the package.json)
+		buildID = pjson.version;
+	}
+	try {
+		var build =  require('../build.json');
+		
+	} catch (err) {
+		build = {build: buildID};
+	}
 	return express.Router()
 		.use(responseTime({digits: 2, header: "X-Version-Response-Time", suffix: true}))
 		.get('*', /* @callback */ function (req, res) {
-			var buildID = configParams.get("orion.buildId") || BUILD_ID;
-			if(buildID === "unknown" && pjson && typeof pjson.version === 'string') {
-				//for the NPM case we want to return the version published to NPM (from the package.json)
-				buildID = pjson.version;
-			}
-			return api.writeResponse(200, res, null, {
-				build: buildID
-			});
+			return api.writeResponse(200, res, null, build);
 		});
 };
