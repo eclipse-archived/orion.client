90b62a66fae169d809b9c977629804a6a6ac4877
Grant Gayed <grant_gayed@ca.ibm.com>
2018-12-17 14:55:14 -0500

    Revert "test output"
    This reverts commit 634a5c676062dfc952632f2ae225c106ef888121

--- modules/orionode/index.js
+++ modules/orionode/index.js
@@ -124,8 +124,6 @@ function tryLoadRouter(endpoint, options) {
 			}
 			next();
 		});
-	} else {
-		logger.warn("test output 2, isEndpoint: " + isEndpoint + " csrf: " + csrf + " last: " + (endpoint.checkCSRF === undefined || endpoint.checkCSRF));
 	}
 	if (endpoint.checkAccess) {
 		args.push(checkAccessRights);
@@ -285,7 +283,6 @@ module.exports = function startServer(options) {
 		/**
 		 * @callback
 		 */
-		logger.warn("test output");
 		options.CSRF = function noop(req, res, next) { next(); };
 	}
 	const additionalEndpoints = options.configParams.get("additional.endpoint") ? require(options.configParams.get("additional.endpoint")) : [];
