6b19c983ec71b764a269f5759cf3c1fb0c477740
Silenio Quarti <Silenio_Quarti@ca.ibm.com>
2019-02-26 14:28:47 -0500

    try tree

--- bundles/org.eclipse.orion.client.ui/web/orion/webui/treetable.js
+++ bundles/org.eclipse.orion.client.ui/web/orion/webui/treetable.js
@@ -147,6 +147,10 @@ define(['i18n!orion/nls/messages', 'orion/webui/littlelib', 'orion/Deferred'], f
 					row.setAttribute("aria-level", indentLevel + 1);
 					row.setAttribute("aria-setsize", children.length);
 					row.setAttribute("aria-posinset", i + 1);
+					row.setAttribute("aria-expanded", false);
+				} else if (this._role === "tree") {
+					row.setAttribute("role", "treeitem");
+					row.setAttribute("aria-expanded", false);
 				}
 				if(this._renderer && typeof this._renderer.initSelectableRow === "function") { //$NON-NLS-0$
 					this._renderer.initSelectableRow(children[i], row);
